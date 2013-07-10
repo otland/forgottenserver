da0e1776cba8988d407c46b87c3da87bbf446e33
Kornelijus Survila <kornholijo@gmail.com>
2013-07-10 01:53:05 -0500

    Moved CXX11 detection to its own file.

---
+++ cmake/FindCXX11.cmake
@@ -0,0 +1,25 @@
+if(__FIND_CXX11_CMAKE__)
+    return()
+endif()
+set(__FIND_CXX11_CMAKE__ TRUE)
+
+include(CheckCXXCompilerFlag)
+enable_language(CXX)
+
+check_cxx_compiler_flag("-std=c++11" COMPILER_KNOWS_CXX11)
+if(COMPILER_KNOWS_CXX11)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+
+    # Tested on Mac OS X 10.8.2 with XCode 4.6 Command Line Tools
+    # Clang requires this to find the correct c++11 headers
+    if(CMAKE_HOST_APPLE)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+    endif()
+else()
+    check_cxx_compiler_flag("-std=c++0x" COMPILER_KNOWS_CXX0X)
+    if(COMPILER_KNOWS_CXX0X)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+    else()
+        message(FATAL_ERROR "Your C++ compiler does not support C++11.")
+    endif()
+endif()
