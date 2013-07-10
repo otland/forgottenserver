b90b7d8c89732212a385cc69bf3e270e2561c8f6
Kornelijus Survila <kornholijo@gmail.com>
2013-07-09 23:28:15 -0500

    Added cmake build files.

---
+++ cmake/FindGMP.cmake
@@ -0,0 +1,14 @@
+# Locate GMP library
+# This module defines
+#   GMP_FOUND
+#   GMP_INCLUDE_DIR
+#   GMP_LIBRARIES
+
+find_path(GMP_INCLUDE_DIR NAMES gmp.h)
+find_library(GMP_LIBRARIES NAMES gmp libgmp)
+find_library(GMPXX_LIBRARIES NAMES gmpxx libgmpxx)
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GMP DEFAULT_MSG GMP_INCLUDE_DIR GMP_LIBRARIES GMPXX_LIBRARIES)
+
+mark_as_advanced(GMP_INCLUDE_DIR GMP_LIBRARIES GMPXX_LIBRARIES)
