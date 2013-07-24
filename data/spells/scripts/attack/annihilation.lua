2527247dbaaab2699a938b4e5a92104549ba216c
nclx <januario.nclx@gmail.com>
2013-07-24 06:03:22 -0430

    Increased damage, now formula is more accurated based on what tibiastats says.

--- data/spells/scripts/attack/annihilation.lua
+++ data/spells/scripts/attack/annihilation.lua
@@ -5,8 +5,8 @@ setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_WEAPONTYPE)
 setCombatParam(combat, COMBAT_PARAM_USECHARGES, true)
 
 function onGetPlayerMinMaxValues(cid, skill, attack, factor)
-	local min = skill * attack / 25 + getPlayerLevel(cid) / 5 + 1
-	local max = skill * attack / 8  + getPlayerLevel(cid) / 5 + 6
+	local min = skill * attack / 15 + getPlayerLevel(cid) / 5 + 1
+	local max = skill * attack / 7  + getPlayerLevel(cid) / 5 + 6
 	return -min, -max
 end
 setCombatCallback(combat, CALLBACK_PARAM_SKILLVALUE, "onGetPlayerMinMaxValues")
