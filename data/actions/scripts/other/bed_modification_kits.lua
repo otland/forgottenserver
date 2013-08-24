a944845b2b6c23fb9cb8f5e94101251815fe0d86
Mark Samman <mark.samman@gmail.com>
2013-08-24 19:43:18 +0200

    Update bed modification kits script

--- data/actions/scripts/other/bed_modification_kits.lua
+++ data/actions/scripts/other/bed_modification_kits.lua
@@ -3,12 +3,12 @@ local BEDS = {
 	[7905] = {{7811, 7812}, {7813, 7814}}, -- green kit
 	[7906] = {{7815, 7816}, {7817, 7818}}, -- red kit
 	[7907] = {{7819, 7820}, {7821, 7822}}, -- yellow kit
-	[20252] = {{20197, 20198}, {20199, 20200}} -- canopy bed
+	[20252] = {{20197, 20198}, {20199, 20200}} -- canopy kit
 }
 
 local function internalBedTransform(item, itemEx, toPosition, ids)
 	doTransformItem(itemEx.uid, ids[1])
-	doTransformItem(getThingfromPos(toPosition).uid, ids[2])
+	doTransformItem(getTileItemByType(toPosition, ITEM_TYPE_BED).uid, ids[2])
 
 	doSendMagicEffect(getThingPos(itemEx.uid), CONST_ME_POFF)
 	doSendMagicEffect(toPosition, CONST_ME_POFF)
@@ -18,28 +18,24 @@ end
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local newBed = BEDS[item.itemid]
-	if(not newBed or not getTileHouseInfo(getCreaturePosition(cid))) then
+	if not newBed or not getTileHouseInfo(getPlayerPosition(cid)) then
 		return false
 	end
 
-	--TODO
-	--Is it possible in real tibia, to use same modification on current used?
-	if(isInArray({newBed[1][1], newBed[2][1]}, itemEx.itemid)) then
-		doPlayerSendCancel(cid, "You already have this bed modification.")
-		return true
+	if itemEx.itemid == newBed[1][1] or itemEx.itemid == newBed[2][1] then
+		return false
 	end
 
 	for kit, bed in pairs(BEDS) do
-		if(bed[1][1] == itemEx.itemid or itemEx.itemid == 1758) then
+		if(bed[1][1] == itemEx.itemid) then
 			toPosition.y = toPosition.y + 1
 			internalBedTransform(item, itemEx, toPosition, newBed[1])
 			break
-		elseif(bed[2][1] == itemEx.itemid or itemEx.itemid == 1756) then
+		elseif(bed[2][1] == itemEx.itemid) then
 			toPosition.x = toPosition.x + 1
 			internalBedTransform(item, itemEx, toPosition, newBed[2])
 			break
 		end
 	end
-
 	return true
 end
