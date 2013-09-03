558b08423e68455af216989bfd112b1e4a8f59f5
Mark Samman <mark.samman@gmail.com>
2013-09-03 18:47:09 +0200

    Add talkaction scripts

---
+++ data/talkactions/scripts/up.lua
@@ -0,0 +1,10 @@
+function onSay(cid, words, param)
+	local player = Player(cid)
+	if not player:getGroup():getAccess() then
+		return false
+	end
+
+	local position = player:getPosition()
+	position.z = position.z - 1
+	player:teleportTo(position)
+end
