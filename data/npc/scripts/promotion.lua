bb8431316b7907a0da68b6de59115b2d026b00e9
PrinterLUA <roberto_potrus@hotmail.com>
2013-09-08 18:47:16 +0200

    Update promotion.lua
    The old promotion npc was to limited and also you could get promoted how many times you wanted.

--- data/npc/scripts/promotion.lua
+++ data/npc/scripts/promotion.lua
@@ -1,14 +1,48 @@
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
+local talkState = {}
+function onCreatureAppear(cid)
+	npcHandler:onCreatureAppear(cid)
+end
+function onCreatureDisappear(cid)
+	npcHandler:onCreatureDisappear(cid)
+end
+function onCreatureSay(cid, type, msg)
+	npcHandler:onCreatureSay(cid, type, msg)
+end
+function onThink()
+	npcHandler:onThink()
+end
+function creatureSayCallback(cid, type, msg)
+	if(not(npcHandler:isFocused(cid))) then
+		return false
+	end
+	
+	local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
+	
+	if msgcontains(msg, "promot") then
+		npcHandler:say("Do you want to be promoted in your vocation for 20000 gold?", cid)
+		Topic[talkUser] = 1
+	elseif msgcontains(msg, "yes") and Topic[talkUser] == 1 then
+		if(getPlayerStorageValue(cid, 30018) == 1) then
+			npcHandler:say('You are already promoted.', cid)
+		elseif(getPlayerLevel(cid) < 20) then
+			npcHandler:say('You need to be at least level 20 in order to be promoted.', cid)
+		elseif getPlayerMoney(cid) < 20000 then
+			npcHandler:say('You do not have enough money.', cid)
+		elseif getConfigInfo("freePremium") == "yes" or isPremium(cid) == TRUE then
+			npcHandler:say("Congratulations! You are now promoted. You have learned new spells.", cid)
+			local promotedVoc = getPromotedVocation(getPlayerVocation(cid))
+			doPlayerSetVocation(cid, promotedVoc)
+			doPlayerRemoveMoney(cid, 20000)
+		else
+			npcHandler:say("You need a premium account in order to promote.", cid)
+		end
+		Topic[talkUser] = 0
+	end
+	return true
+end
 
-function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
-function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
-function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
-function onThink()				npcHandler:onThink()					end
-
-local node1 = keywordHandler:addKeyword({'promot'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'I can promote you for 20000 gold coins. Do you want me to promote you?'})
-	node1:addChildKeyword({'yes'}, StdModule.promotePlayer, {npcHandler = npcHandler, cost = 20000, level = 20, text = 'Congratulations! You are now promoted.'})
-	node1:addChildKeyword({'no'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Alright then, come back when you are ready.', reset = true})
-
+npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:addModule(FocusModule:new())
