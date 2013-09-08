83ec19d22f56f9b30be5143fc601ded5d98ae8d0
PrinterLUA <roberto_potrus@hotmail.com>
2013-09-08 19:18:31 +0200

    Update shovel.lua
    You should be able now to open hidden holes on the sand, with aid: 100

--- data/actions/scripts/tools/shovel.lua
+++ data/actions/scripts/tools/shovel.lua
@@ -1,11 +1,16 @@
 local holes = {468, 481, 483}
+
 function onUse(cid, item, fromPosition, itemEx, toPosition)
+	doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, sayer)
 	if isInArray(holes, itemEx.itemid) == TRUE then
 		doTransformItem(itemEx.uid, itemEx.itemid + 1)
 		doDecayItem(itemEx.uid)
 	elseif itemEx.itemid == 231 then
 		local rand = math.random(1, 100)
-		if rand == 1 then
+		if(itemEx.actionid  == 100 and rand <= 20) then
+			doTransformItem(itemEx.uid, 489)
+			doDecayItem(itemEx.uid)
+		elseif rand == 1 then
 			doCreateItem(2159, 1, toPosition)
 		elseif rand > 95 then
 			doSummonCreature("Scarab", toPosition)
