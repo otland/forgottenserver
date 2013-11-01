f375db5b9d6cbd1f7c3749438adc21c65296d97c
Mark Samman <mark.samman@gmail.com>
2013-11-02 00:07:54 +0100

    Fix bug with account bans

--- data/globalevents/scripts/startup.lua
+++ data/globalevents/scripts/startup.lua
@@ -8,8 +8,9 @@ function onStartup()
 	local resultId = db.storeQuery("SELECT * FROM `account_bans` WHERE `expires_at` != 0 AND `expires_at` <= " .. os.time())
 	if resultId ~= false then
 		repeat
-			db.query("INSERT INTO `account_ban_history` (`account_id`, `reason`, `banned_at`, `expired_at`, `banned_by`) VALUES (" .. result.getDataInt(resultId, "account_id") .. ", " .. db.escapeString(result.getDataString(resultId, "reason")) .. ", " .. result.getDataLong(resultId, "banned_at") .. ", " .. result.getDataLong(resultId, "expires_at") .. ", " .. result.getDataInt(resultId, "banned_by") .. ")")
-			db.query("DELETE FROM `account_bans WHERE `account_id` = " .. accountId);
+			local accountId = result.getDataInt(resultId, "account_id")
+			db.query("INSERT INTO `account_ban_history` (`account_id`, `reason`, `banned_at`, `expired_at`, `banned_by`) VALUES (" .. accountId .. ", " .. db.escapeString(result.getDataString(resultId, "reason")) .. ", " .. result.getDataLong(resultId, "banned_at") .. ", " .. result.getDataLong(resultId, "expires_at") .. ", " .. result.getDataInt(resultId, "banned_by") .. ")")
+			db.query("DELETE FROM `account_bans WHERE `account_id` = " .. accountId)
 		until not result.next(resultId)
 		result.free(resultId)
 	end
