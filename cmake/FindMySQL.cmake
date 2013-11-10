7922fe9381b81afdbb833fc91c73b40551770d55
Mark Samman <mark.samman@gmail.com>
2013-11-10 01:12:43 +0100

    Use libmysqlclient instead of libmysqlclient_r

--- cmake/FindMySQL.cmake
+++ cmake/FindMySQL.cmake
@@ -67,9 +67,9 @@ IF (WIN32)
 				 $ENV{ProgramFiles}/MySQL/*/lib/${libsuffixDist}
 				 $ENV{SystemDrive}/MySQL/*/lib/${libsuffixDist})
 ELSE (WIN32)
-	FIND_LIBRARY(MYSQL_LIB NAMES mysqlclient_r
+	FIND_LIBRARY(MYSQL_LIB NAMES mysqlclient
 				 PATHS
-				 $ENV{MYSQL_DIR}/libmysql_r/.libs
+				 $ENV{MYSQL_DIR}/libmysql/.libs
 				 $ENV{MYSQL_DIR}/lib
 				 $ENV{MYSQL_DIR}/lib/mysql
 				 /usr/lib/mysql
@@ -95,7 +95,7 @@ IF (MYSQL_INCLUDE_DIR AND MYSQL_LIB_DIR)
 	FIND_LIBRARY(MYSQL_ZLIB zlib PATHS ${MYSQL_LIB_DIR})
 	FIND_LIBRARY(MYSQL_YASSL yassl PATHS ${MYSQL_LIB_DIR})
 	FIND_LIBRARY(MYSQL_TAOCRYPT taocrypt PATHS ${MYSQL_LIB_DIR})
-	SET(MYSQL_CLIENT_LIBS mysqlclient_r)
+	SET(MYSQL_CLIENT_LIBS mysqlclient)
 	IF (MYSQL_ZLIB)
 		SET(MYSQL_CLIENT_LIBS ${MYSQL_CLIENT_LIBS} zlib)
 	ENDIF (MYSQL_ZLIB)
