fc71972a7a532b32ff3b1e107bb076e069e99e81
Mark Samman <mark.samman@gmail.com>
2013-11-14 18:17:55 +0100

    Use player userdata in serverinfo.lua

--- data/talkactions/scripts/serverinfo.lua
+++ data/talkactions/scripts/serverinfo.lua
@@ -1,6 +1,7 @@
 function onSay(cid, words, param)
-	doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Server Info:"
-					.. "\nExp rate: " .. Game.getExperienceStage(Player(cid):getLevel())
+	local player = Player(cid)
+	player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Server Info:"
+					.. "\nExp rate: " .. Game.getExperienceStage(player:getLevel())
 					.. "\nSkill rate: " .. configManager.getNumber(configKeys.RATE_SKILL)
 					.. "\nMagic rate: " .. configManager.getNumber(configKeys.RATE_MAGIC)
 					.. "\nLoot rate: " .. configManager.getNumber(configKeys.RATE_LOOT))
