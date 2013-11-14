19ea0092a0cbb8526de49da3dcaf6b3992e20e1e
dalkon <dalkon@live.se>
2013-11-15 00:32:09 +0100

    Missing access

--- data/talkactions/scripts/closeserver.lua
+++ data/talkactions/scripts/closeserver.lua
@@ -1,9 +1,14 @@
 function onSay(cid, words, param)
+	local player = Player(cid)
+	if not player:getGroup():getAccess() then
+		return true
+	end
+	
 	if param == "shutdown" then
 		Game.setGameState(GAME_STATE_SHUTDOWN)
 	else
 		Game.setGameState(GAME_STATE_NORMAL)
-		Player(cid):sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Server is now closed.")
+		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Server is now closed.")
 	end
 	return false
 end
--- data/talkactions/scripts/openserver.lua
+++ data/talkactions/scripts/openserver.lua
@@ -1,5 +1,10 @@
 function onSay(cid, words, param)
+	local player = Player(cid)
+	if not player:getGroup():getAccess() then
+		return true
+	end
+	
 	Game.setGameState(GAME_STATE_NORMAL)
-	Player(cid):sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Server is now open.")
+	player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Server is now open.")
 	return false
 end
