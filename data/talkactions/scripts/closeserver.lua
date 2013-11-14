2e663052607138b0fcc0f6aaf4eb695c67d26ae8
Mark Samman <mark.samman@gmail.com>
2013-11-15 00:59:26 +0100

    Set correct game state in /closeserver

--- data/talkactions/scripts/closeserver.lua
+++ data/talkactions/scripts/closeserver.lua
@@ -7,7 +7,7 @@ function onSay(cid, words, param)
 	if param == "shutdown" then
 		Game.setGameState(GAME_STATE_SHUTDOWN)
 	else
-		Game.setGameState(GAME_STATE_NORMAL)
+		Game.setGameState(GAME_STATE_CLOSED)
 		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Server is now closed.")
 	end
 	return false
--- data/talkactions/scripts/position.lua
+++ data/talkactions/scripts/position.lua
@@ -2,7 +2,7 @@ function onSay(cid, words, param)
 	local player = Player(cid)
 	if player:getGroup():getAccess() and param ~= "" then
 		local split = param:split(",")
-		player:teleportTo(Position(split[1], split[2],split[3]))
+		player:teleportTo(Position(split[1], split[2], split[3]))
 	else
 		local pos = player:getPosition()
 		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Your current position is: " .. pos.x .. ", " .. pos.y .. ", " .. pos.z .. ".")
