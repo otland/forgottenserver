49f3934f34ff1f5d8318899e857aff6bfff02097
Mark Samman <mark.samman@gmail.com>
2013-11-15 06:28:29 +0100

    Remove undefined steps in removething.lua

--- data/talkactions/scripts/removething.lua
+++ data/talkactions/scripts/removething.lua
@@ -3,16 +3,16 @@ function onSay(cid, words, param)
 	if not player:getGroup():getAccess() then
 		return true
 	end
-	
+
 	local position = player:getPosition()
-	position:getNextPosition(player:getDirection(), steps)
-	
+	position:getNextPosition(player:getDirection())
+
 	local tile = position:getTile()
 	if not tile then
 		player:sendCancelMessage("Object not found.")
 		return false
 	end
-	
+
 	local thing = tile:getTopVisibleThing(player)
 	if not thing then
 		player:sendCancelMessage("Thing not found.")
@@ -28,7 +28,7 @@ function onSay(cid, words, param)
 		end
 		thing:remove(tonumber(param) or -1)
 	end
-	
+
 	position:sendMagicEffect(CONST_ME_MAGIC_RED)
 	return false
 end
