ba806425d2a8e0b478541fba0d43e30b5ba57657
Mark Samman <mark.samman@gmail.com>
2013-11-21 02:14:50 +0100

    Rename /newitem to /chameleon

--- data/talkactions/scripts/chameleon.lua
+++ data/talkactions/scripts/chameleon.lua
@@ -3,23 +3,23 @@ function onSay(cid, words, param)
 	if not player:getGroup():getAccess() then
 		return true
 	end
-	
+
 	if player:getAccountType() < ACCOUNT_TYPE_GOD then
 		return false
 	end
-		
+
 	local itemType = ItemType(param)
 	if itemType:getId() == 0 then
 		itemType = ItemType(tonumber(param))
 		if itemType:getId() == 0 then
-			player:sendCancelMessage("There is no look type with that id or name.")
+			player:sendCancelMessage("There is no item with that id or name.")
 			return false
 		end
 	end
-	
+
 	local condition = Condition(CONDITION_OUTFIT, CONDITIONID_COMBAT)
 	condition:setTicks(-1)
 	condition:addOutfit(itemType:getId())
-	player:addCondition(condition)	
+	player:addCondition(condition)
 	return false
 end
