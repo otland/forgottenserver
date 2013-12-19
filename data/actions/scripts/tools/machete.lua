50049112689046a5d90c2bc06fa8cb940ec49d6c
Martin <martin_jojo@hotmail.com>
2013-12-19 02:51:03 -0800

    machete work on more items added rope-fishingRod-shovel to more items
    "I recommend add next feature: on all XML"
    separators   ;
    <action itemid="2120;7731" script="tools/rope.lua"/>

--- data/actions/scripts/tools/machete.lua
+++ data/actions/scripts/tools/machete.lua
@@ -1,8 +1,42 @@
+local JUNGLE_GRASS = { 2782, 3985 }
+local SPIDER_WEB = { 7538, 7539 }
+local BAMBOO_FENCE = { 3798, 3799 }
+local WILD_GROWTH = { 1499, 11099 }
+
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if itemEx.itemid == 2782 then
-		doTransformItem(itemEx.uid, 2781)
+	if(isInArray(JUNGLE_GRASS, itemEx.itemid)) then
+		doTransformItem(itemEx.uid, itemEx.itemid - 1)
 		doDecayItem(itemEx.uid)
 		return TRUE
 	end
+
+	if(isInArray(SPIDER_WEB, itemEx.itemid)) then
+		if math.random(3) == 1 then
+			doTransformItem(itemEx.uid, (itemEx.itemid + 6))
+			doDecayItem(itemEx.uid)
+		end
+		doSendMagicEffect(toPosition, CONST_ME_POFF)
+		return TRUE
+	end
+
+	if(isInArray(BAMBOO_FENCE, itemEx.itemid)) then
+		if math.random(3) == 1 then
+			if(itemEx.itemid == BAMBOO_FENCE[1]) then
+				doTransformItem(itemEx.uid, (itemEx.itemid + 161))
+			elseif(itemEx.itemid == BAMBOO_FENCE[2]) then
+				doTransformItem(itemEx.uid, (itemEx.itemid + 159))
+			end
+			doDecayItem(itemEx.uid)
+		end
+		doSendMagicEffect(toPosition, CONST_ME_POFF)
+		return TRUE
+	end
+
+	if(isInArray(WILD_GROWTH, itemEx.itemid)) then
+		doSendMagicEffect(toPosition, CONST_ME_POFF)
+		doRemoveItem(itemEx.uid)
+		return TRUE
+	end
+
 	return destroyItem(cid, itemEx, toPosition)
 end
