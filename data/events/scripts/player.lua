a50b233413a14ad22a0e6a61c0162337ccc234a7
dalkon <dalkon@live.se>
2013-12-26 14:40:23 +0100

    Player.onLookInBattleList

--- data/events/scripts/player.lua
+++ data/events/scripts/player.lua
@@ -3,6 +3,10 @@ function Player:onLook(thing, position, distance)
 	return true
 end
 
+function Player:onLookInBattleList(creature, distance)
+	return true
+end
+
 function Player:onLookInTrade(partner, item, distance)
 	return true
 end
