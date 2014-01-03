12f6cccaa723c1adf6ecc217ec5182324c426952
Faith2531 <Faith2531@users.noreply.github.com>
2014-01-03 21:42:31 +0100

    Update dice.lua
    code shortened,
    ghost mode checking included

--- data/actions/scripts/other/dice.lua
+++ data/actions/scripts/other/dice.lua
@@ -1,8 +1,6 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local value = math.random(5792, 5797)
-	if fromPosition.x ~= CONTAINER_POSITION then
-		doSendMagicEffect(fromPosition, CONST_ME_CRAPS)
-	end
-	doTransformItem(item.uid, value)
-	return doCreatureSay(cid, getCreatureName(cid) .. ' rolled a ' .. value - 5791 .. '.', TALKTYPE_ORANGE_1, true,true, getThingPos(item.uid))
+	local c,p,v = Player(cid), getThingPos(item.uid), math.random(5792, 5797)
+	c:say(c:getName() .. ' rolled a ' .. v - 5791 .. '.', TALKTYPE_ORANGE_1, c:isInGhostMode(),0,p)
+	doTransformItem(item.uid, v)
+	return c:isInGhostMode() and Position(p):sendMagicEffect(CONST_ME_CRAPS, cid) or Position(p):sendMagicEffect(CONST_ME_CRAPS)
 end
