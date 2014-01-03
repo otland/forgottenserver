8d931632b57c8376ccc5cbc8a32c67ac34cb50d1
Faith2531 <Faith2531@users.noreply.github.com>
2014-01-03 21:50:26 +0100

    Update dice.lua
    I can't shorten it anymore

--- data/actions/scripts/other/dice.lua
+++ data/actions/scripts/other/dice.lua
@@ -1,6 +1,6 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local c,p,v = Player(cid), getThingPos(item.uid), math.random(5792, 5797)
+	local c,p,v = Player(cid), Position(getThingPos(item.uid)), math.random(5792, 5797)
 	c:say(c:getName() .. ' rolled a ' .. v - 5791 .. '.', TALKTYPE_ORANGE_1, c:isInGhostMode(),0,p)
 	doTransformItem(item.uid, v)
-	return c:isInGhostMode() and Position(p):sendMagicEffect(CONST_ME_CRAPS, cid) or Position(p):sendMagicEffect(CONST_ME_CRAPS)
+	return c:isInGhostMode() and p:sendMagicEffect(CONST_ME_CRAPS, cid) or p:sendMagicEffect(CONST_ME_CRAPS)
 end
