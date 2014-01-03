7f58ebafbf8b0b08b35c619943bc66d38cf8d5c6
Faith2531 <Faith2531@users.noreply.github.com>
2014-01-03 06:00:06 +0100

    Update swimming.lua
    splash when player enters/leaves swimming area

--- data/movements/scripts/swimming.lua
+++ data/movements/scripts/swimming.lua
@@ -10,8 +10,18 @@ local outfit =
 
 function onStepIn(cid, item, position, fromPosition)
 	doSetCreatureOutfit(cid, outfit, -1)
+	local npos = fromPosition
+	local next_tile = {x = npos.x, y = npos.y, z = npos.z, stackpos = 0}
+	if not(getThingfromPos(next_tile).itemid >= 4620 and getThingfromPos(next_tile).itemid <= 4625) then
+		return doSendMagicEffect(position, 53)
+	end
 end
 
 function onStepOut(cid, item, position, fromPosition)
 	doRemoveCondition(cid, CONDITION_OUTFIT)
+	local npos = getThingPos(cid)
+	local next_tile = {x = npos.x, y = npos.y, z = npos.z, stackpos = 0}
+	if not(getThingfromPos(next_tile).itemid >= 4620 and getThingfromPos(next_tile).itemid <= 4625) then
+		return doSendMagicEffect(position, 53)
+	end
 end
