177cf9bd0d4a6fa841d48b31eb57d1cbe1e952d2
Faith2531 <Faith2531@users.noreply.github.com>
2014-01-05 02:13:32 +0100

    Update dice.lua
    hope this time it suits your idea

--- data/actions/scripts/other/dice.lua
+++ data/actions/scripts/other/dice.lua
@@ -1,6 +1,9 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local c,p,v = Player(cid), Position(getThingPos(item.uid)), math.random(5792, 5797)
-	c:say(c:getName() .. ' rolled a ' .. v - 5791 .. '.', TALKTYPE_ORANGE_1, c:isInGhostMode(),0,p)
-	doTransformItem(item.uid, v)
-	return c:isInGhostMode() and p:sendMagicEffect(CONST_ME_CRAPS, cid) or p:sendMagicEffect(CONST_ME_CRAPS)
+	local player = Player(cid)
+	local pos = Position(getThingPos(item.uid))
+	local value = math.random(5792, 5797)
+	
+	player:say(player:getName() .. ' rolled a ' .. value - 5791 .. '.', TALKTYPE_ORANGE_1, player:isInGhostMode(),0,pos)
+	doTransformItem(item.uid, value)
+	return player:isInGhostMode() and pos:sendMagicEffect(CONST_ME_CRAPS, cid) or pos:sendMagicEffect(CONST_ME_CRAPS)
 end
