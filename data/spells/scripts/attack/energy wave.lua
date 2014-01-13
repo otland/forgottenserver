88ab825ac731dc18d225b9a2818ee8cedf7b02e2
jpmagg <jpmatheus.agg@hotmail.com>
2014-01-13 05:31:46 -0200

    Update energy wave.lua
    Like this formula : http://tibia.wikia.com/wiki/Formula
    I'll send the others spells

--- data/spells/scripts/attack/energy wave.lua
+++ data/spells/scripts/attack/energy wave.lua
@@ -2,7 +2,14 @@ local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_ENERGYAREA)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -1.3, -30, -1.7, 0)
+
+function onGetFormulaValues(cid, level, maglevel)
+min = -((level*0.2) + (maglevel*4.5))
+max = -((level*0.2) + (maglevel*9))
+return min, max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 local area = createCombatArea(AREA_SQUAREWAVE5, AREADIAGONAL_SQUAREWAVE5)
 setCombatArea(combat, area)
