827b42a41343d625b9dccc806f57d1705bf8cc51
Rey Aleman <rey.aleman@outlook.com>
2014-01-13 17:43:03 -0600

    modified access type

--- data/talkactions/scripts/closeserver.lua
+++ data/talkactions/scripts/closeserver.lua
@@ -3,6 +3,10 @@ function onSay(cid, words, param)
 	if not player:getGroup():getAccess() then
 		return true
 	end
+
+	if player:getAccountType() < ACCOUNT_TYPE_GOD then
+		return false
+	end
 	
 	if param == "shutdown" then
 		Game.setGameState(GAME_STATE_SHUTDOWN)
