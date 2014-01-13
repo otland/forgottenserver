38c5bbd1ef3205380ddb5946810b92e1dace8576
jpmagg <jpmatheus.agg@hotmail.com>
2014-01-13 05:35:25 -0200

    Update fire wave.lua
    Like this formula : http://tibia.wikia.com/wiki/Formula
    I'll send the others spells

--- data/spells/scripts/attack/fire wave.lua
+++ data/spells/scripts/attack/fire wave.lua
@@ -1,7 +1,14 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_FIREDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_HITBYFIRE)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -0.8, 0, -1.3, 0)
+
+function onGetFormulaValues(cid, level, maglevel)
+min = -((level*0.2) + (maglevel*1.25) + 4)
+max = -((level*0.2) + (maglevel*2) + 12)
+return min, max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 local area = createCombatArea(AREA_WAVE4, AREADIAGONAL_WAVE4)
 setCombatArea(combat, area)
