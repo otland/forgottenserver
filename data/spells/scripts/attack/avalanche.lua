d65c3ebda92bf4c8159553b4839484e0fec1abc5
jpmagg <jpmatheus.agg@hotmail.com>
2014-01-12 23:06:14 -0200

    Update avalanche.lua
    Like this formula : http://tibia.wikia.com/wiki/Formula

--- data/spells/scripts/attack/avalanche.lua
+++ data/spells/scripts/attack/avalanche.lua
@@ -2,7 +2,14 @@ local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_ICEAREA)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ICE)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -0.366, 0, -0.641, 0)
+
+function onGetFormulaValues(cid, level, maglevel)
+min = -((level*0.2) + (maglevel*1.2) + 7)
+max = -((level*0.2) + (maglevel*2.8) + 17)
+return min, max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 local area = createCombatArea(AREA_CIRCLE3X3)
 setCombatArea(combat, area)
