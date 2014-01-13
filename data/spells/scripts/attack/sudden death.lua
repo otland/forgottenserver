01c44706bf0bb153e9de5adc72c5d7c7fe38c576
jpmagg <jpmatheus.agg@hotmail.com>
2014-01-13 14:20:39 -0200

    Update sudden death.lua

--- data/spells/scripts/attack/sudden death.lua
+++ data/spells/scripts/attack/sudden death.lua
@@ -2,7 +2,14 @@ local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_DEATHDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MORTAREA)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_SUDDENDEATH)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -1.3, -30, -1.8, 0)
+
+function onGetFormulaValues(cid, level, maglevel)
+min = -((level*0.2) + (maglevel*5.205) + 28)
+max = -((level*0.2) + (maglevel*7.995) + 46)
+return min, max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
