63296a4b0c8231a51a67ea75ba9e9fcd9a8827bf
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 21:34:41 -0200

    Update terra wave.lua

--- data/spells/scripts/attack/terra wave.lua
+++ data/spells/scripts/attack/terra wave.lua
@@ -1,10 +1,17 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_EARTHDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_SMALLPLANTS)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -1.5, 0, -2.0, 0)
 
 local area = createCombatArea(AREA_SQUAREWAVE5, AREADIAGONAL_SQUAREWAVE5)
 setCombatArea(combat, area)
+
+function onGetFormulaValues(cid, level, maglevel)
+	min = -((level / 5) + (maglevel * 3.25) + 5) -- * 3.25) + 5)
+	max = -((level / 5) + (maglevel * 6.75) + 30) -- * 6.75) + 30)
+	return min, max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
