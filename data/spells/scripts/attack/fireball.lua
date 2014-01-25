4c2ff67175af61bc50353162357a7e1ad4a7748e
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 21:40:41 -0200

    Update fireball.lua

--- data/spells/scripts/attack/fireball.lua
+++ data/spells/scripts/attack/fireball.lua
@@ -2,7 +2,14 @@ local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_FIREDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_FIREATTACK)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_FIRE)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -0.2, 0, -0.4, 0)
+
+function onGetFormulaValues(cid, level, maglevel)
+	min = -((level / 5) + (maglevel * 1.8) + 12) -- * 1.8) + 12)
+	max = -((level / 5) + (maglevel * 3) + 17) -- * 3) + 17)
+	return min, max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
