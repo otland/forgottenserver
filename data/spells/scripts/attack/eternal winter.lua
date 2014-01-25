b6cbbd6ced739fe89ff191c4228e7e6b78191035
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 21:26:01 -0200

    Update eternal winter.lua

--- data/spells/scripts/attack/eternal winter.lua
+++ data/spells/scripts/attack/eternal winter.lua
@@ -2,17 +2,17 @@ local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
 
-
 local area = createCombatArea(AREA_CROSS5X5)
 setCombatArea(combat, area)
 
 function onGetFormulaValues(cid, level, maglevel)
-min = -((level*0.2) + (maglevel*6))
-max = -((level*0.2) + (maglevel*12))
-return min, max
+	min = -((level / 5) + (maglevel * 5.5) + 25) -- * 5.5) + 25)
+	max = -((level / 5) + (maglevel * 11) + 50) -- * 11) + 50)
+	return min, max
 end
 
 setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
 end
