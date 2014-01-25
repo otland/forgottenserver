9fcfcc0e830e1e8f0974709cd0dd755be1f48bc4
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 21:16:30 -0200

    Update fierce berserk.lua

--- data/spells/scripts/attack/fierce berserk.lua
+++ data/spells/scripts/attack/fierce berserk.lua
@@ -2,10 +2,16 @@ local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_HITAREA)
 setCombatParam(combat, COMBAT_PARAM_USECHARGES, 1)
-setCombatFormula(combat, COMBAT_FORMULA_SKILL, 0, -350, 0.8, -140)
 
 local area = createCombatArea(AREA_SQUARE1X1)
 setCombatArea(combat, area)
+
+function onGetFormulaValues(cid, skill, attack, factor)
+	local skillTotal, levelTotal = skill * attack, getPlayerLevel(cid) / 5
+	return -(((skillTotal * 0.06) + 13) + (levelTotal)), -(((skillTotal * 0.11) + 27) + (levelTotal))
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_SKILLVALUE, "onGetFormulaValues")
 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
