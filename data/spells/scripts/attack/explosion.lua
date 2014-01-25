7632afb3f34782929ecb5fa34751baa69c047738
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 21:38:02 -0200

    Update explosion.lua

--- data/spells/scripts/attack/explosion.lua
+++ data/spells/scripts/attack/explosion.lua
@@ -2,10 +2,17 @@ local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_EXPLOSIONAREA)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_EXPLOSION)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -0.5, -30, -1.1, 0)
 
 local area = createCombatArea(AREA_CROSS1X1)
 setCombatArea(combat, area)
+
+function onGetFormulaValues(cid, level, maglevel)
+	min = -((level / 5) + (maglevel * 1.6) + 9) -- * 1.6) + 9)
+	max = -((level / 5) + (maglevel * 3.2) + 19) -- * 3.2) + 19)
+	return min, max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
