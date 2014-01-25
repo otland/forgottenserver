a1ece8a0690f23657836e5808721cab4c5c640f4
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 21:06:38 -0200

    Update heavy magic missile.lua

--- data/spells/scripts/attack/heavy magic missile.lua
+++ data/spells/scripts/attack/heavy magic missile.lua
@@ -2,7 +2,14 @@ local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_ENERGYAREA)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -0.2, 0, -0.4, 0)
+
+function onGetFormulaValues(cid, level, maglevel)
+	min = -((level / 5) + (maglevel * 0.8) + 5) -- * 0.8) + 5)
+	max = -((level / 5) + (maglevel * 1.6) + 10) -- * 1.6) + 10)
+	return min, max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
