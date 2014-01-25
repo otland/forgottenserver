a1f09f60f5adcbe57e20639d3d467e33fe450aed
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 10:55:47 -0200

    Update mass healing.lua

--- data/spells/scripts/healing/mass healing.lua
+++ data/spells/scripts/healing/mass healing.lua
@@ -4,19 +4,16 @@ setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
 setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
 
+local area = createCombatArea(AREA_CIRCLE3X3)
+setCombatArea(combat, area)
+
 function onGetFormulaValues(cid, level, maglevel)
-	min = (level * 1 + maglevel * 4) * 2.08
-	max = (level * 1 + maglevel * 4) * 2.7
-	if min < 250 then
-		min = 250
-	end
+	min = ((level / 5) + (maglevel * 4.6) + 100) --* 4.6) + 100)
+	max = ((level / 5) + (maglevel * 9.6) + 125) -- * 9.6) + 125)
 	return min, max
 end
 
 setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
-
-local area = createCombatArea(AREA_CIRCLE3X3)
-setCombatArea(combat, area)
 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
