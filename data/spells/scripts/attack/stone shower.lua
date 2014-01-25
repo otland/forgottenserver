de7c4d23fab6cf8d36a217129e1dd1fe6440b447
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 20:55:33 -0200

    Update stone shower.lua

--- data/spells/scripts/attack/stone shower.lua
+++ data/spells/scripts/attack/stone shower.lua
@@ -2,10 +2,17 @@ local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_EARTHDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_STONES)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_EARTH)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -0.366, 0, -0.641, 0)
 
 local area = createCombatArea(AREA_CIRCLE3X3)
 setCombatArea(combat, area)
+
+function onGetFormulaValues(cid, level, maglevel)
+	min = -((level / 5) + (maglevel * 1) + 6) -- * 1) + 6)
+	max = -((level / 5) + (maglevel * 2.6) + 16) -- * 2.6) + 16)
+	return min, max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
