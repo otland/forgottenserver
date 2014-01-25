75ff904acd4a8aa2343b72c5ff9bd7317f160d96
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 11:03:56 -0200

    Update intense healing.lua

--- data/spells/scripts/healing/intense healing.lua
+++ data/spells/scripts/healing/intense healing.lua
@@ -3,7 +3,14 @@ setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_HEALING)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
 setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, 0.51, 0, 0.85, 0)
+
+function onGetFormulaValues(cid, level, maglevel)
+	min = ((level / 5) + (maglevel * 3.2) + 20) -- * 3.2) + 20)
+	max = ((level / 5) + (maglevel * 5.4) + 40) -- * 5.4) + 40)
+	return min, max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
