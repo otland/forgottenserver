d2a7d0d73d667d5983548929cd5b58d8d3720bc2
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 21:54:58 -0200

    Update rage of the skies.lua

--- data/spells/scripts/attack/rage of the skies.lua
+++ data/spells/scripts/attack/rage of the skies.lua
@@ -6,9 +6,9 @@ local area = createCombatArea(AREA_CROSS6X6)
 setCombatArea(combat, area)
 
 function onGetFormulaValues(cid, level, maglevel)
-min = -((level*0.2) + (maglevel*5))
-max = -((level*0.2) + (maglevel*12))
-return min, max
+	min = -((level / 5) + (maglevel * 4) + 75) -- * 4) + 75)
+	max = -((level / 5) + (maglevel * 10) + 150) -- * 10) + 150)
+	return min, max
 end
 
 setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
