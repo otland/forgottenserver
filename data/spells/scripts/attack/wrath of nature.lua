406ec57d4c0b78f1c5293c6fe668e184171c1293
raymondtfr <raymond.ek@hotmail.com>
2014-01-26 00:01:52 -0200

    Update wrath of nature.lua

--- data/spells/scripts/attack/wrath of nature.lua
+++ data/spells/scripts/attack/wrath of nature.lua
@@ -6,8 +6,8 @@ local area = createCombatArea(AREA_CROSS6X6)
 setCombatArea(combat, area)
 
 function onGetFormulaValues(cid, level, maglevel)
-	min = -((level / 5) + (maglevel * 3) + 32) -- * 3) + 32)
-	max = -((level / 5) + (maglevel * 9) + 40) -- * 9) + 40)
+	min = -((level / 5) + (maglevel * 3) + 32)
+	max = -((level / 5) + (maglevel * 9) + 40)
 	return min, max
 end
 
