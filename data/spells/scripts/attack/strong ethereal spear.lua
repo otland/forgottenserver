24b9a73a1398836ebf9217530be1528ec33f3055
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 22:08:36 -0200

    Update strong ethereal spear.lua

--- data/spells/scripts/attack/strong ethereal spear.lua
+++ data/spells/scripts/attack/strong ethereal spear.lua
@@ -7,10 +7,9 @@ setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ETHEREALSPEAR)
 function onGetPlayerMinMaxValues(cid, level, attackValue)
 	local distanceSkill = Player(cid):getEffectiveSkillLevel(SKILL_DISTANCE)
 	local skillTotal, levelTotal = distanceSkill * attackValue, level / 5
-	return -(((skillTotal * 0.015) + 17) + (levelTotal)) / 1.5, -(((skillTotal * 0.025) + 13) + (levelTotal))
+	return -(((skillTotal * 0.0155) + 7) + (levelTotal)), -(((skillTotal * 0.025) + 13) + (levelTotal))
 end
--- If you prefer to use min values from TS instead of raw values, e.g. '/ 1.5'
--- return -(((skillTotal * 0.0155) + 7) + (levelTotal)), -(((skillTotal * 0.025) + 13) + (levelTotal))
+
 setCombatCallback(combat, CALLBACK_PARAM_SKILLVALUE, "onGetPlayerMinMaxValues")
 
 function onCastSpell(cid, var)
