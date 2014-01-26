562c6593702442aacc19ef412401c738a06eed65
raymondtfr <raymond.ek@hotmail.com>
2014-01-26 00:05:40 -0200

    Update ultimate flame strike.lua

--- data/spells/scripts/attack/ultimate flame strike.lua
+++ data/spells/scripts/attack/ultimate flame strike.lua
@@ -5,8 +5,8 @@ setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_FIREATTACK)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_FIRE)
 
 function onGetFormulaValues(cid, level, maglevel)
-	min = -((level / 5) + (maglevel * 4.5) + 35) -- * 4.5) + 35)
-	max = -((level / 5) + (maglevel * 7.3) + 55) -- * 7.3) + 55)
+	min = -((level / 5) + (maglevel * 4.5) + 35)
+	max = -((level / 5) + (maglevel * 7.3) + 55)
 	return min, max
 end
 
