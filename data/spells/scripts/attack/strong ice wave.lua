f30c51dfcf272cbb3ed4374cc8c12b6fb7c4411d
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 22:02:55 -0200

    Update strong ice wave.lua

--- data/spells/scripts/attack/strong ice wave.lua
+++ data/spells/scripts/attack/strong ice wave.lua
@@ -1,10 +1,17 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_ICEATTACK)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -1.0, 0, -1.5, 0)
-
+-- TODO: CREATE THE CORRECT WAVE AREA FOR THIS SPELL
 local area = createCombatArea(AREA_WAVE4, AREADIAGONAL_WAVE4)
 setCombatArea(combat, area)
+
+function onGetFormulaValues(cid, level, maglevel)
+	min = -((level / 5) + (maglevel * 4.5) + 20) -- * 4.5) + 20)
+	max = -((level / 5) + (maglevel * 7.6) + 48) -- * 7.6) + 48)
+	return min, max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
