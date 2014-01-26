a07b62d4abf9770cfcf9da040160845c2cee60b1
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 23:31:41 -0200

    Update great fireball.lua

--- data/spells/scripts/attack/great fireball.lua
+++ data/spells/scripts/attack/great fireball.lua
@@ -7,8 +7,8 @@ local area = createCombatArea(AREA_CIRCLE3X3)
 setCombatArea(combat, area)
 
 function onGetFormulaValues(cid, level, maglevel)
-	min = -((level / 5) + (maglevel * 1.2) + 7) -- * 1.2) + 7)
-	max = -((level / 5) + (maglevel * 2.85) + 16) -- * 2.85) + 16)
+	min = -((level / 5) + (maglevel * 1.2) + 7)
+	max = -((level / 5) + (maglevel * 2.85) + 16)
 	return min, max
 end
 
