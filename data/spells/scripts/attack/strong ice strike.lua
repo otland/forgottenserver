3c08c921a262578e92c568cbcb60d5db5548e5b0
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 23:54:24 -0200

    Update strong ice strike.lua

--- data/spells/scripts/attack/strong ice strike.lua
+++ data/spells/scripts/attack/strong ice strike.lua
@@ -4,8 +4,8 @@ setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_ICEATTACK)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_SMALLICE)
 
 function onGetFormulaValues(cid, level, maglevel)
-	min = -((level / 5) + (maglevel * 2.8) + 16) -- * 2.8) + 16)
-	max = -((level / 5) + (maglevel * 4.4) + 28) -- * 4.4) + 28)
+	min = -((level / 5) + (maglevel * 2.8) + 16)
+	max = -((level / 5) + (maglevel * 4.4) + 28)
 	return min, max
 end
 
