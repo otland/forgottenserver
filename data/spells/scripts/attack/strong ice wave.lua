d4fa3856f59f12df0f0e95cfecfb2ea502801503
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 23:54:09 -0200

    Update strong ice wave.lua
    We still need to create the correct wave area for this spell as it is a shortened version of Ice Wave.

--- data/spells/scripts/attack/strong ice wave.lua
+++ data/spells/scripts/attack/strong ice wave.lua
@@ -1,13 +1,13 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_ICEATTACK)
--- TODO: CREATE THE CORRECT WAVE AREA FOR THIS SPELL
+
 local area = createCombatArea(AREA_WAVE4, AREADIAGONAL_WAVE4)
 setCombatArea(combat, area)
 
 function onGetFormulaValues(cid, level, maglevel)
-	min = -((level / 5) + (maglevel * 4.5) + 20) -- * 4.5) + 20)
-	max = -((level / 5) + (maglevel * 7.6) + 48) -- * 7.6) + 48)
+	min = -((level / 5) + (maglevel * 4.5) + 20)
+	max = -((level / 5) + (maglevel * 7.6) + 48)
 	return min, max
 end
 
