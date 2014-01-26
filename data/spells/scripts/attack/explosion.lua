232415b2aaae535b26e6ce2d59d99b3deef7ae7a
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 23:04:58 -0200

    Update explosion.lua

--- data/spells/scripts/attack/explosion.lua
+++ data/spells/scripts/attack/explosion.lua
@@ -7,8 +7,8 @@ local area = createCombatArea(AREA_CROSS1X1)
 setCombatArea(combat, area)
 
 function onGetFormulaValues(cid, level, maglevel)
-	min = -((level / 5) + (maglevel * 1.6) + 9) -- * 1.6) + 9)
-	max = -((level / 5) + (maglevel * 3.2) + 19) -- * 3.2) + 19)
+	min = -((level / 5) + (maglevel * 1.6) + 9)
+	max = -((level / 5) + (maglevel * 3.2) + 19)
 	return min, max
 end
 
