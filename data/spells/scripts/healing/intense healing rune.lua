2f9e41d90e50eed351652ded4418b5974583d249
raymondtfr <raymond.ek@hotmail.com>
2014-01-26 00:21:20 -0200

    Update intense healing rune.lua

--- data/spells/scripts/healing/intense healing rune.lua
+++ data/spells/scripts/healing/intense healing rune.lua
@@ -6,8 +6,8 @@ setCombatParam(combat, COMBAT_PARAM_TARGETCASTERORTOPMOST, 1)
 setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
 
 function onGetFormulaValues(cid, level, maglevel)
-	min = ((level / 5) + (maglevel * 3.2) + 20) -- 3.2) + 20)
-	max = ((level / 5) + (maglevel * 5.4) + 40) -- * 5.4) + 40)
+	min = ((level / 5) + (maglevel * 3.2) + 20)
+	max = ((level / 5) + (maglevel * 5.4) + 40)
 	return min, max
 end
 
