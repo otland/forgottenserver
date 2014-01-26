7f5609d74e760448b09722fd08fc94118c7bf8be
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 22:55:41 -0200

    Update energy beam.lua

--- data/spells/scripts/attack/energy beam.lua
+++ data/spells/scripts/attack/energy beam.lua
@@ -6,8 +6,8 @@ local area = createCombatArea(AREA_BEAM5, AREADIAGONAL_BEAM5)
 setCombatArea(combat, area)
 
 function onGetFormulaValues(cid, level, maglevel)
-	min = -((level / 5) + (maglevel * 1.8) + 11) -- * 1.8) + 11)
-	max = -((level / 5) + (maglevel * 3) + 19) -- * 3) + 19)
+	min = -((level / 5) + (maglevel * 1.8) + 11)
+	max = -((level / 5) + (maglevel * 3) + 19)
 	return min, max
 end
 
