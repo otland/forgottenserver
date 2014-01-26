2c77d48d9817f880e9dea8f014406016af09dd3a
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 23:48:29 -0200

    Update physical strike.lua

--- data/spells/scripts/attack/physical strike.lua
+++ data/spells/scripts/attack/physical strike.lua
@@ -4,8 +4,8 @@ setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_EXPLOSIONAREA)
 ssetCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_EXPLOSION)
 
 function onGetFormulaValues(cid, level, maglevel)
-	min = -((level / 5) + (maglevel * 1.6) + 9) -- * 1.6) + 9)
-	max = -((level / 5) + (maglevel * 2.4) + 14) -- * 2.4) + 14)
+	min = -((level / 5) + (maglevel * 1.6) + 9)
+	max = -((level / 5) + (maglevel * 2.4) + 14)
 	return min, max
 end
 
