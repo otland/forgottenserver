4247870cb8350674ecce6da0c4435fd294951308
raymondtfr <raymond.ek@hotmail.com>
2014-01-25 23:58:14 -0200

    Update terra strike.lua

--- data/spells/scripts/attack/terra strike.lua
+++ data/spells/scripts/attack/terra strike.lua
@@ -4,8 +4,8 @@ setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_CARNIPHILA)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_SMALLEARTH)
 
 function onGetFormulaValues(cid, level, maglevel)
-	min = -((level / 5) + (maglevel * 1.4) + 8) -- * 1.4) + 8)
-	max = -((level / 5) + (maglevel * 2.2) + 14) -- * 2.2) + 14)
+	min = -((level / 5) + (maglevel * 1.4) + 8)
+	max = -((level / 5) + (maglevel * 2.2) + 14)
 	return min, max
 end
 
