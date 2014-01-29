91f3964794a23359100180b32a45d1520050a9c3
raymondtfr <raymond.ek@hotmail.com>
2014-01-29 02:32:50 -0200

    Update strong ethereal spear.lua

--- data/spells/scripts/attack/strong ethereal spear.lua
+++ data/spells/scripts/attack/strong ethereal spear.lua
@@ -1,14 +1,15 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_BLOCKARMOR, 1)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_HITAREA)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ETHEREALSPEAR)
 
-function onGetFormulaValues(cid, level, maglevel)
+function onGetFormulaValues(cid, level, attack, factor)
 	local distanceSkill = Player(cid):getEffectiveSkillLevel(SKILL_DISTANCE)
-	local min = (((distanceSkill + 25) / 3) + (level / 5)) * 1.47
-	local max = (distanceSkill + 25 + (level / 5)) * 1.47
-	return -min, -max
+	local skillDouble, levelTotal = distanceSkill * 2, level / 5
+	return -(((skillDouble + attack / 2500) * 0.5) + (levelTotal) + 7), -(((skillDouble + attack / 1875) * 0.75) + (levelTotal) + 13)
 end
+
 setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(cid, var)
