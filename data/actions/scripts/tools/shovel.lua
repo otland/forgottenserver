8ed312568e01a010b428e20ba114b32a2ebf52a1
WibbenZ <wibbenz@gmail.com>
2014-02-08 09:00:11 +0100

    Update shovel.lua

--- data/actions/scripts/tools/shovel.lua
+++ data/actions/scripts/tools/shovel.lua
@@ -3,7 +3,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if(isInArray(holes, itemEx.itemid)) then
 		Item(itemEx.uid):transform(itemEx.itemid + 1)
 		Item(itemEx.uid):decay()
-	elseif(itemEx.itemid == 231 or 9059) then
+	elseif(itemEx.itemid == 231 or itemEx.itemid == 9059) then
 		local rand = math.random(1, 100)
 		if(rand == 1) then
 			doCreateItem(2159, 1, toPosition)
