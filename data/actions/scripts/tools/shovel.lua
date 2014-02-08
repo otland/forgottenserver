b42e7d15525c50610d40df0bbf9cc217d1c98558
WibbenZ <wibbenz@gmail.com>
2014-02-08 01:29:37 +0100

    Missing item id and new functions
    Added item id 9059, and changed to some new functions.

--- data/actions/scripts/tools/shovel.lua
+++ data/actions/scripts/tools/shovel.lua
@@ -1,18 +1,18 @@
 local holes = {468, 481, 483}
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if isInArray(holes, itemEx.itemid) == TRUE then
-		doTransformItem(itemEx.uid, itemEx.itemid + 1)
-		doDecayItem(itemEx.uid)
-	elseif itemEx.itemid == 231 then
+	if(isInArray(holes, itemEx.itemid)) then
+		Item(itemEx.uid):transform(itemEx.itemid + 1)
+		Item(itemEx.uid):decay()
+	elseif(itemEx.itemid == 231 or 9059) then
 		local rand = math.random(1, 100)
-		if rand == 1 then
+		if(rand == 1) then
 			doCreateItem(2159, 1, toPosition)
-		elseif rand > 95 then
+		elseif(rand > 95) then
 			doSummonCreature("Scarab", toPosition)
 		end
-		doSendMagicEffect(toPosition, CONST_ME_POFF)
+		Position(toPosition):sendMagicEffect(CONST_ME_POFF)
 	else
-		return FALSE
+		return false
 	end
-	return TRUE
+return true
 end
