eab5e0f9f9f7cb99bb23afa4a387cf2c66bdb4cf
Royalot <zapf-benny@web.de>
2014-02-22 13:05:07 +0100

    updated fishing.lua
    added waterelemental loot and icefishing

--- data/actions/scripts/tools/fishing.lua
+++ data/actions/scripts/tools/fishing.lua
@@ -1,38 +1,70 @@
-local useWorms = FALSE
+local useWorms = false
 local waterIds = {493, 4608, 4609, 4610, 4611, 4612, 4613, 4614, 4615, 4616, 4617, 4618, 4619, 4620, 4621, 4622, 4623, 4624, 4625}
 local iceholeIds = {7236}
+local waterelementalcorpseIds = {10499}
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if isInArray(waterIds, itemEx.itemid) == TRUE then
+local i = Item(itemEx.uid)
+local p = Player(cid)
+	if isInArray(waterIds, itemEx.itemid) == true then
 		if itemEx.itemid ~= 493 then
-			if useWorms == FALSE or useWorms == TRUE and doPlayerRemoveItem(cid, ITEM_WORM, 1) == TRUE then
-				if math.random(1, (100 + (getPlayerSkill(cid, SKILL_FISHING) / 10))) <= getPlayerSkill(cid, SKILL_FISHING) then
-					doPlayerAddItem(cid, ITEM_FISH, 1)
+			if useWorms == false or useWorms == true and p:removeItem(ITEM_WORM, 1) == true then
+				if math.random(1, (100 + (p:getSkillLevel(SKILL_FISHING) / 10))) <= p:getSkillLevel(SKILL_FISHING) then
+					p:addItem(ITEM_FISH, 1)
 				end
-				doPlayerAddSkillTry(cid, SKILL_FISHING, 1)
+				p:addSkillTries(SKILL_FISHING, 1)
 			end
 		end
-		doSendMagicEffect(toPosition, CONST_ME_LOSEENERGY)
-		return TRUE
-	elseif isInArray(iceholeIds, itemEx.itemid) == TRUE then -- fish in icehole --
-		if useWorms == FALSE or useWorms == TRUE and doPlayerRemoveItem(cid, ITEM_WORM, 1) == TRUE then
-			if math.random(1, (100 + (getPlayerSkill(cid, SKILL_FISHING) / 10))) <= getPlayerSkill(cid, SKILL_FISHING) then
+		toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
+		return true
+	elseif isInArray(iceholeIds, itemEx.itemid) == true then -- fish in ice hole --
+		if useWorms == false or useWorms == true and p:removeItem(ITEM_WORM, 1) == true then
+			if math.random(1, (100 + (p:getSkillLevel(SKILL_FISHING) / 10))) <= p:getSkillLevel(SKILL_FISHING) then
 				local rand = math.random(1, 100)
 				if(rand > 98) then
-					doPlayerAddItem(cid,7158, 1) -- rainbow trout 2% --
+					p:addItem(7158, 1) -- rainbow trout 2% --
 				elseif(rand > 95) then
-					doPlayerAddItem(cid,2669, 1) -- Northern Pike 3% --
+					p:addItem(2669, 1) -- Northern Pike 3% --
 				elseif(rand > 89) then
-					doPlayerAddItem(cid,7159, 1) -- Green Perch 6% --
+					p:addItem(7159, 1) -- Green Perch 6% --
 				else
-					doPlayerAddItem(cid, ITEM_FISH, 1) -- normal fish --
+					p:addItem(ITEM_FISH, 1) -- normal fish --
 				end
-				doPlayerAddSkillTry(cid, SKILL_FISHING, 1)
-				Item(itemEx.uid):transform(7237)
-				Item(itemEx.uid):decay()
+				p:addSkillTries(SKILL_FISHING, 1)
+				i:transform(7237)
+				i:decay()
 			end
-			doSendMagicEffect(toPosition, CONST_ME_LOSEENERGY)
-			return TRUE
+			toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
+			return true
 		end
+	elseif isInArray(waterelementalcorpseIds, itemEx.itemid) == true then -- fish Water elemental Corpse --
+		if math.random(1, (100 + (p:getSkillLevel(SKILL_FISHING) / 10))) <= p:getSkillLevel(SKILL_FISHING) then
+			local rand = math.random(1, 1000)
+			if(rand > 995) then
+				p:addItem(7632, 1) --  Giant Shimmering Pearl 0,5% --
+			elseif(rand > 983) then
+				p:addItem(2149, 1) -- small emerald 1,2% --
+			elseif(rand > 972) then
+				p:addItem(2167, 1) -- energy ring 1,1% --
+			elseif(rand > 962) then
+				p:addItem(7159, 1) -- Green Perch 1% --
+			elseif(rand > 952) then
+				p:addItem(2145, 1) -- Small Diamond 1% --
+			elseif(rand > 942) then
+				p:addItem(7158, 1) -- rainbow trout 1% --
+			elseif(rand > 933) then
+				p:addItem(2168, 1) -- life ring 0,9% --
+			elseif(rand > 924) then
+				p:addItem(2143, 1) -- white pearl 0,9% --
+			elseif(rand > 916) then
+				p:addItem(10220, 1) -- leviathan's amulet 0,8% --
+			elseif(rand > 904) then
+				p:addItem(2146, 1) -- small sapphire 1,2% --	
+			end
+			p:addSkillTries(SKILL_FISHING, 1)
+			i:transform(10500)
+		end
+		toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
+		return true
 	end
-	return FALSE
+	return false
 end
