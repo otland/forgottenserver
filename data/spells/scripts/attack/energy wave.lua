c79f53095c61078c429571483c0ba0e6d0ec57c9
raymondtfr <raymond.ek@hotmail.com>
2014-02-27 20:56:36 -0200

    Update energy wave.lua
    Fixed wrong extra value of max dmg

--- data/spells/scripts/attack/energy wave.lua
+++ data/spells/scripts/attack/energy wave.lua
@@ -8,7 +8,7 @@ setCombatArea(combat, area)
 
 function onGetFormulaValues(cid, level, maglevel)
 	min = -((level / 5) + (maglevel * 4.5) + 20)
-	max = -((level / 5) + (maglevel * 7.6) + 20)
+	max = -((level / 5) + (maglevel * 7.6) + 48)
 	return min, max
 end
 
