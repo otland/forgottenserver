1d4f948946fb6e39787202247f29b6a8a4e0f24e
jpmagg <jamison.usa@hotmail.com>
2014-02-28 20:08:25 -0300

    Fix createbread.lua!

--- data/actions/scripts/other/createbread.lua
+++ data/actions/scripts/other/createbread.lua
@@ -1,15 +1,24 @@
 local liquidContainers = {1775, 2005, 2006, 2007, 2008, 2009, 2011, 2012, 2013, 2014, 2015, 2023, 2031, 2032, 2033}
 local millstones = {1381, 1382, 1383, 1384}
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if item.itemid == 2692 and isInArray(liquidContainers, itemEx.itemid) == TRUE and itemEx.type == 1 then
-		doChangeTypeItem(item.uid, item.type - 1)
-		doPlayerAddItem(cid, 2693, 1)
-		doChangeTypeItem(itemEx.uid, item.type - item.type)
-	elseif isInArray(millstones, itemEx.itemid) == TRUE then
-		doChangeTypeItem(item.uid, item.type - 1)
-		doPlayerAddItem(cid, 2692, 1)
+
+local player = Player(cid)
+local itemx = Item(item.uid)
+local itemex = Item(itemEx.uid)
+
+    if item.itemid == 2692 then
+		if isInArray(liquidContainers, itemEx.itemid) and itemEx.type == 1 then
+			itemx:remove(1)
+			player:addItem(2693, 1)
+			itemex:transform(itemEx.itemid, 0)
+		end
+    elseif item.itemid == 2694 then
+		if isInArray(millstones, itemEx.itemid) then
+			player:addItem(2692, 1)
+			itemx:remove(1)
+		end
 	else
 		return false
-	end
-	return true
+    end
+    return true
 end
