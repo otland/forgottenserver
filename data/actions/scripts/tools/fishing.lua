f84848dca5e5ab2bcc144b73af82d931892a8036
Faith2531 <Faith2531@users.noreply.github.com>
2014-03-12 16:29:17 +0100

    fishing.lua - fixes and improvements
    added ice hole support
    added sandfish fishing
    extra fishing(buff or eq) is now considered
    removed comparision with boolean
    used metatables

--- data/actions/scripts/tools/fishing.lua
+++ data/actions/scripts/tools/fishing.lua
@@ -1,17 +1,42 @@
-local useWorms = FALSE
-local waterIds = {493, 4608, 4609, 4610, 4611, 4612, 4613, 4614, 4615, 4616, 4617, 4618, 4619, 4620, 4621, 4622, 4623, 4624, 4625}
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if isInArray(waterIds, itemEx.itemid) == TRUE then
-		if itemEx.itemid ~= 493 then
-			if useWorms == FALSE or useWorms == TRUE and doPlayerRemoveItem(cid, ITEM_WORM, 1) == TRUE then
-				if math.random(1, (100 + (getPlayerSkill(cid, SKILL_FISHING) / 10))) <= getPlayerSkill(cid, SKILL_FISHING) then
-					doPlayerAddItem(cid, ITEM_FISH, 1)
-				end
-				doPlayerAddSkillTry(cid, SKILL_FISHING, 1)
+	local waterIds = {493, 4608, 4609, 4610, 4611, 4612, 4613, 4614, 4615, 4616, 4617, 4618, 4619, 4620, 4621, 4622, 4623, 4624, 4625, 7236, 15401, 15402}
+	local useWorms = true
+	local player = Player(cid)
+	
+	if(not isInArray(waterIds, itemEx.itemid)) then
+		return false
+	end
+
+	if itemEx.itemid ~= 7236 then
+		doSendMagicEffect(toPosition, CONST_ME_LOSEENERGY)
+	end
+	
+	if itemEx.itemid == 493 or itemEx.itemid == 15402 then
+		return true
+	end
+
+	player:addSkillTries(SKILL_FISHING, 1)
+	if math.random(1, (100 + (player:getEffectiveSkillLevel(SKILL_FISHING) / 10))) <= player:getEffectiveSkillLevel(SKILL_FISHING) then
+		if useWorms then
+			if(not player:removeItem(ITEM_WORM, 1)) then
+				return true
 			end
 		end
-		doSendMagicEffect(toPosition, CONST_ME_LOSEENERGY)
-		return TRUE
+	
+		if itemEx.itemid == 15401 then
+			if math.random(1, 100) >= 97 then
+				player:addItem(15405, 1)
+			else
+				player:addItem(ITEM_FISH, 1)
+			end
+		else
+			player:addItem(ITEM_FISH, 1)
+		end
+		
+		if itemEx.itemid == 7236 or itemEx.itemid == 15401 then
+			doTransformItem(itemEx.uid, itemEx.itemid + 1)
+			doDecayItem(itemEx.uid)
+		end
 	end
-	return FALSE
+	return true
 end
