b875756a434394b175b44f02ef3be2c18c5bb5c3
Mark Samman <mark.samman@gmail.com>
2014-03-24 14:28:53 -0400

    Fix sharpshooter spell
    Fixes issue #354 and #379

--- data/spells/scripts/support/sharpshooter.lua
+++ data/spells/scripts/support/sharpshooter.lua
@@ -1,26 +1,29 @@
-local conditionAttrib = createConditionObject(CONDITION_ATTRIBUTES)
-local conditionSlow = createConditionObject(CONDITION_HASTE)
-local conditionExhaustHeal = createConditionObject(CONDITION_EXHAUST_HEAL)
-
-setConditionParam(conditionAttrib, CONDITION_PARAM_TICKS, 10000)
-setConditionParam(conditionAttrib, CONDITION_PARAM_SKILL_DISTANCEPERCENT, 150)
-setConditionParam(conditionAttrib, CONDITION_PARAM_BUFF_SPELL, 1)
-
-setConditionParam(conditionSlow, CONDITION_PARAM_TICKS, 10000)
-setConditionFormula(conditionSlow, -0.7, 0, -0.7, 0)
-
-setConditionParam(conditionExhaustHeal, CONDITION_PARAM_TICKS, 10000)
-
 local combat = createCombatObject()
-setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_GREEN)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
-setCombatCondition(combat, conditionAttrib)
-setCombatCondition(combat, conditionSlow)
-setCombatCondition(combat, conditionExhaustHeal)
+
+local condition = createConditionObject(CONDITION_ATTRIBUTES)
+setConditionParam(condition, CONDITION_PARAM_TICKS, 10000)
+setConditionParam(condition, CONDITION_PARAM_SKILL_DISTANCEPERCENT, 150)
+setConditionParam(condition, CONDITION_PARAM_SKILL_SHIELDPERCENT, -100)
+setConditionParam(condition, CONDITION_PARAM_BUFF_SPELL, 1)
+setCombatCondition(combat, condition)
+
+local speed = createConditionObject(CONDITION_PARALYZE)
+setConditionParam(speed, CONDITION_PARAM_TICKS, 10000)
+setConditionFormula(speed, -0.7, 56, -0.7, 56)
+setCombatCondition(combat, speed)
+
+local exhaustHealGroup = createConditionObject(CONDITION_SPELLGROUPCOOLDOWN)
+setConditionParam(exhaustHealGroup, CONDITION_PARAM_SUBID, 2)
+setConditionParam(exhaustHealGroup, CONDITION_PARAM_TICKS, 10000)
+setCombatCondition(combat, exhaustHealGroup)
+
+local exhaustSupportGroup = createConditionObject(CONDITION_SPELLGROUPCOOLDOWN)
+setConditionParam(exhaustSupportGroup, CONDITION_PARAM_SUBID, 3)
+setConditionParam(exhaustSupportGroup, CONDITION_PARAM_TICKS, 10000)
+setCombatCondition(combat, exhaustSupportGroup)
 
 function onCastSpell(cid, var)
-	if(doCombat(cid, combat, var) == LUA_NO_ERROR) then
-		return LUA_NO_ERROR
-	end
-	return LUA_ERROR
+	return doCombat(cid, combat, var)
 end
