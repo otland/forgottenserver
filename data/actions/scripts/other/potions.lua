3f6c40436386af96866a5fe15766c94e2636c0c9
andypsylon <github.com@tfm24.de>
2014-03-26 09:05:03 +0100

    /actions only exchange old for new functions

--- data/actions/scripts/other/potions.lua
+++ data/actions/scripts/other/potions.lua
@@ -12,15 +12,15 @@ local greatEmptyPot = 7635
 local strongEmptyPot = 7634
 local emptyPot = 7636
 
-local antidote = createCombatObject()
-setCombatParam(antidote, COMBAT_PARAM_TYPE, COMBAT_HEALING)
-setCombatParam(antidote, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
-setCombatParam(antidote, COMBAT_PARAM_TARGETCASTERORTOPMOST, TRUE)
-setCombatParam(antidote, COMBAT_PARAM_AGGRESSIVE, FALSE)
-setCombatParam(antidote, COMBAT_PARAM_DISPEL, CONDITION_POISON)
+local antidote = Combat()
+antidote:setParameter(COMBAT_PARAM_TYPE, COMBAT_HEALING)
+antidote:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
+antidote:setParameter(COMBAT_PARAM_TARGETCASTERORTOPMOST, true)
+antidote:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
+antidote:setParameter(COMBAT_PARAM_DISPEL, CONDITION_POISON)
 
-local exhaust = createConditionObject(CONDITION_EXHAUST_HEAL)
-setConditionParam(exhaust, CONDITION_PARAM_TICKS, (configManager.getNumber(configKeys.EX_ACTIONS_DELAY_INTERVAL) - 100))
+local exhaust = Condition(CONDITION_EXHAUST_HEAL)
+exhaust:setParameter(CONDITION_PARAM_TICKS, (configManager.getNumber(configKeys.EX_ACTIONS_DELAY_INTERVAL) - 100))
 -- 1000 - 100 due to exact condition timing. -100 doesn't hurt us, and players don't have reminding ~50ms exhaustion.
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
@@ -28,121 +28,123 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		return true
 	end
 
-	if(getCreatureCondition(cid, CONDITION_EXHAUST_HEAL) == TRUE) then
-		doPlayerSendDefaultCancel(cid, RETURNVALUE_YOUAREEXHAUSTED)
-		return TRUE
+	local player = Player(cid)
+	if player:getCondition(CONDITION_EXHAUST_HEAL) then
+		player:sendTextMessage(MESSAGE_STATUS_SMALL, Game.getReturnMessage(RETURNVALUE_YOUAREEXHAUSTED))
+		return true
 	end
 
-	if(item.itemid == antidotePot) then
-		if(doCombat(cid, antidote, numberToVariant(cid)) == LUA_ERROR) then
-			return FALSE
+	if item.itemid == antidotePot then
+		if not doCombat(cid, antidote, numberToVariant(cid)) then
+			return false
 		end
-		doAddCondition(cid, exhaust)
-		doCreatureSay(cid, "Aaaah...", TALKTYPE_MONSTER_SAY)
-		doRemoveItem(item.uid, 1)
-		doPlayerAddItem(cid, emptyPot, 1)
-	elseif(item.itemid == smallHealthPot) then
-		if(doTargetCombatHealth(0, cid, COMBAT_HEALING, 60, 85, CONST_ME_MAGIC_BLUE) == LUA_ERROR) then
-			return FALSE
+		player:addCondition(exhaust)
+		player:say("Aaaah...", TALKTYPE_MONSTER_SAY)
+		Item(item.uid):remove(1)
+		player:addItem(emptyPot, 1)
+	elseif item.itemid == smallHealthPot then
+		if not doTargetCombatHealth(0, cid, COMBAT_HEALING, 60, 85, CONST_ME_MAGIC_BLUE) then
+			return false
 		end
-		doAddCondition(cid, exhaust)
-		doCreatureSay(cid, "Aaaah...", TALKTYPE_MONSTER_SAY)
-		doRemoveItem(item.uid, 1)
-		doPlayerAddItem(cid, emptyPot, 1)
-	elseif(item.itemid == healthPot) then
-		if(doTargetCombatHealth(0, cid, COMBAT_HEALING, 125, 175, CONST_ME_MAGIC_BLUE) == LUA_ERROR) then
-			return FALSE
+		player:addCondition(exhaust)
+		player:say("Aaaah...", TALKTYPE_MONSTER_SAY)
+		Item(item.uid):remove(1)
+		player:addItem(emptyPot, 1)
+	elseif item.itemid == healthPot then
+		if not doTargetCombatHealth(0, cid, COMBAT_HEALING, 125, 175, CONST_ME_MAGIC_BLUE) then
+			return false
 		end
-		doAddCondition(cid, exhaust)
-		doCreatureSay(cid, "Aaaah...", TALKTYPE_MONSTER_SAY)
-		doRemoveItem(item.uid, 1)
-		doPlayerAddItem(cid, emptyPot, 1)
-	elseif(item.itemid == manaPot) then
-		if(doTargetCombatMana(0, cid, 75, 125, CONST_ME_MAGIC_BLUE) == LUA_ERROR) then
-			return FALSE
+		player:addCondition(exhaust)
+		player:say("Aaaah...", TALKTYPE_MONSTER_SAY)
+		Item(item.uid):remove(1)
+		player:addItem(emptyPot, 1)
+	elseif item.itemid == manaPot then
+		if not doTargetCombatMana(0, cid, 75, 125, CONST_ME_MAGIC_BLUE) then
+			return false
 		end
-		doAddCondition(cid, exhaust)
-		doCreatureSay(cid, "Aaaah...", TALKTYPE_MONSTER_SAY)
-		doRemoveItem(item.uid, 1)
-		doPlayerAddItem(cid, emptyPot, 1)
-	elseif(item.itemid == strongHealthPot) then
-		if(not(isKnight(cid) or isPaladin(cid)) or (getPlayerLevel(cid) < 50)) and not(getPlayerGroupId(cid) >= 2) then
-			doCreatureSay(cid, "This potion can only be consumed by paladins and knights of level 50 or higher.", TALKTYPE_MONSTER_SAY)
-			return TRUE
-		end
-
-		if(doTargetCombatHealth(0, cid, COMBAT_HEALING, 250, 350, CONST_ME_MAGIC_BLUE) == LUA_ERROR) then
-			return FALSE
-		end
-		doAddCondition(cid, exhaust)
-		doCreatureSay(cid, "Aaaah...", TALKTYPE_MONSTER_SAY)
-		doRemoveItem(item.uid, 1)
-		doPlayerAddItem(cid, strongEmptyPot, 1)
-	elseif(item.itemid == strongManaPot) then
-		if(not(isSorcerer(cid) or isDruid(cid) or isPaladin(cid)) or (getPlayerLevel(cid) < 50)) and not(getPlayerGroupId(cid) >= 2) then
-			doCreatureSay(cid, "This potion can only be consumed by sorcerers, druids and paladins of level 50 or higher.", TALKTYPE_MONSTER_SAY)
-			return TRUE
+		player:addCondition(exhaust)
+		player:say("Aaaah...", TALKTYPE_MONSTER_SAY)
+		Item(item.uid):remove(1)
+		player:addItem(emptyPot, 1)
+	elseif item.itemid == strongHealthPot then
+		if(not isInArray({3,4,7,8}, player:getVocation():getId()) or player:getLevel() < 50) and not(player:getGroup():getId() >= 2) then
+			player:say("This potion can only be consumed by paladins and knights of level 50 or higher.", TALKTYPE_MONSTER_SAY)
+			return true
 		end
 
-		if(doTargetCombatMana(0, cid, 115, 185, CONST_ME_MAGIC_BLUE) == LUA_ERROR) then
-			return FALSE
+		if not doTargetCombatHealth(0, cid, COMBAT_HEALING, 250, 350, CONST_ME_MAGIC_BLUE) then
+			return false
 		end
-		doAddCondition(cid, exhaust)
-		doCreatureSay(cid, "Aaaah...", TALKTYPE_MONSTER_SAY)
-		doRemoveItem(item.uid, 1)
-		doPlayerAddItem(cid, strongEmptyPot, 1)
-	elseif(item.itemid == greatSpiritPot) then
-		if(not(isPaladin(cid)) or (getPlayerLevel(cid) < 80)) and not(getPlayerGroupId(cid) >= 2) then
-			doCreatureSay(cid, "This potion can only be consumed by paladins of level 80 or higher.", TALKTYPE_MONSTER_SAY)
-			return TRUE
-		end
-
-		if(doTargetCombatHealth(0, cid, COMBAT_HEALING, 250, 350, CONST_ME_MAGIC_BLUE) == LUA_ERROR or doTargetCombatMana(0, cid, 100, 200, CONST_ME_MAGIC_BLUE) == LUA_ERROR) then
-			return FALSE
-		end
-		doAddCondition(cid, exhaust)
-		doCreatureSay(cid, "Aaaah...", TALKTYPE_MONSTER_SAY)
-		doRemoveItem(item.uid, 1)
-		doPlayerAddItem(cid, greatEmptyPot, 1)
-	elseif(item.itemid == greatHealthPot) then
-		if(not(isKnight(cid)) or (getPlayerLevel(cid) < 80)) and not(getPlayerGroupId(cid) >= 2) then
-			doCreatureSay(cid, "This potion can only be consumed by knights of level 80 or higher.", TALKTYPE_MONSTER_SAY)
-			return TRUE
+		player:addCondition(exhaust)
+		player:say("Aaaah...", TALKTYPE_MONSTER_SAY)
+		Item(item.uid):remove(1)
+		player:addItem(strongEmptyPot, 1)
+	elseif item.itemid == strongManaPot then
+		if(not isInArray({1,2,3,5,6,7}, player:getVocation():getId()) or player:getLevel() < 50) and not(player:getGroup():getId() >= 2) then
+			player:say("This potion can only be consumed by sorcerers, druids and paladins of level 50 or higher.", TALKTYPE_MONSTER_SAY)
+			return true
 		end
 
-		if(doTargetCombatHealth(0, cid, COMBAT_HEALING, 425, 575, CONST_ME_MAGIC_BLUE) == LUA_ERROR) then
-			return FALSE
+		if not doTargetCombatMana(0, cid, 115, 185, CONST_ME_MAGIC_BLUE) then
+			return false
 		end
-		doAddCondition(cid, exhaust)
-		doCreatureSay(cid, "Aaaah...", TALKTYPE_MONSTER_SAY)
-		doRemoveItem(item.uid, 1)
-		doPlayerAddItem(cid, greatEmptyPot, 1)
-	elseif(item.itemid == greatManaPot) then
-		if(not(isSorcerer(cid) or isDruid(cid)) or (getPlayerLevel(cid) < 80)) and not(getPlayerGroupId(cid) >= 2) then
-			doCreatureSay(cid, "This potion can only be consumed by sorcerers and druids of level 80 or higher.", TALKTYPE_MONSTER_SAY)
-			return TRUE
-		end
-
-		if(doTargetCombatMana(0, cid, 150, 250, CONST_ME_MAGIC_BLUE) == LUA_ERROR) then
-			return FALSE
-		end
-		doAddCondition(cid, exhaust)
-		doCreatureSay(cid, "Aaaah...", TALKTYPE_MONSTER_SAY)
-		doRemoveItem(item.uid, 1)
-		doPlayerAddItem(cid, greatEmptyPot, 1)
-	elseif(item.itemid == ultimateHealthPot) then
-		if(not(isKnight(cid)) or (getPlayerLevel(cid) < 130)) and not(getPlayerGroupId(cid) >= 2) then
-			doCreatureSay(cid, "This potion can only be consumed by knights of level 130 or higher.", TALKTYPE_MONSTER_SAY)
-			return TRUE
+		player:addCondition(exhaust)
+		player:say("Aaaah...", TALKTYPE_MONSTER_SAY)
+		Item(item.uid):remove(1)
+		player:addItem(strongEmptyPot, 1)
+	elseif item.itemid == greatSpiritPot then
+		if(not isInArray({3, 7}, player:getVocation():getId()) or (player:getLevel() < 80)) and not(player:getGroup():getId() >= 2) then
+			player:say("This potion can only be consumed by paladins of level 80 or higher.", TALKTYPE_MONSTER_SAY)
+			return true
 		end
 
-		if(doTargetCombatHealth(0, cid, COMBAT_HEALING, 650, 850, CONST_ME_MAGIC_BLUE) == LUA_ERROR) then
-			return FALSE
+		if not doTargetCombatHealth(0, cid, COMBAT_HEALING, 250, 350, CONST_ME_MAGIC_BLUE) or not doTargetCombatMana(0, cid, 100, 200, CONST_ME_MAGIC_BLUE) then
+			return false
 		end
-		doAddCondition(cid, exhaust)
-		doCreatureSay(cid, "Aaaah...", TALKTYPE_MONSTER_SAY)
-		doRemoveItem(item.uid, 1)
-		doPlayerAddItem(cid, greatEmptyPot, 1)
+		player:addCondition(exhaust)
+		player:say("Aaaah...", TALKTYPE_MONSTER_SAY)
+		Item(item.uid):remove(1)
+		player:addItem(greatEmptyPot, 1)
+	elseif item.itemid == greatHealthPot then
+		if(not isInArray({4, 8}, player:getVocation():getId()) or player:getLevel() < 80) and not(player:getGroup():getId() >= 2) then
+			player:say("This potion can only be consumed by knights of level 80 or higher.", TALKTYPE_MONSTER_SAY)
+			return true
+		end
+
+		if not doTargetCombatHealth(0, cid, COMBAT_HEALING, 425, 575, CONST_ME_MAGIC_BLUE) then
+			return false
+		end
+		player:addCondition(exhaust)
+		player:say("Aaaah...", TALKTYPE_MONSTER_SAY)
+		Item(item.uid):remove(1)
+		player:addItem(greatEmptyPot, 1)
+	elseif item.itemid == greatManaPot then
+		if(not isInArray({1,2,5,6}, player:getVocation():getId()) or player:getLevel() < 80) and not(player:getGroup():getId() >= 2) then
+			player:say("This potion can only be consumed by sorcerers and druids of level 80 or higher.", TALKTYPE_MONSTER_SAY)
+			return true
+		end
+
+		if not doTargetCombatMana(0, cid, 150, 250, CONST_ME_MAGIC_BLUE) then
+			return false
+		end
+		player:addCondition(exhaust)
+		player:say("Aaaah...", TALKTYPE_MONSTER_SAY)
+		Item(item.uid):remove(1)
+		player:addItem(greatEmptyPot, 1)
+	elseif item.itemid == ultimateHealthPot then
+		if(not isInArray({4, 8}, player:getVocation():getId()) or player:getLevel() < 130) and not(player:getGroup():getId() >= 2) then
+			player:say("This potion can only be consumed by knights of level 130 or higher.", TALKTYPE_MONSTER_SAY)
+			return true
+		end
+
+		if not doTargetCombatHealth(0, cid, COMBAT_HEALING, 650, 850, CONST_ME_MAGIC_BLUE) then
+			return false
+		end
+		player:addCondition(exhaust)
+		player:say("Aaaah...", TALKTYPE_MONSTER_SAY)
+		Item(item.uid):remove(1)
+		player:addItem(greatEmptyPot, 1)
 	end
-	return TRUE
+
+	return true
 end
--- data/actions/scripts/tools/rope.lua
+++ data/actions/scripts/tools/rope.lua
@@ -1,25 +1,30 @@
-local holeId = {294, 369, 370, 383, 392, 408, 409, 410, 427, 428, 430, 462, 469, 470, 482, 484, 485, 489, 924, 3135, 3136, 7933, 7938, 8170, 8286, 8285, 8284, 8281, 8280, 8279, 8277, 8276, 8323, 8567, 8585, 8596, 8595, 8249, 8250, 8251, 8252, 8253, 8254, 8255, 8256, 8972, 9606, 9625, 13190, 14461, 19519, 21536}
+local holeId = {
+	294, 369, 370, 383, 392, 408, 409, 410, 427, 428, 430, 462, 469, 470, 482,
+	484, 485, 489, 924, 3135, 3136, 7933, 7938, 8170, 8286, 8285, 8284, 8281,
+	8280, 8279, 8277, 8276, 8323, 8567, 8585, 8596, 8595, 8249, 8250, 8251,
+	8252, 8253, 8254, 8255, 8256, 8972, 9606, 9625, 13190, 14461, 19519, 21536
+}
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if toPosition.x == CONTAINER_POSITION or (toPosition.x == 0 and toPosition.y == 0 and toPosition.z == 0) then
-		return false
-	end
+	local tile = toPosition:getTile()
 
-	local groundTile = getThingfromPos(toPosition)
-	if isInArray(ropeSpots, groundTile.itemid) then
-		doTeleportThing(cid, {x = toPosition.x, y = toPosition.y + 1, z = toPosition.z - 1}, FALSE)
+	if isInArray(ropeSpots, tile:getGround():getId()) or tile:getItemById(14435) then
+		Player(cid):teleportTo({x = toPosition.x, y = toPosition.y + 1, z = toPosition.z - 1}, false)
 		return true
 	elseif isInArray(holeId, itemEx.itemid) then
-		local thing = getThingfromPos({x = toPosition.x, y = toPosition.y, z = toPosition.z + 1, stackpos = STACKPOS_TOP_MOVEABLE_ITEM_OR_CREATURE})
-		if thing.uid > 0 then
-			if isCreature(thing.uid) and thing.type ~= THING_TYPE_PLAYER then
-				return false
+		local player = Player(cid)
+		toPosition.z = toPosition.z + 1
+		tile = toPosition:getTile()
+		if tile then
+			local thing = tile:getTopVisibleThing()
+			local itemType = thing and thing:getType():isMovable()
+			if itemType then
+				toPosition.y = toPosition.y + 1
+				return thing:moveTo(toPosition)
 			end
-
-			toPosition.y = toPosition.y + 1
-			doTeleportThing(thing.uid, toPosition)
-			return true
 		end
+		return player:sendTextMessage(MESSAGE_STATUS_SMALL, Game.getReturnMessage(RETURNVALUE_NOTPOSSIBLE))
 	end
+
 	return false
 end
