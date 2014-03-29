e30910e91198bd282b38e9dcf0d1509994eadca1
andypsylon <github.com@tfm24.de>
2014-03-29 07:27:41 +0100

    fix fix

--- data/movements/scripts/citizen.lua
+++ data/movements/scripts/citizen.lua
@@ -1,10 +1,11 @@
 function onStepIn(cid, item, position, fromPosition)
 	if item.actionid > 30020 and item.actionid < 30050 then
 		local player = Player(cid)
-		local town = Town(item.actionid - 30020)
-		if player and town then
-			player:setTown(town)
+		if not player then
+			return false
 		end
+		local town = Town(item.actionid - 30020)
+		player:setTown(town)
 	end
 
 	return true
