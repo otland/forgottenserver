54605df0cdea0172153b952cdf23fce92dd9f38b
Rey Aleman <rey.aleman@outlook.com>
2014-04-02 12:55:23 -0600

    rope.lua fix

--- data/actions/scripts/tools/rope.lua
+++ data/actions/scripts/tools/rope.lua
@@ -16,10 +16,8 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		tile = toPosition:getTile()
 		if tile then
 			local thing = tile:getTopVisibleThing()
-			local itemType = thing and thing:getType():isMovable()
-			if itemType then
-				toPosition.y = toPosition.y + 1
-				return thing:moveTo(toPosition)
+			if thing:isItem() and thing:getType():isMovable() then
+				return thing:moveTo({x = toPosition.x, y = toPosition.y + 1, z = toPosition.z - 1})
 			end
 		end
 		return Player(cid):sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
