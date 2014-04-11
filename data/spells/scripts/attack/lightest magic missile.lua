71795dfce812b5280f79d60af24cb268283b624c
raymondtfr <raymond.ek@hotmail.com>
2014-04-11 09:07:49 -0300

    Updating stuff
    I think I made it right now.

---
+++ data/spells/scripts/attack/lightest magic missile.lua
@@ -0,0 +1,16 @@
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_ENERGYAREA)
+setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
+
+function onGetFormulaValues(cid, level, maglevel)
+	min = -3
+	max = -5
+	return min, max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+
+function onCastSpell(cid, var)
+	return doCombat(cid, combat, var)
+end
---
+++ data/spells/scripts/healing/practise healing.lua
@@ -0,0 +1,17 @@
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_HEALING)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
+setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
+setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
+
+function onGetFormulaValues(cid, level, maglevel)
+	min = 4
+	max = 7
+	return min, max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+
+function onCastSpell(cid, var)
+	return doCombat(cid, combat, var)
+end
