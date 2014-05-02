a3218d00905d8426fcb327e6c0293abd159ab5a0
tarantonio <tarantonio@users.noreply.github.com>
2014-05-02 18:34:34 +0200

    Update magic rope.lua

--- data/spells/scripts/support/magic rope.lua
+++ data/spells/scripts/support/magic rope.lua
@@ -1,19 +1,18 @@
-function onCastSpell(cid, var)
-	local pos = getPlayerPosition(cid)
+function onCastSpell(creature, var)
+	local pos = creature:getPosition()
 	pos.stackpos = STACKPOS_GROUND
-	local thing = getThingfromPos(pos)
-	if not isInArray(ropeSpots, thing.itemid) then
-		pos.stackpos = STACKPOS_FIRST_ITEM_ABOVE_GROUNDTILE
-		thing = getThingfromPos(pos)
-		if not isInArray(ropeSpots, thing.itemid) then
-			doPlayerSendDefaultCancel(cid, RETURNVALUE_NOTPOSSIBLE)
-			doSendMagicEffect(pos, CONST_ME_POFF)
+	local tile = Tile(pos)
+	if tile then
+		local thing = tile:getThing()
+		if thing and thing:isItem() and not isInArray(ropeSpots, thing:getType():getId()) then
+			creature:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+			creature:getPosition():sendMagicEffect(CONST_ME_POFF)
 			return false
 		end
 	end
 
 	local destination = {x = pos.x, y = pos.y + 1, z = pos.z - 1}
-	doTeleportThing(cid, destination, false)
-	doSendMagicEffect(destination, CONST_ME_TELEPORT)
+	creature:teleportTo(destination, false)
+	creature:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 	return true
 end
