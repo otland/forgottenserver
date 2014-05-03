27452368e909343c67331b61b7db6f0ad7bfb1e7
WibbenZ <wibbenz@gmail.com>
2014-05-03 20:24:46 +0200

    Added a new compat function
    getPlayerVocationName, could not find it any of my newer or older compat.lua files so added it.

--- data/compat.lua
+++ data/compat.lua
@@ -112,6 +112,7 @@ function getPlayerMaxMana(cid) local p = Player(cid) return p ~= nil and p:getMa
 function getPlayerLevel(cid) local p = Player(cid) return p ~= nil and p:getLevel() or false end
 function getPlayerTown(cid) local p = Player(cid) return p ~= nil and p:getTown():getId() or false end
 function getPlayerVocation(cid) local p = Player(cid) return p ~= nil and p:getVocation():getId() or false end
+function getPlayerVocationName(cid) local p = Player(cid) return p ~= nil and p:getVocation():getName() end
 function getPlayerSoul(cid) local p = Player(cid) return p ~= nil and p:getSoul() or false end
 function getPlayerSex(cid) local p = Player(cid) return p ~= nil and p:getSex() or false end
 function getPlayerStorageValue(cid, key) local p = Player(cid) return p ~= nil and p:getStorageValue(key) or false end
