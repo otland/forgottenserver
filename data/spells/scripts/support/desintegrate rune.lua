8e0396a53039616d4966d5d93077dc887fc990d9
andypsylon <github.com@tfm24.de>
2014-05-16 10:28:03 +0200

    desintegrate rune.lua fix "It will disintegrate an entire square"

--- data/spells/scripts/support/desintegrate rune.lua
+++ data/spells/scripts/support/desintegrate rune.lua
@@ -5,15 +5,21 @@ function onCastSpell(creature, var)
 	local position = variantToPosition(var)
 	local tile = position:getTile()
 	local object = tile and tile:getTopVisibleThing()
-	if object
-		and not(object:isCreature() or tile:hasFlag(TILESTATE_PROTECTIONZONE) or isInArray(dead_human, object:getId()))
-		and object:getType():isMovable()
-		and object:getUniqueId() > 65535
-		and object:getActionId() == 0
-	then
-		object:remove(1)
-		position:sendMagicEffect(CONST_ME_BLOCKHIT)
-		return true
+	if object and not object:isCreature() then
+		local desintegrate = false
+		while not isInArray(dead_human, object:getId())
+			and object:getType():isMovable()
+			and object:getUniqueId() > 65535
+			and object:getActionId() == 0
+		do
+			object:remove()
+			desintegrate = true
+			object = tile:getTopVisibleThing()
+		end
+		if desintegrate then
+			position:sendMagicEffect(CONST_ME_BLOCKHIT)
+			return true
+		end
 	end
 
 	creature:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
