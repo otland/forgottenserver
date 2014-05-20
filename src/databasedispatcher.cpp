ff6d878b6d54614c09517c1367967abb4f03bff0
Alexandre A Severino <alexandre.a.severino@gmail.com>
2014-05-19 22:43:57 -0300

    Changed white spaces for Tabs.

--- src/databasedispatcher.cpp
+++ src/databasedispatcher.cpp
@@ -1,21 +1,21 @@
 /**
- * The Forgotten Server - a free and open-source MMORPG server emulator
- * Copyright (C) 2014  Mark Samman <mark.samman@gmail.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
+* The Forgotten Server - a free and open-source MMORPG server emulator
+* Copyright (C) 2014  Mark Samman <mark.samman@gmail.com>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 
 #include "otpch.h"
 
@@ -23,7 +23,7 @@
 
 DatabaseDispatcher::DatabaseDispatcher()
 {
-    m_executionThread = std::unique_ptr<std::thread>(new std::thread(&DatabaseDispatcher::processQueue, this));
+	m_executionThread = std::unique_ptr<std::thread>(new std::thread(&DatabaseDispatcher::processQueue, this));
 }
 
 DatabaseDispatcher::~DatabaseDispatcher()
@@ -31,68 +31,73 @@ DatabaseDispatcher::~DatabaseDispatcher()
 }
 
 void DatabaseDispatcher::queueSqlCommand(DBCommand_t type,
-                                         std::string query,
-                                         Callback_ptr callback,
-                                         DBInsert_ptr insertStmt,
-                                         DBTransaction_ptr transaction,
-                                         TransactionFunc_ptr transactionFunc, bool mustExecute)
+										 std::string query,
+										 Callback_ptr callback,
+										 DBInsert_ptr insertStmt,
+										 DBTransaction_ptr transaction,
+										 TransactionFunc_ptr transactionFunc)
 {
-    DBCommand sqlCommand(type, query, callback, insertStmt, transaction, transactionFunc, mustExecute);
+	DBCommand sqlCommand(type, query, callback, insertStmt, transaction, transactionFunc);
 
-    m_queueMutex.lock();
-    m_sqlCommandQueue.push(sqlCommand);
-    m_queueMutex.unlock();
+	m_queueMutex.lock();
+	m_sqlCommandQueue.push(sqlCommand);
+	m_queueMutex.unlock();
 }
 
 void DatabaseDispatcher::processQueue()
 {
-    while (true)
-    {
-        std::unique_lock<std::mutex> waitAdd(m_waitQueueMutex);
-        while (m_sqlCommandQueue.empty())
-        {
-            m_QueueAddCondition.wait(waitAdd);
-        }
+	bool succeess = false;
+	int tries = 0;
 
-        m_queueMutex.lock();
-        DBCommand sqlCommand = m_sqlCommandQueue.front();
-        m_sqlCommandQueue.pop();
-        m_queueMutex.unlock();
+	while (true)
+	{
+		std::unique_lock<std::mutex> waitAdd(m_waitQueueMutex);
+		while (m_sqlCommandQueue.empty())
+		{
+			m_QueueAddCondition.wait(waitAdd);
+		}
 
-        sqlCommand.tries++;
+		m_queueMutex.lock();
+		DBCommand sqlCommand = m_sqlCommandQueue.front();
+		m_queueMutex.unlock();
 
-        switch(sqlCommand.type)
-        {
-            case(DBCommand_t::INSERT):
-                break;
-            case(DBCommand_t::SELECT):
-                sqlCommand.result = Database::getInstance()->storeQuery(sqlCommand.query);
-                if (sqlCommand.result != nullptr)
-                    sqlCommand.succeeded = true;
-                break;
-            case(DBCommand_t::UPDATE):
-            case(DBCommand_t::DELETE):
-                if (Database::getInstance()->executeQuery(sqlCommand.query))
-                    sqlCommand.succeeded = true;
-                break;
-            case(DBCommand_t::TRANSACTION):
-                break;
-        }
+		tries++;
 
-        if (!sqlCommand.succeeded)
-        {
-            if (sqlCommand.tries >= 3 && !sqlCommand.mustExecute)
-            {
-                // TODO: log failure.
-                // Give up trying.
-            } else {
-                m_queueMutex.lock();
-                m_sqlCommandQueue.push(sqlCommand); // let's put it in the end of the line.
-                m_queueMutex.unlock();
-            }
-        } else if (sqlCommand.callback != nullptr)
-        {
-            (*sqlCommand.callback)(sqlCommand.result);
-        }
-    }
+		switch(sqlCommand.type)
+		{
+		case(DBCommand_t::INSERT):
+			break;
+		case(DBCommand_t::SELECT):
+			sqlCommand.result = Database::getInstance()->storeQuery(sqlCommand.query);
+			if (sqlCommand.result != nullptr)
+			{
+				success = true;
+				m_sqlCommandQueue.pop();
+			}
+			break;
+		case(DBCommand_t::UPDATE):
+		case(DBCommand_t::DELETE):
+			if (Database::getInstance()->executeQuery(sqlCommand.query))
+				sqlCommand.succeeded = true;
+			break;
+		case(DBCommand_t::TRANSACTION):
+			break;
+		}
+
+		if (!sqlCommand.succeeded)
+		{
+			if (sqlCommand.tries >= 3 && !sqlCommand.mustExecute)
+			{
+				// TODO: log failure.
+				// Give up trying.
+			} else {
+				m_queueMutex.lock();
+				m_sqlCommandQueue.push(sqlCommand); // let's put it in the end of the line.
+				m_queueMutex.unlock();
+			}
+		} else if (sqlCommand.callback != nullptr)
+		{
+			(*sqlCommand.callback)(sqlCommand.result);
+		}
+	}
 }
--- src/databasedispatcher.h
+++ src/databasedispatcher.h
@@ -7,75 +7,68 @@
 #include <condition_variable>
 
 enum DBCommand_t {
-    INSERT,
-    DELETE,
-    SELECT,
-    UPDATE,
-    TRANSACTION
+	INSERT,
+	DELETE,
+	SELECT,
+	UPDATE,
+	TRANSACTION
 };
 
 class DatabaseDispatcher
 {
 private:
-    typedef std::shared_ptr<std::function<void(DBResult_ptr)>> Callback_ptr;
-    typedef std::shared_ptr<std::function<void()>> TransactionFunc_ptr;
+	typedef std::shared_ptr<std::function<void(DBResult_ptr)>> Callback_ptr;
+	typedef std::shared_ptr<std::function<void()>> TransactionFunc_ptr;
 
-    struct DBCommand
-    {
-        DBCommand_t type;
-        std::string query;
-        Callback_ptr callback;
-        DBInsert_ptr insertStmt;
-        DBTransaction_ptr transaction;
-        TransactionFunc_ptr transactionFunc;
-        bool mustExecute = false;
+	struct DBCommand
+	{
+		DBCommand_t type;
+		std::string query;
+		Callback_ptr callback;
+		DBInsert_ptr insertStmt;
+		DBTransaction_ptr transaction;
+		TransactionFunc_ptr transactionFunc;
 
-        DBResult_ptr result = nullptr;
-        bool succeeded = false;
-        int tries = 0;
+		DBCommand() {}
+		DBCommand(DBCommand_t _type,
+				  std::string _query,
+				  Callback_ptr _callback,
+				  DBInsert_ptr _insertStmt,
+				  DBTransaction_ptr _transaction,
+				  TransactionFunc_ptr _transactionFunc):
+			type(_type), query(_query), callback(_callback),
+			insertStmt(_insertStmt), transaction(_transaction),
+			transactionFunc(_transactionFunc) {}
+	};
 
-        DBCommand() {}
-        DBCommand(DBCommand_t _type,
-                  std::string _query,
-                  Callback_ptr _callback,
-                  DBInsert_ptr _insertStmt,
-                  DBTransaction_ptr _transaction,
-                  TransactionFunc_ptr _transactionFunc,
-                  bool _mustExecute):
-            type(_type), query(_query), callback(_callback),
-            insertStmt(_insertStmt), transaction(_transaction),
-            transactionFunc(_transactionFunc), mustExecute(_mustExecute) {}
-    };
+	DatabaseDispatcher();
+	~DatabaseDispatcher();
 
-    DatabaseDispatcher();
-    ~DatabaseDispatcher();
+	void processQueue();
 
-    void processQueue();
+	std::queue<DBCommand> m_sqlCommandQueue;
 
-    std::queue<DBCommand> m_sqlCommandQueue;
+	std::recursive_mutex m_queueMutex;
+	std::mutex m_waitQueueMutex;
+	std::condition_variable m_QueueAddCondition;
 
-    std::recursive_mutex m_queueMutex;
-    std::mutex m_waitQueueMutex;
-    std::condition_variable m_QueueAddCondition;
-
-    std::unique_ptr<std::thread> m_executionThread;
+	std::unique_ptr<std::thread> m_executionThread;
 
 public:
-    static DatabaseDispatcher* getInstance()
-    {
-        static DatabaseDispatcher instance;
-        return &instance;
-    }
+	static DatabaseDispatcher* getInstance()
+	{
+		static DatabaseDispatcher instance;
+		return &instance;
+	}
 
-    void queueSqlCommand(DBCommand_t type,
-                         std::string query,
-                         Callback_ptr callback = nullptr,
-                         DBInsert_ptr insertStmt = nullptr,
-                         DBTransaction_ptr transaction = nullptr,
-                         TransactionFunc_ptr transactionFunc = nullptr,
-                         bool mustExecute = false);
+	void queueSqlCommand(DBCommand_t type,
+						 std::string query,
+						 Callback_ptr callback = nullptr,
+						 DBInsert_ptr insertStmt = nullptr,
+						 DBTransaction_ptr transaction = nullptr,
+						 TransactionFunc_ptr transactionFunc = nullptr);
 
-    void transactionToExecute(std::unique_ptr<DBTransaction> transaction); // nao pode ser um ponteiro
+	void transactionToExecute(std::unique_ptr<DBTransaction> transaction); // nao pode ser um ponteiro
 };
 
 extern DatabaseDispatcher g_databaseDispatcher;
