517faf7c8446148fcfd5534045fea191b39cc5c8
WibbenZ <wibbenz@gmail.com>
2014-06-24 20:27:04 +0200

    Update poison_arrow.lua
    Update to meta.

--- data/weapons/scripts/poison_arrow.lua
+++ data/weapons/scripts/poison_arrow.lua
@@ -1,14 +1,19 @@
-local combat = createCombatObject()
-setCombatParam(combat, COMBAT_PARAM_BLOCKARMOR, 1)
-setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_POISONDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_POISONARROW)
-setCombatFormula(combat, COMBAT_FORMULA_SKILL, 1, 0, 1, 0)
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_BLOCKARMOR, 1)
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_POISONDAMAGE)
+combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_POISONARROW)
+combat:setFormula(COMBAT_FORMULA_SKILL, 1, 0, 1, 0)
 
-local condition = createConditionObject(CONDITION_POISON)
-setConditionParam(condition, CONDITION_PARAM_DELAYED, 1)
-addDamageCondition(condition, 10, 2000, -1)
-setCombatCondition(combat, condition)
+local condition = Condition(CONDITION_POISON)
+condition:setParameter(CONDITION_PARAM_DELAYED, 1)
+condition:addDamage(10, 2000, -1)
+combat:setCondition(condition)
 
 function onUseWeapon(cid, var)
-	return doCombat(cid, combat, var)
+	local ret = combat:execute(cid, var)
+	if(ret == LUA_ERROR) then
+		return LUA_ERROR
+	end
+	
+	return ret
 end
