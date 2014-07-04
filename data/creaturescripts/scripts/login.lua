6db6dbac72bebde30130a761274a26fe57dd6e44
WibbenZ <WibbenZ@gmail.com>
2014-07-04 23:47:05 +0200

    test

--- data/creaturescripts/scripts/login.lua
+++ data/creaturescripts/scripts/login.lua
@@ -5,6 +5,16 @@ function onLogin(cid)
 	if player:getLastLoginSaved() <= 0 then
 		loginStr = loginStr .. " Please choose your outfit."
 		player:sendOutfitWindow()
+		
+		local firstItems = {2050, 2382}
+		for i = 1, #firstItems do
+			player:addItem(firstItems[i], 1)
+		end
+		
+		player:addItem(player:getSex() == 0 and 2651 or 2650, 1)
+		
+		local bag = player:addItem(1987, 1)
+		Container(bag:getUniqueId()):addItem(2674, 1)
 	else
 		if loginStr ~= "" then
 			player:sendTextMessage(MESSAGE_STATUS_DEFAULT, loginStr)
