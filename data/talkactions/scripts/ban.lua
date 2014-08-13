1260fb3aae94e5abaea2a813abd78d50f321b024
Mark Samman <mark.samman@gmail.com>
2014-08-13 23:17:10 +0200

    Fix bug with ban command when specifying reason

--- data/talkactions/scripts/ban.lua
+++ data/talkactions/scripts/ban.lua
@@ -30,11 +30,11 @@ function onSay(cid, words, param)
 	db:query("INSERT INTO `account_bans` (`account_id`, `reason`, `banned_at`, `expires_at`, `banned_by`) VALUES (" ..
 			accountId .. ", " .. db.escapeString(reason) .. ", " .. timeNow .. ", " .. timeNow + (banDays * 86400) .. ", " .. player:getGuid() .. ")")
 
-	local target = Player(param)
+	local target = Player(name)
 	if target ~= nil then
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, target:getName() .. " has been banned.")
 		target:remove()
 	else
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, param .. " has been banned.")
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, name .. " has been banned.")
 	end
 end
