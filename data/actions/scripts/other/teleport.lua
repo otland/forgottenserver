5d000ba6e099f54a06820bd647e835ef4078a385
Zbizu <Zbizu@users.noreply.github.com>
2014-11-05 22:39:35 +0100

    improved ladder
    closes #710
    partial fix for #351

--- data/actions/scripts/other/teleport.lua
+++ data/actions/scripts/other/teleport.lua
@@ -3,9 +3,24 @@ function onUse(player, item, fromPosition, itemEx, toPosition, isHotkey)
 	if isInArray(upFloorIds, item.itemid) then
 		fromPosition.y = fromPosition.y + 1
 		fromPosition.z = fromPosition.z - 1
+		newDir = SOUTH
+		local tile = Tile(fromPosition)
+		if tile then
+			if not tile:isWalkable() then
+				fromPosition.y = fromPosition.y - 2
+				newDir = NORTH
+			end
+		else
+			fromPosition.y = fromPosition.y - 2
+			newDir = NORTH
+		end
 	else
 		fromPosition.z = fromPosition.z + 1
 	end
-	player:teleportTo(fromPosition, false)
+	
+	player:teleportTo(fromPosition, true)
+	if newDir then
+		player:setDirection(newDir)
+	end
 	return true
 end
