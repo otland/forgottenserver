95f2468cfd6fcd3f686d45a0d8c577b723d4aaf3
Zbizu <Zbizu@users.noreply.github.com>
2014-11-18 18:23:32 +0100

    improved ladder (rewritten)
    - mw and growths are now detected properly
    - rewritten tile checking
    - another function used to check tile blocking

--- data/actions/scripts/other/teleport.lua
+++ data/actions/scripts/other/teleport.lua
@@ -1,11 +1,40 @@
 local upFloorIds = {1386, 3678, 5543}
+
 function onUse(player, item, fromPosition, itemEx, toPosition, isHotkey)
-	if isInArray(upFloorIds, item.itemid) then
-		fromPosition.y = fromPosition.y + 1
-		fromPosition.z = fromPosition.z - 1
-	else
+	if not isInArray(upFloorIds, item.itemid) then
 		fromPosition.z = fromPosition.z + 1
+		player:teleportTo(fromPosition, true)
+		return true
 	end
-	player:teleportTo(fromPosition, false)
+	
+	local posA = {x = fromPosition.x, y = fromPosition.y + 1, z = fromPosition.z - 1}
+	local posB = {x = fromPosition.x, y = fromPosition.y - 1, z = fromPosition.z - 1}
+	local tileA = Tile(posA)
+	local tileB = Tile(posB)
+	local newDir = SOUTH
+	
+	if tileA then
+		if tileA:hasProperty(CONST_PROP_IMMOVABLEBLOCKSOLID) and not tileA:getItemByType(ITEM_TYPE_MAGICFIELD) then
+			if tileB then
+				if tileB:hasProperty(CONST_PROP_IMMOVABLEBLOCKSOLID) and not tileB:getItemByType(ITEM_TYPE_MAGICFIELD) then
+					return false
+				end
+
+				newDir = NORTH
+			else
+				return false
+			end
+		end
+	else
+		newDir = NORTH
+	end
+	
+	if newDir == SOUTH then
+		player:teleportTo(posA, true)
+	else
+		player:teleportTo(posB, true)
+	end
+	
+	player:setDirection(newDir)
 	return true
 end
