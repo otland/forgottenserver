00a102487e4c1c4807d30e1059e0faa43f18a0cc
Mark Samman <mark.samman@gmail.com>
2014-12-02 23:02:52 +0100

    Remove old comment in Database class

--- src/database.h
+++ src/database.h
@@ -33,9 +33,7 @@ class Database
 		/**
 		 * Singleton implementation.
 		 *
-		 * Retruns instance of database handler. Don't create database (or drivers) instances in your code - instead of it use Database::instance(). This method stores static instance of connection class internaly to make sure exacly one instance of connection is created for entire system.
-		 *
-		 * @return database connection handler singletor
+		 * @return database connection handler singleton
 		 */
 		static Database* getInstance()
 		{
