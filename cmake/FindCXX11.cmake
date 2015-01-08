6cafa3b99ab59ec2e05f174e3c47b57d57b20b15
Kornelijus Survila <kornholijo@gmail.com>
2015-01-08 12:47:35 -0600

    Update FindCXX11
    Clang on most other platforms uses gnu's libstd++

--- cmake/FindCXX11.cmake
+++ cmake/FindCXX11.cmake
@@ -13,7 +13,7 @@ if(COMPILER_KNOWS_CXX11)
     # Tested on Mac OS X 10.8.2 with XCode 4.6 Command Line Tools
     # Clang requires this to find the correct c++11 headers
     check_cxx_compiler_flag("-stdlib=libc++" COMPILER_KNOWS_STDLIB)
-    if(COMPILER_KNOWS_STDLIB)
+    if(APPLE AND COMPILER_KNOWS_STDLIB)
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
     endif()
 else()
