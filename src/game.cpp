c9f0b837f838ec04b6167d9f58f381ac544cd8a5
dalkon <dalkon@live.se>
2015-04-23 22:14:39 +0200

    Fix #1308

--- src/game.cpp
+++ src/game.cpp
@@ -866,7 +866,7 @@ ReturnValue Game::internalMoveCreature(Creature& creature, Tile& toTile, uint32_
 	if (fromCylinder) {
 		const Position& fromPosition = fromCylinder->getPosition();
 		const Position& toPosition = toCylinder->getPosition();
-		if (fromPosition.z != toPosition.z) {
+		if (fromPosition.z != toPosition.z && (fromPosition.x != toPosition.x || fromPosition.y != toPosition.y)) {
 			internalCreatureTurn(&creature, getDirectionTo(fromPosition, toPosition));
 		}
 	}
