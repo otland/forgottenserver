ad9f9973e7a0a37930fdbca8bf289e0aa7677810
gugahoa <gugahoa@gmail.com>
2015-05-08 01:26:06 -0300

    Move enchant spear from conjure to instant

---
+++ data/spells/scripts/support/enchant staff.lua
@@ -0,0 +1,19 @@
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
+combat:setParameter(COMBAT_PARAM_AGGRESSIVE, 0)
+
+function onCastSpell(creature, var)
+	local item = creature:getSlotItem(CONST_SLOT_LEFT)
+	if not item or item:getId() ~= 2401 then
+		item = creature:getSlotItem(CONST_SLOT_RIGHT)
+	end
+
+	if not item or item:getId() ~= 2401 then
+		creature:getPosition():sendMagicEffect(CONST_ME_POFF)
+		return false
+	end
+
+	item:transform(2433)
+	item:decay()
+	return combat:execute(creature, var)
+end
