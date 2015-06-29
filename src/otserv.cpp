19530c36df0a6de13ec3799e617389726b7ef945
Damian Jarek <damian.jarek93@gmail.com>
2015-06-29 11:19:54 +0200

    Added UNIX signal handling for SIGTERM, SIGHUP, SIGINT, SIGUSR1.
    Using signal_set allows us to use the same interface for handling
    signals on UNIX-likes and console events on Windows.
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- src/otserv.cpp
+++ src/otserv.cpp
@@ -25,10 +25,6 @@
 
 #include "iomarket.h"
 
-#ifndef _WIN32
-#include <csignal> // for sigemptyset()
-#endif
-
 #include "configmanager.h"
 #include "scriptmanager.h"
 #include "rsa.h"
@@ -74,15 +70,6 @@ int main(int argc, char* argv[])
 	// Setup bad allocation handler
 	std::set_new_handler(badAllocationHandler);
 
-#ifndef _WIN32
-	// ignore sigpipe...
-	struct sigaction sigh;
-	sigh.sa_handler = SIG_IGN;
-	sigh.sa_flags = 0;
-	sigemptyset(&sigh.sa_mask);
-	sigaction(SIGPIPE, &sigh, nullptr);
-#endif
-
 	ServiceManager serviceManager;
 
 	g_dispatcher.start();
@@ -94,19 +81,6 @@ int main(int argc, char* argv[])
 
 	if (serviceManager.is_running()) {
 		std::cout << ">> " << g_config.getString(ConfigManager::SERVER_NAME) << " Server Online!" << std::endl << std::endl;
-#ifdef _WIN32
-		SetConsoleCtrlHandler([](DWORD) -> BOOL {
-			g_dispatcher.addTask(createTask([]() {
-				g_dispatcher.addTask(createTask(
-					std::bind(&Game::shutdown, &g_game)
-				));
-				g_scheduler.stop();
-				g_databaseTasks.stop();
-				g_dispatcher.stop();
-			}));
-			ExitThread(0);
-		}, 1);
-#endif
 		serviceManager.run();
 	} else {
 		std::cout << ">> No services running. The server is NOT online." << std::endl;
