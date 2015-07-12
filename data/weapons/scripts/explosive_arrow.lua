887354e0071707b0bc2a5ac78804c4f5a6c58b20
WibbenZ <wibbenz@gmail.com>
2015-07-12 23:44:52 +0200

    Explosive arrow Meta

--- data/weapons/scripts/explosive_arrow.lua
+++ data/weapons/scripts/explosive_arrow.lua
@@ -1,14 +1,18 @@
-local combat = createCombatObject()
-setCombatParam(combat, COMBAT_PARAM_BLOCKARMOR, 1)
-setCombatParam(combat, COMBAT_PARAM_BLOCKSHIELD, 1)
-setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_FIREAREA)
-setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_BURSTARROW)
-setCombatFormula(combat, COMBAT_FORMULA_SKILL, 1, 0, 1, 0)
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_BLOCKARMOR, 1)
+combat:setParameter(COMBAT_PARAM_BLOCKSHIELD, 1)
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_FIREAREA)
+combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_BURSTARROW)
+combat:setFormula(COMBAT_FORMULA_SKILL, 1, 0, 1, 0)
 
-local area = createCombatArea( { {1, 1, 1}, {1, 3, 1}, {1, 1, 1} } )
-setCombatArea(combat, area)
+local area = createCombatArea({
+	{1, 1, 1},
+	{1, 3, 1},
+	{1, 1, 1}
+})
+combat:setArea(area)
 
 function onUseWeapon(player, var)
-	return doCombat(player, combat, var)
+	return combat:execute(player, var)
 end
