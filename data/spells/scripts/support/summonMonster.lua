628bf3801097f31d044c2474ffa686189f0da7b4
WibbenZ <WibbenZ@gmail.com>
2015-08-09 14:59:20 +0200

    Add the actual file, removed the test file.

--- data/spells/scripts/support/summonMonster.lua
+++ data/spells/scripts/support/summonMonster.lua
@@ -2,7 +2,7 @@ function onCastSpell(creature, var)
 	local monsterName = Variant.getString(var)
 	local monsterType = MonsterType(monsterName)
 
-	if monsterType then
+	if not monsterType then
 		creature:sendTextMessage(MESSAGE_STATUS_SMALL, RETURNVALUE_NOTPOSSIBLE)
 		creature:getPosition():sendMagicEffect(CONST_ME_POFF)
 		return false
@@ -21,12 +21,22 @@ function onCastSpell(creature, var)
 			return false
 		end
 
-		if #creature:getSummons() >= 3 then
+		if #creature:getSummons() >= 2 then
 			creature:sendTextMessage(MESSAGE_STATUS_SMALL, "You cannot summon more creatures.")
 			creature:getPosition():sendMagicEffect(CONST_ME_POFF)
 			return false
 		end
 	end
+
+	local manaToRemove = monsterType:getManaCost()
+	if creature:getMana() < manaToRemove and not getPlayerFlagValue(creature, PlayerFlag_HasInfiniteMana) then
+		creature:sendCancelMessage(RETURNVALUE_NOTENOUGHMANA)
+		creature:getPosition():sendMagicEffect(CONST_ME_POFF)
+		return false
+	end
+
+	creature:addMana(-manaToRemove)
+	creature:addManaSpent(manaToRemove)
 
 	local position = creature:getPosition()
 	local summon = Game.createMonster(monsterName, position, true)
