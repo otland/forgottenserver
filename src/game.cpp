be8f2862ecdc1932b1f4b93b1a31dfe659164b8d
WibbenZ <WibbenZ@gmail.com>
2015-08-15 18:02:55 +0200

    Remove extra returns

--- src/game.cpp
+++ src/game.cpp
@@ -5572,11 +5572,9 @@ bool Game::reload(ReloadTypes_t reloadType)
 		if (!g_spells->reload()) {
 			std::cout << "[Error - Game::reload] Failed to reload spells." << std::endl;
 			std::terminate();
-			return false;
 		} else if (!g_monsters.reload()) {
 			std::cout << "[Error - Game::reload] Failed to reload monsters." << std::endl;
 			std::terminate();
-			return false;
 		}
 		return true;
 	} else if (reloadType == RELOAD_TYPE_TALKCTIONS) {
