2b795fb3d8ddc62593a4c5ac9bf344ff126a5d3f
raymondtfr <raymond.ek@hotmail.com>
2016-01-07 11:29:46 -0800

    Enhanced utori-like spells & soul fire rune
    Rounds (ranged), time (constant or may differ each damage round), and damage (start/decrementing range).
    
    All spells effect now lasts according to the player's Level and Magic/Melee Skill Level.
    All those informations have been researched from and are based on vanilla's.
    All tests were made in monsters (and players) with neutral resistance to that spell damage type.
    
    Moved a small bunch of code similar in all onTargetCreature callbacks (except for Holy Flash)
    to the now updated function in spells.lib.
    Shortened damage decrementing range code.

--- data/spells/scripts/attack/holy_flash.lua
+++ data/spells/scripts/attack/holy_flash.lua
@@ -3,10 +3,22 @@ combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_HOLYDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_HOLYDAMAGE)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_SMALLHOLY)
 
-local condition = Condition(CONDITION_DAZZLED)
-condition:setParameter(CONDITION_PARAM_DELAYED, true)
-condition:addDamage(50, 10000, -10)
-combat:setCondition(condition)
+function onTargetCreature(creature, target)
+	local damage = target:isPlayer() and 10 or 20
+	local time = math.random(10, 12) * 1000
+	local min = (creature:getLevel() * 0.01) + (creature:getMagicLevel() * 0.3) + 2
+	local max = (creature:getLevel() * 0.01) + (creature:getMagicLevel() * 0.5) + 3
+	local rounds = math.random(math.floor(min), math.floor(max))
+
+	local condition = Condition(CONDITION_DAZZLED)
+	condition:setParameter(CONDITION_PARAM_OWNER, creature:getId())
+	condition:setParameter(CONDITION_PARAM_DELAYED, true)
+	condition:addDamage(rounds, time, -damage)
+	target:addCondition(condition)
+	return true
+end
+
+combat:setCallback(CALLBACK_PARAM_TARGETCREATURE, "onTargetCreature")
 
 function onCastSpell(creature, variant)
 	return combat:execute(creature, variant)
