079a256b344d16d51ddd47bd32cb2b43303b7146
raymondtfr <raymond.ek@hotmail.com>
2016-01-07 17:41:43 -0800

    New spells - weapons enhancement
    Added new spells from 10.55 update -- with correct cooldown ids.
    * The limit for the player's level and magic level with these spell
    formulas is 20 -- based on vanilla.
    * Buzz
    * Chill Out
    * Light Stone Shower rune
    * Lightest Missile rune
    * Mud Attack
    * Scorch
    * Bruise Bane
    * Magic Patch
    
    Added a few new items -- from 10.55 update that are needed for some
    spells
    * Light Stone Shower rune
    * Lightest Missile rune
    * Simple arrow
    
    Enhanced weapon files
    * Removed incorrect block type "BLOCKSHIELD". The correct (and only)
    block type is "BLOCKARMOR"
    * Modified setFormula(mina, minb, maxa, maxb)
    "combat:setFormula(COMBAT_FORMULA_SKILL, 0, 0, 1, 0)" so that the value
    generated by WeaponDistance::getWeaponDamage will not get modified by
    setFormula().
    * Renamed "var" to "variant" in onUseWeapon().
    * Renamed "1" to "true" in condition/combat (since #1661) parameters,
    where a boolean is expected.
    * Updated condition damages of "poison_arrow" and "viper_star" -- based
    on vanilla.
    * Removed comment lines of (burst/poison arrow; viper star) since the
    file name is pretty evident.
    * Leave only 1 whitespace between comment lines.
    
    * explosion_arrow.lua
    * Renamed to burst_arrow.lua
    * Replaced the current combat effect on target (CONST_ME_FIREAREA) with
    newer effect (CONST_ME_EXPLOSIONAREA).
    
    * poison_arrow
    * Replaced incorrect damage type (COMBAT_POISONDAMAGE) with the correct
    damage type (COMBAT_PHYSICALDAMAGE) -- based on vanilla.
    * viper star
    
    Delete explosion_arrow.lua
    
    Use math.min(level, 20) instead of if's.
    
    Added missing -- math.min(maglevel, 20).

--- data/weapons/scripts/poison_arrow.lua
+++ data/weapons/scripts/poison_arrow.lua
@@ -1,14 +1,16 @@
 local combat = Combat()
-combat:setParameter(COMBAT_PARAM_BLOCKARMOR, 1)
-combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_POISONDAMAGE)
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_POISONARROW)
-combat:setFormula(COMBAT_FORMULA_SKILL, 1, 0, 1, 0)
+combat:setParameter(COMBAT_PARAM_BLOCKARMOR, true)
+combat:setFormula(COMBAT_FORMULA_SKILL, 0, 0, 1, 0)
 
 local condition = Condition(CONDITION_POISON)
-condition:setParameter(CONDITION_PARAM_DELAYED, 1)
-condition:addDamage(10, 2000, -1)
+condition:setParameter(CONDITION_PARAM_DELAYED, true)
+condition:addDamage(4, 4000, -3)
+condition:addDamage(9, 4000, -2)
+condition:addDamage(20, 4000, -1)
 combat:setCondition(condition)
 
-function onUseWeapon(player, var)
-	return combat:execute(player, var)
+function onUseWeapon(player, variant)
+	return combat:execute(player, variant)
 end
