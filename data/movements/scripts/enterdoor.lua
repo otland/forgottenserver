56e90c14da432e571475c8fa21cec68524f1db3e
PrinterLUA <mastero_90@hotmail.com>
2016-01-27 14:37:50 +0100

    Added Text also split logic between quest door and level door

--- data/movements/scripts/enterdoor.lua
+++ data/movements/scripts/enterdoor.lua
@@ -4,10 +4,19 @@ function onStepIn(creature, item, position, fromPosition)
 		return false
 	end
 
-	if player:getLevel() < item.actionid - 1000 then
-		player:teleportTo(fromPosition, true)
-		return false
+	local itemId = item:getId()
+	if isInArray(questDoors, itemId) then
+		if player:getStorageValue(item.actionid) == -1 then
+			player:sendTextMessage(MESSAGE_INFO_DESCR, "The door seems to be sealed against unwanted intruders.")
+			player:teleportTo(fromPosition, true)
+			return false
+		end
+	elseif isInArray(levelDoors, itemId) then
+		if player:getLevel() < item.actionid - 1000 then
+			player:sendTextMessage(MESSAGE_INFO_DESCR, "Only the worthy may pass.")
+			player:teleportTo(fromPosition, true)
+			return false
+		end
 	end
-
 	return true
 end
