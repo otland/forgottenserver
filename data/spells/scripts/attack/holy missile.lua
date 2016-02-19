f791635f60664652a01d88d4e40a6b754b604d1f
raymondtfr <raymond.ek@hotmail.com>
2016-02-18 17:39:17 -0800

    Remove unused combat parameter effect

--- data/spells/scripts/attack/holy missile.lua
+++ data/spells/scripts/attack/holy missile.lua
@@ -1,6 +1,5 @@
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_HOLYDAMAGE)
-combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_HOLYDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_HOLYAREA)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_HOLY)
 
