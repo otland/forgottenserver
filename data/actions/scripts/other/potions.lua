4d0ac6ec62ebfa30858afb16c913d7352d103d18
Nickolas Coppem <slavidodo@gmail.com>
2016-04-27 02:19:34 +0200

    Update potions.lua

--- data/actions/scripts/other/potions.lua
+++ data/actions/scripts/other/potions.lua
@@ -1,23 +1,67 @@
-local ultimateHealthPot = 8473
-local greatHealthPot = 7591
-local greatManaPot = 7590
-local greatSpiritPot = 8472
-local strongHealthPot = 7588
-local strongManaPot = 7589
-local healthPot = 7618
-local manaPot = 7620
-local smallHealthPot = 8704
-local antidotePot = 8474
-local greatEmptyPot = 7635
-local strongEmptyPot = 7634
-local emptyPot = 7636
-
+--- CONFIGURATION ---
+potions = {
+	removepots = true,
+	addemptypots = true, -- Only works if removepots is true
+	[8473] = { -- ultimate health potion
+		vocations = {4},
+		minLevel = 130,
+		health = {650, 850},  -- min, max
+		empty = 7635
+	},
+	[7591] = { -- great health potion
+		vocations = {4},
+		minLevel = 80,
+		health = {425, 575},
+		empty = 7635
+	},
+	[7590] = { -- great mana potion
+		vocations = {1, 2},
+		minLevel = 80,
+		mana = {150, 250},
+		empty = 7635
+	},
+	[8472] = { -- great spitit potion
+		vocations = {3},
+		minLevel = 80,
+		health = {250, 350},
+		mana = {100, 200},
+		empty = 7635
+	},
+	[7588] = { -- strong health potion
+		vocations = {3, 4},
+		minLevel = 50,
+		health = {250, 350},
+		empty = 7634
+	},
+	[7589] = { -- strong mana potion
+		vocations = {1, 2, 3},
+		minLevel = 50,
+		mana = {115, 185},
+		empty = 7634
+	},
+	[7618] = { -- health potion
+		health = {75, 125},
+		empty = 7636
+	},
+	[7620] = { -- mana potion
+		mana = {125, 175},
+		empty = 7636
+	},
+	[8704] = { -- small health potion
+		health = {60, 90},
+		empty = 7636
+	}
+}
+-- antidote
 local antidote = Combat()
 antidote:setParameter(COMBAT_PARAM_TYPE, COMBAT_HEALING)
 antidote:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 antidote:setParameter(COMBAT_PARAM_TARGETCASTERORTOPMOST, true)
 antidote:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
 antidote:setParameter(COMBAT_PARAM_DISPEL, CONDITION_POISON)
+potions[8474] = {combat = antidote, empty = 7636}
+
+--- FUNCTIONS ---
 
 local exhaust = Condition(CONDITION_EXHAUST_HEAL)
 exhaust:setParameter(CONDITION_PARAM_TICKS, (configManager.getNumber(configKeys.EX_ACTIONS_DELAY_INTERVAL) - 100))
@@ -33,126 +77,81 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		return true
 	end
 
-	local itemId = item:getId()
-	if itemId == antidotePot then
-		if not antidote:execute(target, numberToVariant(target:getId())) then
-			return false
+	local stats = potions[item:getId()]
+	if stats then
+		if ( stats.vocations or stats.minLevel ) and not player:getGroup():getAccess() then
+			if not stats.minLevel then
+				stats.minLevel = 1
+			end
+			if not stats.vocations then
+				stats.vocations = {}
+			end
+			
+			local voclist, mvoc = {}, target:getVocation()
+			while mvoc do
+				table.insert(voclist, mvoc:getId())
+				mvoc = mvoc:getDemotion()
+			end
+			-- Checking vocation, level
+			local isdifferentvocation = true
+			for k, v in ipairs(stats.vocations) do
+				if isInArray(v, voclist) then
+					isdifferentvocation = false
+					break
+				end
+			end
+			voclist = nil
+			if isdifferentvocation or target:getLevel() < stats.minLevel then
+				local str, vocations = "", stats.vocations
+				if #vocations > 0 then
+					for i = 1, #vocations do
+						local voc = Vocation(vocations[i])
+						if not voc:getDemotion() then
+							if i == #vocations then
+								str = str .. (#vocations ~= 1 and " and " or "") .. voc:getName():lower() .. "s"
+							else
+								str = str .. (i ~= 1 and ", " or "") .. voc:getName():lower() .. "s"
+							end
+						end
+					end
+				end
+				vocations = nil
+				if str == "" then
+					str = "all vocations"
+				end
+				player:say("This potion can only be consumed by ".. str .. " of level " .. stats.minLevel .. " or higher.", TALKTYPE_MONSTER_SAY)
+				return false
+			end
 		end
-
+		if stats.health then
+			local hmin, hmax = stats.health[1], stats.health[2]
+			if not doTargetCombatHealth(0, target, COMBAT_HEALING, hmin, hmax, CONST_ME_MAGIC_BLUE) then
+				return false
+			end
+		end
+		if stats.mana then
+			local mmin, mmax = stats.mana[1], stats.mana[2]
+			if not doTargetCombatMana(0, target, mmin, mmax, CONST_ME_MAGIC_BLUE) then
+				return false
+			end
+		end
+		
+		if stats.combat then
+			if not stats.combat:execute(target, numberToVariant(target:getId())) then
+				return false
+			end
+		end
+		
 		player:addCondition(exhaust)
 		target:say("Aaaah...", TALKTYPE_MONSTER_SAY)
-		item:remove(1)
-		player:addItem(emptyPot, 1)
-	elseif itemId == smallHealthPot then
-		if not doTargetCombatHealth(0, target, COMBAT_HEALING, 60, 90, CONST_ME_MAGIC_BLUE) then
-			return false
+		
+		if potions.removepots then
+			item:remove(1)
+			if potions.addemptypots and stats.empty then
+				player:addItem(stats.empty, 1)
+			end
 		end
-
-		player:addCondition(exhaust)
-		target:say("Aaaah...", TALKTYPE_MONSTER_SAY)
-		item:remove(1)
-		player:addItem(emptyPot, 1)
-	elseif itemId == healthPot then
-		if not doTargetCombatHealth(0, target, COMBAT_HEALING, 125, 175, CONST_ME_MAGIC_BLUE) then
-			return false
-		end
-
-		player:addCondition(exhaust)
-		target:say("Aaaah...", TALKTYPE_MONSTER_SAY)
-		item:remove(1)
-		player:addItem(emptyPot, 1)
-	elseif itemId == manaPot then
-		if not doTargetCombatMana(0, target, 75, 125, CONST_ME_MAGIC_BLUE) then
-			return false
-		end
-
-		player:addCondition(exhaust)
-		target:say("Aaaah...", TALKTYPE_MONSTER_SAY)
-		item:remove(1)
-		player:addItem(emptyPot, 1)
-	elseif itemId == strongHealthPot then
-		if (not isInArray({3, 4, 7, 8}, target:getVocation():getId()) or target:getLevel() < 50) and not getPlayerFlagValue(player, PlayerFlag_IgnoreSpellCheck) then
-			player:say("This potion can only be consumed by paladins and knights of level 50 or higher.", TALKTYPE_MONSTER_SAY)
-			return true
-		end
-
-		if not doTargetCombatHealth(0, target, COMBAT_HEALING, 250, 350, CONST_ME_MAGIC_BLUE) then
-			return false
-		end
-
-		player:addCondition(exhaust)
-		target:say("Aaaah...", TALKTYPE_MONSTER_SAY)
-		item:remove(1)
-		player:addItem(strongEmptyPot, 1)
-	elseif itemId == strongManaPot then
-		if (not isInArray({1, 2, 3, 5, 6, 7}, target:getVocation():getId()) or target:getLevel() < 50) and not getPlayerFlagValue(player, PlayerFlag_IgnoreSpellCheck) then
-			player:say("This potion can only be consumed by sorcerers, druids and paladins of level 50 or higher.", TALKTYPE_MONSTER_SAY)
-			return true
-		end
-
-		if not doTargetCombatMana(0, target, 115, 185, CONST_ME_MAGIC_BLUE) then
-			return false
-		end
-
-		player:addCondition(exhaust)
-		target:say("Aaaah...", TALKTYPE_MONSTER_SAY)
-		item:remove(1)
-		player:addItem(strongEmptyPot, 1)
-	elseif itemId == greatSpiritPot then
-		if (not isInArray({3, 7}, target:getVocation():getId()) or target:getLevel() < 80) and not getPlayerFlagValue(player, PlayerFlag_IgnoreSpellCheck) then
-			player:say("This potion can only be consumed by paladins of level 80 or higher.", TALKTYPE_MONSTER_SAY)
-			return true
-		end
-
-		if not doTargetCombatHealth(0, target, COMBAT_HEALING, 250, 350, CONST_ME_MAGIC_BLUE) or not doTargetCombatMana(0, target, 100, 200, CONST_ME_MAGIC_BLUE) then
-			return false
-		end
-
-		player:addCondition(exhaust)
-		target:say("Aaaah...", TALKTYPE_MONSTER_SAY)
-		item:remove(1)
-		player:addItem(greatEmptyPot, 1)
-	elseif itemId == greatHealthPot then
-		if (not isInArray({4, 8}, target:getVocation():getId()) or target:getLevel() < 80) and not getPlayerFlagValue(player, PlayerFlag_IgnoreSpellCheck) then
-			player:say("This potion can only be consumed by knights of level 80 or higher.", TALKTYPE_MONSTER_SAY)
-			return true
-		end
-
-		if not doTargetCombatHealth(0, target, COMBAT_HEALING, 425, 575, CONST_ME_MAGIC_BLUE) then
-			return false
-		end
-
-		player:addCondition(exhaust)
-		target:say("Aaaah...", TALKTYPE_MONSTER_SAY)
-		item:remove(1)
-		player:addItem(greatEmptyPot, 1)
-	elseif itemId == greatManaPot then
-		if (not isInArray({1,2,5,6}, target:getVocation():getId()) or target:getLevel() < 80) and not getPlayerFlagValue(player, PlayerFlag_IgnoreSpellCheck) then
-			player:say("This potion can only be consumed by sorcerers and druids of level 80 or higher.", TALKTYPE_MONSTER_SAY)
-			return true
-		end
-
-		if not doTargetCombatMana(0, target, 150, 250, CONST_ME_MAGIC_BLUE) then
-			return false
-		end
-		player:addCondition(exhaust)
-		target:say("Aaaah...", TALKTYPE_MONSTER_SAY)
-		item:remove(1)
-		player:addItem(greatEmptyPot, 1)
-	elseif itemId == ultimateHealthPot then
-		if (not isInArray({4, 8}, target:getVocation():getId()) or target:getLevel() < 130) and not getPlayerFlagValue(player, PlayerFlag_IgnoreSpellCheck) then
-			player:say("This potion can only be consumed by knights of level 130 or higher.", TALKTYPE_MONSTER_SAY)
-			return true
-		end
-
-		if not doTargetCombatHealth(0, target, COMBAT_HEALING, 650, 850, CONST_ME_MAGIC_BLUE) then
-			return false
-		end
-
-		player:addCondition(exhaust)
-		target:say("Aaaah...", TALKTYPE_MONSTER_SAY)
-		item:remove(1)
-		player:addItem(greatEmptyPot, 1)
 	end
+	
 	return true
 end
