3e84a3a3bcd229fdb542aa35397fa4d9e81e865f
Nickolas Coppem <slavidodo@gmail.com>
2016-05-02 23:06:10 +0200

    Minor script fixes, some updates!
     - Fixed some missing issues.
     - Fixed empty potion place.
     - Renamed some options.

--- data/actions/scripts/other/potions.lua
+++ data/actions/scripts/other/potions.lua
@@ -1,56 +1,57 @@
---- CONFIGURATION ---
 potions = {
 	removePots = true,
 	addEmptyPots = true, -- Only works if removepots isn't false or nil
 	words = "Aaaah...",
+	-- Health & Spirit Potions
 	[8473] = { -- ultimate health potion
 		vocations = {4, 8},
 		minLevel = 130,
 		health = { min = 650, max = 850},
-		empty = 7635
+		emptyPot = 7635
 	},
 	[7591] = { -- great health potion
 		vocations = {4, 8},
 		minLevel = 80,
 		health = { min = 425, max = 575},
-		empty = 7635
-	},
-	[7590] = { -- great mana potion
-		vocations = {1, 2, 5, 6},
-		minLevel = 80,
-		mana = { min = 150, max = 250},
-		empty = 7635
+		emptyPot = 7635
 	},
 	[8472] = { -- great spitit potion
 		vocations = {3, 7},
 		minLevel = 80,
 		health = { min = 250, max = 350},
 		mana = { min = 100, max = 200},
-		empty = 7635
+		emptyPot = 7635
 	},
 	[7588] = { -- strong health potion
 		vocations = {3, 4, 7, 8},
 		minLevel = 50,
 		health = { min = 250, max = 350},
-		empty = 7634
+		emptyPot = 7634
+	},
+	[7618] = { -- health potion
+		health = { min = 125, max = 175},
+		emptyPot = 7636
+	},
+	-- Mana Potions
+	[7590] = { -- great mana potion
+		vocations = {1, 2, 5, 6},
+		minLevel = 80,
+		mana = { min = 150, max = 250},
+		emptyPot = 7635
 	},
 	[7589] = { -- strong mana potion
 		vocations = {1, 2, 3, 5, 6, 7},
 		minLevel = 50,
 		mana = { min = 115, max = 185},
-		empty = 7634
+		emptyPot = 7634
 	},
 	[7620] = { -- mana potion
 		mana = { min = 75, max = 125},
-		empty = 7636
-	},
-	[7618] = { -- health potion
-		health = { min = 125, max = 175},
-		empty = 7636
+		emptyPot = 7636
 	},
 	[8704] = { -- small health potion
 		health = { min = 60, max = 90},
-		empty = 7636
+		emptyPot = 7636
 	}
 }
 -- antidote
@@ -60,7 +61,7 @@ antidote:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 antidote:setParameter(COMBAT_PARAM_TARGETCASTERORTOPMOST, true)
 antidote:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
 antidote:setParameter(COMBAT_PARAM_DISPEL, CONDITION_POISON)
-potions[8474] = {combat = antidote, empty = 7636}
+potions[8474] = {combat = antidote, emptyPot = 7636}
 
 --- FUNCTIONS ---
 
@@ -72,7 +73,7 @@ function onUse(player, potion, fromPosition, target, toPosition, isHotkey)
 	if target == nil or not target:isPlayer() then
 		return true
 	end
-	
+
 	if player:getCondition(CONDITION_EXHAUST_HEAL) then
 		player:sendTextMessage(MESSAGE_STATUS_SMALL, Game.getReturnMessage(RETURNVALUE_YOUAREEXHAUSTED))
 		return true
@@ -85,10 +86,10 @@ function onUse(player, potion, fromPosition, target, toPosition, isHotkey)
 				stats.minLevel = 1
 			end
 			if not stats.vocations then
-				stats.vocations = {}
+				stats.vocations = { [1] = player:getVocation():getId()}
 			end
-			
-			if not inArray(states.vocations, player:getVocation():getId()) or player:getLevel() < stats.minLevel then
+
+			if not isInArray(stats.vocations, player:getVocation():getId()) or player:getLevel() < stats.minLevel then
 				local str, vocations = "", {}
 				-- We only show unpromoted vocations in description.
 				for k, v in ipairs(stats.vocations) do
@@ -117,33 +118,39 @@ function onUse(player, potion, fromPosition, target, toPosition, isHotkey)
 			end
 		end
 		if stats.health then
-			if not doTargetCombatHealth(0, target, COMBAT_HEALING, stats.health.min, stats.health.max, CONST_ME_MAGIC_BLUE) then
+			if not doTargetCombatHealth(player.uid, target, COMBAT_HEALING, stats.health.min, stats.health.max, CONST_ME_MAGIC_BLUE) then
 				return false
 			end
 		end
+
 		if stats.mana then
-			if not doTargetCombatMana(0, target, stats.mana.min, stats.mana.max, CONST_ME_MAGIC_BLUE) then
+			if not doTargetCombatMana(player.uid, target, stats.mana.min, stats.mana.max, CONST_ME_MAGIC_BLUE) then
 				return false
 			end
 		end
-		
+
 		if stats.combat then
 			if not stats.combat:execute(target, numberToVariant(target:getId())) then
 				return false
 			end
 		end
-		
+
 		player:addCondition(exhaust)
 		if potions.words then
 			target:say(potions.words, TALKTYPE_MONSTER_SAY)
 		end
 		if potions.removePots then
-			item:remove(1)
-			if potions.addEmptyPots and stats.empty then
-				player:addItem(stats.empty, 1)
+			potion:remove(1)
+			if potions.addEmptyPots and stats.emptyPot then
+				-- As in tibia, empty should be put in right place!
+				if potion:getTopParent().uid == player.uid then
+					player:addItem(stats.emptyPot)
+				else
+					Game.createItem(stats.emptyPot, 1, potion:getPosition())
+				end
 			end
 		end
 	end
-	
+
 	return true
 end
