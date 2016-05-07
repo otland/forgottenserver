2a3bb138ad5415dd428c8e6e0b63b74773d27a39
Nickolas Coppem <slavidodo@gmail.com>
2016-05-07 12:44:54 +0200

    Update!
    New functionality (condition) | Renamed some variables

--- data/actions/scripts/other/potions.lua
+++ data/actions/scripts/other/potions.lua
@@ -30,56 +30,57 @@ potions = {
 	[8473] = { -- ultimate health potion
 		vocations = {4, 8},
 		minLevel = 130,
-		health = { min = 650, max = 850 },
+		healthToAdd = { min = 650, max = 850 },
 		emptyPotion = 7635
 	},
 	[7591] = { -- great health potion
 		vocations = {4, 8},
 		minLevel = 80,
-		health = { min = 425, max = 575},
+		healthToAdd = { min = 425, max = 575},
 		emptyPotion = 7635
 	},
 	[8472] = { -- great spitit potion
 		vocations = {3, 7},
 		minLevel = 80,
-		health = { min = 250, max = 350},
-		mana = { min = 100, max = 200},
+		healthToAdd = { min = 250, max = 350},
+		manaToAdd = { min = 100, max = 200},
 		emptyPotion = 7635
 	},
 	[7588] = { -- strong health potion
 		vocations = {3, 4, 7, 8},
 		minLevel = 50,
-		health = { min = 250, max = 350},
+		healthToAdd = { min = 250, max = 350},
 		emptyPotion = 7634
 	},
 	[7618] = { -- health potion
-		health = { min = 125, max = 175},
+		healthToAdd = { min = 125, max = 175},
 		emptyPotion = 7636
 	},
 	-- Mana Potions
 	[7590] = { -- great mana potion
 		vocations = {1, 2, 5, 6},
 		minLevel = 80,
-		mana = { min = 150, max = 250},
+		manaToAdd = { min = 150, max = 250},
 		emptyPotion = 7635
 	},
 	[7589] = { -- strong mana potion
 		vocations = {1, 2, 3, 5, 6, 7},
 		minLevel = 50,
-		mana = { min = 115, max = 185},
+		manaToAdd = { min = 115, max = 185},
 		emptyPotion = 7634
 	},
 	[7620] = { -- mana potion
-		mana = { min = 75, max = 125},
+		manaToAdd = { min = 75, max = 125},
 		emptyPotion = 7636
 	},
 	[8704] = { -- small health potion
-		health = { min = 60, max = 90},
+		healthToAdd = { min = 60, max = 90},
 		emptyPotion = 7636
 	}
 }
 --[[ available custom methods!
 	combat : MetatableCombat
+	
 ]]
 -- antidote
 local antidote = Combat()
@@ -96,26 +97,26 @@ potions.exhaust = Condition(CONDITION_EXHAUST_HEAL)
 potions.exhaust:setParameter(CONDITION_PARAM_TICKS, (configManager.getNumber(configKeys.EX_ACTIONS_DELAY_INTERVAL) - 100))
 -- 1000 - 100 due to exact condition timing. -100 doesn't hurt us, and players don't have reminding ~50ms exhaustion.
 
-function onUse(player, potion, fromPosition, target, toPosition, isHotkey)
+function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	if target == nil or not target:isPlayer() then
 		return true
 	end
 
-	local stats = potions[potion:getId()]
-	if stats then
-		if (stats.vocations or stats.minLevel) and not player:getGroup():getAccess() then
-			if not stats.minLevel then
-				stats.minLevel = 1
+	local potion = potions[item:getId()]
+	if potion then
+		if (potion.vocations or potion.minLevel) and not player:getGroup():getAccess() then
+			if not potion.minLevel then
+				potion.minLevel = 1
 			end
 
-			if not stats.vocations then
-				stats.vocations = { [1] = player:getVocation():getId() }
+			if not potion.vocations then
+				potion.vocations = { [1] = player:getVocation():getId() }
 			end
 
-			if not isInArray(stats.vocations, player:getVocation():getId()) or player:getLevel() < stats.minLevel then
+			if not isInArray(potion.vocations, player:getVocation():getId()) or player:getLevel() < potion.minLevel then
 				local str, vocations = "", {}
 				-- We only show unpromoted vocations in description.
-				for k, v in ipairs(stats.vocations) do
+				for k, v in ipairs(potion.vocations) do
 					if not Vocation(v):getDemotion() then
 						table.insert(vocations, v)
 					end
@@ -134,8 +135,8 @@ function onUse(player, potion, fromPosition, target, toPosition, isHotkey)
 						return false
 					end
 
-					for k, v in ipairs(stats.vocations) do
-						if not tablefind(stats.vocations, Vocation(v):getDemotion():getId()) then
+					for k, v in ipairs(potion.vocations) do
+						if not tablefind(potion.vocations, Vocation(v):getDemotion():getId()) then
 							table.insert(vocations, v)
 						end
 					end
@@ -155,7 +156,7 @@ function onUse(player, potion, fromPosition, target, toPosition, isHotkey)
 					str = "players"
 				end
 
-				player:say(string.format(potions.stringOfVocation, str, stats.minLevel), TALKTYPE_MONSTER_SAY)
+				player:say(string.format(potions.stringOfVocation, str, potion.minLevel), TALKTYPE_MONSTER_SAY)
 				return false
 			end
 		end
@@ -166,22 +167,28 @@ function onUse(player, potion, fromPosition, target, toPosition, isHotkey)
 			return true
 		end
 
-		if stats.health then
-			if not doTargetCombatHealth((target.uid == player.uid and player.uid or 0), target, COMBAT_HEALING, stats.health.min * (potions.healthRate and potions.healthRate or 1),
-					stats.health.max * (potions.healthRate and potions.healthRate or 1), CONST_ME_MAGIC_BLUE) then
+		if potion.healthToAdd then
+			if not doTargetCombatHealth((target.uid == player.uid and player.uid or 0), target, COMBAT_HEALING, potion.healthToAdd.min * (potions.healthRate and potions.healthRate or 1),
+					potion.healthToAdd.max * (potions.healthRate and potions.healthRate or 1), CONST_ME_MAGIC_BLUE) then
 				return false
 			end
 		end
 
-		if stats.mana then
-			if not doTargetCombatMana((target.uid == player.uid and player.uid or 0), target, stats.mana.min * (potions.manaRate and potions.manaRate or 1),
-					stats.mana.max * (potions.manaRate and potions.manaRate or 1), CONST_ME_MAGIC_BLUE) then
+		if potion.manaToAdd then
+			if not doTargetCombatMana((target.uid == player.uid and player.uid or 0), target, potion.manaToAdd.min * (potions.manaRate and potions.manaRate or 1),
+					potion.manaToAdd.max * (potions.manaRate and potions.manaRate or 1), CONST_ME_MAGIC_BLUE) then
 				return false
 			end
 		end
 
-		if stats.combat then
-			if not stats.combat:execute(target, numberToVariant(target:getId())) then
+		if potion.combat then
+			if not potion.combat:execute(target, numberToVariant(target:getId())) then
+				return false
+			end
+		end
+
+		if potion.condition and potiom.condition:getType() ~= CONDITION_EXHAUST_HEAL then
+			if not player:addCondition(potion.condition) then
 				return false
 			end
 		end
@@ -191,16 +198,16 @@ function onUse(player, potion, fromPosition, target, toPosition, isHotkey)
 		end
 
 		if potions.removePotions then
-			if potions.addEmptyPotions and stats.emptyPotion then
+			if potions.addEmptyPotions and potion.emptyPotion then
 				-- potion's empty potion should be in player's backpack only if the potion was in his backpack!
 				if potion:getTopParent().uid == player.uid then
-					player:addItem(stats.emptyPotion)
+					player:addItem(potion.emptyPotion)
 				else
-					Game.createItem(stats.emptyPotion, 1, potion:getPosition())
+					Game.createItem(potion.emptyPotion, 1, potion:getPosition())
 				end
 			end
 			-- Removing should be last part not to cause errors if only 1 potion (nil value).
-			potion:remove(1)
+			item:remove(1)
 		end
 
 		-- Access members aren't affected by the exaust!
