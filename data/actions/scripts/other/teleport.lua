897f0ece9bb3e4b1fb7b2fa3eeb5d185bc2a6662
wgrriffel <wgrriffel@gmail.com>
2016-05-11 08:33:39 -0300

    Block players that has pz lock from level change.

--- data/actions/scripts/other/teleport.lua
+++ data/actions/scripts/other/teleport.lua
@@ -5,6 +5,11 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	else
 		fromPosition.z = fromPosition.z + 1
 	end
+	local t = Tile(fromPosition)
+	if t:hasFlag(TILESTATE_PROTECTIONZONE) and player:isPzLocked() then
+		player:sendTextMessage(MESSAGE_STATUS_SMALL, "You can not enter a protection zone after attacking another player.")
+		return true
+	end
 	player:teleportTo(fromPosition, false)
 	return true
 end
