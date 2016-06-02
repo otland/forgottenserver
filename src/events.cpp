592a78703c16aa482568512d1615cb8f82914fe6
PrinterLUA <mastero_90@hotmail.com>
2016-06-02 12:02:21 +0200

    OnMoveItem accept cylinder, close #1446

--- src/events.cpp
+++ src/events.cpp
@@ -491,9 +491,9 @@ bool Events::eventPlayerOnLookInShop(Player* player, const ItemType* itemType, u
 	return scriptInterface.callFunction(3);
 }
 
-bool Events::eventPlayerOnMoveItem(Player* player, Item* item, uint16_t count, const Position& fromPosition, const Position& toPosition)
+bool Events::eventPlayerOnMoveItem(Player* player, Item* item, uint16_t count, const Position& fromPosition, const Position& toPosition, Cylinder* toCylinder)
 {
-	// Player:onMoveItem(item, count, fromPosition, toPosition) or Player.onMoveItem(self, item, count, fromPosition, toPosition)
+	// Player:onMoveItem(item, count, fromPosition, toPosition) or Player.onMoveItem(self, item, count, fromPosition, toPosition, toCylinder)
 	if (playerOnMoveItem == -1) {
 		return true;
 	}
@@ -518,8 +518,10 @@ bool Events::eventPlayerOnMoveItem(Player* player, Item* item, uint16_t count, c
 	lua_pushnumber(L, count);
 	LuaScriptInterface::pushPosition(L, fromPosition);
 	LuaScriptInterface::pushPosition(L, toPosition);
+	
+	LuaScriptInterface::pushCylinder(L, toCylinder);
 
-	return scriptInterface.callFunction(5);
+	return scriptInterface.callFunction(6);
 }
 
 bool Events::eventPlayerOnMoveCreature(Player* player, Creature* creature, const Position& fromPosition, const Position& toPosition)
