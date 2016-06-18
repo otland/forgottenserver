5c30d21eb2f65709b120a5bb17963477902d87d0
Wagner Riffel <wgrriffel@users.noreply.github.com>
2016-06-18 19:50:26 -0300

    fix rope.lua (#1838)

--- data/actions/scripts/tools/rope.lua
+++ data/actions/scripts/tools/rope.lua
@@ -12,7 +12,7 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	end
 
 	if isInArray(ropeSpots, tile:getGround():getId()) or tile:getItemById(14435) then
-		if player:isPzLocked() and Tile(toPosition:moveUpstairs()):hasFlag(TILESTATE_PROTECTIONZONE) then
+		if Tile(toPosition:moveUpstairs()):hasFlag(TILESTATE_PROTECTIONZONE) and player:isPzLocked() then
 			player:sendTextMessage(MESSAGE_STATUS_SMALL, Game.getReturnMessage(RETURNVALUE_PLAYERISPZLOCKED))
 			return true
 		end
@@ -24,7 +24,7 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		if tile then
 			local thing = tile:getTopVisibleThing()
 			if thing:isPlayer() then
-				if thing:isPzLocked() and Tile(toPosition:moveUpstairs()):hasFlag(TILESTATE_PROTECTIONZONE) then
+				if Tile(toPosition:moveUpstairs()):hasFlag(TILESTATE_PROTECTIONZONE) and thing:isPzLocked() then
 					return false
 				end
 				return thing:teleportTo(toPosition, false)
