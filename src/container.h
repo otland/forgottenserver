af23f6b96d9c86e20704b14e1a701f294cf1c52d
Damian Jarek <damian.jarek93@gmail.com>
2016-08-16 20:32:04 -0300

    Fileloader refactor
    The map loading function will now use the new simplified loader.
    It has also been split into multiple functions to make it easier to
    read. Use mmap to load the OTB file. This improves load time slightly
    by mmaping the loaded map file into memory to reduce unnecessary
    copying. Moved OTB loader related code to the OTB namespace which makes
    it clearer that the OTB submodule is mostly indepenent from the rest of
    the application.
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- src/container.h
+++ src/container.h
@@ -75,7 +75,7 @@ class Container : public Item, public Cylinder
 		}
 
 		Attr_ReadValue readAttr(AttrTypes_t attr, PropStream& propStream) override;
-		bool unserializeItemNode(FileLoader& f, NODE node, PropStream& propStream) override;
+		bool unserializeItemNode(OTB::Loader& loader, const OTB::Node& node, PropStream& propStream) override;
 		std::string getContentDescription() const;
 
 		size_t size() const {
