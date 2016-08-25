3fed4d98e3aa4f8b57045739e765b404a344daa1
raymondtfr <raymond.ek@hotmail.com>
2016-08-25 07:04:34 -0800

    use round method instead of ceil

--- src/monster.cpp
+++ src/monster.cpp
@@ -603,7 +603,7 @@ BlockType_t Monster::blockHit(Creature* attacker, CombatType_t combatType, int32
 		}
 
 		if (elementMod != 0) {
-			damage = static_cast<int32_t>(std::ceil(damage * ((100 - elementMod) / 100.)));
+			damage = static_cast<int32_t>(std::round(damage * ((100 - elementMod) / 100.)));
 			if (damage <= 0) {
 				damage = 0;
 				blockType = BLOCK_ARMOR;
