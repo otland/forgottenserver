f2394f5b2275d8e6f64b235dc88cdddd1896c122
Ray <raymondtfr@hotmail.com>
2016-08-28 08:51:57 -0800

    fix hell's core and wrath of nature spell areas (#1869)
    * Updated the spells Hell's Core and Wrath of Nature to use the correct combat areas.
    * Renamed AREA_CROSS5X5 and AREA_CROSS6X6 to AREA_CIRCLE5X5 and AREA_CIRCLE6X6, respectively.
    * Updated the spell Wrath of Nature to use the correct combat parameter effect (CONST_ME_SMALLPLANTS) in place of (CONST_ME_PLANTATTACK).

--- data/spells/scripts/attack/hells_core.lua
+++ data/spells/scripts/attack/hells_core.lua
@@ -1,7 +1,7 @@
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_FIREDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_FIREAREA)
-combat:setArea(createCombatArea(AREA_CROSS5X5))
+combat:setArea(createCombatArea(AREA_CIRCLE5X5))
 
 function onGetFormulaValues(player, level, maglevel)
 	local min = (level / 5) + (maglevel * 8) + 50
