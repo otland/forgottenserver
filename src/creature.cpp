6360f6caa0da87ce24e576ccb1929514f005f49b
Mark Samman <mark.samman@gmail.com>
2016-10-01 21:36:13 +0200

    Fix #1621 (#1932)
    Reverts part of 2550261c2001fa98c64b72c1e0c7c6c13b5d45a8

--- src/creature.cpp
+++ src/creature.cpp
@@ -390,12 +390,16 @@ void Creature::onRemoveTileItem(const Tile* tile, const Position& pos, const Ite
 	}
 }
 
-void Creature::onCreatureAppear(Creature* creature, bool)
+void Creature::onCreatureAppear(Creature* creature, bool isLogin)
 {
 	if (creature == this) {
 		if (useCacheMap()) {
 			isMapLoaded = true;
 			updateMapCache();
+		}
+
+		if (isLogin) {
+			setLastPosition(getPosition());
 		}
 	} else if (isMapLoaded) {
 		if (creature->getPosition().z == getPosition().z) {
