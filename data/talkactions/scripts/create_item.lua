8b22b71fb39d810743c77c2dea2e6256ddb94341
raymondtfr <raymond.ek@hotmail.com>
2016-10-01 18:02:27 -0300

    avoid debug assertions from specific parameter ids.

--- data/talkactions/scripts/create_item.lua
+++ data/talkactions/scripts/create_item.lua
@@ -1,3 +1,5 @@
+local debugAssertionIds = {1, 2, 3, 4, 5, 6, 7, 10, 11, 13, 14, 15, 19, 21, 26, 27, 28, 35, 43}
+
 function onSay(player, words, param)
 	if not player:getGroup():getAccess() then
 		return true
@@ -16,6 +18,10 @@ function onSay(player, words, param)
 			player:sendCancelMessage("There is no item with that id or name.")
 			return false
 		end
+	end
+
+	if isInArray(debugAssertionIds, itemType:getId()) then
+		return false
 	end
 
 	local count = tonumber(split[2])
