55203895e5f0b29efa7051e75e0cbc06b13d7372
Bruno Minervino <brunominervino1993@gmail.com>
2016-10-05 22:33:59 -0300

    Increase max outfits display in client

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -2646,7 +2646,7 @@ void ProtocolGame::sendOutfitWindow()
 		}
 
 		protocolOutfits.emplace_back(outfit.name, outfit.lookType, addons);
-		if (protocolOutfits.size() == 50) { // Game client doesn't allow more than 50 outfits
+		if (protocolOutfits.size() == 100) { // Game client doesn't allow more than 100 outfits
 			break;
 		}
 	}
