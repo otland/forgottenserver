4f955a07c1881f49f0f45e59065b6329eafe4d6b
Ray <raymondtfr@hotmail.com>
2016-11-26 21:02:07 -0300

    small updates/fixes (#2004)
    * updated spells
    
    * updated monster files with new soulfire spell
    
    * small fix
    
    * make function local

--- data/spells/scripts/attack/inflict_wound.lua
+++ data/spells/scripts/attack/inflict_wound.lua
@@ -3,16 +3,20 @@ combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_DRAWBLOOD)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_WEAPONTYPE)
 
-function onTargetCreature(creature, target)
-	local skill = 0
-	for i = SKILL_CLUB, SKILL_AXE do
-		if skill < creature:getEffectiveSkillLevel(i) then
-			skill = creature:getEffectiveSkillLevel(i)
+local function getHighestSkillLevel(creature)
+	local skillLevel = -1
+	for skillType = SKILL_CLUB, SKILL_AXE do
+		if skillLevel < creature:getEffectiveSkillLevel(skillType) then
+			skillLevel = creature:getEffectiveSkillLevel(skillType)
 		end
 	end
+	return skillLevel
+end
 
-	local min = (creature:getLevel() * 0.01) + (skill * 0.2) + 2
-	local max = (creature:getLevel() * 0.01) + (skill * 0.4) + 2
+function onTargetCreature(creature, target)
+	local skill = getHighestSkillLevel(creature)
+	local min = (creature:getLevel() / 80) + (skill * 0.2) + 2
+	local max = (creature:getLevel() / 80) + (skill * 0.4) + 2
 	local damage = math.random(math.floor(min), math.floor(max))
 	creature:addDamageCondition(target, CONDITION_BLEEDING, 1, target:isPlayer() and math.floor(damage / 4 + 0.5) or damage)
 	return true
