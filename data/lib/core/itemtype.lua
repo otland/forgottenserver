e29750d2e7025362a6ea6cd5cca582ab3e2ab1f9
raymondtfr <raymondtfr@hotmail.com>
2016-12-11 18:54:33 -0300

    new function added to itemtype.lua

--- data/lib/core/itemtype.lua
+++ data/lib/core/itemtype.lua
@@ -11,6 +11,24 @@ local slotBits = {
 	[CONST_SLOT_AMMO] = SLOTP_AMMO
 }
 
+local slotPositions = {
+	[16] = SLOTP_RIGHT, -- 16
+	[32] = SLOTP_LEFT, -- 32
+	[49] = SLOTP_HEAD, -- 1
+	[50] = SLOTP_NECKLACE, -- 2
+	[52] = SLOTP_BACKPACK, -- 4
+	[56] = SLOTP_ARMOR, -- 8
+	[112] = SLOTP_LEGS, -- 64
+	[176] = SLOTP_FEET, -- 128
+	[304] = SLOTP_RING, -- 256
+	[560] = SLOTP_AMMO, -- 512
+	[2096] = SLOTP_TWO_HAND, -- 2048
+}
+
 function ItemType.usesSlot(self, slot)
 	return bit.band(self:getSlotPosition(), slotBits[slot] or 0) ~= 0
+end
+
+function ItemType.getRealSlotPosition(self)
+	return slotPositions[self:getSlotPosition()]
 end
