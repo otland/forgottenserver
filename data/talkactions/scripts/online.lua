7dd2d979a4e3751a7f5a1e5efe535e320080a4a7
Colandus <nabil_noobish@hotmail.com>
2017-01-02 11:45:28 +0100

    Update online.lua

--- data/talkactions/scripts/online.lua
+++ data/talkactions/scripts/online.lua
@@ -1,36 +1,24 @@
+local maxPlayersPerMessage = 10
+
 function onSay(player, words, param)
 	local hasAccess = player:getGroup():getAccess()
 	local players = Game.getPlayers()
-	local playerCount = Game.getPlayerCount()
+	local onlineList = {}
 
-	player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, playerCount .. " players online.")
-
-	local i = 0
-	local msg = ""
 	for k, targetPlayer in ipairs(players) do
 		if hasAccess or not targetPlayer:isInGhostMode() then
-			if i > 0 then
-				msg = msg .. ", "
-			end
-			msg = msg .. targetPlayer:getName() .. " [" .. targetPlayer:getLevel() .. "]"
-			i = i + 1
-		end
-
-		if i == 10 then
-			if k == playerCount then
-				msg = msg .. "."
-			else
-				msg = msg .. ","
-			end
-			player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, msg)
-			msg = ""
-			i = 0
+			table.insert(onlineList, targetPlayer:getName() .. " [" .. targetPlayer:getLevel() .. "]")
 		end
 	end
 
-	if i > 0 then
-		msg = msg .. "."
-		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, msg)
+	local playersOnline = #onlineList
+	player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, playersOnline .. " players online.")
+	
+	local i = 1
+	while i <= playersOnline do
+		local msg = table.concat(onlineList, ", ", i, math.min(i + maxPlayersPerMessage - 1, playersOnline))
+		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, msg .. ".")
+		i = i + maxPlayersPerMessage
 	end
 	return false
 end
