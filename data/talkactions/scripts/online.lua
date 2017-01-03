e8d62148c6a0bb8592035ca28e37df1a94662ce9
Colandus <nabil_noobish@hotmail.com>
2017-01-03 09:21:40 +0100

    Update online.lua

--- data/talkactions/scripts/online.lua
+++ data/talkactions/scripts/online.lua
@@ -5,14 +5,14 @@ function onSay(player, words, param)
 	local players = Game.getPlayers()
 	local onlineList = {}
 
-	for k, targetPlayer in ipairs(players) do
+	for _, targetPlayer in ipairs(players) do
 		if hasAccess or not targetPlayer:isInGhostMode() then
 			table.insert(onlineList, ("%s [%d]"):format(targetPlayer:getName(), targetPlayer:getLevel()))
 		end
 	end
 
 	local playersOnline = #onlineList
-	player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, playersOnline .. " players online.")
+	player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, ("%d players online."):format(playersOnline))
 	
 	for i = 1, playersOnline, maxPlayersPerMessage do
 		local msg = table.concat(onlineList, ", ", i, math.min(i + maxPlayersPerMessage - 1, playersOnline))
