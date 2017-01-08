383449ee6e023140eb97db355061116e1e3bbec5
Gustavo Aguiar <gugahoa@gmail.com>
2017-01-08 00:54:03 -0200

    Update player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -2,7 +2,7 @@
  * The Forgotten Server - a free and open-source MMORPG server emulator
  * Copyright (C) 2017 Mark Samman <mark.samman@gmail.com>
  *
- * This program is free tisoftware; you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
