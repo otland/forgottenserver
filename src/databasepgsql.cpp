8e9892ee55d2ce21c4b953ab9938bfd74e2e464e
Felipe Borges <felipe.b.t@hotmail.com>
2017-01-16 22:32:03 -0200

    update cmakelist, copyright, remove unused variable, and change return false to return nullptr

--- src/databasepgsql.cpp
+++ src/databasepgsql.cpp
@@ -1,3 +1,22 @@
+/**
+ * The Forgotten Server - a free and open-source MMORPG server emulator
+ * Copyright (C) 2017  Mark Samman <mark.samman@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
 #include "otpch.h"
 #include "databasepgsql.h"
 #include <boost/algorithm/string/replace.hpp>
@@ -34,7 +53,6 @@ bool DatabasePGsql::executeQuery(const std::string& query)
 
 	std::string _query = (std::string)query;
 	boost::replace_all(_query, "`", "");
-	bool success = true;
 	PGresult* res;
 	// executes query
 	
@@ -68,7 +86,7 @@ DBResult_ptr DatabasePGsql::storeQuery(const std::string& query)
 			std::cout << "PQexec() internalSelectQuery-PQresultErrorMessage: " << PQresultErrorMessage(res) << std::endl;
 
 			PQclear(res);
-			return false;	
+			return nullptr;
 		}
 
 	databaseLock.unlock();
