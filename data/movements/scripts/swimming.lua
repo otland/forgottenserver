e753d701dda7125566a0018a4cd88179dc5afb6b
WibbenZ <WibbenZ@gmail.com>
2017-01-17 06:20:55 +0100

    Update

--- data/movements/scripts/swimming.lua
+++ data/movements/scripts/swimming.lua
@@ -1,5 +1,5 @@
 local condition = Condition(CONDITION_OUTFIT)
-condition:setOutfit({lookType = 267, lookHead = 0, lookBody = 0, lookLegs = 0, lookFeet = 0, lookTypeEx = 0, lookAddons = 0})
+condition:setOutfit({lookType = 267})
 condition:setTicks(-1)
 
 function onStepIn(creature, item, position, fromPosition)
