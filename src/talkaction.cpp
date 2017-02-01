9f011fff22f95fe4b9adfce60b528e60c0dfbe3f
Mkalo <Mkalo@users.noreply.github.com>
2017-02-01 20:14:36 -0300

    Fix memory leak in talkaction
    That was my mistake when changing it to list of objs instead of pointers.

--- src/talkaction.cpp
+++ src/talkaction.cpp
@@ -61,7 +61,9 @@ Event* TalkActions::getEvent(const std::string& nodeName)
 
 bool TalkActions::registerEvent(Event* event, const pugi::xml_node&)
 {
-	talkActions.push_front(*static_cast<TalkAction*>(event)); // event is guaranteed to be a TalkAction
+	TalkAction* talkAction = static_cast<TalkAction*>(event) // event is guaranteed to be a TalkAction
+	talkActions.push_front(*talkAction);
+	delete talkAction;
 	return true;
 }
 
