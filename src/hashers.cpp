f6320d5da900089683604f4cbbf4989679071fdc
Ranieri Althoff <ranisalt@gmail.com>
2017-02-02 09:26:26 -0200

    Use crypto++ for SHA1 hashing

--- src/hashers.cpp
+++ src/hashers.cpp
@@ -19,6 +19,9 @@
 
 #include "otpch.h"
 
+#include <cryptopp/hex.h>
+#include <cryptopp/sha.h>
+
 #include "hashers.h"
 
 namespace hashers {
@@ -29,73 +32,15 @@ std::unique_ptr<Hasher> hashers[] = {
 		std::unique_ptr<Hasher>(new SHA1Hasher)
 };
 
-constexpr uint32_t circularShift(int bits, uint32_t value)
+std::string hexencode(const byte* raw, std::size_t length)
 {
-	return (value << bits) | (value >> (32 - bits));
+	std::string output;
+	CryptoPP::HexEncoder encoder{new CryptoPP::StringSink{output}, false};
+	encoder.Put(raw, length);
+	encoder.MessageEnd();
+	return output;
 }
 
-void processSHA1MessageBlock(const uint8_t* messageBlock, uint32_t* H)
-{
-	uint32_t W[80];
-	for (int i = 0; i < 16; ++i) {
-		const size_t offset = i << 2;
-		W[i] = messageBlock[offset] << 24 | messageBlock[offset + 1] << 16 |
-			   messageBlock[offset + 2] << 8 | messageBlock[offset + 3];
-	}
-
-	for (int i = 16; i < 80; ++i) {
-		W[i] = circularShift(1, W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16]);
-	}
-
-	uint32_t A = H[0], B = H[1], C = H[2], D = H[3], E = H[4];
-
-	for (int i = 0; i < 20; ++i) {
-		const uint32_t tmp =
-				circularShift(5, A) + ((B & C) | ((~B) & D)) + E + W[i] +
-				0x5A827999;
-		E = D;
-		D = C;
-		C = circularShift(30, B);
-		B = A;
-		A = tmp;
-	}
-
-	for (int i = 20; i < 40; ++i) {
-		const uint32_t tmp =
-				circularShift(5, A) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1;
-		E = D;
-		D = C;
-		C = circularShift(30, B);
-		B = A;
-		A = tmp;
-	}
-
-	for (int i = 40; i < 60; ++i) {
-		const uint32_t tmp =
-				circularShift(5, A) + ((B & C) | (B & D) | (C & D)) + E + W[i] +
-				0x8F1BBCDC;
-		E = D;
-		D = C;
-		C = circularShift(30, B);
-		B = A;
-		A = tmp;
-	}
-
-	for (int i = 60; i < 80; ++i) {
-		const uint32_t tmp =
-				circularShift(5, A) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6;
-		E = D;
-		D = C;
-		C = circularShift(30, B);
-		B = A;
-		A = tmp;
-	}
-
-	H[0] += A;
-	H[1] += B;
-	H[2] += C;
-	H[3] += D;
-	H[4] += E;
 }
 
 const Hasher& getHasher(const std::string& algorithm)
@@ -123,71 +68,10 @@ const Hasher& identifyHasher(const std::string& encoded)
 
 std::string SHA1Hasher::encode(const std::string& input, const std::string&) const
 {
-	uint32_t H[] = {
-			0x67452301,
-			0xEFCDAB89,
-			0x98BADCFE,
-			0x10325476,
-			0xC3D2E1F0
-	};
-
-	uint8_t messageBlock[64];
-	size_t index = 0;
-
-	uint32_t length_low = 0;
-	uint32_t length_high = 0;
-	for (char ch : input) {
-		messageBlock[index++] = ch;
-
-		length_low += 8;
-		if (length_low == 0) {
-			length_high++;
-		}
-
-		if (index == 64) {
-			processSHA1MessageBlock(messageBlock, H);
-			index = 0;
-		}
-	}
-
-	messageBlock[index++] = 0x80;
-
-	if (index > 56) {
-		while (index < 64) {
-			messageBlock[index++] = 0;
-		}
-
-		processSHA1MessageBlock(messageBlock, H);
-		index = 0;
-	}
-
-	while (index < 56) {
-		messageBlock[index++] = 0;
-	}
-
-	messageBlock[56] = length_high >> 24;
-	messageBlock[57] = length_high >> 16;
-	messageBlock[58] = length_high >> 8;
-	messageBlock[59] = length_high;
-
-	messageBlock[60] = length_low >> 24;
-	messageBlock[61] = length_low >> 16;
-	messageBlock[62] = length_low >> 8;
-	messageBlock[63] = length_low;
-
-	processSHA1MessageBlock(messageBlock, H);
-
-	std::string hexstring(40, 0);
-
-	static const char hexDigits[] = {"0123456789abcdef"};
-	for (int hashByte = 20; --hashByte >= 0;) {
-		const uint8_t byte = H[hashByte >> 2] >> (((3 - hashByte) & 3) << 3);
-		index = hashByte << 1;
-		hexstring[index] = hexDigits[byte >> 4];
-		hexstring[index + 1] = hexDigits[byte & 15];
-	}
-
-	return hexstring;
+	CryptoPP::SHA1 hasher;
+	byte digest[CryptoPP::SHA1::DIGESTSIZE];
+	hasher.CalculateDigest(digest, reinterpret_cast<const byte*>(input.c_str()), input.size());
+	return hexencode(digest, sizeof(digest));
 }
 
 bool SHA1Hasher::verify(const std::string& input, const std::string& encoded) const
