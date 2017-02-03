49b2f1873251a329295e3aa68f63113e5347acd9
Ranieri Althoff <ranisalt@gmail.com>
2017-02-02 22:08:34 -0200

    Use classes instead of structs

--- src/hashers.h
+++ src/hashers.h
@@ -24,19 +24,21 @@
 
 namespace hashers {
 
-struct Hasher {
-	virtual std::string algorithm() const = 0;
-	virtual std::string encode(const std::string& input, const std::string& salt) const = 0;
-	virtual bool verify(const std::string& input, const std::string& encoded) const = 0;
+class Hasher {
+	public:
+		virtual std::string algorithm() const = 0;
+		virtual std::string encode(const std::string& input, const std::string& salt) const = 0;
+		virtual bool verify(const std::string& input, const std::string& encoded) const = 0;
 };
 
 const Hasher& getHasher(const std::string& algorithm);
 const Hasher& identifyHasher(const std::string& encoded);
 
-struct SHA1Hasher : public Hasher {
-	std::string algorithm() const override { return "sha1"; }
-	std::string encode(const std::string& input, const std::string& salt = "") const override;
-	bool verify(const std::string& input, const std::string& encoded) const override;
+class SHA1Hasher : public Hasher {
+	public:
+		std::string algorithm() const override { return "deprecated_sha1"; }
+		std::string encode(const std::string& input, const std::string& salt = "") const override;
+		bool verify(const std::string& input, const std::string& encoded) const override;
 };
 
 }
