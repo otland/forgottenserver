9952ff9423ee0cd6175852b170022b885098d9c1
Damian Jarek <damian.jarek93@gmail.com>
2017-02-23 00:29:32 +0100

    Add documentation to the main routes file
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- data/routes/routes.lua
+++ data/routes/routes.lua
@@ -1,3 +1,10 @@
+-- The Authenticator manages access to HTTP URLs. By default it allows access to all clients.
+-- To restrict access you will need to specify what permissions are required for a particular page
+-- in the router::register() call e.g. GET = {} means that no permissions are required to perform
+-- a GET request on a partiular HTTP resource. It is completely up to the user how these are stored
+-- in the table for each object and how the Authenticator manages them.
+-- Please make sure that resources are released in Authenticator:onSessionClose() if any resources
+-- are allocated during authentication.
 local Authenticator = {}
 
 function Authenticator:hasPermissions(session, permissions)
@@ -16,9 +23,10 @@ end
 Router.authenticator = Authenticator;
 
 local function registerRoutes()
+	--uses the luajson library
 	local json = require 'json'
 	local stdApi = require('data.routes.scripts.standard_ot_api')
-	stdApi.register{router = Router, json = json}
+	stdApi.register({router = Router, json = json})
 end
 
 registerRoutes()
