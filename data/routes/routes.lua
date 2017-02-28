54a24bf02089f176415b71a6deb165fb77a91d9d
Damian Jarek <damian.jarek93@gmail.com>
2017-03-01 00:50:34 +0100

    Add luajson to luapath
    This is required so that the server can successfully load the library.
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- data/routes/routes.lua
+++ data/routes/routes.lua
@@ -1,3 +1,6 @@
+-- Required to find the json library
+package.path = package.path .. ';ext/luajson/lua/?.lua'
+
 -- The Authenticator manages access to HTTP URLs. By default it allows access to all clients.
 -- To restrict access you will need to specify what permissions are required for a particular page
 -- in the router::register() call e.g. GET = {} means that no permissions are required to perform
