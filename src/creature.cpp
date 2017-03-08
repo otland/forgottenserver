otland/forgottenserver

commit 5ba1ef3926695237a8da3d969bd99e779a0ae7f1

Fix setMaster behaviour (#2131)

src/creature.cpp
@@ -43,7 +43,6 @@ Creature::~Creature()
 	for (Creature* summon : summons) {
 		summon->setAttackedCreature(nullptr);
 		summon->setMaster(nullptr);
-		summon->decrementReferenceCounter();
 	}
 
 	for (Condition* condition : conditions) {
@@ -413,7 +412,7 @@ void Creature::onRemoveCreature(Creature* creature, bool)
 	onCreatureDisappear(creature, true);
 	if (creature == this) {
 		if (master && !master->isRemoved()) {
-			master->removeSummon(this);
+			setMaster(nullptr);
 		}
 	} else if (isMapLoaded) {
 		if (creature->getPosition().z == getPosition().z) {
@@ -674,7 +673,7 @@ void Creature::onDeath()
 	death(lastHitCreature);
 
 	if (master) {
-		master->removeSummon(this);
+		setMaster(nullptr);
 	}
 
 	if (droppedCorpse) {
@@ -1121,25 +1120,26 @@ void Creature::onGainExperience(uint64_t gainExp, Creature* target)
 	}
 }
 
-void Creature::addSummon(Creature* creature)
-{
-	creature->setDropLoot(false);
-	creature->setLossSkill(false);
-	creature->setMaster(this);
-	creature->incrementReferenceCounter();
-	summons.push_back(creature);
-}
-
-void Creature::removeSummon(Creature* creature)
-{
-	auto cit = std::find(summons.begin(), summons.end(), creature);
-	if (cit != summons.end()) {
-		creature->setDropLoot(false);
-		creature->setLossSkill(true);
-		creature->setMaster(nullptr);
-		creature->decrementReferenceCounter();
-		summons.erase(cit);
+bool Creature::setMaster(Creature* newMaster) {
+	if (!newMaster && !master) {
+		return false;
 	}
+
+	if (newMaster) {
+		incrementReferenceCounter();
+		newMaster->summons.push_back(this);
+	}
+
+	if (master) {
+		auto summon = std::find(master->summons.begin(), master->summons.end(), this);
+		if (summon != master->summons.end()) {
+			decrementReferenceCounter();
+			master->summons.erase(summon);
+		}
+	}
+
+	master = newMaster;
+	return true;
 }
 
 bool Creature::addCondition(Condition* condition, bool force/* = false*/)

src/creature.h
@@ -276,9 +276,8 @@ class Creature : virtual public Thing
 		virtual BlockType_t blockHit(Creature* attacker, CombatType_t combatType, int32_t& damage,
 		                             bool checkDefense = false, bool checkArmor = false, bool field = false);
 
-		void setMaster(Creature* creature) {
-			master = creature;
-		}
+		bool setMaster(Creature* newMaster);
+
 		bool isSummon() const {
 			return master != nullptr;
 		}
@@ -286,8 +285,6 @@ class Creature : virtual public Thing
 			return master;
 		}
 
-		void addSummon(Creature* creature);
-		void removeSummon(Creature* creature);
 		const std::list<Creature*>& getSummons() const {
 			return summons;
 		}
@@ -412,7 +409,7 @@ class Creature : virtual public Thing
 		void setDropLoot(bool lootDrop) {
 			this->lootDrop = lootDrop;
 		}
-		void setLossSkill(bool skillLoss) {
+		void setSkillLoss(bool skillLoss) {
 			this->skillLoss = skillLoss;
 		}
 

src/monster.cpp
@@ -932,15 +932,14 @@ void Monster::onThinkDefense(uint32_t interval)
 
 			Monster* summon = Monster::createMonster(summonBlock.name);
 			if (summon) {
-				const Position& summonPos = getPosition();
-
-				addSummon(summon);
-
-				if (!g_game.placeCreature(summon, summonPos, false, summonBlock.force)) {
-					removeSummon(summon);
-				} else {
+				if (g_game.placeCreature(summon, getPosition(), false, summonBlock.force)) {
+					summon->setDropLoot(false);
+					summon->setSkillLoss(false);
+					summon->setMaster(this);
 					g_game.addMagicEffect(getPosition(), CONST_ME_MAGIC_BLUE);
 					g_game.addMagicEffect(summon->getPosition(), CONST_ME_TELEPORT);
+				} else {
+					delete summon;
 				}
 			}
 		}
@@ -1751,7 +1750,6 @@ void Monster::death(Creature*)
 	for (Creature* summon : summons) {
 		summon->changeHealth(-summon->getHealth());
 		summon->setMaster(nullptr);
-		summon->decrementReferenceCounter();
 	}
 	summons.clear();
 
@@ -1930,12 +1928,10 @@ bool Monster::convinceCreature(Creature* creature)
 			return false;
 		}
 
-		Creature* oldMaster = getMaster();
-		oldMaster->removeSummon(this);
 	}
-
-	creature->addSummon(this);
-
+	setMaster(creature);
+	setDropLoot(false);
+	setSkillLoss(false);
 	setFollowCreature(nullptr);
 	setAttackedCreature(nullptr);
 
@@ -1943,7 +1939,6 @@ bool Monster::convinceCreature(Creature* creature)
 	for (Creature* summon : summons) {
 		summon->changeHealth(-summon->getHealth());
 		summon->setMaster(nullptr);
-		summon->decrementReferenceCounter();
 	}
 	summons.clear();
 

data/actions/scripts/other/music.lua
@@ -46,7 +46,7 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 			), false, true)
 
 			if monster then
-				monster:setMaster(player)
+				player:addSummon(monster)
 			end
 		end
 

data/lib/compat/compat.lua
@@ -546,7 +546,7 @@ function doConvinceCreature(cid, target)
 		return false
 	end
 
-	targetCreature:setMaster(creature)
+	creature:addSummon(targetCreature)
 	return true
 end
 

data/lib/core/creature.lua
@@ -84,3 +84,33 @@ function Creature:setItemOutfit(item, time)
 
 	return true
 end
+
+function Creature:addSummon(monster)
+	local summon = Monster(monster)
+	if not summon then
+		return false
+	end
+
+	summon:setTarget(nil)
+	summon:setFollowCreature(nil)
+	summon:setDropLoot(false)
+	summon:setSkillLoss(false)
+	summon:setMaster(self)
+
+	return true
+end
+
+function Creature:removeSummon(monster)
+	local summon = Monster(monster)
+	if not summon or summon:getMaster() ~= self then
+		return false
+	end
+
+	summon:setTarget(nil)
+	summon:setFollowCreature(nil)
+	summon:setDropLoot(true)
+	summon:setSkillLoss(true)
+	summon:setMaster(nil)
+
+	return true
+end

data/spells/scripts/support/animate_dead_rune.lua
@@ -9,7 +9,7 @@ function onCastSpell(creature, variant, isHotkey)
 				local monster = Game.createMonster("Skeleton", position)
 				if monster then
 					corpse:remove()
-					monster:setMaster(creature)
+					creature:addSummon(monster)
 					position:sendMagicEffect(CONST_ME_MAGIC_BLUE)
 					return true
 				end

data/spells/scripts/support/summon_creature.lua
@@ -39,8 +39,7 @@ function onCastSpell(creature, variant)
 
 	creature:addMana(-manaCost)
 	creature:addManaSpent(manaCost)
-
-	summon:setMaster(creature)
+	creature:addSummon(summon)
 	position:sendMagicEffect(CONST_ME_MAGIC_BLUE)
 	summon:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 	return true

commit 155ea351565b8091393ff27d69c587a7baedf5a9

Fix master summons crashes in Creature::setMaster (Fixes #2190)
Bugs introduced in introduced in #2131 (5ba1ef3926695237a8da3d969bd99e779a0ae7f1)

src/creature.cpp
@@ -42,7 +42,7 @@ Creature::~Creature()
 {
 	for (Creature* summon : summons) {
 		summon->setAttackedCreature(nullptr);
-		summon->setMaster(nullptr);
+		summon->removeMaster();
 	}
 
 	for (Condition* condition : conditions) {
@@ -1130,15 +1130,16 @@ bool Creature::setMaster(Creature* newMaster) {
 		newMaster->summons.push_back(this);
 	}
 
-	if (master) {
-		auto summon = std::find(master->summons.begin(), master->summons.end(), this);
-		if (summon != master->summons.end()) {
+	Creature* oldMaster = master;
+	master = newMaster;
+
+	if (oldMaster) {
+		auto summon = std::find(oldMaster->summons.begin(), oldMaster->summons.end(), this);
+		if (summon != oldMaster->summons.end()) {
+			oldMaster->summons.erase(summon);
 			decrementReferenceCounter();
-			master->summons.erase(summon);
 		}
 	}
-
-	master = newMaster;
 	return true;
 }
 

src/creature.h
@@ -278,6 +278,13 @@ class Creature : virtual public Thing
 
 		bool setMaster(Creature* newMaster);
 
+		void removeMaster() {
+			if (master) {
+				master = nullptr;
+				decrementReferenceCounter();
+			}
+		}
+
 		bool isSummon() const {
 			return master != nullptr;
 		}

src/monster.cpp
@@ -1749,7 +1749,7 @@ void Monster::death(Creature*)
 
 	for (Creature* summon : summons) {
 		summon->changeHealth(-summon->getHealth());
-		summon->setMaster(nullptr);
+		summon->removeMaster();
 	}
 	summons.clear();
 
@@ -1938,7 +1938,7 @@ bool Monster::convinceCreature(Creature* creature)
 	//destroy summons
 	for (Creature* summon : summons) {
 		summon->changeHealth(-summon->getHealth());
-		summon->setMaster(nullptr);
+		summon->removeMaster();
 	}
 	summons.clear();
 