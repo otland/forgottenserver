f3f6d54e9bc8c205159b82fba43a8a42b05301bd
Tomasz Martyński <tomek@martynski.pl>
2017-03-16 23:03:11 +0100

    Refactored and moved to its own method code responsible for sending VIP entries (#2187)
    * Refactored and moved to it's own method code responsible for sending VIP entries
    
    * Fixed coding style and shortened if statement
    
    * Fixed sendVIPEntries declaration in header

--- src/protocolgame.h
+++ src/protocolgame.h
@@ -225,6 +225,7 @@ class ProtocolGame final : public Protocol
 
 		void sendUpdatedVIPStatus(uint32_t guid, VipStatus_t newStatus);
 		void sendVIP(uint32_t guid, const std::string& name, const std::string& description, uint32_t icon, bool notify, VipStatus_t status);
+		void sendVIPEntries();
 
 		void sendPendingStateEntered();
 		void sendEnterWorld();
