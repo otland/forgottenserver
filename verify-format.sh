afb3b94680b735fefb710d569136d2acc2d39530
Damian Jarek <damian.jarek93@gmail.com>
2017-03-19 22:44:53 +0100

    Fix invalid return value on success
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- verify-format.sh
+++ verify-format.sh
@@ -3,7 +3,11 @@
 cpp_files=$(find src -type f -iname "*.cpp")
 h_files=$(find src -type f -iname "*.h")
 diff -u <(clang-format-3.8 -style=file $cpp_files) <(cat $cpp_files) && \
-diff -u <(clang-format-3.8 -style=file $h_files) <(cat $h_files) || \
-echo -e "\e[31mFormatting errors detected !\e[0m" && exit 1
+diff -u <(clang-format-3.8 -style=file $h_files) <(cat $h_files)
 
-exit 0
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mFormatting errors detected !\e[0m"
+    exit -1
+else
+    exit 0
+fi
