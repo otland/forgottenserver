b263d4a2baddc97b2f2dc59113b01c9446c62303
Ranieri Althoff <ranisalt@gmail.com>
2017-08-23 22:44:56 -0300

    Add helper functions to fetch guild ranks

--- src/guild.h
+++ src/guild.h
@@ -55,7 +55,11 @@ class Guild
 			memberCount = count;
 		}
 
+		const std::vector<GuildRank>& getRanks() const {
+			return ranks;
+		}
 		GuildRank* getRankById(uint32_t id);
+		const GuildRank* getRankByName(const std::string& name) const;
 		const GuildRank* getRankByLevel(uint8_t level) const;
 		void addRank(uint32_t id, const std::string& name, uint8_t level);
 
