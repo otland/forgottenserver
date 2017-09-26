30cc2e7622838c6222bd6f97b73be8bfe568773a
RaymondTFR <raymondtfr@users.noreply.github.com>
2017-09-25 22:55:40 -0300

    Updated soulfire spells

--- data/spells/scripts/monster/hellspawn_soulfire.lua
+++ data/spells/scripts/monster/hellspawn_soulfire.lua
@@ -3,10 +3,11 @@ combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_FIREDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_HITBYFIRE)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_FIRE)
 
-local condition = Condition(CONDITION_FIRE)
-condition:setParameter(CONDITION_PARAM_DELAYED, true)
-condition:addDamage(26, 9000, -10)
-combat:addCondition(condition)
+function onTargetCreature(creature, target)
+	creature:addDamageCondition(target, CONDITION_FIRE, 2, 10, {8, 10}, 26)
+end
+
+combat:setCallback(CALLBACK_PARAM_TARGETCREATURE, "onTargetCreature")
 
 function onCastSpell(creature, variant)
 	return combat:execute(creature, variant)
--- data/spells/scripts/monster/massive_fire_elemental_soulfire.lua
+++ data/spells/scripts/monster/massive_fire_elemental_soulfire.lua
@@ -1,12 +1,13 @@
-local condition = Condition(CONDITION_FIRE)
-condition:setParameter(CONDITION_PARAM_DELAYED, true)
-condition:addDamage(20, 9000, -10)
-
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_FIREDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
 combat:setArea(createCombatArea(AREA_CIRCLE6X6))
-combat:addCondition(condition)
+
+function onTargetCreature(creature, target)
+	creature:addDamageCondition(target, CONDITION_FIRE, 2, 10, {8, 10}, 20)
+end
+
+combat:setCallback(CALLBACK_PARAM_TARGETCREATURE, "onTargetCreature")
 
 function onCastSpell(creature, variant)
 	return combat:execute(creature, variant)
