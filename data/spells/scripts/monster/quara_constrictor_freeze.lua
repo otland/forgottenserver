30a65ae5ea69c3866b962ebd2c543b8141983d68
RaymondTFR <raymondtfr@users.noreply.github.com>
2017-09-25 22:56:10 -0300

    Updated freeze spell

--- data/spells/scripts/monster/quara_constrictor_freeze.lua
+++ data/spells/scripts/monster/quara_constrictor_freeze.lua
@@ -1,12 +1,13 @@
-local condition = Condition(CONDITION_FREEZING)
-condition:setParameter(CONDITION_PARAM_DELAYED, true)
-condition:addDamage(10, 8000, -8)
-
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_GREEN_RINGS)
 combat:setArea(createCombatArea(AREA_SQUARE1X1))
-combat:addCondition(condition)
+
+function onTargetCreature(creature, target)
+	creature:addDamageCondition(target, CONDITION_FREEZING, 2, 8, {8, 10}, 10)
+end
+
+combat:setCallback(CALLBACK_PARAM_TARGETCREATURE, "onTargetCreature")
 
 function onCastSpell(creature, variant)
 	return combat:execute(creature, variant)
