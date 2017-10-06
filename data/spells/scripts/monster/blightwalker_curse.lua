4b64371c01127268f484aa69590e24b585bef5d5
RaymondTFR <raymondtfr@users.noreply.github.com>
2017-10-05 21:12:11 -0300

    Use damage list type constants in curse spells

--- data/spells/scripts/monster/blightwalker_curse.lua
+++ data/spells/scripts/monster/blightwalker_curse.lua
@@ -4,7 +4,7 @@ combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_SMALLCLOUDS)
 combat:setArea(createCombatArea(AREA_CIRCLE6X6))
 
 function onTargetCreature(creature, target)
-	creature:addDamageCondition(target, CONDITION_CURSED, 0, math.random(52, 154))
+	creature:addDamageCondition(target, CONDITION_CURSED, DAMAGELIST_EXPONENTIAL_DAMAGE, math.random(52, 154))
 end
 
 combat:setCallback(CALLBACK_PARAM_TARGETCREATURE, "onTargetCreature")
--- data/spells/scripts/monster/draken_abomination_curse.lua
+++ data/spells/scripts/monster/draken_abomination_curse.lua
@@ -4,7 +4,7 @@ combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_SMALLCLOUDS)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_DEATH)
 
 function onTargetCreature(creature, target)
-	creature:addDamageCondition(target, CONDITION_CURSED, 0, math.random(62, 128))
+	creature:addDamageCondition(target, CONDITION_CURSED, DAMAGELIST_EXPONENTIAL_DAMAGE, math.random(62, 128))
 end
 
 combat:setCallback(CALLBACK_PARAM_TARGETCREATURE, "onTargetCreature")
