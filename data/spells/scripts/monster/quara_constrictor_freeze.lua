af1be7060d87fcd9090bd69230d12e2547358371
RaymondTFR <raymondtfr@users.noreply.github.com>
2017-10-05 21:11:06 -0300

    Use damage list type constants in freeze spell

--- data/spells/scripts/monster/quara_constrictor_freeze.lua
+++ data/spells/scripts/monster/quara_constrictor_freeze.lua
@@ -4,7 +4,7 @@ combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_GREEN_RINGS)
 combat:setArea(createCombatArea(AREA_SQUARE1X1))
 
 function onTargetCreature(creature, target)
-	creature:addDamageCondition(target, CONDITION_FREEZING, 2, 8, {8, 10}, 10)
+	creature:addDamageCondition(target, CONDITION_FREEZING, DAMAGELIST_VARYING_PERIOD, 8, {8, 10}, 10)
 end
 
 combat:setCallback(CALLBACK_PARAM_TARGETCREATURE, "onTargetCreature")
