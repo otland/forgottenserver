31369127258cf4a351dd4ccd72c1c144b72b9b2c
Cristofer Martins <cristofermartins@hotmail.com>
2017-10-16 00:53:04 -0200

    * check if eventList is empty to prevent an invalid task

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -36,6 +36,7 @@ void Scheduler::threadMain()
 
 		// the mutex is locked again now...
 		if (ret == std::cv_status::timeout) {
+		if ((ret == std::cv_status::timeout) && !eventList.empty()) {
 			// ok we had a timeout, so there has to be an event we have to execute...
 			SchedulerTask* task = eventList.top();
 			eventList.pop();
