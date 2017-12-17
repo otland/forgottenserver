416cfb7d27786ee4a6516b0182ca228b8f5348b8
Riverlance <biel94moraes@msn.com>
2017-12-17 18:25:46 -0200

    Add Tile:isWalkable() (#2120)
    * Add Position:isWalkable()
    * Added position.lua changes
    * Update position.lua
    * Update position.lua
    * Tile.isWalkable(self, check) has been improved
    * Fixed spaces of Tile.isWalkable(self, check)
    * Update tile.lua
    * Use a callback instead of preset conditions

--- data/lib/core/tile.lua
+++ data/lib/core/tile.lua
@@ -36,3 +36,24 @@ function Tile.relocateTo(self, toPosition)
 
 	return true
 end
+
+function Tile.isWalkable(self, condition)
+	if condition and condition(self) then
+		return true
+	end
+
+	local ground = self:getGround()
+	if not ground or ground:hasProperty(CONST_PROP_BLOCKSOLID) then
+		return false
+	end
+
+	local items = self:getItems()
+	for i = 1, self:getItemCount() do
+		local item = items[i]
+		local itemType = item:getType()
+		if itemType:getType() ~= ITEM_TYPE_MAGICFIELD and not itemType:isMovable() and item:hasProperty(CONST_PROP_BLOCKSOLID) then
+			return false
+		end
+	end
+	return true
+end
