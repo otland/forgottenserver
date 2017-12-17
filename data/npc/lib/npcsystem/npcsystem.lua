5c7db594a84adb81f9456881f43aa621fc6d169e
Daniel Speichert <DSpeichert@users.noreply.github.com>
2017-12-17 13:00:40 -0500

    Update npcsystem.lua

--- data/npc/lib/npcsystem/npcsystem.lua
+++ data/npc/lib/npcsystem/npcsystem.lua
@@ -51,7 +51,7 @@ if not NpcSystem then
 	-- Gets an npcparameter with the specified key. Returns nil if no such parameter is found.
 	function NpcSystem.getParameter(key)
 		local ret = getNpcParameter(tostring(key))
-		if (type(ret) == 'number' and ret == 0) or not ret then
+		if (type(ret) == 'number' and ret == 0) then
 			return nil
 		else
 			return ret
