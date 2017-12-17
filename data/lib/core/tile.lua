795e89196a6140bdb180adaa27c06994f398b60a
ninjalulz <danielbjorkholm@gmail.com>
2017-12-17 21:24:29 +0100

    Use a callback instead of preset conditions

--- data/lib/core/tile.lua
+++ data/lib/core/tile.lua
@@ -33,44 +33,21 @@ function Tile.relocateTo(self, toPosition)
 	return true
 end
 
-function Tile.isWalkable(self, check)
-	local ground = self:getGround()
-	if not ground or ground:hasProperty(CONST_PROP_BLOCKSOLID) or
-		check and (check.actionId == true and ground:getActionId() ~= 0 or check.actionId == ground:getActionId() or
-		check.uniqueId == true and ground:getUniqueId() ~= 0 or check.uniqueId == ground:getUniqueId()) then
-		return false
+function Tile.isWalkable(self, condition)
+	if condition and condition(self) then
+		return true
 	end
 
-	if check then
-		if check.protection and self:hasFlag(TILESTATE_PROTECTIONZONE) or
-			check.floorChange and self:hasFlag(TILESTATE_FLOORCHANGE) or
-			check.house and self:getHouse() or
-			check.tileCondition and check.tileCondition(self) then
-			return false
-		elseif check.creatures or check.players or check.monsters or check.npcs then
-			local creatures = self:getCreatures()
-			for i = 1, self:getCreatureCount() do
-				local creature = creatures[i]
-				if check.creatures and creature:isCreature() or
-					check.players and creature:isPlayer() or
-					check.monsters and creature:isMonster() or
-					check.npcs and creature:isNpc() then
-					return false
-				end
-			end
-		end
+	local ground = self:getGround()
+	if not ground or ground:hasProperty(CONST_PROP_BLOCKSOLID) then
+		return false
 	end
 
 	local items = self:getItems()
 	for i = 1, self:getItemCount() do
 		local item = items[i]
 		local itemType = item:getType()
-		if item:hasProperty(CONST_PROP_BLOCKSOLID) or
-			check and (check.teleport and itemType:getType() == ITEM_TYPE_TELEPORT or
-			check.magicField and itemType:getType() == ITEM_TYPE_MAGICFIELD or
-			check.actionId == true and item:getActionId() ~= 0 or check.actionId == item:getActionId() or
-			check.uniqueId == true and item:getUniqueId() ~= 0 or check.uniqueId == item:getUniqueId() or
-			check.itemCondition and check.itemCondition(item)) then
+		if itemType:getType() ~= ITEM_TYPE_MAGICFIELD and not itemType:isMovable() and item:hasProperty(CONST_PROP_BLOCKSOLID) then
 			return false
 		end
 	end
