1239add3d1b04f2ae09d685e012f7a5c6e2602ea
Ranieri Althoff <ranisalt@gmail.com>
2017-12-19 04:43:00 -0200

    Move rune functions to Lua scripts (#2102)
    * Move chameleon rune to Lua
    * Move convince creature rune to Lua

---
+++ data/spells/scripts/support/convince_creature.lua
@@ -0,0 +1,36 @@
+function onCastSpell(creature, variant, isHotkey)
+	local target = Creature(variant:getNumber())
+	if not target or not target:isMonster() then
+		creature:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+		creature:getPosition():sendMagicEffect(CONST_ME_POFF)
+		return false
+	end
+
+	local monsterType = target:getType()
+	if not getPlayerFlagValue(creature, PlayerFlag_CanConvinceAll) then
+		if not monsterType:isConvinceable() then
+			creature:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+			creature:getPosition():sendMagicEffect(CONST_ME_POFF)
+			return false
+		end
+
+		if #creature:getSummons() >= 2 then
+			creature:sendCancelMessage("You cannot control more creatures.")
+			creature:getPosition():sendMagicEffect(CONST_ME_POFF)
+			return false
+		end
+	end
+
+	local manaCost = target:getType():getManaCost()
+	if creature:getMana() < manaCost and not getPlayerFlagValue(creature, PlayerFlag_HasInfiniteMana) then
+		creature:sendCancelMessage(RETURNVALUE_NOTENOUGHMANA)
+		creature:getPosition():sendMagicEffect(CONST_ME_POFF)
+		return false
+	end
+
+	creature:addMana(-manaCost)
+	creature:addManaSpent(manaCost)
+	creature:addSummon(target)
+	creature:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
+	return true
+end
