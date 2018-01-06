8deeab31e61ea8cbeba511eb022b78591be2ab5a
Daniel Speichert <daniel@speichert.pl>
2018-01-06 01:01:44 -0500

    Handle >8 players on 1 tile

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -517,7 +517,10 @@ void ProtocolGame::GetTileDescription(const Tile* tile, NetworkMessage& msg)
 		for (auto it = items->getBeginTopItem(), end = items->getEndTopItem(); it != end; ++it) {
 			msg.addItem(*it);
 
-			if (++count == 10) {
+			count++;
+			if (tile->getPosition() == player->getPosition() && count == 8) {
+				break;
+			} else if (count == 9) {
 				return;
 			}
 		}
@@ -525,9 +528,18 @@ void ProtocolGame::GetTileDescription(const Tile* tile, NetworkMessage& msg)
 
 	const CreatureVector* creatures = tile->getCreatures();
 	if (creatures) {
+		bool playerAdded = false;
 		for (const Creature* creature : boost::adaptors::reverse(*creatures)) {
 			if (!player->canSeeCreature(creature)) {
 				continue;
+			}
+
+			if (tile->getPosition() == player->getPosition() && count == 8 && !playerAdded) {
+				creature = player;
+			}
+
+			if (creature->getID() == player->getID()) {
+				playerAdded = true;
 			}
 
 			bool known;
