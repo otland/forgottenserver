64795231067727a16e5c12a4a3f96954e130a5b3
Codex-NG17 <a76.casey@gmail.com>
2018-01-07 18:09:20 -0500

    animated text support - 10.98/99 otclient

--- src/game.cpp
+++ src/game.cpp
@@ -4234,6 +4234,23 @@ void Game::addCreatureHealth(const SpectatorHashSet& spectators, const Creature*
 	}
 }
 
+void Game::addAnimatedText(const std::string& message, const Position& pos, TextColor_t color)
+{
+	SpectatorHashSet spectators;
+	map.getSpectators(spectators, pos, true, true);
+	addAnimatedText(spectators, message, pos, color);
+}
+
+
+void Game::addAnimatedText(const SpectatorHashSet& spectators, const std::string& message, const Position& pos, TextColor_t color)
+{
+	for (Creature* spectator : spectators) {
+		if (Player* tmpPlayer = spectator->getPlayer()) {
+			tmpPlayer->sendAnimatedText(message, pos, color);
+		}
+	}
+}
+
 void Game::addMagicEffect(const Position& pos, uint8_t effect)
 {
 	SpectatorHashSet spectators;
