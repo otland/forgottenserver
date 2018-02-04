d5cfc9d6bdb080a1b90703f9933c518db2e4315c
RaymondTFR <raymondtfr@users.noreply.github.com>
2018-02-04 11:49:42 -0300

    Set new combat parameter to spell Mass Healing

--- data/spells/scripts/healing/mass_healing.lua
+++ data/spells/scripts/healing/mass_healing.lua
@@ -1,28 +1,18 @@
 local combat = Combat()
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_HEALING)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 combat:setParameter(COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
+combat:setParameter(COMBAT_PARAM_TARGETPLAYERSORSUMMONS, true)
 combat:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
 combat:setArea(createCombatArea(AREA_CIRCLE3X3))
 
-local healMonsters = false
-
-function onTargetCreature(creature, target)
-	local player = creature:getPlayer()
-	local min = (player:getLevel() / 5) + (player:getMagicLevel() * 4.6) + 100
-	local max = (player:getLevel() / 5) + (player:getMagicLevel() * 9.6) + 125
-
-	if not healMonsters then
-		local master = target:getMaster()
-		if target:isMonster() and not master or master and master:isMonster() then
-			return true
-		end
-	end
-
-	doTargetCombatHealth(creature:getId(), target, COMBAT_HEALING, min, max, CONST_ME_NONE)
-	return true
+function onGetFormulaValues(player, level, magicLevel)
+	local min = (level / 5) + (magicLevel * 4.6) + 100
+	local max = (level / 5) + (magicLevel * 9.6) + 125
+	return min, max
 end
 
-combat:setCallback(CALLBACK_PARAM_TARGETCREATURE, "onTargetCreature")
+combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(creature, variant)
 	return combat:execute(creature, variant)
