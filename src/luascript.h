149e7bb4ebbfe500c5ee89f94455133b10260e0a
Vulcanx <spaghetti90314@gmail.com>
2018-04-07 01:15:39 -0500

    use table instead of userdata

--- src/luascript.h
+++ src/luascript.h
@@ -326,6 +326,7 @@ class LuaScriptInterface
 		static Position getPosition(lua_State* L, int32_t arg);
 		static Outfit_t getOutfit(lua_State* L, int32_t arg);
 		static LuaVariant getVariant(lua_State* L, int32_t arg);
+		static InstantSpell* getInstantSpell(lua_State* L, int32_t arg);
 
 		static Thing* getThing(lua_State* L, int32_t arg);
 		static Creature* getCreature(lua_State* L, int32_t arg);
