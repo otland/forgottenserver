335e373e237f96596c7c0b550f8d1048238783ff
Joseph Bingham <jo3bingham@users.noreply.github.com>
2018-06-11 22:52:25 +0100

    Player::isPartner: Check that player isn't self (#2472)
    Fixes: #2471

--- src/player.cpp
+++ src/player.cpp
@@ -3998,7 +3998,7 @@ bool Player::isInviting(const Player* player) const
 
 bool Player::isPartner(const Player* player) const
 {
-	if (!player || !party) {
+	if (!player || !party || player == this) {
 		return false;
 	}
 	return party == player->party;
