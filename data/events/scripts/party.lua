0f89b918eac90bcb74c1a264acc817cc125cbb4a
Daniel Speichert <daniel@speichert.pl>
2018-07-07 22:10:12 -0400

    Moved shared experience formula to Lua
    Co-authored-by: edumntg <edumntg@gmail.com>

--- data/events/scripts/party.lua
+++ data/events/scripts/party.lua
@@ -9,3 +9,28 @@ end
 function Party:onDisband()
 	return true
 end
+
+function Party:onShareExperience(exp)
+	local sharedExperienceMultiplier = 1.20 --20%
+	local vocationsIds = {}
+	
+	table.insert(vocationsIds, self:getLeader():getVocation():getId())
+	local vocationId = self:getLeader():getVocation():getId()
+	if vocationId ~= VOCATION_NONE then
+		table.insert(vocationsIds, vocationId)
+	end
+	
+	for _, member in ipairs(self:getMembers()) do
+		vocationId = member:getVocation():getId()
+		if not isInArray(vocationsIds, vocationId) and vocationId ~= VOCATION_NONE then
+			table.insert(vocationsIds, vocationId)
+		end
+	end
+
+	local size = #vocationsIds
+	if size > 1 then
+		sharedExperienceMultiplier = 1.0 + ((size * (5 * (size - 1) + 10)) / 100)
+	end
+
+	return (exp * sharedExperienceMultiplier) / (self:getMembers() + 1)
+end
