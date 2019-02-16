a62389ae00b49c5de2664c596d434fb9e68b429a
aymanm419 <40718764+aymanm419@users.noreply.github.com>
2019-02-16 08:04:47 +0200

    Fix skull ticks overflow (#2525)
    Changed skull ticks from millisecond to seconds to avoid overflow.
    
    Fixes #2323

--- data/migrations/22.lua
+++ data/migrations/22.lua
@@ -1,3 +1,5 @@
 function onUpdateDatabase()
-	return false
+	print("> Updating database to version 23 (Fix skulltime)")
+	db.query("ALTER TABLE players MODIFY skulltime BIGINT(20) NOT NULL DEFAULT 0")
+	return true
 end
