4ccac7856c3bc766096bc87533038826a4e9546d
Nekiro <nekiroboy@gmail.com>
2019-02-17 18:03:31 +0100

    Missing migration

--- data/migrations/23.lua
+++ data/migrations/23.lua
@@ -1,3 +1,5 @@
 function onUpdateDatabase()
-	return false
+	print("> Updating database to version 24 (Guildhall support)")
+	db.query("ALTER TABLE `guilds` ADD `house_id` int(11) NOT NULL")
+	return true
 end
