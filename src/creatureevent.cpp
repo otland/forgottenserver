07f919fed5a876a2ff77fe14ee7893c6f74ed125
ninjalulz <danielbjorkholm@gmail.com>
2019-02-27 08:26:55 +0100

    Fix unwanted behavior (#2535)
    The origin of an attack should not reset to `ORIGIN_NONE`

--- src/creatureevent.cpp
+++ src/creatureevent.cpp
@@ -545,7 +545,11 @@ void CreatureEvent::executeManaChange(Creature* creature, Creature* attacker, Co
 	if (scriptInterface->protectedCall(L, 7, 4) != 0) {
 		LuaScriptInterface::reportError(nullptr, LuaScriptInterface::popString(L));
 	} else {
-		damage = LuaScriptInterface::getCombatDamage(L);
+		damage.primary.value = LuaScriptInterface::getNumber<int32_t>(L, -4);
+		damage.primary.type = LuaScriptInterface::getNumber<CombatType_t>(L, -3);
+		damage.secondary.value = LuaScriptInterface::getNumber<int32_t>(L, -2);
+		damage.secondary.type = LuaScriptInterface::getNumber<CombatType_t>(L, -1);
+		lua_pop(L, 4);
 	}
 
 	scriptInterface->resetScriptEnv();
--- src/luascript.cpp
+++ src/luascript.cpp
@@ -693,18 +693,6 @@ void LuaScriptInterface::setCreatureMetatable(lua_State* L, int32_t index, const
 }
 
 // Get
-CombatDamage LuaScriptInterface::getCombatDamage(lua_State* L)
-{
-	CombatDamage damage;
-	damage.primary.value = getNumber<int32_t>(L, -4);
-	damage.primary.type = getNumber<CombatType_t>(L, -3);
-	damage.secondary.value = getNumber<int32_t>(L, -2);
-	damage.secondary.type = getNumber<CombatType_t>(L, -1);
-
-	lua_pop(L, 4);
-	return damage;
-}
-
 std::string LuaScriptInterface::getString(lua_State* L, int32_t arg)
 {
 	size_t len;
