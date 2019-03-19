b5a390fdff0326a3f351abcf52b91d96a9a11329
Ray <raymondtfr@hotmail.com>
2019-03-19 18:11:14 -0300

    Use Combat:getTargets on player spells

--- data/spells/scripts/attack/electrify.lua
+++ data/spells/scripts/attack/electrify.lua
@@ -3,15 +3,12 @@ combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ENERGYAREA)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
 
-function onTargetCreature(creature, target)
+function onCastSpell(creature, variant)
 	local min = (creature:getLevel() / 80) + (creature:getMagicLevel() * 0.15) + 1
 	local max = (creature:getLevel() / 80) + (creature:getMagicLevel() * 0.25) + 1
 	local rounds = math.random(math.floor(min), math.floor(max))
-	creature:addDamageCondition(target, CONDITION_ENERGY, DAMAGELIST_VARYING_PERIOD, target:isPlayer() and 13 or 25, {10, 12}, rounds)
-end
-
-combat:setCallback(CALLBACK_PARAM_TARGETCREATURE, "onTargetCreature")
-
-function onCastSpell(creature, variant)
-	return combat:execute(creature, variant)
+	for _, target in ipairs(combat:getTargets(creature, variant)) do
+		creature:addDamageCondition(target, CONDITION_ENERGY, DAMAGELIST_VARYING_PERIOD, target:isPlayer() and 13 or 25, {10, 12}, rounds)
+	end
+	return true
 end
