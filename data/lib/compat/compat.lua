1184310b72b69bd1644e8b1228be33e0301ccc01
EvilHero90 <Shadow280690@hotmail.de>
2019-04-13 02:29:47 +0200

    minor change on createFunctions

--- data/lib/compat/compat.lua
+++ data/lib/compat/compat.lua
@@ -1231,15 +1231,17 @@ function createFunctions(class)
 	local exclude = {"get", "set", "is", "add", "can"}
 	local temp = {}
 	for name, func in pairs(class) do
-		if not isInArray(exclude, name:sub(1,3)) then
+		if not table.contains(exclude, name:sub(1,3)) then
 			local str = name:sub(1,1):upper()..name:sub(2)
+			local getFunc = function(self) return func(self) end
+			local setFunc = function(self, ...) return func(self, ...) end
 			local get = "get".. str
 			local set = "set".. str
-			table.insert(temp, {set, get, func})
+			table.insert(temp, {set, setFunc, get, getFunc})
 		end
 	end
 	for _,func in ipairs(temp) do
-		rawset(class, func[1], func[3])
-		rawset(class, func[2], func[3])
+		rawset(class, func[1], func[2])
+		rawset(class, func[3], func[4])
 	end
 end
