b562535aa6887a9f3b63fc90024122092ddbd854
EvilHero90 <Shadow280690@hotmail.de>
2019-04-19 02:04:01 +0200

    trailing line space
    - remove a trailing line space

--- src/scriptmanager.cpp
+++ src/scriptmanager.cpp
@@ -66,7 +66,6 @@ bool ScriptingManager::loadScriptSystems()
 
 	g_scripts = new Scripts();
 	std::cout << ">> Loading lua libs" << std::endl;
-  
 	if (!g_scripts->loadScripts("scripts/lib", true, false)) {
 		std::cout << "> ERROR: Unable to load lua libs!" << std::endl;
 		return false;
