a0fe1b42cb09e6cf6e163d35d4f37d46b0c7d07a
EvilHero90 <Shadow280690@hotmail.de>
2019-04-26 15:16:18 +0200

    Revert "condensed function"
    This reverts commit a8f1623e3d6d35e480ca03c44f8e73d2d7b78512.

--- src/movement.cpp
+++ src/movement.cpp
@@ -949,9 +949,15 @@ bool MoveEvent::executeStep(Creature* creature, Item* item, const Position& pos)
 uint32_t MoveEvent::fireEquip(Player* player, Item* item, slots_t slot, bool isCheck)
 {
 	if (scripted) {
-		if (!equipFunction || executeEquip(player, item, slot, isCheck) == 1) {
+		if (!equipFunction) {
 			if (executeEquip(player, item, slot, isCheck)) {
 				return 1;
+			}
+		} else {
+			if (equipFunction(this, player, item, slot, isCheck) == 1) {
+				if (executeEquip(player, item, slot, isCheck)) {
+					return 1;
+				}
 			}
 		}
 		return 0;
