otland/forgottenserver

commit 38eb0cde9eb579765a7e9588eb3d214312d55a19

Moved drop loot mechanism to Lua (#2569)
Added a new class to events called Monster and a new method called onDropLoot.
Added party to Lua core.
Added a new function to party class called broadcastPartyLoot (based on sources).
Replicated (or tried?) getLootRandom and createLootItem to Lua.
Added a new function to container class called createLootContainer (based on sources).
Added two new values to enums MESSAGE_LOOT and MAX_LOOTCHANCE.
Added two new container methods (getContentDescription and getCorpseOwner).

data/lib/core/container.lua
@@ -1,3 +1,48 @@
 function Container.isContainer(self)
 	return true
 end
+
+function Container.createLootItem(self, item)
+	if self:getEmptySlots() == 0 then
+        return true
+	end
+
+	local itemCount = 0
+	local randvalue = getLootRandom()
+	if randvalue < item.chance then
+		if ItemType(item):isStackable() then
+			itemCount = randvalue % item.countmax + 1
+		else 
+			itemCount = 1
+		end
+	end
+	
+	if itemCount > 0 then
+		local tmpItem = self:addItem(item.itemId, math.min(itemCount, 100))
+		if not tmpItem then
+			return false
+		end
+
+		if tmpItem:isContainer() then
+			for i = 1, #item.childLoot do
+				if not tmpItem:createLootItem(item.childLoot[i]) then
+					tmpItem:remove()
+					return false
+				end
+			end
+		end
+
+		if item.subType ~= -1 then
+			tmpItem:setAttribute(ITEM_ATTRIBUTE_CHARGES, item.subType)
+		end
+
+		if item.actionId ~= -1 then
+			tmpItem:setActionId(item.actionId)
+		end
+
+		if item.text and item.text ~= "" then
+			tmpItem:setText(item.text)
+		end
+	end
+	return true
+end

data/events/events.xml
@@ -28,4 +28,7 @@
 	<event class="Player" method="onGainExperience" enabled="1" />
 	<event class="Player" method="onLoseExperience" enabled="0" />
 	<event class="Player" method="onGainSkillTries" enabled="1" />
+
+	<!-- Monster methods -->
+	<event class="Monster" method="onDropLoot" enabled="1" />
 </events>

data/events/scripts/monster.lua
@@ -1,0 +1,35 @@
+function Monster:onDropLoot(corpse)
+	if configManager.getNumber(configKeys.RATE_LOOT) == 0 then
+		return
+	end
+
+	local player = Player(corpse:getCorpseOwner())
+	local mType = self:getType()
+	if not player or player:getStamina() > 840 then
+		local monsterLoot = mType:getLoot()
+		for i = 1, #monsterLoot do
+			local item = corpse:createLootItem(monsterLoot[i])
+			if not item then
+				print('[Warning] DropLoot:', 'Could not add loot item to corpse.')
+			end
+		end
+
+		if player then 
+			local text = ("Loot of %s: %s"):format(mType:getNameDescription(), corpse:getContentDescription())
+			local party = player:getParty()
+			if party then
+				party:broadcastPartyLoot(text)
+			else
+				player:sendTextMessage(MESSAGE_LOOT, text)
+			end
+		end
+	else
+		local text = ("Loot of %s: nothing (due to low stamina)"):format(mType:getNameDescription())
+		local party = player:getParty()
+		if party then
+			party:broadcastPartyLoot(text)
+		else
+			player:sendTextMessage(MESSAGE_LOOT, text)
+		end
+	end
+end

data/global.lua
@@ -33,6 +33,11 @@ function getFormattedWorldTime()
 	return hours .. ':' .. minutes
 end
 
+function getLootRandom()
+	math.randomseed(os.mtime())
+	return math.random(0, MAX_LOOTCHANCE) / configManager.getNumber(configKeys.RATE_LOOT)
+end
+
 table.contains = function(array, value)
 	for _, targetColumn in pairs(array) do
 		if targetColumn == value then

data/lib/core/core.lua
@@ -10,3 +10,4 @@ dofile('data/lib/core/position.lua')
 dofile('data/lib/core/teleport.lua')
 dofile('data/lib/core/tile.lua')
 dofile('data/lib/core/vocation.lua')
+dofile('data/lib/core/party.lua')

data/lib/core/party.lua
@@ -1,0 +1,10 @@
+function Party.broadcastPartyLoot(self, text)
+	self:getLeader():sendTextMessage(MESSAGE_INFO_DESCR, text)
+	local membersList = self:getMembers()
+	for i = 1, #membersList do
+		local player = membersList[i]
+		if player then
+			player:sendTextMessage(MESSAGE_INFO_DESCR, text)
+		end
+	end
+end

src/events.cpp
@@ -119,6 +119,12 @@ bool Events::load()
 			} else {
 				std::cout << "[Warning - Events::load] Unknown player method: " << methodName << std::endl;
 			}
+		} else if (className == "Monster") {
+			if (methodName == "onDropLoot") {
+				info.monsterOnDropLoot = event;
+			} else {
+				std::cout << "[Warning - Events::load] Unknown monster method: " << methodName << std::endl;
+			}
 		} else {
 			std::cout << "[Warning - Events::load] Unknown class: " << className << std::endl;
 		}
@@ -857,4 +863,31 @@ void Events::eventPlayerOnGainSkillTries(Player* player, skills_t skill, uint64_
 	}
 
 	scriptInterface.resetScriptEnv();
+}
+
+void Events::eventMonsterOnDropLoot(Monster* monster, Container* corpse)
+{
+	// Monster:onDropLoot(corpse)
+	if (info.monsterOnDropLoot == -1) {
+		return;
+	}
+
+	if (!scriptInterface.reserveScriptEnv()) {
+		std::cout << "[Error - Events::eventMonsterOnDropLoot] Call stack overflow" << std::endl;
+		return;
+	}
+
+	ScriptEnvironment* env = scriptInterface.getScriptEnv();
+	env->setScriptId(info.monsterOnDropLoot, &scriptInterface);
+
+	lua_State* L = scriptInterface.getLuaState();
+	scriptInterface.pushFunction(info.monsterOnDropLoot);
+
+	LuaScriptInterface::pushUserdata<Monster>(L, monster);
+	LuaScriptInterface::setMetatable(L, -1, "Monster");
+
+	LuaScriptInterface::pushUserdata<Container>(L, corpse);
+	LuaScriptInterface::setMetatable(L, -1, "Container");
+
+	return scriptInterface.callVoidFunction(2);
 }

src/events.h
@@ -57,6 +57,9 @@ class Events
 		int32_t playerOnGainExperience = -1;
 		int32_t playerOnLoseExperience = -1;
 		int32_t playerOnGainSkillTries = -1;
+
+		// Monster
+		int32_t monsterOnDropLoot = -1;
 	};
 
 	public:
@@ -92,6 +95,7 @@ class Events
 		void eventPlayerOnGainExperience(Player* player, Creature* source, uint64_t& exp, uint64_t rawExp);
 		void eventPlayerOnLoseExperience(Player* player, uint64_t& exp);
 		void eventPlayerOnGainSkillTries(Player* player, skills_t skill, uint64_t& tries);
+		void eventMonsterOnDropLoot(Monster* monster, Container* corpse);
 
 	private:
 		LuaScriptInterface scriptInterface;

commit 29dace1c2d6dbd969b9cf229d1738d112ce70330

Fix to stackable item loot drops, bug which was introduced in #2569
Fixed some missing changes related to stackable items, reported here: https://otland.net/threads/mobs-only-drop-one-of-each-item.264492/#post-2556086

data/lib/core/container.lua
@@ -10,8 +10,8 @@ function Container.createLootItem(self, item)
 	local itemCount = 0
 	local randvalue = getLootRandom()
 	if randvalue < item.chance then
-		if ItemType(item):isStackable() then
-			itemCount = randvalue % item.countmax + 1
+		if ItemType(item.itemId):isStackable() then
+			itemCount = randvalue % item.maxCount + 1
 		else 
 			itemCount = 1
 		end