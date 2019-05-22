665852fd33cad3398ba076331f9dc6859bb174ff
Márcio Porto <44486916+marcinho1994@users.noreply.github.com>
2019-05-22 19:22:48 -0300

    Update spells.cpp | fix very rare crash bug. (#2582)
    Fixing a very rare bug. In very specific situations, when the creature or target is removed, it can cause a crash, because in this part of the code the methods "creature->getPosition() and target->getPosition()" are used without first checking that they exist. This causes a bug in combat.cpp when trying to use a position that does not exist.

--- src/monster.cpp
+++ src/monster.cpp
@@ -774,7 +774,11 @@ void Monster::doAttacking(uint32_t interval)
 
 	for (const spellBlock_t& spellBlock : mType->info.attackSpells) {
 		bool inRange = false;
-
+		
+		if (attackedCreature == nullptr) {
+			break;
+		}
+		
 		if (canUseSpell(myPos, targetPos, spellBlock, interval, inRange, resetTicks)) {
 			if (spellBlock.chance >= static_cast<uint32_t>(uniform_random(1, 100))) {
 				if (updateLook) {
