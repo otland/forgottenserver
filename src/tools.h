adbec427c58ba6343ed7c443a63d100c133dd228
souzajunior <36310159+souzajunior@users.noreply.github.com>
2019-07-03 11:26:13 -0300

    Update arguments function to get CombatType
    review changes by djarek

--- src/tools.h
+++ src/tools.h
@@ -84,7 +84,7 @@ std::string getWeaponName(WeaponType_t weaponType);
 
 size_t combatTypeToIndex(CombatType_t combatType);
 CombatType_t indexToCombatType(size_t v);
-CombatType_t getCombatTypeByName(const std::string strValue);
+CombatType_t getCombatTypeByName(const std::string& strValue);
 
 uint8_t serverFluidToClient(uint8_t serverFluid);
 uint8_t clientFluidToServer(uint8_t clientFluid);
