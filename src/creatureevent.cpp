28798bb75ead6041273146a22061ba13d74b9224
Mario Tettei <Shadow280690@hotmail.de>
2019-08-09 00:16:11 +0200

    fix /reload creaturescripts (#2634)
    * fix #2623  /reload creaturescripts
    
    - this fixes the crash bug with /reload creaturescripts
    made the implement with emplace

--- src/creatureevent.cpp
+++ src/creatureevent.cpp
@@ -31,11 +31,9 @@ CreatureEvents::CreatureEvents() :
 
 void CreatureEvents::clear(bool fromLua)
 {
-	for (auto it = creatureEvents.begin(); it != creatureEvents.end(); ) {
+	for (auto it = creatureEvents.begin(); it != creatureEvents.end(); ++it) {
 		if (fromLua == it->second.fromLua) {
-			it = creatureEvents.erase(it);
-		} else {
-			++it;
+			it->second.clearEvent();
 		}
 	}
 
@@ -68,12 +66,20 @@ bool CreatureEvents::registerEvent(Event_ptr event, const pugi::xml_node&)
 		return false;
 	}
 
-	auto result = creatureEvents.emplace(creatureEvent->getName(), std::move(*creatureEvent));
-	if (!result.second) {
-		std::cout << "[Warning - CreatureEvents::registerEvent] Duplicate registered creature event with name: " << creatureEvent->getName() << std::endl;
-	}
+	CreatureEvent* oldEvent = getEventByName(creatureEvent->getName(), false);
+	if (oldEvent) {
+		//if there was an event with the same that is not loaded
+		//(happens when realoading), it is reused
+		if (!oldEvent->isLoaded() && oldEvent->getEventType() == creatureEvent->getEventType()) {
+			oldEvent->copyEvent(creatureEvent.get());
+		}
 
-	return result.second;
+		return false;
+	} else {
+		//if not, register it normally
+		creatureEvents.emplace(creatureEvent->getName(), std::move(*creatureEvent));
+		return true;
+	}
 }
 
 bool CreatureEvents::registerLuaEvent(CreatureEvent* event)
@@ -84,12 +90,20 @@ bool CreatureEvents::registerLuaEvent(CreatureEvent* event)
 		return false;
 	}
 
-	auto result = creatureEvents.emplace(creatureEvent->getName(), std::move(*creatureEvent));
-	if (!result.second) {
-		std::cout << "[Warning - CreatureEvents::registerEvent] Duplicate registered creature event with name: " << creatureEvent->getName() << std::endl;
-	}
+	CreatureEvent* oldEvent = getEventByName(creatureEvent->getName(), false);
+	if (oldEvent) {
+		//if there was an event with the same that is not loaded
+		//(happens when realoading), it is reused
+		if (!oldEvent->isLoaded() && oldEvent->getEventType() == creatureEvent->getEventType()) {
+			oldEvent->copyEvent(creatureEvent.get());
+		}
 
-	return result.second;
+		return false;
+	} else {
+		//if not, register it normally
+		creatureEvents.emplace(creatureEvent->getName(), std::move(*creatureEvent));
+		return true;
+	}
 }
 
 CreatureEvent* CreatureEvents::getEventByName(const std::string& name, bool forceLoaded /*= true*/)
