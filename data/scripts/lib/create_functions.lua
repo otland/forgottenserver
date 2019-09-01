8815326b0b59608c9d5671d782f8dc40b7c2d6d3
Ray <raymondtfr@hotmail.com>
2019-09-01 19:31:08 -0300

    Load spells library

--- data/scripts/lib/create_functions.lua
+++ data/scripts/lib/create_functions.lua
@@ -1,2 +1,4 @@
 createFunctions(MonsterType) -- creates get/set functions for MonsterType
 createFunctions(Spell) -- creates get/set functions for Spell
+
+dofile('data/scripts/spells/lib/spells.lua')
