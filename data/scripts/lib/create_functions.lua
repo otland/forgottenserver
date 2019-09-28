496683dcea3754ea48f6ec9065b778167a6d88cc
Ray <raymondtfr@hotmail.com>
2019-09-28 06:28:56 -0300

    Removed "dofile" from "create_functions.lua"

--- data/scripts/lib/create_functions.lua
+++ data/scripts/lib/create_functions.lua
@@ -1,4 +1,2 @@
 createFunctions(MonsterType) -- creates get/set functions for MonsterType
 createFunctions(Spell) -- creates get/set functions for Spell
-
-dofile('data/scripts/spells/lib/spells.lua')
