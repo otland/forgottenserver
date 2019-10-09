36416a43e617d7952b1c8d4117e95dcae9df1cbf
Luan Luciano <luanluciano@outlook.com>
2019-10-09 18:27:18 -0300

    Update serversave.lua

--- data/globalevents/scripts/serversave.lua
+++ data/globalevents/scripts/serversave.lua
@@ -16,17 +16,17 @@ local function serverSave()
 end
 
 local function secondServerSaveWarning()
-	Game.broadcastMessage("Server is saving game in one minute. Please logout.")
+	Game.broadcastMessage("Server is saving game in one minute. Please logout.", MESSAGE_STATUS_WARNING)
 	addEvent(serverSave, 60000)
 end
 
 local function firstServerSaveWarning()
-	Game.broadcastMessage("Server is saving game in 3 minutes. Please logout.")
+	Game.broadcastMessage("Server is saving game in 3 minutes. Please logout.", MESSAGE_STATUS_WARNING)
 	addEvent(secondServerSaveWarning, 120000)
 end
 
 function onTime(interval)
-	Game.broadcastMessage("Server is saving game in 5 minutes. Please logout.")
+	Game.broadcastMessage("Server is saving game in 5 minutes. Please logout.", MESSAGE_STATUS_WARNING)
 	Game.setGameState(GAME_STATE_STARTUP)
 	addEvent(firstServerSaveWarning, 120000)
 	return not shutdownAtServerSave
