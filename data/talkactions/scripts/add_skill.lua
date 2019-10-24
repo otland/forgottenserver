786325688119533db2f54b9b90914add97809a5c
Stefan A. Brannfjell <stefan_brannfjell@live.no>
2019-10-25 01:00:17 +0200

    Implements function string:splitTrimmed (#2688)
    And modifies two scripts that benefits from the additional trim

--- data/talkactions/scripts/add_skill.lua
+++ data/talkactions/scripts/add_skill.lua
@@ -30,7 +30,7 @@ function onSay(player, words, param)
 		return false
 	end
 
-	local split = param:split(",")
+	local split = param:splitTrimmed(",")
 	if not split[2] then
 		player:sendCancelMessage("Insufficient parameters.")
 		return false
@@ -41,9 +41,6 @@ function onSay(player, words, param)
 		player:sendCancelMessage("A player with that name is not online.")
 		return false
 	end
-
-	-- Trim left
-	split[2] = split[2]:gsub("^%s*(.-)$", "%1")
 
 	local count = 1
 	if split[3] then
