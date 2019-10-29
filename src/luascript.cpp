380903887a92d0abcf00158f37f59094d6340bfa
Evil Puncker <EPuncker@users.noreply.github.com>
2019-10-29 18:42:30 -0300

    Added shopping bag to modules (#2701)
    - Added shopping bag to NPC "buy with backpacks" function as of Tibia 10.55 update
    - Changed some commands to meta-table commands
    - Added a few items into enums for future use
    - Changed coins name
    - Added missing registerEnum ITEM_SHOPPING_BAG

--- src/luascript.cpp
+++ src/luascript.cpp
@@ -1502,6 +1502,7 @@ void LuaScriptInterface::registerFunctions()
 	registerEnum(ITEM_TYPE_RUNE)
 
 	registerEnum(ITEM_BAG)
+	registerEnum(ITEM_SHOPPING_BAG)
 	registerEnum(ITEM_GOLD_COIN)
 	registerEnum(ITEM_PLATINUM_COIN)
 	registerEnum(ITEM_CRYSTAL_COIN)
