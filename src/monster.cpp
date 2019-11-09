8e1998fe190725c06dda5925ce426c93727874d1
Znote <stefan_brannfjell@live.no>
2019-11-09 05:07:11 +0100

    Challenge creature stops monsters from running away.

--- src/monster.cpp
+++ src/monster.cpp
@@ -866,6 +866,14 @@ void Monster::onThinkTarget(uint32_t interval)
 		if (mType->info.changeTargetSpeed != 0) {
 			bool canChangeTarget = true;
 
+			if (targetExetaCooldown > 0) {
+				targetExetaCooldown -= interval;
+
+				if (targetExetaCooldown <= 0) {
+					targetExetaCooldown = 0;
+				}
+			}
+
 			if (targetChangeCooldown > 0) {
 				targetChangeCooldown -= interval;
 
@@ -883,6 +891,10 @@ void Monster::onThinkTarget(uint32_t interval)
 				if (targetChangeTicks >= mType->info.changeTargetSpeed) {
 					targetChangeTicks = 0;
 					targetChangeCooldown = mType->info.changeTargetSpeed;
+
+					if (targetExetaCooldown > 0) {
+						targetExetaCooldown = 0;
+					}
 
 					if (mType->info.changeTargetChance >= uniform_random(1, 100)) {
 						if (mType->info.targetDistance <= 1) {
@@ -1948,6 +1960,7 @@ bool Monster::challengeCreature(Creature* creature)
 	bool result = selectTarget(creature);
 	if (result) {
 		targetChangeCooldown = 8000;
+		targetExetaCooldown = targetChangeCooldown;
 		targetChangeTicks = 0;
 	}
 	return result;
