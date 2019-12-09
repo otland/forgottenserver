9972271e064678448e6012e4d1b482c9fe1f22f8
Sundance <19597322+Sundance@users.noreply.github.com>
2019-12-09 17:43:39 +0100

    cleanup

--- data/globalevents/scripts/sendworldlight.lua
+++ data/globalevents/scripts/sendworldlight.lua
@@ -2,10 +2,10 @@ local intensity, color = getWorldLight()
 function onThink(interval)
 	local newIntensity, newColor = getWorldLight()
 	if newIntensity ~= intensity or newColor ~= color then
-		for k,v in pairs(Game.getPlayers()) do
+		for k, v in pairs(Game.getPlayers()) do
 			v:sendWorldLight(intensity, color)
 		end
-		intensity,color = getWorldLight()
+		intensity, color = getWorldLight()
 	end
 	return true
 end
