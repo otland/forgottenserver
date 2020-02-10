7f8b401843340286018480cf8d1bb368d90089a4
Evil Puncker <EPuncker@users.noreply.github.com>
2020-02-10 20:48:07 -0300

    Added Very Noble-Looking Watch
    I don't know if this is how it should be implemented, but I guess so
    
    https://tibia.fandom.com/wiki/Very_Noble-Looking_Watch

--- data/actions/scripts/other/watch.lua
+++ data/actions/scripts/other/watch.lua
@@ -1,4 +1,9 @@
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	player:sendTextMessage(MESSAGE_INFO_DESCR, "The time is " .. getFormattedWorldTime() .. ".")
+	if item.itemid == 6092 then
+		item:transform(item.itemid-1)
+		item:decay()
+		return true
+	end
 	return true
 end
