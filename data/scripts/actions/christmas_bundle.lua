3ca2f811e96f717b0dd124df1544549c528e7856
Evil Puncker <EPuncker@users.noreply.github.com>
2020-02-22 11:03:40 -0300

    Added christmas bundle
    They were added on 7.9 update, script fixed by @streamside

---
+++ data/scripts/actions/christmas_bundle.lua
@@ -0,0 +1,43 @@
+local christmasBundle = Action()
+
+function christmasBundle.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	local presents = {
+		[6507] = { -- red christmas bundle
+			{6569, 15}, {2687, 20}, {2688, 10}, {2675, 10}, {2674, 5}, 6501, 6502, 6490, 6504, 6388
+		},
+		[6508] = { -- blue christmas bundle
+			{6569, 15}, {2687, 20}, {2688, 10}, {2675, 10}, {2674, 5}, 6501, 6502, 6490, 6505, 6388
+		},
+		[6509] = { -- green christmas bundle
+			{6569, 15}, {2687, 20}, {2688, 10}, {2675, 10}, {2674, 5}, 6501, 6502, 6490, 6503, 6388
+		}
+	}
+
+	local targetItem = presents[item.itemid]
+	if not targetItem then
+		return true
+	end
+
+	local rewards = {}
+	repeat
+		local count = 1
+		local rand = math.random(#targetItem)
+		local gift = targetItem[rand]
+		if type(gift) == "table" then
+			count = gift[2]
+			gift = gift[1]
+		end
+		rewards[#rewards + 1] = {gift, count}
+		table.remove(targetItem, rand)
+	until #rewards == 7
+
+	for i = 1, #rewards do
+		player:addItem(rewards[i][1], rewards[i][2])
+	end
+	item:remove(1)
+	fromPosition:sendMagicEffect(CONST_ME_GIFT_WRAPS)
+	return true
+end
+
+christmasBundle:id(6507, 6508, 6509)
+christmasBundle:register()
