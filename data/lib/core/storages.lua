f9738e6148f1f3d547a1156af68af04c09691306
Evil Puncker <EPuncker@users.noreply.github.com>
2020-03-06 16:50:59 -0300

    Updated missing storages
    followup to https://github.com/otland/forgottenserver/commit/437419d38ee9d24b19d779fc72c56ecd12d342b2

--- data/lib/core/storages.lua
+++ data/lib/core/storages.lua
@@ -1,8 +1,8 @@
 PlayerStorageKeys = {
-    annihilatorReward = 30015,
-    promotion = 30018,
-    delayLargeSeaShell = 30019,
-    firstRod = 30020,
+	annihilatorReward = 30015,
+	promotion = 30018,
+	delayLargeSeaShell = 30019,
+	firstRod = 30020,
 }
 
 GlobalStorageKeys = {
