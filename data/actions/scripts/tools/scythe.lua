6abee035577ff29eca2e78ee7153a03626726c68
Evil Puncker <EPuncker@users.noreply.github.com>
2020-03-08 00:08:56 -0300

    Added missing items

--- data/actions/scripts/tools/scythe.lua
+++ data/actions/scripts/tools/scythe.lua
@@ -5,5 +5,11 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		Game.createItem(2694, 1, toPosition)
 		return true
 	end
+	if target.itemid == 5465 then
+		target:transform(5464)
+		target:decay()
+		Game.createItem(5467, 1, toPosition)
+		return true
+	end
 	return destroyItem(player, target, toPosition)
 end
