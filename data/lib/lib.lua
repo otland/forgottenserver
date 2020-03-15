fd98c1d3c9c404e93485e63239b21634664032d5
Znote <stefan_brannfjell@live.no>
2020-03-15 23:01:28 +0100

    dump debug functions for debugging and Lua development

--- data/lib/lib.lua
+++ data/lib/lib.lua
@@ -3,3 +3,6 @@ dofile('data/lib/core/core.lua')
 
 -- Compatibility library for our old Lua API
 dofile('data/lib/compat/compat.lua')
+
+-- Debugging helper function for Lua developers
+dofile('data/lib/debugging/dump.lua')
