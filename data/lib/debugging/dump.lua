38aa9ef257a2b746bbc81de582c27d28d7b87ab3
Znote <stefan_brannfjell@live.no>
2020-03-16 00:20:07 +0100

    Review improvement

--- data/lib/debugging/dump.lua
+++ data/lib/debugging/dump.lua
@@ -1,34 +1,42 @@
 -- recursive dump function
 function dumpLevel(input, level)
-   local indent = ''
-   for i = 1, level do
-      indent = indent .. '    '
-   end
+	local indent = ''
+	
+	for i = 1, level do
+		indent = indent .. '    '
+	end
 
-   if type(input) == 'table' then
-      local str = '{ \n'
-      local lines = {}
-      for k,v in pairs(input) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         if type(v) == 'string' then v = '"'..v..'"' end
-         table.insert(lines, indent .. '    ['..k..'] = ' .. dumpLevel(v, level+1))
-      end
-      return str .. table.concat(lines, ',\n') .. '\n' .. indent .. '}'
-   end
+	if type(input) == 'table' then
+		local str = '{ \n'
+		local lines = {}
+		
+		for k, v in pairs(input) do
+			if type(k) ~= 'number' then 
+				k = '"' .. k .. '"' 
+			end
+			
+			if type(v) == 'string' then 
+				v = '"' .. v .. '"' 
+			end
+			
+			table.insert(lines, indent .. '    [' .. k .. '] = ' .. dumpLevel(v, level + 1))
+		end
+		return str .. table.concat(lines, ',\n') .. '\n' .. indent .. '}'
+	end
 
-   return tostring(input)
+	return tostring(input)
 end
 
 -- Return a string representation of input for debugging purposes
 function dump(input)
-   return dumpLevel(input,0)
+	return dumpLevel(input, 0)
 end
 
 -- Call the dump function and print it to console
 function pdump(input)
-   local dump_str = dump(input)
-   print(dump_str)
-   return dump_str
+	local dump_str = dump(input)
+	print(dump_str)
+	return dump_str
 end
 
 -- Call the dump function with a title and print it beautifully to the console
