1c5fe11ea95d8f065522ec840e3718517fb0a96f
Nekiro <nekiroboy@gmail.com>
2020-03-15 15:01:40 +0100

    Fixing the logic, teleports should pass flag_nolimit

--- src/tile.cpp
+++ src/tile.cpp
@@ -680,6 +680,10 @@ ReturnValue Tile::queryAdd(int32_t, const Thing& thing, uint32_t, uint32_t flags
 						continue;
 					}
 
+					if (!item->isPickupable()) {
+						return RETURNVALUE_NOTENOUGHROOM;
+					}
+
 					if (!iiType.hasHeight || iiType.pickupable || iiType.isBed()) {
 						return RETURNVALUE_NOTENOUGHROOM;
 					}
