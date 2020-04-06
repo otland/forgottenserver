42c57b28e715913b1155ca3070e2c1460256b01c
eduardodantas <eduardo.dantas@hotmail.com.br>
2020-04-06 15:57:38 -0300

    Globalevents to revscriptsys

--- data/scripts/globalevents/record.lua
+++ data/scripts/globalevents/record.lua
@@ -1,4 +1,8 @@
-function onRecord(current, old)
+local record = GlobalEvent("Player Record")
+
+function record.onRecord(current, old)
 	addEvent(broadcastMessage, 150, "New record: " .. current .. " players are logged in.", MESSAGE_STATUS_DEFAULT)
 	return true
 end
+
+record:register()
--- data/scripts/globalevents/startup.lua
+++ data/scripts/globalevents/startup.lua
@@ -1,4 +1,7 @@
-function onStartup()
+local startup = GlobalEvent("Startup")
+
+function startup.onStartup()
+	math.randomseed(os.mtime())
 
 	db.query("TRUNCATE TABLE `players_online`")
 	db.asyncQuery("DELETE FROM `guild_wars` WHERE `status` = 0")
@@ -10,8 +13,8 @@ function onStartup()
 	local resultId = db.storeQuery("SELECT * FROM `account_bans` WHERE `expires_at` != 0 AND `expires_at` <= " .. os.time())
 	if resultId ~= false then
 		repeat
-			local accountId = result.getNumber(resultId, "account_id")
-			db.asyncQuery("INSERT INTO `account_ban_history` (`account_id`, `reason`, `banned_at`, `expired_at`, `banned_by`) VALUES (" .. accountId .. ", " .. db.escapeString(result.getString(resultId, "reason")) .. ", " .. result.getNumber(resultId, "banned_at") .. ", " .. result.getNumber(resultId, "expires_at") .. ", " .. result.getNumber(resultId, "banned_by") .. ")")
+			local accountId = result.getDataInt(resultId, "account_id")
+			db.asyncQuery("INSERT INTO `account_ban_history` (`account_id`, `reason`, `banned_at`, `expired_at`, `banned_by`) VALUES (" .. accountId .. ", " .. db.escapeString(result.getDataString(resultId, "reason")) .. ", " .. result.getDataLong(resultId, "banned_at") .. ", " .. result.getDataLong(resultId, "expires_at") .. ", " .. result.getDataInt(resultId, "banned_by") .. ")")
 			db.asyncQuery("DELETE FROM `account_bans` WHERE `account_id` = " .. accountId)
 		until not result.next(resultId)
 		result.free(resultId)
@@ -21,11 +24,11 @@ function onStartup()
 	local resultId = db.storeQuery("SELECT `id`, `highest_bidder`, `last_bid`, (SELECT `balance` FROM `players` WHERE `players`.`id` = `highest_bidder`) AS `balance` FROM `houses` WHERE `owner` = 0 AND `bid_end` != 0 AND `bid_end` < " .. os.time())
 	if resultId ~= false then
 		repeat
-			local house = House(result.getNumber(resultId, "id"))
+			local house = House(result.getDataInt(resultId, "id"))
 			if house then
-				local highestBidder = result.getNumber(resultId, "highest_bidder")
-				local balance = result.getNumber(resultId, "balance")
-				local lastBid = result.getNumber(resultId, "last_bid")
+				local highestBidder = result.getDataInt(resultId, "highest_bidder")
+				local balance = result.getDataLong(resultId, "balance")
+				local lastBid = result.getDataInt(resultId, "last_bid")
 				if balance >= lastBid then
 					db.query("UPDATE `players` SET `balance` = " .. (balance - lastBid) .. " WHERE `id` = " .. highestBidder)
 					house:setOwnerGuid(highestBidder)
@@ -43,3 +46,5 @@ function onStartup()
 		db.query("INSERT INTO `towns` (`id`, `name`, `posx`, `posy`, `posz`) VALUES (" .. town:getId() .. ", " .. db.escapeString(town:getName()) .. ", " .. position.x .. ", " .. position.y .. ", " .. position.z .. ")")
 	end
 end
+
+startup:register()
