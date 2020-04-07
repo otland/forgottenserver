d2a9ee3527a0f636de2b9d6b4af69ebfaaad1680
Sarah Wesker <sarahelizabetwesker@gmail.com>
2020-04-07 01:47:16 -0400

    Update Container.createLootItem
    Con estos cambios podras aumentar el numero maximo de objetos apilables en un itemLoot

--- data/lib/core/container.lua
+++ data/lib/core/container.lua
@@ -10,38 +10,55 @@ function Container.createLootItem(self, item)
 	local itemCount = 0
 	local randvalue = getLootRandom()
 	if randvalue < item.chance then
-		if ItemType(item.itemId):isStackable() then
-			itemCount = randvalue % item.maxCount + 1
-		else
-			itemCount = 1
+		local maxCount = math.min(item.maxCount, 100)
+		local isStackable = ItemType(item.itemId):isStackable()
+		for i = 1, math.ceil(item.maxCount / 100) do
+			if isStackable then
+				itemCount = itemCount + (randvalue % maxCount + 1)
+				item.maxCount = item.maxCount - maxCount
+				maxCount = math.min(item.maxCount, 100)
+			else
+				itemCount = itemCount + 1
+			end
 		end
 	end
 
 	if itemCount > 0 then
-		local tmpItem = self:addItem(item.itemId, math.min(itemCount, 100))
-		if not tmpItem then
+		local tmpItem = {}
+		repeat
+			local addCount = math.min(itemCount, 100)
+			local itm = self:addItem(item.itemId, addCount)
+			if itm then
+				tmpItem[#tmpItem + 1] = itm
+			end
+			itemCount = itemCount - addCount
+		until itemCount < 1
+		
+		if #tmpItem == 0 then
 			return false
 		end
 
-		if tmpItem:isContainer() then
-			for i = 1, #item.childLoot do
-				if not tmpItem:createLootItem(item.childLoot[i]) then
-					tmpItem:remove()
-					return false
+		for index, tmp in pairs(tmpItem) do
+			if tmp:isContainer() then
+				for i = 1, #item.childLoot do
+					if not tmp:createLootItem(item.childLoot[i]) then
+						tmp:remove()
+						return false
+					end
 				end
 			end
-		end
 
-		if item.subType ~= -1 then
-			tmpItem:setAttribute(ITEM_ATTRIBUTE_CHARGES, item.subType)
-		end
+			if item.subType ~= -1 then
+				tmp:setAttribute(ITEM_ATTRIBUTE_CHARGES, item.subType)
+			end
 
-		if item.actionId ~= -1 then
-			tmpItem:setActionId(item.actionId)
-		end
+			if item.actionId ~= -1 then
+				tmp:setActionId(item.actionId)
+			end
 
-		if item.text and item.text ~= "" then
-			tmpItem:setText(item.text)
+			if item.text and item.text ~= "" then
+				tmp:setText(item.text)
+			end
 		end
 	end
 	return true
