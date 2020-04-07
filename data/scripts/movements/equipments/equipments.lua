d300d4069c47045a57f4736df7aac40927f2c689
eduardodantas <eduardo.dantas@hotmail.com.br>
2020-04-07 00:02:01 -0300

    Fixing loot names

---
+++ data/scripts/movements/equipments/equipments.lua
@@ -0,0 +1,227 @@
+MoveEvent {
+id = 813,
+slot = CONST_SLOT_FEET,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"sorcerer", "druid"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 818,
+slot = CONST_SLOT_FEET,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"sorcerer", "druid"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 819,
+slot = CONST_SLOT_FEET,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"sorcerer", "druid"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 820,
+slot = CONST_SLOT_FEET,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"sorcerer", "druid"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 827,
+slot = CONST_SLOT_HEAD,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"sorcerer", "druid"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 828,
+slot = CONST_SLOT_HEAD,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"sorcerer", "druid"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 829,
+slot = CONST_SLOT_HEAD,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"sorcerer", "druid"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 830,
+slot = CONST_SLOT_HEAD,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"sorcerer", "druid"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 3210,
+slot = CONST_SLOT_HEAD,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"sorcerer", "druid"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 7992,
+slot = CONST_SLOT_HEAD,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"sorcerer", "druid"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 9653,
+slot = CONST_SLOT_HEAD,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"sorcerer", "druid"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 11674,
+slot = CONST_SLOT_HEAD,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"sorcerer", "druid"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 12599,
+slot = CONST_SLOT_HEAD,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"sorcerer", "druid"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 13996,
+slot = CONST_SLOT_LEGS,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"sorcerer", "druid"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 19366,
+slot = CONST_SLOT_LEGS,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"sorcerer", "druid"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 3571,
+slot = CONST_SLOT_ARMOR,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"paladin"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 8044,
+slot = CONST_SLOT_ARMOR,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"paladin"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 8063,
+slot = CONST_SLOT_ARMOR,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"paladin"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
+
+MoveEvent {
+id = 8095,
+slot = CONST_SLOT_LEGS,
+level = 0,
+magicLevel = 0,
+premium = false,
+vocation = {"paladin"},
+onEquip = function (player, item, slot, isCheck)
+return RETURNVALUE_NOERROR
+end
+}
