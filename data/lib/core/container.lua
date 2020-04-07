639896ee3644ddf87690a6898bc04aad0b7191bf
Sarah Wesker <sarahelizabetwesker@gmail.com>
2020-04-07 15:12:55 -0400

    Container.createLootItem fix final

--- data/lib/core/container.lua
+++ data/lib/core/container.lua
@@ -2,71 +2,53 @@ function Container.isContainer(self)
 	return true
 end
 
-function Container.createLootItem(self, item)
+function Container:createLootItem(item, player, bonus)
 	if self:getEmptySlots() == 0 then
-		return true
+        return true
 	end
 
 	local itemCount = 0
-	local randvalue = getLootRandom()
-	if randvalue < item.chance then
-		local maxCount = math.min(item.maxCount, 100)
-		local isStackable = ItemType(item.itemId):isStackable()
+	local maxCount = math.min(item.maxCount, 100)
+	if ItemType(item.itemId):isStackable() then
 		local stacks = math.ceil(item.maxCount / 100)
 		for i = 1, stacks do
-			if isStackable then
+			local randvalue = getLootRandom()
+			if randvalue < item.chance then
 				itemCount = itemCount + (randvalue % maxCount + 1)
 				item.maxCount = item.maxCount - maxCount
 				maxCount = math.min(item.maxCount, 100)
-			else
-				itemCount = itemCount + 1
-			end
-			if stacks > 1 then
-				randvalue = getLootRandom()
-				if randvalue >= item.chance then
-					i = i + 1
-				end
 			end
 		end
+	elseif getLootRandom(player, bonus) < item.chance then
+		itemCount = 1
 	end
 
-	if itemCount > 0 then
-		local tmpItem = {}
-		repeat
-			local addCount = math.min(itemCount, 100)
-			local itm = self:addItem(item.itemId, addCount)
-			if itm then
-				tmpItem[#tmpItem + 1] = itm
-			end
-			itemCount = itemCount - addCount
-		until itemCount < 1
-		
-		if #tmpItem == 0 then
-			return false
-		end
-
-		for index, tmp in pairs(tmpItem) do
-			if tmp:isContainer() then
+	repeat
+		local addCount = math.min(itemCount, 100)
+		local itm = self:addItem(item.itemId, addCount)
+		if itm then
+			if itm:isContainer() then
 				for i = 1, #item.childLoot do
-					if not tmp:createLootItem(item.childLoot[i]) then
-						tmp:remove()
+					if not itm:createLootItem(item.childLoot[i]) then
+						itm:remove()
 						return false
 					end
 				end
 			end
 
 			if item.subType ~= -1 then
-				tmp:setAttribute(ITEM_ATTRIBUTE_CHARGES, item.subType)
+				itm:setAttribute(ITEM_ATTRIBUTE_CHARGES, item.subType)
 			end
 
 			if item.actionId ~= -1 then
-				tmp:setActionId(item.actionId)
+				itm:setActionId(item.actionId)
 			end
-
+			
 			if item.text and item.text ~= "" then
-				tmp:setText(item.text)
+				itm:setAttribute(ITEM_ATTRIBUTE_TEXT, item.text)
 			end
 		end
-	end
+		itemCount = itemCount - addCount
+	until itemCount < 1
 	return true
 end
