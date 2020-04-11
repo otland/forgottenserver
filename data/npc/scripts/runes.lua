24fe2b68eb2f2fc559fabf049c1b989490c457ac
Evil Puncker <EPuncker@users.noreply.github.com>
2020-04-10 22:08:10 -0300

    Update runes.lua

--- data/npc/scripts/runes.lua
+++ data/npc/scripts/runes.lua
@@ -7,7 +7,7 @@ function onCreatureDisappear(cid)           npcHandler:onCreatureDisappear(cid) 
 function onCreatureSay(cid, type, msg)      npcHandler:onCreatureSay(cid, type, msg)    end
 function onThink()                          npcHandler:onThink()                        end
 
-local voices = {text = "Runes, wands, rods, health and mana potions! Have a look!"}
+local voices = { {text = "Runes, wands, rods, health and mana potions! Have a look!"} }
 npcHandler:addModule(VoiceModule:new(voices))
 
 local shopModule = ShopModule:new()
