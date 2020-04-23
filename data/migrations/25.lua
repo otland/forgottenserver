d9fcafa88d117b43555ff26de5d73061d86e869a
infernumx <spaghetti90314@gmail.com>
2020-04-23 09:19:26 -0700

    Add database migration

--- data/migrations/25.lua
+++ data/migrations/25.lua
@@ -1,3 +1,5 @@
 function onUpdateDatabase()
-	return false
+	print('> Updating database to version 26 (Change storage values to 64bit signed integers)')
+	db.query('ALTER TABLE `player_storage` MODIFY COLUMN `value` bigint(20) NOT NULL DEFAULT 0')
+	return true
 end
