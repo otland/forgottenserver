4e4fa79cdcea99bd268a0844a0172971010c2d9f
Evil Puncker <EPuncker@users.noreply.github.com>
2020-04-28 14:01:20 -0300

    fixed indentation

--- data/scripts/actions/others/flower_pot.lua
+++ data/scripts/actions/others/flower_pot.lua
@@ -69,9 +69,9 @@ function flowerPot.onUse(player, item, fromPosition, target, toPosition, isHotke
 				else
 					target:transform(flower.after[i])
 				end
-			toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
-			player:say(flower.msg[i], TALKTYPE_MONSTER_SAY)
-			target:decay()
+				toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
+				player:say(flower.msg[i], TALKTYPE_MONSTER_SAY)
+				target:decay()
 			end
 		break
 		end
