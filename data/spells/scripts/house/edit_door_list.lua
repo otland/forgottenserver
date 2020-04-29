6cfe4d2928efb575349339b244d7c4487868a0e9
Sarah Wesker <sarahelizabetwesker@gmail.com>
2020-04-29 12:02:47 -0400

    Fix the problem #2983

--- data/spells/scripts/house/edit_door_list.lua
+++ data/spells/scripts/house/edit_door_list.lua
@@ -1,6 +1,8 @@
 function onCastSpell(creature, variant)
-	local house = creature:getTile():getHouse()
-	local doorId = house and house:getDoorIdByPosition(variant:getPosition())
+	local cPosition = creature:getPosition()
+	local house = Tile(cPosition):getHouse()
+	cPosition:getNextPosition(creature:getDirection())
+	local doorId = house and house:getDoorIdByPosition(cPosition)
 	if not doorId or not house:canEditAccessList(doorId, creature) then
 		creature:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
 		creature:getPosition():sendMagicEffect(CONST_ME_POFF)
