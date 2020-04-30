df4d63ced28287ad147fd88decf86e5902ee2f0f
Evil Puncker <EPuncker@users.noreply.github.com>
2020-04-30 19:16:10 -0300

    Added achievements system (#2847)

--- data/scripts/creaturescripts/gs_wyda_death.lua
+++ data/scripts/creaturescripts/gs_wyda_death.lua
@@ -2,6 +2,7 @@ local creatureevent = CreatureEvent("GiantSpiderWyda")
 
 function creatureevent.onDeath(creature, corpse, lasthitkiller, mostdamagekiller, lasthitunjustified, mostdamageunjustified)
 	creature:say("It seems this was just an illusion.", TALKTYPE_MONSTER_SAY)
+	creature:addAchievement("Someone's Bored")
 	return true
 end
 
