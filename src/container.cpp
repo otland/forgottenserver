77936646a69b5521ef16b8be030c0200babb7657
Zbizu <>
2020-05-03 18:49:08 +0200

    container->getItem()

--- src/container.cpp
+++ src/container.cpp
@@ -85,6 +85,11 @@ Container* Container::getParentContainer()
 	return thing->getContainer();
 }
 
+std::string Container::getName() const {
+	const ItemType& it = items[id];
+	return getNameDescription(it, this, -1, false);
+}
+
 bool Container::hasParent() const
 {
 	return getID() != ITEM_BROWSEFIELD && dynamic_cast<const Player*>(getParent()) == nullptr;
--- src/item.cpp
+++ src/item.cpp
@@ -1510,7 +1510,10 @@ std::string Item::getNameDescription(const ItemType& it, const Item* item /*= nu
 			s << name;
 		}
 	} else {
-		s << "an item of type " << it.id;
+		if (addArticle) {
+			s << "an ";
+		}
+		s << "item of type " << it.id;
 	}
 	return s.str();
 }
