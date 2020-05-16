9ac863e79df1a87996d2ea0f6af944fecef7191d
Lyuzera <imlyuzera@gmail.com>
2020-05-16 00:31:30 -0300

    improve lua code

--- data/stages.lua
+++ data/stages.lua
@@ -14,33 +14,20 @@ useLastStageLevel = false
 lastStageLevel = 0
 
 for _, stage in ipairs(experienceStages) do
-	local minlevel, maxlevel, multiplier
+	local minlevel = stage.minlevel or 1
+	local maxlevel = stage.maxlevel or 0
+	local multiplier = stage.multiplier or 1.0
 
-	if stage.minlevel then
-		minlevel = stage.minlevel
-	else
-		minlevel = 1
-	end
-
-	if stage.maxlevel then
-		maxlevel = stage.maxlevel
-	else
-		maxlevel = 0
+	if not stage.maxlevel then
 		lastStageLevel = minlevel
 		useLastStageLevel = true
 	end
 
-	if stage.multiplier then
-		multiplier = stage.multiplier
-	else
-		multiplier = 1.0
-	end
-
-	if useLastStageLevel then
-		stagesMap[lastStageLevel] = multiplier
-	else
+	if not useLastStageLevel then
 		for i = stage.minlevel, stage.maxlevel do
 			stagesMap[i] = stage.multiplier
 		end
+	else
+		stagesMap[lastStageLevel] = multiplier
 	end
 end
