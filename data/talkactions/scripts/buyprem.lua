788f11bde9eb93a0205203463030bde469c1a95d
Evil Puncker <EPuncker@users.noreply.github.com>
2020-05-20 22:02:59 -0300

    Added additional config

--- data/talkactions/scripts/buyprem.lua
+++ data/talkactions/scripts/buyprem.lua
@@ -1,20 +1,18 @@
-local config = {
-	days = 90,
-	maxDays = 365,
-	price = 10000
-}
-
 function onSay(player, words, param)
 	if configManager.getBoolean(configKeys.FREE_PREMIUM) then
 		return true
 	end
 
+	if not buyPremiumEnabled then
+		return false
+	end
+
 	if player:getPremiumDays() <= config.maxDays then
-		if player:removeMoney(config.price) then
-			player:addPremiumDays(config.days)
+		if player:removeTotalMoney(premiumPrice) then
+			player:addPremiumDays(premiumDays)
 			player:sendTextMessage(MESSAGE_INFO_DESCR, "You have bought " .. config.days .." days of premium account.")
 		else
-			player:sendCancelMessage("You don't have enough money, " .. config.maxDays .. " days premium account costs " .. config.price .. " gold coins.")
+			player:sendCancelMessage("You don't have enough money, " .. config.maxDays .. " days premium account costs " .. premiumPrice .. " gold coins.")
 			player:getPosition():sendMagicEffect(CONST_ME_POFF)
 		end
 	else
---
+++ script_config.lua
@@ -0,0 +1,18 @@
+--[[Attention: This file is used for settings that weren't necessary to be source coded, but to have them all in one place]]--
+--[[for the usual settings, use config.lua]]--
+
+-- House settings
+premiumToBuyHouse = true
+levelToBuyHouse = 8
+
+-- Buy premium talkaction
+buyPremiumEnabled = true
+premiumDays = 90
+premiumPrice = 10000
+premiumMaxDays = 365
+
+-- Others
+changeSexEnabled = true
+changeSexDaysCost = 3
+
+print(">> Loading additional script config")
