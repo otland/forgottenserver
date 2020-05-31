7fda0cd966b9c9a22601f9e35291498d4521d911
Marco Oliveira <marcomoa@hotmail.com>
2020-05-31 17:48:15 -0300

    Print duplicate storages
    I believe that this will not be added to the repository, but it can be useful for customs servers.
    
    Function taken from: https://github.com/opentibiabr/otservbr-global/pull/1158/commits
    
    Credits to the created: @andersonfaaria
    
    Co-Authored-By: Anderson Faria <andercfaria@hotmail.com>

--- data/lib/core/storages.lua
+++ data/lib/core/storages.lua
@@ -17,3 +17,31 @@ PlayerStorageKeys = {
 
 GlobalStorageKeys = {
 }
+
+-- Values extraction function
+local function extractValues(tab, ret)
+	if type(tab) == "number" then
+		table.insert(ret, tab)
+	else
+		for _, v in pairs(tab) do
+			extractValues(v, ret)
+		end
+	end
+end
+
+local benchmark = os.clock()
+local extraction = {}
+extractValues(PlayerStorageKeys, extraction)  -- Call function
+table.sort(extraction) -- Sort the table
+-- The choice of sorting is due to the fact that sorting is very cheap O (n log2 (n)) and then we can simply compare one by one the elements finding duplicates in O(n)
+
+-- Scroll through the extracted table for duplicates
+if #extraction > 1 then
+  	for i = 1, #extraction - 1 do
+    	if extraction[i] == extraction[i+1] then
+      		print("Duplicate storage value found: ".. extraction[i])
+		end
+	end
+
+	print(string.format("Processed in %.4f(s)", os.clock() - benchmark))
+end
