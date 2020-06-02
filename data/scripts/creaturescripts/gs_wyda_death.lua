6a30a79f5f560301faa8cbfc16ea4450d45c566d
Evil Puncker <EPuncker@users.noreply.github.com>
2020-06-02 14:14:47 -0300

    Fixed #3074

--- data/scripts/creaturescripts/gs_wyda_death.lua
+++ data/scripts/creaturescripts/gs_wyda_death.lua
@@ -2,7 +2,9 @@ local creatureevent = CreatureEvent("GiantSpiderWyda")
 
 function creatureevent.onDeath(creature, corpse, lasthitkiller, mostdamagekiller, lasthitunjustified, mostdamageunjustified)
 	creature:say("It seems this was just an illusion.", TALKTYPE_MONSTER_SAY)
-	creature:addAchievement("Someone's Bored")
+	if mostdamagekiller:isPlayer() then
+		mostdamagekiller:addAchievement("Someone's Bored")
+	end
 	return true
 end
 
