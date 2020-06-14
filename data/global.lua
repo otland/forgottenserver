1817386221f4ae0b0ce868d0d392db710805737a
Evil Puncker <EPuncker@users.noreply.github.com>
2020-06-14 17:21:28 -0300

    Update global.lua

--- data/global.lua
+++ data/global.lua
@@ -69,27 +69,98 @@ if not nextUseStaminaTime then
 	nextUseStaminaTime = {}
 end
 
-function capitalize(str)
-	return str:gsub("%a+", function(c) return c:sub(1, 1):upper() .. c:sub(2) end)
-end
+function getPlayerDatabaseInfo(name_or_guid)
+	local sql_where = ""
 
--- check if offline player exist
-function playerExists(name)
-	local query = db.storeQuery("SELECT `name` FROM `players` WHERE `name` = " .. db.escapeString(name))
-	if query then
-		result.free(query)
-		return true
+	if type(name_or_guid) == 'string' then
+		sql_where = "WHERE `p`.`name`=" .. db.escapeString(name_or_guid) .. ""
+	elseif type(name_or_guid) == 'number' then
+		sql_where = "WHERE `p`.`id`='" .. name_or_guid .. "'"
+	else
+		return false
 	end
-	return false
-end
 
--- return vocation ID of offline player by name
-function getPlayerVocationAux(name)
-	local query = db.storeQuery("SELECT `vocation` FROM `players` WHERE `name` = " .. db.escapeString(name))
+	local sql_query = [[
+		SELECT
+			`p`.`id` as `guid`,
+			`p`.`name`,
+			CASE WHEN `po`.`player_id` IS NULL
+				THEN 0
+				ELSE 1
+			END AS `online`,
+			`p`.`group_id`,
+			`p`.`level`,
+			`p`.`experience`,
+			`p`.`vocation`,
+			`p`.`maglevel`,
+			`p`.`skill_fist`,
+			`p`.`skill_club`,
+			`p`.`skill_sword`,
+			`p`.`skill_axe`,
+			`p`.`skill_dist`,
+			`p`.`skill_shielding`,
+			`p`.`skill_fishing`,
+			`p`.`town_id`,
+			`p`.`balance`,
+			`gm`.`guild_id`,
+			`gm`.`nick`,
+			`g`.`name` AS `guild_name`,
+			CASE WHEN `p`.`id` = `g`.`ownerid`
+				THEN 1
+				ELSE 0
+			END AS `is_leader`,
+			`gr`.`name` AS `rank_name`,
+			`gr`.`level` AS `rank_level`,
+			`h`.`id` AS `house_id`,
+			`h`.`name` AS `house_name`,
+			`h`.`town_id` AS `house_town`
+		FROM `players` AS `p`
+		LEFT JOIN `players_online` AS `po`
+			ON `p`.`id` = `po`.`player_id`
+		LEFT JOIN `guild_membership` AS `gm`
+			ON `p`.`id` = `gm`.`player_id`
+		LEFT JOIN `guilds` AS `g`
+			ON `gm`.`guild_id` = `g`.`id`
+		LEFT JOIN `guild_ranks` AS `gr`
+			ON `gm`.`rank_id` = `gr`.`id`
+		LEFT JOIN `houses` AS `h`
+			ON `p`.`id` = `h`.`owner`
+	]] .. sql_where
+
+	local query = db.storeQuery(sql_query)
 	if not query then
 		return false
 	end
-	local value = result.getNumber(query, "vocation")
+
+	local info = {
+		["guid"] = result.getNumber(query, "guid"),
+		["name"] = result.getString(query, "name"),
+		["online"] = result.getNumber(query, "online"),
+		["group_id"] = result.getNumber(query, "group_id"),
+		["level"] = result.getNumber(query, "level"),
+		["experience"] = result.getNumber(query, "experience"),
+		["vocation"] = result.getNumber(query, "vocation"),
+		["maglevel"] = result.getNumber(query, "maglevel"),
+		["skill_fist"] = result.getNumber(query, "skill_fist"),
+		["skill_club"] = result.getNumber(query, "skill_club"),
+		["skill_sword"] = result.getNumber(query, "skill_sword"),
+		["skill_axe"] = result.getNumber(query, "skill_axe"),
+		["skill_dist"] = result.getNumber(query, "skill_dist"),
+		["skill_shielding"] = result.getNumber(query, "skill_shielding"),
+		["skill_fishing"] = result.getNumber(query, "skill_fishing"),
+		["town_id"] = result.getNumber(query, "town_id"),
+		["balance"] = result.getNumber(query, "balance"),
+		["guild_id"] = result.getNumber(query, "guild_id"),
+		["nick"] = result.getString(query, "nick"),
+		["guild_name"] = result.getString(query, "guild_name"),
+		["is_leader"] = result.getNumber(query, "is_leader"),
+		["rank_name"] = result.getString(query, "rank_name"),
+		["rank_level"] = result.getNumber(query, "rank_level"),
+		["house_id"] = result.getNumber(query, "house_id"),
+		["house_name"] = result.getString(query, "house_name"),
+		["house_town"] = result.getNumber(query, "house_town")
+	}
+
 	result.free(query)
-	return value
+	return info
 end
