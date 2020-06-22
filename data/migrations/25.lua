33ceac0e248c1e58f03e0d692a418df190455245
Znote <stefan_brannfjell@live.no>
2020-06-22 09:53:52 +0200

    Uppercase unsigned in sql statements

--- data/migrations/25.lua
+++ data/migrations/25.lua
@@ -1,7 +1,7 @@
 function onUpdateDatabase()
 	print("> Updating database to version 26 (guildhalls, guild banks #2213)")
 	db.query("ALTER TABLE `houses` ADD `type` ENUM('HOUSE', 'GUILDHALL') NOT NULL DEFAULT 'HOUSE' AFTER `id`")
-	db.query("ALTER TABLE `guilds` ADD `balance` bigint(20) unsigned NOT NULL DEFAULT '0'")
+	db.query("ALTER TABLE `guilds` ADD `balance` bigint(20) UNSIGNED NOT NULL DEFAULT '0'")
 	db.query([[
 		CREATE TABLE IF NOT EXISTS `guild_transactions` (
 			`id` int(11) NOT NULL AUTO_INCREMENT,
@@ -9,7 +9,7 @@ function onUpdateDatabase()
 			`from_guild` int(11) DEFAULT NULL,
 			`player_id` int(11) DEFAULT NULL,
 			`type` ENUM('DEPOSIT', 'WITHDRAW') NOT NULL,
-			`balance` bigint(20) unsigned NOT NULL DEFAULT '0',
+			`balance` bigint(20) UNSIGNED NOT NULL DEFAULT '0',
 			`time` bigint(20) NOT NULL,
 			PRIMARY KEY (`id`),
 			FOREIGN KEY (`to_guild`) REFERENCES `guilds`(`id`) ON DELETE CASCADE,
