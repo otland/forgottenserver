6e50d8f6228f9d975f1c41f4893756507e0e4c03
Johncore <32520764+Johncorex@users.noreply.github.com>
2020-07-30 19:04:00 -0300

    fix paralyze rune correct
    will work correctly as needed

--- data/spells/scripts/attack/paralyze_rune.lua
+++ data/spells/scripts/attack/paralyze_rune.lua
@@ -1,13 +1,27 @@
-local combat = Combat()
-combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
+function onCastSpell(creature, var)
+	local combat = Combat()
+	combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
+	local condition = Condition(CONDITION_PARALYZE)
+	condition:setParameter(CONDITION_PARAM_TICKS, 20000)
 
-local condition = Condition(CONDITION_PARALYZE)
-condition:setParameter(CONDITION_PARAM_TICKS, 20000)
-condition:setFormula(-1, 80, -1, 80)
-combat:addCondition(condition)
+	if creature:isPlayer() then
+		local target = Creature(var.number)
+		if not target or not target:isPlayer() then
+			condition:setFormula(-0.55, 0, -0.75, 0)
+		else
+			local speed = target:getBaseSpeed() - 80
+			if (speed - 80) <= 0 then
+				speed = speed + 80
+			end
 
-function onCastSpell(creature, variant, isHotkey)
-	if not combat:execute(creature, variant) then
+			condition:setParameter(CONDITION_PARAM_SPEED, -speed)
+		end
+	else
+		condition:setFormula(-0.55, 0, -0.75, 0)
+	end
+
+	combat:addCondition(condition)
+	if not combat:execute(creature, var) then
 		return false
 	end
 
