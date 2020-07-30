877397cd9debf11c014f0cd1680b13d406037149
Kairion <github@kairion.eu.org>
2020-07-30 03:48:25 -0300

    Removed CanTalkRedChannelAnonymous from const.h
    Removed deprecated player flag CanTalkRedChannelAnonymous.

--- src/const.h
+++ src/const.h
@@ -542,7 +542,6 @@ enum PlayerFlags : uint64_t {
 	PlayerFlag_SetMaxSpeed = 1 << 29,
 	PlayerFlag_SpecialVIP = 1 << 30,
 	PlayerFlag_NotGenerateLoot = static_cast<uint64_t>(1) << 31,
-	PlayerFlag_CanTalkRedChannelAnonymous = static_cast<uint64_t>(1) << 32,
 	PlayerFlag_IgnoreProtectionZone = static_cast<uint64_t>(1) << 33,
 	PlayerFlag_IgnoreSpellCheck = static_cast<uint64_t>(1) << 34,
 	PlayerFlag_IgnoreWeaponCheck = static_cast<uint64_t>(1) << 35,
