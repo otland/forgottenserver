dbcd023680ad6ae9946d5a0dd74e25a015cd15bf
Sarah Wesker <sarahelizabetwesker@gmail.com>
2020-08-15 14:24:36 -0400

    Update talkaction.cpp

--- src/talkaction.cpp
+++ src/talkaction.cpp
@@ -109,7 +109,7 @@ TalkActionResult_t TalkActions::playerSaySpell(Player* player, SpeakClasses type
 				}
 			}
 
-			if (it->second.executeSay(player, param, type)) {
+			if (it->second.executeSay(player, talkactionWords, param, type)) {
 				return TALKACTION_CONTINUE;
 			} else {
 				return TALKACTION_BREAK;
