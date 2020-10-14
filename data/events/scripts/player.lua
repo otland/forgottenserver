0c9ad59f7dd60d676fdb3de139b4948468bc7306
Marco Oliveira <marcomoa@hotmail.com>
2020-10-14 12:55:49 -0300

    Walkthrough everything Ctrl + Arrow keys
    * Adding an old functionality present in tfs 0.4 that was later removed.
    * I did tests and it seems all right, I didn't find any problem when using it.

--- data/events/scripts/player.lua
+++ data/events/scripts/player.lua
@@ -200,6 +200,12 @@ function Player:onReportBug(message, position, category)
 end
 
 function Player:onTurn(direction)
+	if self:getGroup():getAccess() and self:getDirection() == direction then
+		local nextPosition = self:getPosition()
+		nextPosition:getNextPosition(direction)
+
+		self:teleportTo(nextPosition, true)
+    end
 	return true
 end
 
