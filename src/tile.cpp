7357e91edcb250ba1012d67b52b6518af134f015
Sam <TheSumm@users.noreply.github.com>
2020-10-17 13:50:20 +0200

    Fix crash related to map cleaning (#3182)

--- src/tile.cpp
+++ src/tile.cpp
@@ -401,7 +401,7 @@ void Tile::onAddTileItem(Item* item)
 		spectator->onAddTileItem(this, cylinderMapPos);
 	}
 
-	if ((!hasFlag(TILESTATE_PROTECTIONZONE) || (g_config.getBoolean(ConfigManager::CLEAN_PROTECTION_ZONES) && hasFlag(TILESTATE_PROTECTIONZONE))) && item->isCleanable()) {
+	if ((!hasFlag(TILESTATE_PROTECTIONZONE) || g_config.getBoolean(ConfigManager::CLEAN_PROTECTION_ZONES)) && item->isCleanable()) {
 		if (!dynamic_cast<HouseTile*>(this)) {
 			g_game.addTileToClean(this);
 		}
@@ -473,15 +473,15 @@ void Tile::onRemoveTileItem(const SpectatorVec& spectators, const std::vector<in
 		spectator->onRemoveTileItem(this, cylinderMapPos, iType, item);
 	}
 
-	if (!hasFlag(TILESTATE_PROTECTIONZONE) || (g_config.getBoolean(ConfigManager::CLEAN_PROTECTION_ZONES) && hasFlag(TILESTATE_PROTECTIONZONE))) {
-		auto it = getItemList();
-		if (it->empty()) {
+	if (!hasFlag(TILESTATE_PROTECTIONZONE) || g_config.getBoolean(ConfigManager::CLEAN_PROTECTION_ZONES)) {
+		auto items = getItemList();
+		if (!items || items->empty()) {
 			g_game.removeTileToClean(this);
 			return;
 		}
 
 		bool ret = false;
-		for (auto toCheck : *it) {
+		for (auto toCheck : *items) {
 			if (toCheck->isCleanable()) {
 				ret = true;
 				break;
