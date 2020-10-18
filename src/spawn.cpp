118986696cd8f575d8c3c7a89dc4e04172902d1d
Evil Puncker <EPuncker@users.noreply.github.com>
2020-10-18 19:35:53 -0300

    Update spawn.cpp

--- src/spawn.cpp
+++ src/spawn.cpp
@@ -33,8 +33,8 @@ extern Monsters g_monsters;
 extern Game g_game;
 extern Events* g_events;
 
-static constexpr int32_t MINSPAWN_INTERVAL = 10000; // 10 seconds to match RME
-static constexpr int32_t MAXSPAWN_INTERVAL = 86400000; // 1 day
+static constexpr int32_t MINSPAWN_INTERVAL = 10 * 1000; // 10 seconds to match RME
+static constexpr int32_t MAXSPAWN_INTERVAL = 24 * 60 * 60 * 1000; // 1 day
 
 bool Spawns::loadFromXml(const std::string& filename)
 {
