00b6a4af50b25c351717e451e308539f43a20d33
Evil Puncker <EPuncker@users.noreply.github.com>
2020-11-30 22:43:12 -0300

    config: Make player walkthrough optional (#3230)

--- src/player.cpp
+++ src/player.cpp
@@ -705,7 +705,7 @@ bool Player::canWalkthrough(const Creature* creature) const
 	}
 
 	const Player* player = creature->getPlayer();
-	if (!player) {
+	if (!player || !g_config.getBoolean(ConfigManager::ALLOW_WALKTHROUGH)) {
 		return false;
 	}
 
@@ -741,7 +741,7 @@ bool Player::canWalkthroughEx(const Creature* creature) const
 	}
 
 	const Player* player = creature->getPlayer();
-	if (!player) {
+	if (!player || !g_config.getBoolean(ConfigManager::ALLOW_WALKTHROUGH)) {
 		return false;
 	}
 
