8453704e419327fe555bd83bfba99663dd11bbcd
Evil Puncker <EPuncker@users.noreply.github.com>
2020-12-27 16:51:08 -0300

    migration file

--- data/migrations/27.lua
+++ data/migrations/27.lua
@@ -1,3 +1,5 @@
 function onUpdateDatabase()
-	return false
+	print("> Updating database to version 27 (manaspent to bigint)")
+	db.query("ALTER TABLE `players` CHANGE `manaspent` `manaspent` bigint unsigned NOT NULL DEFAULT '0'");
+	return true
 end
