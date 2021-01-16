4165ab4b1aef09971518f64df5828c18e2939a9c
Ranieri Althoff <1993083+ranisalt@users.noreply.github.com>
2021-01-16 23:49:10 +0000

    Remove useless comment

--- src/luascript.h
+++ src/luascript.h
@@ -446,8 +446,6 @@ class LuaScriptInterface
 		static int luaGetWorldTime(lua_State* L);
 		static int luaGetWorldUpTime(lua_State* L);
 		static int luaGetWorldLight(lua_State* L);
-
-		//set world info
 		static int luaSetWorldLight(lua_State* L);
 
 		//type validation
