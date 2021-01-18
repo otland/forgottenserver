bd1c1ed3fe137b44b09c627d8a897a31a2c87e79
gigastar <nheinz1995@live.com>
2021-01-18 13:18:39 -0800

    Update map.cpp

--- src/map.cpp
+++ src/map.cpp
@@ -16,7 +16,6 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-x
 #include "otpch.h"
 
 #include "iomap.h"
