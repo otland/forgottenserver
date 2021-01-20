5a85d84f319f73b8428abfe91c2c601262c44c9d
nekiro <nekiroboy@gmail.com>
2021-01-20 17:43:30 +0100

    Missing const

--- src/luascript.cpp
+++ src/luascript.cpp
@@ -7717,7 +7717,7 @@ int LuaScriptInterface::luaCreatureGetDamageMap(lua_State* L)
 	}
 
 	lua_createtable(L, creature->damageMap.size(), 0);
-	for (auto& damageEntry : creature->damageMap) {
+	for (const auto& damageEntry : creature->damageMap) {
 		lua_createtable(L, 0, 2);
 		setField(L, "total", damageEntry.second.total);
 		setField(L, "ticks", damageEntry.second.ticks);
