003de509e4e397dcbeb97db256f3cee4c6a1dbda
Evil Puncker <EPuncker@users.noreply.github.com>
2021-03-09 21:27:03 -0300

    Decay time random
    closes #3148
    
    Co-Authored-By: Sarah Wesker <sarahelizabetwesker@gmail.com>

--- src/luascript.cpp
+++ src/luascript.cpp
@@ -2702,6 +2702,9 @@ void LuaScriptInterface::registerFunctions()
 
 	registerMethod("ItemType", "isStoreItem", LuaScriptInterface::luaItemTypeIsStoreItem);
 
+	registerMethod("ItemType", "getDefaultDurationMin", LuaScriptInterface::luaItemTypeGetDefaultDurationMin);
+	registerMethod("ItemType", "getDefaultDurationMax", LuaScriptInterface::luaItemTypeGetDefaultDurationMax);
+
 	// Combat
 	registerClass("Combat", "", LuaScriptInterface::luaCombatCreate);
 	registerMetaMethod("Combat", "__eq", LuaScriptInterface::luaUserdataCompare);
@@ -12187,6 +12190,30 @@ int LuaScriptInterface::luaItemTypeIsStoreItem(lua_State* L)
 	const ItemType* itemType = getUserdata<const ItemType>(L, 1);
 	if (itemType) {
 		pushBoolean(L, itemType->storeItem);
+	} else {
+		lua_pushnil(L);
+	}
+	return 1;
+}
+
+int LuaScriptInterface::luaItemTypeIsStoreItem(lua_State* L)
+{
+	// itemType:isStoreItem()
+	const ItemType* itemType = getUserdata<const ItemType>(L, 1);
+	if (itemType) {
+		pushBoolean(L, itemType->storeItem);
+	} else {
+		lua_pushnil(L);
+	}
+	return 1;
+}
+
+int LuaScriptInterface::luaItemTypeGetDefaultDurationMin(lua_State* L)
+{
+	// itemType:getDefaultDurationMin()
+	const ItemType* itemType = getUserdata<const ItemType>(L, 1);
+	if (itemType) {
+		lua_pushnumber(L, itemType->decayTime);
 	} else {
 		lua_pushnil(L);
 	}
--- src/luascript.h
+++ src/luascript.h
@@ -1205,6 +1205,9 @@ class LuaScriptInterface
 
 		static int luaItemTypeIsStoreItem(lua_State* L);
 
+		static int luaItemTypeGetDefaultDurationMin(lua_State* L);
+		static int luaItemTypeGetDefaultDurationMax(lua_State* L);
+
 		// Combat
 		static int luaCombatCreate(lua_State* L);
 
