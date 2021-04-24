4a12cd9bc92cf87eb2e814887432c0842f82078e
Emil Bergström <emil92b@gmail.com>
2021-04-24 09:59:04 +0200

    trap fix

--- data/movements/scripts/trap.lua
+++ data/movements/scripts/trap.lua
@@ -27,7 +27,10 @@ function onStepIn(creature, item, position, fromPosition)
 		return true
 	end
 
-	if creature:isMonster() or creature:isPlayer() then
+	local master = creature:getMaster()
+	if creature:isPlayer() or creature:isNpc() or (master and master:isPlayer()) then
+		position:sendMagicEffect(CONST_ME_POFF)
+	else
 		doTargetCombat(0, creature, trap.type or COMBAT_PHYSICALDAMAGE, trap.damage[1], trap.damage[2], CONST_ME_NONE, true, false, false)
 	end
 
@@ -39,14 +42,5 @@ end
 
 function onStepOut(creature, item, position, fromPosition)
 	item:transform(item.itemid - 1)
-	return true
-end
-
-function onRemoveItem(item, tile, position)
-	local itemPosition = item:getPosition()
-	if itemPosition:getDistance(position) > 0 then
-		item:transform(item.itemid - 1)
-		itemPosition:sendMagicEffect(CONST_ME_POFF)
-	end
 	return true
 end
