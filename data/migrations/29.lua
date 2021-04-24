70a45420c6069ec389b4d08c3e631fb086bf650b
Emil Bergström <emil92b@gmail.com>
2021-04-24 10:01:44 +0200

    db default outfit colors

--- data/migrations/29.lua
+++ data/migrations/29.lua
@@ -1,3 +1,9 @@
 function onUpdateDatabase()
-	return false
+	print("> Updating database to version 29 (setting default outfit colors)")
+	db.query("ALTER TABLE `players` CHANGE `lookbody` `lookbody` int NOT NULL DEFAULT '69'")
+	db.query("ALTER TABLE `players` CHANGE `lookfeet` `lookfeet` int NOT NULL DEFAULT '76'")
+	db.query("ALTER TABLE `players` CHANGE `lookhead` `lookhead` int NOT NULL DEFAULT '78'")
+	db.query("ALTER TABLE `players` CHANGE `looklegs` `looklegs` int NOT NULL DEFAULT '58'")
+	db.query("ALTER TABLE `players` CHANGE `looktype` `looktype` int NOT NULL DEFAULT '128'")
+	return true
 end
