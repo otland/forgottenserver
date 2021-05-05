79288f32503e6d759cfc32330fe3a55f3fab803e
Ramon Bernardo <ramonbernardo.contato@gmail.com>
2021-05-05 10:28:46 -0300

    Use bed without house

--- src/actions.cpp
+++ src/actions.cpp
@@ -371,6 +371,10 @@ ReturnValue Actions::internalUseItem(Player* player, const Position& pos, uint8_
 
 	if (BedItem* bed = item->getBed()) {
 		if (!bed->canUse(player)) {
+			if (!bed->getHouse()) {
+				return RETURNVALUE_YOUCANNOTUSETHISBED;
+			}
+
 			if (!player->isPremium()) {
 				return RETURNVALUE_YOUNEEDPREMIUMACCOUNT;
 			}
@@ -443,6 +447,11 @@ bool Actions::useItem(Player* player, const Position& pos, uint8_t index, Item* 
 	}
 
 	ReturnValue ret = internalUseItem(player, pos, index, item, isHotkey);
+	if (ret == RETURNVALUE_YOUCANNOTUSETHISBED) {
+		g_game.internalCreatureSay(player, TALKTYPE_MONSTER_SAY, getReturnMessage(ret), false);
+		return false;
+	}
+
 	if (ret != RETURNVALUE_NOERROR) {
 		player->sendCancelMessage(ret);
 		return false;
