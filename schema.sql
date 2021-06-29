5c531cbbcb4acbdd99647b64428f1fbdbfeee83d
amatria <i.amatria@udc.es>
2021-06-29 23:54:58 +0200

    change tabs for spaces to be consistent with the codebase

--- schema.sql
+++ schema.sql
@@ -97,11 +97,11 @@ CREATE TABLE IF NOT EXISTS `account_ban_history` (
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
 CREATE TABLE IF NOT EXISTS `account_storage` (
-	`account_id` int NOT NULL,
-	`key` int unsigned NOT NULL,
-	`value` int NOT NULL,
-	PRIMARY KEY (`account_id`, `key`),
-	FOREIGN KEY (`account_id`) REFERENCES `accounts`(`id`) ON DELETE CASCADE
+  `account_id` int NOT NULL,
+  `key` int unsigned NOT NULL,
+  `value` int NOT NULL,
+  PRIMARY KEY (`account_id`, `key`),
+  FOREIGN KEY (`account_id`) REFERENCES `accounts`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
 CREATE TABLE IF NOT EXISTS `ip_bans` (
