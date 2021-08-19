20abe3859c9c5d99a88a65bd345edfcd9f6abd78
Erza <exposedmail@protonmail.ch>
2021-08-19 22:40:11 +0200

    Add missing method definition

--- src/luascript.h
+++ src/luascript.h
@@ -547,6 +547,7 @@ class LuaScriptInterface
 		static int luaGameLoadMap(lua_State* L);
 
 		static int luaGameGetExperienceStage(lua_State* L);
+		static int luaGameGetExperienceForLevel(lua_State* L);
 		static int luaGameGetMonsterCount(lua_State* L);
 		static int luaGameGetPlayerCount(lua_State* L);
 		static int luaGameGetNpcCount(lua_State* L);
