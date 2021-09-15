fa9e9e30d0ed16c3f12ad656c687750a102ce705
Evil Puncker <EPuncker@users.noreply.github.com>
2021-09-15 19:33:09 -0300

    Fixed poison field damage (#3653)
    fixes #3642
    
    Co-Authored-By: Zbizu <Zbizu@users.noreply.github.com>

--- src/items.cpp
+++ src/items.cpp
@@ -1240,8 +1240,8 @@ void Items::parseItemNode(const pugi::xml_node& itemNode, uint16_t id)
 						// initDamage = -1 (undefined, override initDamage with damage)
 						if (initDamage > 0 || initDamage < -1) {
 							conditionDamage->setInitDamage(-initDamage);
-						} else if (initDamage == -1 && damage != 0) {
-							conditionDamage->setInitDamage(damage);
+						} else if (initDamage == -1 && start != 0) {
+							conditionDamage->setInitDamage(start);
 						}
 
 						conditionDamage->setParam(CONDITION_PARAM_FIELD, 1);
