a4eb272a0642fbbdff412ea7f4b44c9e6669aa1e
Zbizu <zbirox@gmail.com>
2021-09-22 16:13:32 +0200

    fixed levitate and parcel walking accuracy

--- data/spells/scripts/support/levitate.lua
+++ data/spells/scripts/support/levitate.lua
@@ -6,10 +6,10 @@ local function levitate(creature, parameter)
 		toPosition:getNextPosition(creature:getDirection())
 
 		local tile = Tile(parameter == "up" and Position(fromPosition.x, fromPosition.y, fromPosition.z - 1) or toPosition)
-		if not tile or not tile:getGround() and not tile:hasFlag(parameter == "up" and TILESTATE_IMMOVABLEBLOCKSOLID or TILESTATE_BLOCKSOLID) then
+		if not tile or not tile:getGround() and not tile:hasFlag(TILESTATE_IMMOVABLEBLOCKSOLID) then
 			tile = Tile(toPosition.x, toPosition.y, toPosition.z + (parameter == "up" and -1 or 1))
 
-			if tile and tile:getGround() and not tile:hasFlag(bit.bor(TILESTATE_IMMOVABLEBLOCKSOLID, TILESTATE_FLOORCHANGE)) then
+			if tile and tile:getGround() and not tile:hasFlag(ILESTATE_IMMOVABLEBLOCKSOLID) then
 				return creature:move(tile, bit.bor(FLAG_IGNOREBLOCKITEM, FLAG_IGNOREBLOCKCREATURE))
 			end
 		end
