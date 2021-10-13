9aeb1a7a8fc44f6badd68d40eeba36c8b4d80700
Ramon Bernardo <ramonbernardo.contato@gmail.com>
2021-10-13 19:54:19 -0300

    Fix reload log order (#3728)

--- src/signals.cpp
+++ src/signals.cpp
@@ -103,10 +103,10 @@ void sighupHandler()
 	g_game.raids.startup();
 	std::cout << "Reloaded raids." << std::endl;
 
-	g_spells->reload();
+	g_monsters.reload();
 	std::cout << "Reloaded monsters." << std::endl;
 
-	g_monsters.reload();
+	g_spells->reload();
 	std::cout << "Reloaded spells." << std::endl;
 
 	g_talkActions->reload();
