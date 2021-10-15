7e0d616fa251355a3680f244f76dbc2fbefbdecb
Ramon Bernardo <ramonbernardo.contato@gmail.com>
2021-10-15 00:46:07 -0300

    Fix Container::updateItemWeight stackoverflow crash

--- src/container.cpp
+++ src/container.cpp
@@ -149,8 +149,9 @@ bool Container::unserializeItemNode(OTB::Loader& loader, const OTB::Node& node, 
 void Container::updateItemWeight(int32_t diff)
 {
 	totalWeight += diff;
-	if (Container* parentContainer = getParentContainer()) {
-		parentContainer->updateItemWeight(diff);
+	Container* parentContainer = this;
+	while ((parentContainer = parentContainer->getParentContainer()) != nullptr) {
+		parentContainer->totalWeight += diff;
 	}
 }
 
