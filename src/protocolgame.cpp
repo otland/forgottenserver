88758ad0b57db5be91d47cae845f7c751a0b5418
Zbizu <zbirox@gmail.com>
2021-10-22 21:34:08 +0200

    cosmetic fix for capacity display for gods

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -3339,7 +3339,7 @@ void ProtocolGame::AddPlayerStats(NetworkMessage& msg)
 	msg.add<uint16_t>(std::min<int32_t>(player->getHealth(), std::numeric_limits<uint16_t>::max()));
 	msg.add<uint16_t>(std::min<int32_t>(player->getMaxHealth(), std::numeric_limits<uint16_t>::max()));
 
-	msg.add<uint32_t>(player->getFreeCapacity());
+	msg.add<uint32_t>(player->hasFlag(PlayerFlag_HasInfiniteCapacity) ? 1 : player->getFreeCapacity());
 	msg.add<uint64_t>(player->getExperience());
 
 	msg.add<uint16_t>(player->getLevel());
@@ -3390,8 +3390,8 @@ void ProtocolGame::AddPlayerSkills(NetworkMessage& msg)
 	}
 
 	// to do: bonus cap
-	msg.add<uint32_t>(player->getCapacity()); // base + bonus capacity
-	msg.add<uint32_t>(player->getCapacity()); // base capacity
+	msg.add<uint32_t>(player->hasFlag(PlayerFlag_HasInfiniteCapacity) ? 1 : player->getCapacity()); // base + bonus capacity
+	msg.add<uint32_t>(player->hasFlag(PlayerFlag_HasInfiniteCapacity) ? 1 : player->getCapacity()); // base capacity
 }
 
 void ProtocolGame::AddOutfit(NetworkMessage& msg, const Outfit_t& outfit)
