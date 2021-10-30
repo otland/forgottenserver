4d58d7edb427b8b804b298457232aea9e0a4726b
Diego-OT <52421038+Diego-OT@users.noreply.github.com>
2021-10-30 05:33:25 -0300

    fix potential crash (#3732)

--- src/luascript.cpp
+++ src/luascript.cpp
@@ -88,7 +88,7 @@ void ScriptEnvironment::resetEnv()
 	auto it = pair.first;
 	while (it != pair.second) {
 		Item* item = it->second;
-		if (item->getParent() == VirtualCylinder::virtualCylinder) {
+		if (item && item->getParent() == VirtualCylinder::virtualCylinder) {
 			g_game.ReleaseItem(item);
 		}
 		it = tempItems.erase(it);
