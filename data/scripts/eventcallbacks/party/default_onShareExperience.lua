dc1fa30b8f4555773a337f20a83585f739fab83b
Ramon Bernardo <ramonbernardo.contato@gmail.com>
2021-11-06 19:28:54 -0300

    Tab indent

--- data/scripts/eventcallbacks/party/default_onShareExperience.lua
+++ data/scripts/eventcallbacks/party/default_onShareExperience.lua
@@ -7,19 +7,19 @@ ec.onShareExperience = function(self, exp)
 
 	local vocationId = self:getLeader():getVocation():getBase():getId()
 	if vocationId ~= VOCATION_NONE then
-	table.insert(vocationsIds, vocationId)
+		table.insert(vocationsIds, vocationId)
 	end
 
 	for _, member in ipairs(self:getMembers()) do
-	vocationId = member:getVocation():getBase():getId()
-	if not table.contains(vocationsIds, vocationId) and vocationId ~= VOCATION_NONE then
-	table.insert(vocationsIds, vocationId)
-	end
+		vocationId = member:getVocation():getBase():getId()
+		if not table.contains(vocationsIds, vocationId) and vocationId ~= VOCATION_NONE then
+			table.insert(vocationsIds, vocationId)
+		end
 	end
 
 	local size = #vocationsIds
 	if size > 1 then
-	sharedExperienceMultiplier = 1.0 + ((size * (5 * (size - 1) + 10)) / 100)
+		sharedExperienceMultiplier = 1.0 + ((size * (5 * (size - 1) + 10)) / 100)
 	end
 
 	exp = math.ceil((exp * sharedExperienceMultiplier) / (#self:getMembers() + 1))
