71a8d535a2182e68fa34816290fdbbb3174f21c7
Ramon Bernardo <ramonbernardo.contato@gmail.com>
2021-11-06 18:04:43 -0300

    Move onShareExperience callback from party event

--- data/events/scripts/party.lua
+++ data/events/scripts/party.lua
@@ -23,27 +23,5 @@ function Party:onDisband()
 end
 
 function Party:onShareExperience(exp)
-	local sharedExperienceMultiplier = 1.20 --20%
-	local vocationsIds = {}
-	local rawExp = exp
-
-	local vocationId = self:getLeader():getVocation():getBase():getId()
-	if vocationId ~= VOCATION_NONE then
-		table.insert(vocationsIds, vocationId)
-	end
-
-	for _, member in ipairs(self:getMembers()) do
-		vocationId = member:getVocation():getBase():getId()
-		if not table.contains(vocationsIds, vocationId) and vocationId ~= VOCATION_NONE then
-			table.insert(vocationsIds, vocationId)
-		end
-	end
-
-	local size = #vocationsIds
-	if size > 1 then
-		sharedExperienceMultiplier = 1.0 + ((size * (5 * (size - 1) + 10)) / 100)
-	end
-
-	exp = math.ceil((exp * sharedExperienceMultiplier) / (#self:getMembers() + 1))
 	return hasEventCallback(EVENT_CALLBACK_ONSHAREEXPERIENCE) and EventCallback(EVENT_CALLBACK_ONSHAREEXPERIENCE, self, exp, rawExp) or exp
 end
---
+++ data/scripts/eventcallbacks/party/default_onShareExperience.lua
@@ -0,0 +1,29 @@
+local ec = EventCallback
+
+ec.onShareExperience = function(self, exp)
+	local sharedExperienceMultiplier = 1.20 --20%
+	local vocationsIds = {}
+	local rawExp = exp
+
+	local vocationId = self:getLeader():getVocation():getBase():getId()
+	if vocationId ~= VOCATION_NONE then
+	table.insert(vocationsIds, vocationId)
+	end
+
+	for _, member in ipairs(self:getMembers()) do
+	vocationId = member:getVocation():getBase():getId()
+	if not table.contains(vocationsIds, vocationId) and vocationId ~= VOCATION_NONE then
+	table.insert(vocationsIds, vocationId)
+	end
+	end
+
+	local size = #vocationsIds
+	if size > 1 then
+	sharedExperienceMultiplier = 1.0 + ((size * (5 * (size - 1) + 10)) / 100)
+	end
+
+	exp = math.ceil((exp * sharedExperienceMultiplier) / (#self:getMembers() + 1))
+	return exp
+end
+
+ec:register()
