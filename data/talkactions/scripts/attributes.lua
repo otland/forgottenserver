bdede9f68e9afdad5f922f6992d42334fe05c3f1
Evil Puncker <EPuncker@users.noreply.github.com>
2021-11-19 23:11:54 -0300

    Removed all uses of MESSAGE_STATUS_CONSOLE_BLUE
    It is not present in the client anymore

--- data/talkactions/scripts/attributes.lua
+++ data/talkactions/scripts/attributes.lua
@@ -8,19 +8,19 @@ function onSay(player, words, param)
 
 	local tile = Tile(position)
 	if not tile then
-		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "There is no tile in front of you.")
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "There is no tile in front of you.")
 		return false
 	end
 
 	local thing = tile:getTopVisibleThing(player)
 	if not thing then
-		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "There is an empty tile in front of you.")
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "There is an empty tile in front of you.")
 		return false
 	end
 
 	local separatorPos = param:find(',')
 	if not separatorPos then
-		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, string.format("Usage: %s attribute, value.", words))
+		player:sendTextMessage(MESSAGE_INFO_DESCR, string.format("Usage: %s attribute, value.", words))
 		return false
 	end
 
@@ -30,19 +30,19 @@ function onSay(player, words, param)
 	if thing:isItem() then
 		local attributeId = Game.getItemAttributeByName(attribute)
 		if attributeId == ITEM_ATTRIBUTE_NONE then
-			player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Invalid attribute name.")
+			player:sendTextMessage(MESSAGE_INFO_DESCR, "Invalid attribute name.")
 			return false
 		end
 
 		if not thing:setAttribute(attribute, value) then
-			player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Could not set attribute.")
+			player:sendTextMessage(MESSAGE_INFO_DESCR, "Could not set attribute.")
 			return false
 		end
 
-		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, string.format("Attribute %s set to: %s", attribute, thing:getAttribute(attributeId)))
+		player:sendTextMessage(MESSAGE_INFO_DESCR, string.format("Attribute %s set to: %s", attribute, thing:getAttribute(attributeId)))
 		position:sendMagicEffect(CONST_ME_MAGIC_GREEN)
 	else
-		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Thing in front of you is not supported.")
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "Thing in front of you is not supported.")
 		return false
 	end
 end
