2e36fe6635d0d3ae7c4418db44f8d667e527076d
Zbizu <zbirox@gmail.com>
2021-11-23 21:17:26 +0100

    more info in onLook

--- data/scripts/eventcallbacks/player/default_onLook.lua
+++ data/scripts/eventcallbacks/player/default_onLook.lua
@@ -5,10 +5,11 @@ ec.onLook = function(self, thing, position, distance, description)
 	if self:getGroup():getAccess() then
 		if thing:isItem() then
 			description = string.format("%s\nItem ID: %d", description, thing:getId())
-
+			description = string.format("%s\nClient ID: %d", description, ItemType(thing:getId()):getClientId())
+			
 			local actionId = thing:getActionId()
 			if actionId ~= 0 then
-				description = string.format("%s, Action ID: %d", description, actionId)
+				description = string.format("%s\nAction ID: %d", description, actionId)
 			end
 
 			local uniqueId = thing:getAttribute(ITEM_ATTRIBUTE_UNIQUEID)
@@ -45,11 +46,38 @@ ec.onLook = function(self, thing, position, distance, description)
 		)
 
 		if thing:isCreature() then
+			local outfit = thing:getOutfit()
+			if outfit.lookTypeEx == 0 then
+				description = string.format("%s\nOutfit: %d", description, outfit.lookType)
+				if outfit.lookHead > 0 or outfit.lookBody > 0 or outfit.lookLegs > 0 or outfit.lookFeet > 0 then
+					description = string.format("%s\nColors: %d, %d, %d, %d", description, outfit.lookHead, outfit.lookBody, outfit.lookLegs, outfit.lookFeet)
+				end
+				
+				if outfit.lookAddons > 0 then
+					description = string.format("%s\nAddons: %d", description, outfit.lookAddons)
+				end
+			else
+				local itemType = ItemType(outfit.lookTypeEx)
+				local clientId = 0
+				if itemType then
+					clientId = itemType:getClientId()
+				end
+				
+				description = string.format("%s\nItem ID: %d\nClient ID: %d", description, outfit.lookTypeEx, clientId)
+			end
+			
+			if outfit.lookMount ~= 0 then
+				description = string.format("%s\nMount: %d", description, outfit.lookMount)
+				if outfit.lookMountHead > 0 or outfit.lookMountBody > 0 or outfit.lookMountLegs > 0 or outfit.lookMountFeet > 0 then
+					description = string.format("%s\nColors: %d, %d, %d, %d", description, outfit.lookMountHead, outfit.lookMountBody, outfit.lookMountLegs, outfit.lookMountFeet)
+				end
+			end
+			
 			description = string.format("%s\ncid: %s", description, thing:getId())
-		
+			
 			if thing:isPlayer() then
 				description = string.format("%s\nGUID: %s", description, thing:getGuid())
-				description = string.format("%s\nIP: %s.", description, Game.convertIpToString(thing:getIp()))
+				description = string.format("%s\nIP: %s", description, Game.convertIpToString(thing:getIp()))
 			end
 		end
 	end
--- data/scripts/eventcallbacks/player/default_onLookInBattleList.lua
+++ data/scripts/eventcallbacks/player/default_onLookInBattleList.lua
@@ -15,6 +15,33 @@ ec.onLookInBattleList = function(self, creature, distance)
 			description, position.x, position.y, position.z
 		)
 
+		local outfit = creature:getOutfit()
+		if outfit.lookTypeEx == 0 then
+			description = string.format("%s\nOutfit: %d", description, outfit.lookType)
+			if outfit.lookHead > 0 or outfit.lookBody > 0 or outfit.lookLegs > 0 or outfit.lookFeet > 0 then
+				description = string.format("%s\nColors: %d, %d, %d, %d", description, outfit.lookHead, outfit.lookBody, outfit.lookLegs, outfit.lookFeet)
+			end
+			
+			if outfit.lookAddons > 0 then
+				description = string.format("%s\nAddons: %d", description, outfit.lookAddons)
+			end
+		else
+			local itemType = ItemType(outfit.lookTypeEx)
+			local clientId = 0
+			if itemType then
+				clientId = itemType:getClientId()
+			end
+			
+			description = string.format("%s\nItem ID: %d\nClient ID: %d", description, outfit.lookTypeEx, clientId)
+		end
+		
+		if outfit.lookMount ~= 0 then
+			description = string.format("%s\nMount: %d", description, outfit.lookMount)
+			if outfit.lookMountHead > 0 or outfit.lookMountBody > 0 or outfit.lookMountLegs > 0 or outfit.lookMountFeet > 0 then
+				description = string.format("%s\nColors: %d, %d, %d, %d", description, outfit.lookMountHead, outfit.lookMountBody, outfit.lookMountLegs, outfit.lookMountFeet)
+			end
+		end
+		
 		description = string.format("%s\ncid: %s", description, creature:getId())
 		
 		if creature:isPlayer() then
