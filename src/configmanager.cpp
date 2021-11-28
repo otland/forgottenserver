f00fc578931dd634a781997b02906200c7d481e1
Marco Oliveira <marcomoa@hotmail.com>
2021-11-28 01:35:50 -0300

    [12.70] Premium players can store 15,000 in depot (#3774)

--- src/configmanager.cpp
+++ src/configmanager.cpp
@@ -291,7 +291,7 @@ bool ConfigManager::load()
 	integer[VIP_FREE_LIMIT] = getGlobalNumber(L, "vipFreeLimit", 20);
 	integer[VIP_PREMIUM_LIMIT] = getGlobalNumber(L, "vipPremiumLimit", 100);
 	integer[DEPOT_FREE_LIMIT] = getGlobalNumber(L, "depotFreeLimit", 2000);
-	integer[DEPOT_PREMIUM_LIMIT] = getGlobalNumber(L, "depotPremiumLimit", 10000);
+	integer[DEPOT_PREMIUM_LIMIT] = getGlobalNumber(L, "depotPremiumLimit", 15000);
 
 	expStages = loadXMLStages();
 	if (expStages.empty()) {
