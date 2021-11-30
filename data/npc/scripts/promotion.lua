9e2f35f6d91cd21795ec5d39ee7749d9d103230c
Marco Oliveira <marcomoa@hotmail.com>
2021-11-30 12:35:09 -0300

    Missing tab

--- data/npc/scripts/promotion.lua
+++ data/npc/scripts/promotion.lua
@@ -8,8 +8,8 @@ function onCreatureSay(cid, type, msg)      npcHandler:onCreatureSay(cid, type, 
 function onThink()                          npcHandler:onThink()                        end
 
 local node1 = keywordHandler:addKeyword({'promot'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'I can promote you for 20000 gold coins. Do you want me to promote you?'})
-node1:addChildKeyword({'yes'}, StdModule.promotePlayer, {npcHandler = npcHandler, cost = 20000, level = 20, text = 'Congratulations! You are now promoted.'})
-node1:addChildKeyword({'no'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Alright then, come back when you are ready.', reset = true})
+	node1:addChildKeyword({'yes'}, StdModule.promotePlayer, {npcHandler = npcHandler, cost = 20000, level = 20, text = 'Congratulations! You are now promoted.'})
+	node1:addChildKeyword({'no'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Alright then, come back when you are ready.', reset = true})
 
 local function creatureSayCallback(cid, type, msg)
 	if not npcHandler:isFocused(cid) then
