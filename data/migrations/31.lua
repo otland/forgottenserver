a9d71d42ff765c6450e97b28679eeabdb6a48392
Evil Puncker <EPuncker@users.noreply.github.com>
2021-12-03 20:48:36 -0300

    Update 31.lua

--- data/migrations/31.lua
+++ data/migrations/31.lua
@@ -1,5 +1,5 @@
 function onUpdateDatabase()
-	print("> Updating database to version 31 (biger price support)")
+	print("> Updating database to version 31 (bigger market price support)")
 	db.query("ALTER TABLE `market_history` CHANGE `price` `price` bigint unsigned NOT NULL DEFAULT '0'");
 	db.query("ALTER TABLE `market_offers` CHANGE `price` `price` bigint unsigned NOT NULL DEFAULT '0'");
 	return true
