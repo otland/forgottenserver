06d2cf7acb040f93dd9f400f12b90e967148e335
Marco Oliveira <marcomoa@hotmail.com>
2021-12-03 18:26:18 +0000

    Fix hungry condition

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -3299,7 +3299,7 @@ void ProtocolGame::AddPlayerStats(NetworkMessage& msg)
 	msg.add<uint16_t>(player->getStaminaMinutes());
 	msg.add<uint16_t>(player->getBaseSpeed() / 2);
 
-	Condition* condition = player->getCondition(CONDITION_REGENERATION);
+	Condition* condition = player->getCondition(CONDITION_REGENERATION, CONDITIONID_DEFAULT);
 	msg.add<uint16_t>(condition ? condition->getTicks() / 1000 : 0x00);
 
 	msg.add<uint16_t>(player->getOfflineTrainingTime() / 60 / 1000);
