ae2bc01de9977d69450f1fdeb7fd621d1399c4bf
MillhioreBT <sarahelizabetwesker@gmail.com>
2021-12-04 23:17:08 -0400

    a more modern and easy way to register new eventcallbacks
    some small but worth mentioning improvements on line 113, previously 142, checking a boolean is faster than iterating a table to check if a value exists or not

--- data/scripts/lib/event_callbacks.lua
+++ data/scripts/lib/event_callbacks.lua
@@ -1,84 +1,55 @@
+-- registerEnum mechanism
+local callbacks, updateableParameters, isReturnValue, autoID = {}, {}, {}, 0
+local registerEnum = setmetatable({}, {
+	__newindex = function (self, globalVariableName, enum)
+		autoID = autoID +1
+		_G[globalVariableName] = autoID
+		callbacks[enum[1]] = autoID
+		isReturnValue[autoID] = enum.returnValue
+		if enum.update then
+			updateableParameters[autoID] = enum.update
+		end
+	end
+})
+
+-- update: this makes the system update the parameter [n] with the result in the given index, example: [3]=1
+-- returnValue: this determines that the value returned by the function is a RETURNVALUE_...
+
 -- Creature
-EVENT_CALLBACK_ONCHANGEOUTFIT = 1
-EVENT_CALLBACK_ONCHANGEMOUNT = 2
-EVENT_CALLBACK_ONAREACOMBAT = 3
-EVENT_CALLBACK_ONTARGETCOMBAT = 4
-EVENT_CALLBACK_ONHEAR = 5
+registerEnum.EVENT_CALLBACK_ONCHANGEOUTFIT = { "onChangeOutfit" }
+registerEnum.EVENT_CALLBACK_ONCHANGEMOUNT = { "onChangeMount" }
+registerEnum.EVENT_CALLBACK_ONAREACOMBAT = { "onAreaCombat", returnValue=true }
+registerEnum.EVENT_CALLBACK_ONTARGETCOMBAT = { "onTargetCombat", returnValue=true }
+registerEnum.EVENT_CALLBACK_ONHEAR = { "onHear" }
 -- Party
-EVENT_CALLBACK_ONJOIN = 6
-EVENT_CALLBACK_ONLEAVE = 7
-EVENT_CALLBACK_ONDISBAND = 8
-EVENT_CALLBACK_ONSHAREEXPERIENCE = 9
+registerEnum.EVENT_CALLBACK_ONJOIN = { "onJoin" }
+registerEnum.EVENT_CALLBACK_ONLEAVE = { "onLeave" }
+registerEnum.EVENT_CALLBACK_ONDISBAND = { "onDisband" }
+registerEnum.EVENT_CALLBACK_ONSHAREEXPERIENCE = { "onShareExperience" }
 -- Player
-EVENT_CALLBACK_ONBROWSEFIELD = 10
-EVENT_CALLBACK_ONLOOK = 11
-EVENT_CALLBACK_ONLOOKINBATTLELIST = 12
-EVENT_CALLBACK_ONLOOKINTRADE = 13
-EVENT_CALLBACK_ONLOOKINSHOP = 14
-EVENT_CALLBACK_ONTRADEREQUEST = 15
-EVENT_CALLBACK_ONTRADEACCEPT = 16
-EVENT_CALLBACK_ONTRADECOMPLETED = 17
-EVENT_CALLBACK_ONMOVEITEM = 18
-EVENT_CALLBACK_ONITEMMOVED = 19
-EVENT_CALLBACK_ONMOVECREATURE = 20
-EVENT_CALLBACK_ONREPORTRULEVIOLATION = 21
-EVENT_CALLBACK_ONREPORTBUG = 22
-EVENT_CALLBACK_ONTURN = 23
-EVENT_CALLBACK_ONGAINEXPERIENCE = 24
-EVENT_CALLBACK_ONLOSEEXPERIENCE = 25
-EVENT_CALLBACK_ONGAINSKILLTRIES = 26
-EVENT_CALLBACK_ONWRAPITEM = 27
+registerEnum.EVENT_CALLBACK_ONBROWSEFIELD = { "onBrowseField" }
+registerEnum.EVENT_CALLBACK_ONLOOK = { "onLook", update={[5]=1} }
+registerEnum.EVENT_CALLBACK_ONLOOKINBATTLELIST = { "onLookInBattleList", update={[4]=1} }
+registerEnum.EVENT_CALLBACK_ONLOOKINTRADE = { "onLookInTrade", update={[5]=1} }
+registerEnum.EVENT_CALLBACK_ONLOOKINSHOP = { "onLookInShop", update={[4]=1} }
+registerEnum.EVENT_CALLBACK_ONTRADEREQUEST = { "onTradeRequest" }
+registerEnum.EVENT_CALLBACK_ONTRADEACCEPT = { "onTradeAccept" }
+registerEnum.EVENT_CALLBACK_ONTRADECOMPLETED = { "onTradeCompleted" }
+registerEnum.EVENT_CALLBACK_ONMOVEITEM = { "onMoveItem" }
+registerEnum.EVENT_CALLBACK_ONITEMMOVED = { "onItemMoved" }
+registerEnum.EVENT_CALLBACK_ONMOVECREATURE = { "onMoveCreature" }
+registerEnum.EVENT_CALLBACK_ONREPORTRULEVIOLATION = { "onReportRuleViolation" }
+registerEnum.EVENT_CALLBACK_ONREPORTBUG = { "onReportBug" }
+registerEnum.EVENT_CALLBACK_ONTURN = { "onTurn" }
+registerEnum.EVENT_CALLBACK_ONGAINEXPERIENCE = { "onGainExperience", update={[3]=1} }
+registerEnum.EVENT_CALLBACK_ONLOSEEXPERIENCE = { "onLoseExperience", update={[2]=1} }
+registerEnum.EVENT_CALLBACK_ONGAINSKILLTRIES = { "onGainSkillTries", update={[3]=1} }
+registerEnum.EVENT_CALLBACK_ONWRAPITEM = { "onWrapItem" }
 -- Monster
-EVENT_CALLBACK_ONDROPLOOT = 28
-EVENT_CALLBACK_ONSPAWN = 29
+registerEnum.EVENT_CALLBACK_ONDROPLOOT = { "onDropLoot" }
+registerEnum.EVENT_CALLBACK_ONSPAWN = { "onSpawn" }
 -- last (for correct table counting)
 EVENT_CALLBACK_LAST = EVENT_CALLBACK_ONSPAWN
-
-local callbacks = {
-	-- Creature
-	["onChangeOutfit"] = EVENT_CALLBACK_ONCHANGEOUTFIT,
-	["onChangeMount"] = EVENT_CALLBACK_ONCHANGEMOUNT,
-	["onAreaCombat"] = EVENT_CALLBACK_ONAREACOMBAT,
-	["onTargetCombat"] = EVENT_CALLBACK_ONTARGETCOMBAT,
-	["onHear"] = EVENT_CALLBACK_ONHEAR,
-	-- Party
-	["onJoin"] = EVENT_CALLBACK_ONJOIN,
-	["onLeave"] = EVENT_CALLBACK_ONLEAVE,
-	["onDisband"] = EVENT_CALLBACK_ONDISBAND,
-	["onShareExperience"] = EVENT_CALLBACK_ONSHAREEXPERIENCE,
-	-- Player
-	["onBrowseField"] = EVENT_CALLBACK_ONBROWSEFIELD,
-	["onLook"] = EVENT_CALLBACK_ONLOOK,
-	["onLookInBattleList"] = EVENT_CALLBACK_ONLOOKINBATTLELIST,
-	["onLookInTrade"] = EVENT_CALLBACK_ONLOOKINTRADE,
-	["onLookInShop"] = EVENT_CALLBACK_ONLOOKINSHOP,
-	["onTradeRequest"] = EVENT_CALLBACK_ONTRADEREQUEST,
-	["onTradeAccept"] = EVENT_CALLBACK_ONTRADEACCEPT,
-	["onTradeCompleted"] = EVENT_CALLBACK_ONTRADECOMPLETED,
-	["onMoveItem"] = EVENT_CALLBACK_ONMOVEITEM,
-	["onItemMoved"] = EVENT_CALLBACK_ONITEMMOVED,
-	["onMoveCreature"] = EVENT_CALLBACK_ONMOVECREATURE,
-	["onReportRuleViolation"] = EVENT_CALLBACK_ONREPORTRULEVIOLATION,
-	["onReportBug"] = EVENT_CALLBACK_ONREPORTBUG,
-	["onTurn"] = EVENT_CALLBACK_ONTURN,
-	["onGainExperience"] = EVENT_CALLBACK_ONGAINEXPERIENCE,
-	["onLoseExperience"] = EVENT_CALLBACK_ONLOSEEXPERIENCE,
-	["onGainSkillTries"] = EVENT_CALLBACK_ONGAINSKILLTRIES,
-	["onWrapItem"] = EVENT_CALLBACK_ONWRAPITEM,
-	-- Monster
-	["onDropLoot"] = EVENT_CALLBACK_ONDROPLOOT,
-	["onSpawn"] = EVENT_CALLBACK_ONSPAWN
-}
-
-local updateableParameters = {
-	[EVENT_CALLBACK_ONLOOK] = {[5] = 1},
-	[EVENT_CALLBACK_ONLOOKINBATTLELIST] = {[4] = 1},
-	[EVENT_CALLBACK_ONLOOKINTRADE] = {[5] = 1},
-	[EVENT_CALLBACK_ONLOOKINSHOP] = {[4] = 1},
-	[EVENT_CALLBACK_ONGAINEXPERIENCE] = {[3] = 1},
-	[EVENT_CALLBACK_ONLOSEEXPERIENCE] = {[2] = 1},
-	[EVENT_CALLBACK_ONGAINSKILLTRIES] = {[3] = 1}
-}
 
 EventCallbackData = {}
 hasEventCallback = function (type)
@@ -139,7 +110,7 @@ setmetatable(EventCallback, {
 					return false
 				end
 				-- If the call of type returnvalue returns noerror then we continue the loop
-				if table.contains({EVENT_CALLBACK_ONAREACOMBAT, EVENT_CALLBACK_ONTARGETCOMBAT}, eventCallback) then
+				if isReturnValue[eventCallback] then
 					if output == RETURNVALUE_NOERROR then
 						break
 					end
