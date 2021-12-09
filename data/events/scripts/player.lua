a4f53ef2fc7800a2a470adfdc88e0d34fd9258fd
Ramon Bernardo <ramonbernardo.contato@gmail.com>
2021-12-09 16:24:27 -0300

    Use nextUseStaminaTime only player gain experience (#3779)

--- data/events/scripts/player.lua
+++ data/events/scripts/player.lua
@@ -108,6 +108,10 @@ local function useStamina(player)
 	end
 
 	local playerId = player:getId()
+	if not nextUseStaminaTime[playerId] then
+		nextUseStaminaTime[playerId] = 0
+	end
+
 	local currentTime = os.time()
 	local timePassed = currentTime - nextUseStaminaTime[playerId]
 	if timePassed <= 0 then
