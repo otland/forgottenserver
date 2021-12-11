7ce962c9ba613706bcf5e34a7dcc66964afdff03
Marco Oliveira <marcomoa@hotmail.com>
2021-12-11 16:31:11 -0300

    Update tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -637,10 +637,10 @@ MagicEffectNames magicEffectNames = {
 	{"redteleport",		CONST_ME_REDTELEPORT},
 	{"orangeteleport",	CONST_ME_ORANGETELEPORT},
 	{"greyteleport",	CONST_ME_GREYTELEPORT},
-	{"lightblueteleport",	CONST_ME_LIGHTBLUETELEPORT},
-	{"fatal", 				CONST_ME_FATAL},
-	{"dodge", 				CONST_ME_DODGE},
-	{"hourglass",			CONST_ME_HOURGLASS},
+	{"lightblueteleport", CONST_ME_LIGHTBLUETELEPORT},
+	{"fatal", 			CONST_ME_FATAL},
+	{"dodge", 			CONST_ME_DODGE},
+	{"hourglass",		CONST_ME_HOURGLASS},
 };
 
 ShootTypeNames shootTypeNames = {
