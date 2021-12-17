6112b720af3b433bd4b9df055d61de30ce48ca59
Evil Puncker <EPuncker@users.noreply.github.com>
2021-12-17 00:01:25 -0300

    Small update to channel requirements

--- data/chatchannels/scripts/advertising.lua
+++ data/chatchannels/scripts/advertising.lua
@@ -16,8 +16,8 @@ function onSpeak(player, type, message)
 		return true
 	end
 
-	if player:getLevel() == 1 then
-		player:sendCancelMessage("You may not speak into channels as long as you are on level 1.")
+	if player:getLevel() < 20 or not player:isPremium() then
+		player:sendCancelMessage("You may not speak in this channel unless you have reached level 20 or your account has premium status.")
 		return false
 	end
 
@@ -37,4 +37,10 @@ function onSpeak(player, type, message)
 		end
 	end
 	return type
+end
+
+function onJoin(player)
+	sendChannelMessage(CHANNEL_ADVERTISING, MESSAGE_GUILD, "Here you can advertise all kinds of things. Among others, you can trade items, advertise ingame events, seek characters for a quest or a hunting group, find members for your guild or look for somebody to help you with something.")
+	sendChannelMessage(CHANNEL_ADVERTISING, MESSAGE_GUILD, "It goes without saying that all advertisements must conform to the Rules, e.g. it is illegal to advertise trades including real money.")
+	return true
 end
