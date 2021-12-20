0bef66b0cf116ab70f9afbfce9960ef02f2a9009
Evil Puncker <EPuncker@users.noreply.github.com>
2021-12-20 19:50:53 -0300

    Added Game.getOutfits(playerSex) and Game.getMounts()
    these will be used by podium description
    
    Co-Authored-By: Zbizu <Zbizu@users.noreply.github.com>

--- src/luascript.h
+++ src/luascript.h
@@ -40,6 +40,7 @@
 #include "enums.h"
 #include "position.h"
 #include "outfit.h"
+#include "mounts.h"
 
 class Thing;
 class Creature;
@@ -396,6 +397,7 @@ class LuaScriptInterface
 		static void pushPosition(lua_State* L, const Position& position, int32_t stackpos = 0);
 		static void pushOutfit(lua_State* L, const Outfit_t& outfit);
 		static void pushOutfit(lua_State* L, const Outfit* outfit);
+		static void pushMount(lua_State* L, const Mount* mount);
 		static void pushLoot(lua_State* L, const std::vector<LootBlock>& lootList);
 
 		//
@@ -556,6 +558,8 @@ class LuaScriptInterface
 
 		static int luaGameGetTowns(lua_State* L);
 		static int luaGameGetHouses(lua_State* L);
+		static int luaGameGetOutfits(lua_State* L);
+		static int luaGameGetMounts(lua_State* L);
 
 		static int luaGameGetGameState(lua_State* L);
 		static int luaGameSetGameState(lua_State* L);
