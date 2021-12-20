f1cd3e7b676d53b9551781ab0c7e59590cc75425
Sarah Wesker <sarahelizabetwesker@gmail.com>
2021-12-20 15:04:30 -0400

    Update monster.lua

--- data/events/scripts/monster.lua
+++ data/events/scripts/monster.lua
@@ -1,11 +1,11 @@
 function Monster:onDropLoot(corpse)
-  local player = Player(corpse:getCorpseOwner())
-  if player then
-    player:updateKillTracker(self, corpse)
-  end
-  if EventCallback.onDropLoot then
-    EventCallback.onDropLoot(self, corpse)
-  end
+	local player = Player(corpse:getCorpseOwner())
+	if player then
+		player:updateKillTracker(self, corpse)
+	end
+	if EventCallback.onDropLoot then
+		EventCallback.onDropLoot(self, corpse)
+	end
 end
 
 function Monster:onSpawn(position, startup, artificial)
