af103df4afe6f0b331930e3951b58ee4fe5c5246
Evil Puncker <EPuncker@users.noreply.github.com>
2022-01-10 22:44:13 -0300

    Update luascript.cpp

--- src/luascript.cpp
+++ src/luascript.cpp
@@ -1741,6 +1741,11 @@ void LuaScriptInterface::registerFunctions()
 	registerEnum(SPECIALSKILL_MANALEECHCHANCE)
 	registerEnum(SPECIALSKILL_MANALEECHAMOUNT)
 
+	registerEnum(STAT_MAXHITPOINTS)
+	registerEnum(STAT_MAXMANAPOINTS)
+	registerEnum(STAT_SOULPOINTS)
+	registerEnum(STAT_MAGICPOINTS)
+
 	registerEnum(SKULL_NONE)
 	registerEnum(SKULL_YELLOW)
 	registerEnum(SKULL_GREEN)
