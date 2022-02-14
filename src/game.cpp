225ce3f991a0b74aaf58c537d57b3e6426109d98
nekiro <nekiroboy@gmail.com>
2022-02-14 02:12:49 +0100

    Fix trade stacking items (#3248)

--- src/game.cpp
+++ src/game.cpp
@@ -1099,7 +1099,6 @@ ReturnValue Game::internalMoveItem(Cylinder* fromCylinder, Cylinder* toCylinder,
 
 	while ((subCylinder = toCylinder->queryDestination(index, *item, &toItem, flags)) != toCylinder) {
 		toCylinder = subCylinder;
-		flags = 0;
 
 		//to prevent infinite loop
 		if (++floorN >= MAP_MAX_LAYERS) {
