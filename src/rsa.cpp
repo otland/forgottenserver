418d07f145e0cb42d1daae22b25a335b3fa62e55
Daniel Speichert <daniel@speichert.pl>
2022-02-14 18:12:54 -0500

    rsa: tolerate whitespaces better

--- src/rsa.cpp
+++ src/rsa.cpp
@@ -23,6 +23,7 @@
 
 #include <cryptopp/base64.h>
 #include <cryptopp/osrng.h>
+#include <fmt/color.h>
 
 #include <fstream>
 #include <sstream>
@@ -36,7 +37,7 @@ void RSA::decrypt(char* msg) const
 		auto c = pk.CalculateInverse(prng, m);
 		c.Encode(reinterpret_cast<uint8_t*>(msg), 128);
 	} catch (const CryptoPP::Exception& e) {
-		std::cout << e.what() << '\n';
+		fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold, e.what(), "\n");
 	}
 }
 
@@ -49,21 +50,23 @@ void RSA::loadPEM(const std::string& filename)
 
 	if (!file.is_open()) {
 		throw std::runtime_error("Missing file " + filename + ".");
- 	}
+	}
 
 	std::ostringstream oss;
 	for (std::string line; std::getline(file, line); oss << line);
 	std::string key = oss.str();
 
-	if (key.substr(0, header.size()) != header) {
-		throw std::runtime_error("Missing RSA private key header.");
+	auto pos1 = key.find(header);
+	if (pos1 == std::string::npos) {
+		throw std::runtime_error("Missing RSA private key PEM header.");
 	}
 
-	if (key.substr(key.size() - footer.size(), footer.size()) != footer) {
-		throw std::runtime_error("Missing RSA private key footer.");
+	auto pos2 = key.find(footer, pos1 + 1);
+	if (pos2 == std::string::npos) {
+		throw std::runtime_error("Missing RSA private key PEM footer.");
 	}
 
-	key = key.substr(header.size(), key.size() - footer.size());
+	key = key.substr(pos1 + header.size(), pos2 - pos1 - header.size());
 
 	CryptoPP::ByteQueue queue;
 	CryptoPP::Base64Decoder decoder;
@@ -71,13 +74,9 @@ void RSA::loadPEM(const std::string& filename)
 	decoder.Put(reinterpret_cast<const uint8_t*>(key.c_str()), key.size());
 	decoder.MessageEnd();
 
-	try {
-		pk.BERDecodePrivateKey(queue, false, queue.MaxRetrievable());
+	pk.BERDecodePrivateKey(queue, false, queue.MaxRetrievable());
 
-		if (!pk.Validate(prng, 3)) {
-			throw std::runtime_error("RSA private key is not valid.");
-		}
-	} catch (const CryptoPP::Exception& e) {
-		std::cout << e.what() << '\n';
+	if (!pk.Validate(prng, 3)) {
+		throw std::runtime_error("RSA private key is not valid.");
 	}
 }
