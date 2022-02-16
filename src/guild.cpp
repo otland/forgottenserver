455e36c705e4d67826762b7c7b9219520e96667a
Ranieri Althoff <ranisalt@gmail.com>
2022-02-16 21:56:52 +0100

    Merge ioguild into guild

--- src/guild.cpp
+++ src/guild.cpp
@@ -74,3 +74,51 @@ void Guild::addRank(uint32_t rankId, const std::string& rankName, uint8_t level)
 {
 	ranks.emplace_back(std::make_shared<GuildRank>(rankId, rankName, level));
 }
+
+namespace IOGuild {
+
+Guild* loadGuild(uint32_t guildId)
+{
+	Database& db = Database::getInstance();
+	if (DBResult_ptr result = db.storeQuery(fmt::format("SELECT `name` FROM `guilds` WHERE `id` = {:d}", guildId))) {
+		Guild* guild = new Guild(guildId, result->getString("name"));
+
+		if ((result = db.storeQuery(fmt::format("SELECT `id`, `name`, `level` FROM `guild_ranks` WHERE `guild_id` = {:d}", guildId)))) {
+			do {
+				guild->addRank(result->getNumber<uint32_t>("id"), result->getString("name"), result->getNumber<uint16_t>("level"));
+			} while (result->next());
+		}
+		return guild;
+	}
+	return nullptr;
+}
+
+uint32_t getGuildIdByName(const std::string& name)
+{
+	Database& db = Database::getInstance();
+
+	DBResult_ptr result = db.storeQuery(fmt::format("SELECT `id` FROM `guilds` WHERE `name` = {:s}", db.escapeString(name)));
+	if (!result) {
+		return 0;
+	}
+	return result->getNumber<uint32_t>("id");
+}
+
+void getWarList(uint32_t guildId, GuildWarVector& guildWarVector)
+{
+	DBResult_ptr result = Database::getInstance().storeQuery(fmt::format("SELECT `guild1`, `guild2` FROM `guild_wars` WHERE (`guild1` = {:d} OR `guild2` = {:d}) AND `ended` = 0 AND `status` = 1", guildId, guildId));
+	if (!result) {
+		return;
+	}
+
+	do {
+		uint32_t guild1 = result->getNumber<uint32_t>("guild1");
+		if (guildId != guild1) {
+			guildWarVector.push_back(guild1);
+		} else {
+			guildWarVector.push_back(result->getNumber<uint32_t>("guild2"));
+		}
+	} while (result->next());
+}
+
+} // namespace IOGuild
