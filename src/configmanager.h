8e245b0dd78e1cfe3c0bd64fb81a745f42653f5e
Artur <krecikondexin@gmail.com>
2022-03-10 15:45:57 +0100

    Exp tracker

--- src/configmanager.h
+++ src/configmanager.h
@@ -53,6 +53,7 @@ class ConfigManager
 			REMOVE_ON_DESPAWN,
 			PLAYER_CONSOLE_LOGS,
 			TWO_FACTOR_AUTH,
+			EXP_ANALYSER_SEND_TRUE_RAW_EXP,
 
 			LAST_BOOLEAN_CONFIG /* this must be the last one */
 		};
