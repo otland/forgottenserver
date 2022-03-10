3fb22fb9031840b2dcc33d640514327b340db0d0
Ranieri Althoff <1993083+ranisalt@users.noreply.github.com>
2022-03-10 03:07:39 +0100

    Replace slower boost::lexical_cast with stdlib conversions (#3997)

--- src/pugicast.h
+++ src/pugicast.h
@@ -4,20 +4,25 @@
 #ifndef FS_PUGICAST_H
 #define FS_PUGICAST_H
 
-#include <boost/lexical_cast.hpp>
-
 namespace pugi {
-	template<typename T>
-	T cast(const pugi::char_t* str)
-	{
-		T value;
-		try {
-			value = boost::lexical_cast<T>(str);
-		} catch (boost::bad_lexical_cast&) {
-			value = T();
-		}
-		return value;
-	}
+
+template<class T> T cast(const char* str);
+
+template<> inline float cast(const char* str) { return std::strtof(str, nullptr); }
+template<> inline double cast(const char* str) { return std::strtod(str, nullptr); }
+template<> inline long cast(const char* str) { return std::strtol(str, nullptr, 0); }
+template<> inline long long cast(const char* str) { return std::strtoll(str, nullptr, 0); }
+template<> inline unsigned long cast(const char* str) { return std::strtoul(str, nullptr, 0); }
+template<> inline unsigned long long cast(const char* str) { return std::strtoull(str, nullptr, 0); }
+
+template<> inline char cast(const char* str) { return static_cast<char>(cast<long>(str)); }
+template<> inline signed char cast(const char* str) { return static_cast<signed char>(cast<long>(str)); }
+template<> inline short cast(const char* str) { return static_cast<short>(cast<long>(str)); }
+template<> inline int cast(const char* str) { return static_cast<int>(cast<long>(str)); }
+template<> inline unsigned char cast(const char* str) { return static_cast<unsigned char>(cast<unsigned long>(str)); }
+template<> inline unsigned short cast(const char* str) { return static_cast<unsigned short>(cast<unsigned long>(str)); }
+template<> inline unsigned int cast(const char* str) { return static_cast<unsigned int>(cast<unsigned long>(str)); }
+
 }
 
 #endif // FS_PUGICAST_H
