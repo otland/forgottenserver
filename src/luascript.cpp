87b1b543b09e3a8ebd2a44bb0f3523d0e80ea231
Ramon Bernardo <ramonbernardo.contato@gmail.com>
2022-03-12 10:00:00 -0300

    Adds non-existing loot error description

--- src/luascript.cpp
+++ src/luascript.cpp
@@ -255,6 +255,7 @@ std::string LuaScriptInterface::getErrorDesc(ErrorCode_t code)
 		case LUA_ERROR_VARIANT_UNKNOWN: return "Unknown variant type";
 		case LUA_ERROR_SPELL_NOT_FOUND: return "Spell not found";
 		case LUA_ERROR_MOUNT_NOT_FOUND: return "Mount not found";
+		case LUA_ERROR_LOOT_NOT_FOUND: return "Loot not found";
 		default: return "Bad error code";
 	}
 }
