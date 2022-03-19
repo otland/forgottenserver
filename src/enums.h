bf04ae85cbcf66121e1f4329f7674241fd80474d
Daniel Speichert <daniel@speichert.pl>
2022-03-18 21:58:13 -0400

    enums: fixed conversion warning

--- src/enums.h
+++ src/enums.h
@@ -631,7 +631,7 @@ struct Reflect {
 
 	Reflect& operator+=(const Reflect& other) {
 		percent += other.percent;
-		chance = std::min(100, chance + other.chance);
+		chance = std::min<uint16_t>(100, chance + other.chance);
 		return *this;
 	}
 
