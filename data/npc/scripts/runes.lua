7cc878bdc411dbfad909d9c7cc56c0542d0b9683
Ramon Bernardo <ramonbernardo.contato@gmail.com>
2022-03-19 09:42:52 -0300

    Fix npc, movements and player

--- data/npc/scripts/runes.lua
+++ data/npc/scripts/runes.lua
@@ -3,7 +3,7 @@ local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
 
 function onCreatureAppear(cid)				npcHandler:onCreatureAppear(cid)			end
-function onCreatureDisappear(cid) 			npcHandler:onCreatureDisappear(cid)			end
+function onCreatureDisappear(cid)			npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)	end
 function onThink()							npcHandler:onThink()						end
 
