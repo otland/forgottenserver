d1afe9eb73e46d9290e67b4568dc3a3c7055e244
torresmon <70948057+torresmon@users.noreply.github.com>
2022-04-30 10:01:33 -0300

    Revert "Spells update (#4095)"
    This reverts commit 873301a5cdabbf938abf3f9aa7c7087b652cd66b.

--- data/spells/scripts/healing/heal_friend.lua
+++ data/spells/scripts/healing/heal_friend.lua
@@ -13,11 +13,6 @@ end
 combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(creature, variant)
-	if creature:isPlayer() and variant:getNumber() == creature:getId() then
-		creature:sendCancelMessage("You can't cast this spell on yourself.")
-		creature:getPosition():sendMagicEffect(CONST_ME_POFF)
-		return false
-	end
 	creature:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
 	return combat:execute(creature, variant)
 end
--- data/spells/scripts/support/sharpshooter.lua
+++ data/spells/scripts/support/sharpshooter.lua
@@ -4,7 +4,7 @@ combat:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
 
 local skill = Condition(CONDITION_ATTRIBUTES)
 skill:setParameter(CONDITION_PARAM_TICKS, 10000)
-skill:setParameter(CONDITION_PARAM_SKILL_DISTANCEPERCENT, 140)
+skill:setParameter(CONDITION_PARAM_SKILL_DISTANCEPERCENT, 150)
 skill:setParameter(CONDITION_PARAM_DISABLE_DEFENSE, true)
 skill:setParameter(CONDITION_PARAM_BUFF_SPELL, true)
 combat:addCondition(skill)
