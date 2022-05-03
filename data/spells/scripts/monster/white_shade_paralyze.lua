c3142f6e205646a94b0055ec61787bb3585b24dc
Marco Oliveira <marcomoa@hotmail.com>
2022-05-03 19:29:38 -0300

    Fix: White shade paralyze

--- data/spells/scripts/monster/white_shade_paralyze.lua
+++ data/spells/scripts/monster/white_shade_paralyze.lua
@@ -1,12 +1,11 @@
-local combat = Combat()
-combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_HITAREA)
-combat:addCondition(condition)
-combat:setArea(createCombatArea(AREA_CIRCLE2X2))
-combat:addCondition(condition)
-
 local condition = Condition(CONDITION_PARALYZE)
 condition:setParameter(CONDITION_PARAM_TICKS, 20000)
 condition:setFormula(-0.3, 0, -0.45, 0)
+
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_HITAREA)
+combat:setArea(createCombatArea(AREA_CIRCLE2X2))
+combat:addCondition(condition)
 
 function onCastSpell(creature, var)
 	return combat:execute(creature, var)
