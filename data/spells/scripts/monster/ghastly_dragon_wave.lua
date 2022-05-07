a8209adbc3719a7887fa2a053f8aa85228bbb1ea
Marco Oliveira <marcomoa@hotmail.com>
2022-05-07 15:59:02 -0300

    Fix: console logs

---
+++ data/spells/scripts/monster/ghastly_dragon_wave.lua
@@ -0,0 +1,16 @@
+area = {
+	{0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
+	{0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0}
+}
+
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_DEATHDAMAGE)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_LOSEENERGY)
+combat:setArea(createCombatArea(area))
+
+function onCastSpell(creature, var)
+	return combat:execute(creature, var)
+end
