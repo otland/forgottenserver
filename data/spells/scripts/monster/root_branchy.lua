0bffed081aa10de522f23686a9a7e5949edfc427
Marco Oliveira <marcomoa@hotmail.com>
2022-05-09 21:24:36 -0300

    Revert: root_branchy chages

--- data/spells/scripts/monster/root_branchy.lua
+++ data/spells/scripts/monster/root_branchy.lua
@@ -6,6 +6,6 @@ local condition = Condition(CONDITION_ROOT)
 condition:setParameter(CONDITION_PARAM_TICKS, 3000)
 combat:addCondition(condition)
 
-function onCastSpell(creature, variant)
-	return combat:execute(creature, variant)
+function onCastSpell(creature, var)
+	return combat:execute(creature, var)
 end
