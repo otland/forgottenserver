1c5f93c135ea4a325c69186bcb4c660c0c089ad6
Evil Puncker <EPuncker@users.noreply.github.com>
2022-07-19 17:38:30 -0300

    stuff

--- src/definitions.h
+++ src/definitions.h
@@ -9,7 +9,7 @@ static constexpr auto STATUS_SERVER_VERSION = "1.5";
 static constexpr auto STATUS_SERVER_DEVELOPERS = "The Forgotten Server Team";
 
 static constexpr auto CLIENT_VERSION_MIN = 1290;
-static constexpr auto CLIENT_VERSION_MAX = 1290;
+static constexpr auto CLIENT_VERSION_MAX = 1291;
 static constexpr auto CLIENT_VERSION_STR = "12.90";
 
 static constexpr auto AUTHENTICATOR_DIGITS = 6U;
