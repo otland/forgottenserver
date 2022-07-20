abcd6977e45cee45ee299f941093662bfa4e0f65
Łukasz Zbiżek <Zbizu@users.noreply.github.com>
2022-07-20 14:41:29 +0200

    fix variable scope in npc.lua
    should be local variable

--- data/npc/lib/npc.lua
+++ data/npc/lib/npc.lua
@@ -14,6 +14,8 @@ function doNpcSellItem(cid, itemid, amount, subType, ignoreCap, inBackpacks, bac
 	local amount = amount or 1
 	local subType = subType or 0
 	local item = 0
+	local stuff
+	
 	if ItemType(itemid):isStackable() then
 		if inBackpacks then
 			stuff = Game.createItem(backpack, 1)
