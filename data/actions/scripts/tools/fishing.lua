35983c9451ee36f38dd6d29824bd3332f948141e
gigastar <nheinz1995@live.com>
2022-09-26 03:42:44 -0700

    Update Fishing System
    Check main post for information on everything changed.

--- data/actions/scripts/tools/fishing.lua
+++ data/actions/scripts/tools/fishing.lua
@@ -1,20 +1,93 @@
-local waterIds = {493, 4608, 4609, 4610, 4611, 4612, 4613, 4614, 4615, 4616, 4617, 4618, 4619, 4620, 4621, 4622, 4623, 4624, 4625, 7236, 10499, 15401, 15402}
+-- Water Ids + 1 no fish -1 with fish
+local waterWithFish = {4608, 4610, 4612, 4614, 4616, 4618, 4664, 7236, 15401}
+local waterWithoutFish = {4609, 4611, 4613, 4615, 4617, 4619, 4665, 7237, 15402}
+local waterSpiritId = 10449
+
+local fish = {
+	[1] = {itemid = 2667, baseChance = 25000}, -- Fish : 25%
+	[2] = {itemid = 7159, baseChance = 2250}, -- Green Perch : 2.25%
+	[3] = {itemid = 2669, baseChance = 1100}, -- Northern Pike : 1.1%
+	[4] = {itemid = 2667, baseChance = 700}, -- Rainbow Trout : 0.7%
+	[5] = {itemid = 15405, baseChance = 3000}, -- Sand Fish : 3%
+	[6] = {itemid = 10224, baseChance = 3000} -- Mechanical Fish : 3%
+}
+
 local lootTrash = {2234, 2238, 2376, 2509, 2667}
 local lootCommon = {2152, 2167, 2168, 2669, 7588, 7589}
 local lootRare = {2143, 2146, 2149, 7158, 7159}
 local lootVeryRare = {7632, 7633, 10220}
-local useWorms = true
 
-local fishers = {}
-local fishingSpotCooldown = 2200 -- Seconds
+local useWorms = true
+local useNails = true
+
+local rodId = 2580
+local mechanicalRodId = 10223
+
+local wormId = 3976
+local nailId = 8309
 
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	local targetId = target.itemid
-	if not table.contains(waterIds, targetId) then
-		return false
+	if not target.itemid then return false end
+	
+	-- Normal Fishing --
+	if table.contains(waterWithFish, target.itemid) or table.contains(waterWithoutFish, target.itemid) then
+		if targetId ~= 7236 and targetId ~= 7237 then
+			toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
+		end
+		
+		-- Fishing from PZ. Dont use worms, add skill, or catch a fish --
+		if Tile(toPosition):hasFlag(TILESTATE_PROTECTIONZONE) then
+			return true
+		end
+		
+		-- Remove worm if we use them --
+		if useWorms and not player:removeItem(wormId, 1) then
+			return true
+		end
+		
+		if table.contains(waterWithFish, target.itemid) then
+			player:addSkillTries(SKILL_FISHING, 1)
+			
+			local skillChance = math.min(math.max(10 + (player:getEffectiveSkillLevel(SKILL_FISHING) - 10) * 0.597, 10), 50) * 1000
+			local rand = math.random(100000)
+			for i = 1, #fish do
+				-- Loop is at sand fish. They must be fishing on itemid 15401 --
+				if i > 4 and targetId ~= 15401 then
+					break
+				end
+				
+				-- Loop is at mechanical fish, we must be using a mechanical rod --
+				if i > 5 and item.itemid ~= 10223 then
+					break
+					-- Remove a nail if we use them. It is required to catch mechanical fish --
+				elseif i > 5 and useNails and not player:removeItem(nailId, 1) then
+					break
+				end
+				
+				-- Chance to catch fish is baseChance + skill Chance
+				if rand <= math.floor(fish[i].baseChance + skillChance) then
+					player:addItem(fish[i].itemid, 1)
+					target:transform(target.itemid + 1)
+					target:decay()
+					
+					-- Add achievement based on which waterId is being fished on --
+					if target.itemid == 7236 then
+						player:addAchievementProgress("Exquisite Taste", 250)
+					elseif target.itemid == 15401 then
+						player:addAchievementProgress("Desert Fisher")
+					else
+						player:addAchievementProgress("Here, Fishy Fishy!", 1000)
+					end
+					
+					break
+				end
+			end
+		end
+		return true
 	end
-
-	if targetId == 10499 then
+	
+	-- Water Spirit Fishing --
+	if targetId == waterSpiritId then
 		local owner = target:getAttribute(ITEM_ATTRIBUTE_CORPSEOWNER)
 		if owner ~= 0 and owner ~= player:getId() then
 			player:sendTextMessage(MESSAGE_STATUS_SMALL, "You are not the owner.")
@@ -35,84 +108,7 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		else
 			player:addItem(lootTrash[math.random(#lootTrash)], 1)
 		end
-		return true
-	end
-
-	if targetId ~= 7236 then
-		toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
-	end
-
-	if targetId == 493 or targetId == 15402 then
-		return true
-	end
-	
-	if Tile(toPosition):hasFlag(TILESTATE_PROTECTIONZONE) then
-		return true
-	end
-	
-	-- We caught a fish at this spot. We cant gain anymore skill or catch another for a while.
-	local posIndex = toPosition.x + toPosition.y + toPosition.z
-	if fishers[player:getName()] and fishers[player:getName()].positions[posIndex] then
-		if fishers[player:getName()].positions[posIndex] then
-			return true
-		end
-	end
-	
-	player:addSkillTries(SKILL_FISHING, 1)
-	
-	if math.random(1, 100) <= math.min(math.max(10 + (player:getEffectiveSkillLevel(SKILL_FISHING) - 10) * 0.597, 10), 50) then
-		if useWorms and not player:removeItem(3976, 1) then
-			return true
-		end
-
-		if targetId == 15401 then
-			target:transform(targetId + 1)
-			target:decay()
-
-			if math.random(1, 100) >= 97 then
-				player:addItem(15405, 1)
-				player:addAchievement("Desert Fisher")
-				return true
-			end
-		elseif targetId == 7236 then
-			target:transform(targetId + 1)
-			target:decay()
-			player:addAchievementProgress("Exquisite Taste", 250)
-
-			local rareChance = math.random(1, 100)
-			if rareChance == 1 then
-				player:addItem(7158, 1)
-				return true
-			elseif rareChance <= 4 then
-				player:addItem(2669, 1)
-				return true
-			elseif rareChance <= 10 then
-				player:addItem(7159, 1)
-				return true
-			end
-		end
-		
-		local playerName = player:getName()
-		if not fishers[playerName] then
-			fishers[playerName] = {positions = {}}
-		end
-
-		fishers[player:getName()].positions[posIndex] = true
-		addEvent(resetFishingSpot, fishingSpotCooldown * 1000, playerName, posIndex)
-		
-		target:transform(noFishWater)
-		target:decay()
-		
-		player:addAchievementProgress("Here, Fishy Fishy!", 1000)
-		player:addItem(2667, 1)
 	end
 	return true
 end
 
-function resetFishingSpot(name, posIndex)
-	if fishers[playerName] and fishers[playerName].positions then
-		if fishers[playerName].positions[posIndex] then
-			fishers[playerName].positions[posIndex] = nil
-		end
-	end
-end
