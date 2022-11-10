cbe6bf4ca46e6937a071a472c59c323264d3171e
ramon-bernardo <ramonbernardo.contato@gmail.com>
2022-11-09 21:43:13 -0300

    Fix file defination

--- src/spectators.h
+++ src/spectators.h
@@ -1,8 +1,8 @@
 // Copyright 2022 The Forgotten Server Authors. All rights reserved.
 // Use of this source code is governed by the GPL-2.0 License that can be found in the LICENSE file.
 
-#ifndef FS_Spectators_H
-#define FS_Spectators_H
+#ifndef FS_SPECTATORS_H
+#define FS_SPECTATORS_H
 
 class Creature;
 
@@ -57,4 +57,4 @@ private:
 	Vec spectators;
 };
 
-#endif // FS_Spectators_H
+#endif // FS_SPECTATORS_H
