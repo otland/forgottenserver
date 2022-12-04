003183584a98c59c64639eb21fa051f43270b991
Marcin Jałocha <nekiro.dev@gmail.com>
2022-12-04 16:14:23 +0100

    Refactor formatDate and formatDateShort (#4249)

--- src/tools.cpp
+++ src/tools.cpp
@@ -8,6 +8,7 @@
 #include "configmanager.h"
 
 #include <chrono>
+#include <fmt/chrono.h>
 
 extern ConfigManager g_config;
 
@@ -315,32 +316,12 @@ std::string convertIPToString(uint32_t ip)
 
 std::string formatDate(time_t time)
 {
-	const tm* tms = localtime(&time);
-	if (!tms) {
-		return {};
-	}
-
-	char buffer[20];
-	int res = sprintf(buffer, "%02d/%02d/%04d %02d:%02d:%02d", tms->tm_mday, tms->tm_mon + 1, tms->tm_year + 1900, tms->tm_hour, tms->tm_min, tms->tm_sec);
-	if (res < 0) {
-		return {};
-	}
-	return {buffer, 19};
+	return fmt::format("{:%d/%m/%Y %H:%M:%S}", fmt::localtime(time));
 }
 
 std::string formatDateShort(time_t time)
 {
-	const tm* tms = localtime(&time);
-	if (!tms) {
-		return {};
-	}
-
-	char buffer[12];
-	size_t res = strftime(buffer, 12, "%d %b %Y", tms);
-	if (res == 0) {
-		return {};
-	}
-	return {buffer, 11};
+	return fmt::format("{:%d %b %Y}", fmt::localtime(time));
 }
 
 Direction getDirection(const std::string& string)
