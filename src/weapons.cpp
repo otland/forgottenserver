otland/forgottenserver

commit 07c27637fedc454ad78388beb46db114d8e1b31e

Change vocation map to unordered_set and improve wea.cpponsp code format (#4226)

src/weapons.cpp
@@ -189,10 +189,10 @@ bool Weapon::configureEvent(const pugi::xml_node& node)
 
 		int32_t vocationId = g_vocations.getVocationId(attr.as_string());
 		if (vocationId != -1) {
-			vocWeaponMap[vocationId] = true;
+			vocationWeaponSet.insert(vocationId);
 			int32_t promotedVocation = g_vocations.getPromotedVocation(vocationId);
 			if (promotedVocation != VOCATION_NONE) {
-				vocWeaponMap[promotedVocation] = true;
+				vocationWeaponSet.insert(promotedVocation);
 			}
 
 			if (vocationNode.attribute("showInDescription").as_bool(true)) {
@@ -262,61 +262,83 @@ int32_t Weapon::playerWeaponCheck(Player* player, Creature* target, uint8_t shoo
 		return 0;
 	}
 
-	if (!player->hasFlag(PlayerFlag_IgnoreWeaponCheck)) {
-		if (!enabled) {
-			return 0;
-		}
-
-		if (player->getMana() < getManaCost(player)) {
-			return 0;
-		}
-
-		if (player->getHealth() < getHealthCost(player)) {
-			return 0;
-		}
-
-		if (player->getSoul() < soul) {
-			return 0;
-		}
-
-		if (isPremium() && !player->isPremium()) {
-			return 0;
-		}
-
-		if (!vocWeaponMap.empty()) {
-			if (vocWeaponMap.find(player->getVocationId()) == vocWeaponMap.end()) {
-				return 0;
-			}
-		}
-
-		int32_t damageModifier = 100;
-		if (player->getLevel() < getReqLevel()) {
-			damageModifier = (isWieldedUnproperly() ? damageModifier / 2 : 0);
-		}
-
-		if (player->getMagicLevel() < getReqMagLv()) {
-			damageModifier = (isWieldedUnproperly() ? damageModifier / 2 : 0);
-		}
-		return damageModifier;
+	if (player->hasFlag(PlayerFlag_IgnoreWeaponCheck)) {
+		return 100;
 	}
 
-	return 100;
+	if (!enabled) {
+		return 0;
+	}
+
+	if (player->getMana() < getManaCost(player)) {
+		return 0;
+	}
+
+	if (player->getHealth() < getHealthCost(player)) {
+		return 0;
+	}
+
+	if (player->getSoul() < soul) {
+		return 0;
+	}
+
+	if (isPremium() && !player->isPremium()) {
+		return 0;
+	}
+
+	if (hasVocationWeaponSet(player->getVocationId())) {
+		return 0;
+	}
+
+	int32_t damageModifier = 100;
+
+	if (player->getLevel() < getReqLevel()) {
+		damageModifier = (isWieldedUnproperly() ? damageModifier / 2 : 0);
+	}
+
+	if (player->getMagicLevel() < getReqMagLv()) {
+		damageModifier = (isWieldedUnproperly() ? damageModifier / 2 : 0);
+	}
+
+	return damageModifier;
 }
 
 bool Weapon::ammoCheck(const Player* player) const
 {
-	if (!player->hasFlag(PlayerFlag_IgnoreWeaponCheck)) {
-		if (!enabled || player->getMana() < getManaCost(player) || player->getHealth() < getHealthCost(player) ||
-		    (isPremium() && !player->isPremium()) || player->getLevel() < getReqLevel() ||
-		    player->getMagicLevel() < getReqMagLv() || player->getSoul() < soul) {
-			return false;
-		}
+	if (player->hasFlag(PlayerFlag_IgnoreWeaponCheck)) {
+		return true;
+	}
 
-		if (!vocWeaponMap.empty()) {
-			if (vocWeaponMap.find(player->getVocationId()) == vocWeaponMap.end()) {
-				return false;
-			}
-		}
+	if (!enabled) {
+		return false;
+	}
+
+	if (player->getMana() < getManaCost(player)) {
+		return false;
+	}
+
+	if (player->getHealth() < getHealthCost(player)) {
+		return false;
+	}
+
+	if (isPremium() && !player->isPremium()) {
+		return false;
+	}
+
+	if (player->getLevel() < getReqLevel()) {
+		return false;
+	}
+
+	if (player->getMagicLevel() < getReqMagLv()) {
+		return false;
+	}
+
+	if (player->getSoul() < soul) {
+		return false;
+	}
+
+	if (hasVocationWeaponSet(player->getVocationId())) {
+		return false;
 	}
 
 	return true;

src/weapons.h
@@ -105,12 +105,17 @@ public:
 	uint32_t getWieldInfo() const { return wieldInfo; }
 	void setWieldInfo(uint32_t info) { wieldInfo |= info; }
 
-	void addVocWeaponMap(std::string vocName)
+	const auto& getVocationWeaponSet() const { return vocationWeaponSet; }
+	void addVocationWeaponSet(const std::string& vocationName)
 	{
-		int32_t vocationId = g_vocations.getVocationId(vocName);
+		int32_t vocationId = g_vocations.getVocationId(vocationName);
 		if (vocationId != -1) {
-			vocWeaponMap[vocationId] = true;
+			vocationWeaponSet.insert(vocationId);
 		}
+	}
+	bool hasVocationWeaponSet(uint16_t vocationId) const
+	{
+		return !vocationWeaponSet.empty() && vocationWeaponSet.find(vocationId) != vocationWeaponSet.end();
 	}
 
 	const std::string& getVocationString() const { return vocationString; }
@@ -119,7 +124,7 @@ public:
 	WeaponAction_t action = WEAPONACTION_NONE;
 	CombatParams params;
 	WeaponType_t weaponType;
-	std::map<uint16_t, bool> vocWeaponMap;
+	std::unordered_set<uint16_t> vocationWeaponSet;
 
 protected:
 	void internalUseWeapon(Player* player, Item* item, Creature* target, int32_t damageModifier) const;

src/luascript.cpp
@@ -10413,8 +10413,7 @@ int LuaScriptInterface::luaPlayerCanLearnSpell(lua_State* L)
 		return 1;
 	}
 
-	const auto& vocMap = spell->getVocMap();
-	if (vocMap.count(player->getVocationId()) == 0) {
+	if (!spell->hasVocationSpellMap(player->getVocationId())) {
 		pushBoolean(L, false);
 	} else if (player->getLevel() < spell->getLevel()) {
 		pushBoolean(L, false);
@@ -16114,8 +16113,8 @@ int LuaScriptInterface::luaSpellVocation(lua_State* L)
 	if (lua_gettop(L) == 1) {
 		lua_createtable(L, 0, 0);
 		int i = 0;
-		for (auto& voc : spell->getVocMap()) {
-			std::string name = g_vocations.getVocation(voc.first)->getVocName();
+		for (auto& vocation : spell->getVocationSpellMap()) {
+			std::string name = g_vocations.getVocation(vocation.first)->getVocName();
 			pushString(L, name);
 			lua_rawseti(L, -2, ++i);
 		}
@@ -16123,8 +16122,7 @@ int LuaScriptInterface::luaSpellVocation(lua_State* L)
 		int parameters = lua_gettop(L) - 1; // - 1 because self is a parameter aswell, which we want to skip ofc
 		for (int i = 0; i < parameters; ++i) {
 			std::vector<std::string> vocList = explodeString(getString(L, 2 + i), ";");
-			spell->addVocMap(g_vocations.getVocationId(vocList[0]),
-			                 vocList.size() > 1 ? booleanString(vocList[1]) : false);
+			spell->addVocationSpellMap(vocList[0], vocList.size() > 1 ? booleanString(vocList[1]) : false);
 		}
 		pushBoolean(L, true);
 	}
@@ -16987,7 +16985,7 @@ int LuaScriptInterface::luaMoveEventVocation(lua_State* L)
 	// moveevent:vocation(vocName[, showInDescription = false, lastVoc = false])
 	MoveEvent* moveevent = getUserdata<MoveEvent>(L, 1);
 	if (moveevent) {
-		moveevent->addVocEquipMap(getString(L, 2));
+		moveevent->addVocationEquipSet(getString(L, 2));
 		moveevent->setWieldInfo(WIELDINFO_VOCREQ);
 		std::string tmp;
 		bool showInDescription = false;
@@ -17602,7 +17600,7 @@ int LuaScriptInterface::luaWeaponVocation(lua_State* L)
 	// weapon:vocation(vocName[, showInDescription = false, lastVoc = false])
 	Weapon* weapon = getUserdata<Weapon>(L, 1);
 	if (weapon) {
-		weapon->addVocWeaponMap(getString(L, 2));
+		weapon->addVocationWeaponSet(getString(L, 2));
 		weapon->setWieldInfo(WIELDINFO_VOCREQ);
 		std::string tmp;
 		bool showInDescription = getBoolean(L, 3, false);

src/movement.cpp
@@ -642,7 +642,7 @@ bool MoveEvent::configureEvent(const pugi::xml_node& node)
 
 			int32_t vocationId = g_vocations.getVocationId(vocationNameAttribute.as_string());
 			if (vocationId != -1) {
-				vocEquipMap[vocationId] = true;
+				vocationEquipSet.insert(vocationId);
 				if (vocationNode.attribute("showInDescription").as_bool(true)) {
 					vocStringList.push_back(
 					    boost::algorithm::to_lower_copy<std::string>(vocationNameAttribute.as_string()));
@@ -650,7 +650,7 @@ bool MoveEvent::configureEvent(const pugi::xml_node& node)
 			}
 		}
 
-		if (!vocEquipMap.empty()) {
+		if (!vocationEquipSet.empty()) {
 			wieldInfo |= WIELDINFO_VOCREQ;
 		}
 
@@ -703,8 +703,7 @@ uint32_t MoveEvent::RemoveItemField(Item*, Item*, const Position&) { return 1; }
 ReturnValue MoveEvent::EquipItem(MoveEvent* moveEvent, Player* player, Item* item, slots_t slot, bool isCheck)
 {
 	if (!player->hasFlag(PlayerFlag_IgnoreWeaponCheck) && moveEvent->getWieldInfo() != 0) {
-		const VocEquipMap& vocEquipMap = moveEvent->getVocEquipMap();
-		if (!vocEquipMap.empty() && vocEquipMap.find(player->getVocationId()) == vocEquipMap.end()) {
+		if (!moveEvent->hasVocationEquipSet(player->getVocationId())) {
 			return RETURNVALUE_YOUDONTHAVEREQUIREDPROFESSION;
 		}
 

src/movement.h
@@ -35,8 +35,6 @@ struct MoveEventList
 	std::list<MoveEvent> moveEvent[MOVE_EVENT_LAST];
 };
 
-using VocEquipMap = std::map<uint16_t, bool>;
-
 class MoveEvents final : public BaseEvents
 {
 public:
@@ -119,13 +117,17 @@ public:
 	const std::string& getVocationString() const { return vocationString; }
 	void setVocationString(const std::string& str) { vocationString = str; }
 	uint32_t getWieldInfo() const { return wieldInfo; }
-	const VocEquipMap& getVocEquipMap() const { return vocEquipMap; }
-	void addVocEquipMap(std::string vocName)
+	const auto& getVocationEquipSet() const { return vocationEquipSet; }
+	void addVocationEquipSet(const std::string& vocationName)
 	{
-		int32_t vocationId = g_vocations.getVocationId(vocName);
+		int32_t vocationId = g_vocations.getVocationId(vocationName);
 		if (vocationId != -1) {
-			vocEquipMap[vocationId] = true;
+			vocationEquipSet.insert(vocationId);
 		}
+	}
+	bool hasVocationEquipSet(uint16_t vocationId) const
+	{
+		return !vocationEquipSet.empty() && vocationEquipSet.find(vocationId) != vocationEquipSet.end();
 	}
 	bool getTileItem() const { return tileItem; }
 	void setTileItem(bool b) { tileItem = b; }
@@ -176,7 +178,7 @@ private:
 	bool premium = false;
 	std::string vocationString;
 	uint32_t wieldInfo = 0;
-	VocEquipMap vocEquipMap;
+	std::unordered_set<uint16_t> vocationEquipSet;
 	bool tileItem = false;
 
 	std::vector<uint32_t> itemIdRange;

src/spells.cpp
@@ -15,7 +15,6 @@
 extern Game g_game;
 extern Spells* g_spells;
 extern Monsters g_monsters;
-extern Vocations g_vocations;
 extern ConfigManager g_config;
 extern LuaEnvironment g_luaEnvironment;
 
@@ -510,7 +509,7 @@ bool Spell::configureSpell(const pugi::xml_node& node)
 		int32_t vocationId = g_vocations.getVocationId(attr.as_string());
 		if (vocationId != -1) {
 			attr = vocationNode.attribute("showInDescription");
-			vocSpellMap[vocationId] = !attr || attr.as_bool();
+			vocationSpellMap[vocationId] = !attr || attr.as_bool();
 		} else {
 			std::cout << "[Warning - Spell::configureSpell] Wrong vocation name: " << attr.as_string() << std::endl;
 		}
@@ -592,7 +591,7 @@ bool Spell::playerSpellCheck(Player* player) const
 			g_game.addMagicEffect(player->getPosition(), CONST_ME_POFF);
 			return false;
 		}
-	} else if (!vocSpellMap.empty() && vocSpellMap.find(player->getVocationId()) == vocSpellMap.end()) {
+	} else if (!hasVocationSpellMap(player->getVocationId())) {
 		player->sendCancelMessage(RETURNVALUE_YOURVOCATIONCANNOTUSETHISSPELL);
 		g_game.addMagicEffect(player->getPosition(), CONST_ME_POFF);
 		return false;
@@ -1056,7 +1055,7 @@ bool InstantSpell::canCast(const Player* player) const
 			return true;
 		}
 	} else {
-		if (vocSpellMap.empty() || vocSpellMap.find(player->getVocationId()) != vocSpellMap.end()) {
+		if (vocationSpellMap.empty() || hasVocationSpellMap(player->getVocationId())) {
 			return true;
 		}
 	}

src/spells.h
@@ -9,14 +9,16 @@
 #include "creature.h"
 #include "luascript.h"
 #include "talkaction.h"
+#include "vocation.h"
 
 class InstantSpell;
 class RuneSpell;
 class Spell;
 
-using VocSpellMap = std::map<uint16_t, bool>;
 using InstantSpell_ptr = std::unique_ptr<InstantSpell>;
 using RuneSpell_ptr = std::unique_ptr<RuneSpell>;
+
+extern Vocations g_vocations;
 
 class Spells final : public BaseEvents
 {
@@ -131,8 +133,18 @@ public:
 	bool isLearnable() const { return learnable; }
 	void setLearnable(bool l) { learnable = l; }
 
-	const VocSpellMap& getVocMap() const { return vocSpellMap; }
-	void addVocMap(uint16_t n, bool b) { vocSpellMap[n] = b; }
+	const auto& getVocationSpellMap() const { return vocationSpellMap; }
+	void addVocationSpellMap(const std::string& vocationName, bool showInDescription)
+	{
+		int32_t vocationId = g_vocations.getVocationId(vocationName);
+		if (vocationId != -1) {
+			vocationSpellMap[vocationId] = showInDescription;
+		}
+	}
+	bool hasVocationSpellMap(uint16_t vocationId) const
+	{
+		return !vocationSpellMap.empty() && vocationSpellMap.find(vocationId) != vocationSpellMap.end();
+	}
 
 	const SpellGroup_t getGroup() const { return group; }
 	void setGroup(SpellGroup_t g) { group = g; }
@@ -173,7 +185,7 @@ protected:
 	bool playerInstantSpellCheck(Player* player, const Position& toPos);
 	bool playerRuneSpellCheck(Player* player, const Position& toPos);
 
-	VocSpellMap vocSpellMap;
+	std::map<uint16_t, bool> vocationSpellMap;
 
 	SpellGroup_t group = SPELLGROUP_NONE;
 	SpellGroup_t secondaryGroup = SPELLGROUP_NONE;

commit 071eed302abe4228825a9d8947058e3079047f32

weapon problems introduced in #4226 fixed.

src/weapons.cpp
@@ -286,7 +286,7 @@ int32_t Weapon::playerWeaponCheck(Player* player, Creature* target, uint8_t shoo
 		return 0;
 	}
 
-	if (hasVocationWeaponSet(player->getVocationId())) {
+	if (!hasVocationWeaponSet(player->getVocationId())) {
 		return 0;
 	}
 
@@ -337,7 +337,7 @@ bool Weapon::ammoCheck(const Player* player) const
 		return false;
 	}
 
-	if (hasVocationWeaponSet(player->getVocationId())) {
+	if (!hasVocationWeaponSet(player->getVocationId())) {
 		return false;
 	}
 

src/weapons.h
@@ -115,7 +115,7 @@ public:
 	}
 	bool hasVocationWeaponSet(uint16_t vocationId) const
 	{
-		return !vocationWeaponSet.empty() && vocationWeaponSet.find(vocationId) != vocationWeaponSet.end();
+		return vocationWeaponSet.empty() || vocationWeaponSet.find(vocationId) != vocationWeaponSet.end();
 	}
 
 	const std::string& getVocationString() const { return vocationString; }