d821607ce1ed261749e39f17f83dd8a035daa7c3
Matheus Belo <matheus_belo@id.uff.br>
2022-12-14 19:43:06 -0300

    Fix bug in Map::placeCreature when using extended positions (#4270)
    Co-authored-by: Cezar Guimarães <cezar.guimaraes@outlook.com>

--- src/map.cpp
+++ src/map.cpp
@@ -199,13 +199,8 @@ bool Map::placeCreature(const Position& centerPos, Creature* creature, bool exte
 	}
 
 	if (!foundTile) {
-		static std::vector<std::pair<int32_t, int32_t>> extendedRelList {
-			                   {0, -2},
-			         {-1, -1}, {0, -1}, {1, -1},
-			{-2, 0}, {-1,  0},          {1,  0}, {2, 0},
-			         {-1,  1}, {0,  1}, {1,  1},
-			                   {0,  2}
-		};
+		static std::vector<std::pair<int32_t, int32_t>> extendedRelList{
+		    {0, -2}, {2, 0}, {0, 2}, {-2, 0}, {-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
 
 		static std::vector<std::pair<int32_t, int32_t>> normalRelList {
 			{-1, -1}, {0, -1}, {1, -1},
