3a180d82f7d406884c8593830eb0e4f9de730033
Zbizu <Zbizu@users.noreply.github.com>
2023-01-30 03:17:50 +0100

    fix invalid argument in spell:group (#4305)

--- src/luascript.cpp
+++ src/luascript.cpp
@@ -15794,7 +15794,7 @@ int LuaScriptInterface::luaSpellGroup(lua_State* L)
 			}
 		} else {
 			SpellGroup_t primaryGroup = getNumber<SpellGroup_t>(L, 2);
-			SpellGroup_t secondaryGroup = getNumber<SpellGroup_t>(L, 2);
+			SpellGroup_t secondaryGroup = getNumber<SpellGroup_t>(L, 3);
 			if (primaryGroup && secondaryGroup) {
 				spell->setGroup(primaryGroup);
 				spell->setSecondaryGroup(secondaryGroup);
