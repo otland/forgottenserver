ff0d387be6fa0a792af5c2af3bcf9d730ff066e0
Marco Oliveira <marcomoa@hotmail.com>
2023-02-01 19:07:57 -0300

    Change default value

--- src/configmanager.cpp
+++ src/configmanager.cpp
@@ -230,7 +230,7 @@ bool ConfigManager::load()
 	boolean[REMOVE_ON_DESPAWN] = getGlobalBoolean(L, "removeOnDespawn", true);
 	boolean[PLAYER_CONSOLE_LOGS] = getGlobalBoolean(L, "showPlayerLogInConsole", true);
 	boolean[TWO_FACTOR_AUTH] = getGlobalBoolean(L, "enableTwoFactorAuth", true);
-	boolean[CHECK_DUPLICATES_VALUES] = getGlobalBoolean(L, "checkDuplicatesValues", true);
+	boolean[CHECK_DUPLICATES_VALUES] = getGlobalBoolean(L, "checkDuplicatesValues", false);
 
 	string[DEFAULT_PRIORITY] = getGlobalString(L, "defaultPriority", "high");
 	string[SERVER_NAME] = getGlobalString(L, "serverName", "");
