8326237054d6118e6c2ace56477713110504f876
Marco Oliveira <marcomoa@hotmail.com>
2023-02-18 19:15:05 -0300

    feat: Monster overspawn (#4328)
    Authored-By: Marcin Michalski <57528542+marmichalski@users.noreply.github.com>

--- src/configmanager.cpp
+++ src/configmanager.cpp
@@ -231,6 +231,7 @@ bool ConfigManager::load()
 	boolean[PLAYER_CONSOLE_LOGS] = getGlobalBoolean(L, "showPlayerLogInConsole", true);
 	boolean[TWO_FACTOR_AUTH] = getGlobalBoolean(L, "enableTwoFactorAuth", true);
 	boolean[CHECK_DUPLICATE_STORAGE_KEYS] = getGlobalBoolean(L, "checkDuplicateStorageKeys", false);
+	boolean[MONSTER_OVERSPAWN] = getGlobalBoolean(L, "monsterOverspawn", false);
 
 	string[DEFAULT_PRIORITY] = getGlobalString(L, "defaultPriority", "high");
 	string[SERVER_NAME] = getGlobalString(L, "serverName", "");
