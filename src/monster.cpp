37f4f86165b5154a220972d248428c220218ac7a
Marco Oliveira <marcomoa@hotmail.com>
2023-04-02 00:00:55 -0300

    Update monster.cpp
    Co-Authored-By: Marcin Michalski <57528542+marmichalski@users.noreply.github.com>

--- src/monster.cpp
+++ src/monster.cpp
@@ -810,7 +810,7 @@ void Monster::doAttacking(uint32_t interval)
 	for (const spellBlock_t& spellBlock : mType->info.attackSpells) {
 		bool inRange = false;
 
-		if (attackedCreature == nullptr) {
+		if (!attackedCreature) {
 			break;
 		}
 
