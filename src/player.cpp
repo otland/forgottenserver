b4c826518ae4cb2b28a6441a71ace97e60acee89
Marco Oliveira <marcomoa@hotmail.com>
2023-04-05 02:36:44 -0300

    Add option to look at items from NPC trade window (#4372)
    Co-authored-by: Diego-OT <52421038+Diego-OT@users.noreply.github.com>

--- src/player.cpp
+++ src/player.cpp
@@ -3343,7 +3343,7 @@ bool Player::hasShopItemForSale(uint32_t itemId, uint8_t subType) const
 {
 	const ItemType& itemType = Item::items[itemId];
 	return std::any_of(shopItemList.begin(), shopItemList.end(), [&](const ShopInfo& shopInfo) {
-		return shopInfo.itemId == itemId && shopInfo.buyPrice != 0 &&
+		return shopInfo.itemId == itemId && (shopInfo.buyPrice != 0 || shopInfo.sellPrice != 0) &&
 		       (!itemType.isFluidContainer() || shopInfo.subType == subType);
 	});
 }
