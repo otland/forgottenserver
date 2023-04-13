52e357772b3b5c4d41bedbe0ff0e94ba76391480
Sarah Wesker <sarahelizabetwesker@gmail.com>
2023-04-12 22:48:21 -0400

    fix lastQuestUpdate
    Co-authored-by: Zbizu <Zbizu@users.noreply.github.com>

--- data/scripts/eventcallbacks/player/default_onUpdateStorage.lua
+++ data/scripts/eventcallbacks/player/default_onUpdateStorage.lua
@@ -13,7 +13,7 @@ ec.onUpdateStorage = function(player, key, value, oldValue, isLogin)
         lastQuestUpdate[playerId] = now
     end
 
-    if now - lastQuestUpdate[playerId] <= 0 and Game.isQuestStorage(key, value, oldValue) then
+    if lastQuestUpdate[playerId] - now <= 0 and Game.isQuestStorage(key, value, oldValue) then
         lastQuestUpdate[playerId] = os.mtime() + 100
         player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Your questlog has been updated.")
     end
