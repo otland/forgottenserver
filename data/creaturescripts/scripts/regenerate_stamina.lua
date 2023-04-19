3f0b89392c918658139b9955d4b2318acd1f791c
Marco Oliveira <marcomoa@hotmail.com>
2023-04-19 15:39:50 -0300

    improvements

--- data/creaturescripts/scripts/regenerate_stamina.lua
+++ data/creaturescripts/scripts/regenerate_stamina.lua
@@ -12,14 +12,14 @@ function onLogin(player)
 	end
 
 	local staminaMinutes = player:getStamina()
-	local maxNormalStaminaRegen = math.floor((3600 / 6) * (100 / (100 + staminaMinutes)))
+	local maxNormalStaminaRegen = 2340 - math.min(2340, staminaMinutes)
 
-	local regainStaminaSeconds = offlineTime / configManager.getNumber(configKeys.STAMINA_REGEN_MINUTE) * 60
-	if regainStaminaSeconds > maxNormalStaminaRegen then
-		local happyHourStaminaRegen = (regainStaminaSeconds - maxNormalStaminaRegen) * configManager.getNumber(configKeys.STAMINA_REGEN_PREMIUM) / 180
-		staminaMinutes = math.min(2520, math.max(2340, staminaMinutes) + happyHourStaminaRegen / 60)
+	local regainStaminaMinutes = offlineTime / configManager.getNumber(configKeys.STAMINA_REGEN_MINUTE)
+	if regainStaminaMinutes > maxNormalStaminaRegen then
+		local happyHourStaminaRegen = (offlineTime - (maxNormalStaminaRegen * 180)) / configManager.getNumber(configKeys.STAMINA_REGEN_PREMIUM)
+		staminaMinutes = math.min(2520, math.max(2340, staminaMinutes) + happyHourStaminaRegen)
 	else
-		staminaMinutes = staminaMinutes + regainStaminaSeconds / 60
+		staminaMinutes = staminaMinutes + regainStaminaMinutes
 	end
 
 	player:setStamina(staminaMinutes)
