51c13d0edd9269db74eec99846bc3ac74762d715
MillhioreBT <sarahelizabetwesker@gmail.com>
2023-04-24 17:48:03 -0400

    remove unnecessary reserve

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -3136,7 +3136,6 @@ void ProtocolGame::sendOutfitWindow()
 	msg.add<uint16_t>(0); // current familiar looktype
 
 	std::vector<ProtocolOutfit> protocolOutfits;
-	protocolOutfits.reserve(outfits.size());
 	if (player->isAccessPlayer()) {
 		protocolOutfits.emplace_back("Gamemaster", 75, 0);
 	}
@@ -3236,7 +3235,6 @@ void ProtocolGame::sendPodiumWindow(const Item* item)
 
 	// add GM outfit for staff members
 	std::vector<ProtocolOutfit> protocolOutfits;
-	protocolOutfits.reserve(outfits.size());
 	if (player->isAccessPlayer()) {
 		protocolOutfits.emplace_back("Gamemaster", 75, 0);
 	}
