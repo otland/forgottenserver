22bcd024920441ce52871dc261e0549c054e4dd3
Marco Oliveira <marcomoa@hotmail.com>
2023-05-02 01:12:38 -0300

    fix for display update

--- data/scripts/creaturescripts/update_client_xp_gain_rate.lua
+++ data/scripts/creaturescripts/update_client_xp_gain_rate.lua
@@ -1,7 +1,7 @@
 local updateClientExpGainRate = CreatureEvent("Update Client Xp Gain Rate")
 
 function updateClientExpGainRate.onAdvance(player, skill, oldLevel, newLevel)
-	if skill ~= SKILL_LEVEL or newLevel <= oldLevel then
+	if skill ~= SKILL_LEVEL then
 		return true
 	end
 
