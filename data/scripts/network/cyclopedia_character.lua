711a074956c2c28a88a65f1d8187fbbe3896a7ea
Marco Oliveira <marcomoa@hotmail.com>
2023-05-24 02:09:14 -0300

    Cyclopedia Character: Send General Stats

--- data/scripts/network/cyclopedia_character.lua
+++ data/scripts/network/cyclopedia_character.lua
@@ -24,10 +24,78 @@ local function sendBasicInfo(self, msg)
 	return true
 end
 
+local function getLevelPercent(player)
+	local currentLevelExp = Game.getExperienceForLevel(player:getLevel())
+	local nextLvlExp = Game.getExperienceForLevel(player:getLevel() +1)
+	local playerExp = player:getExperience()
+	return (playerExp - currentLevelExp) * 100 / (nextLvlExp - currentLevelExp)
+end
+
+local function sendGeneralStats(self, msg)
+	msg:addU64(self:getExperience())
+	msg:addU16(self:getLevel())
+	msg:addByte(math.floor(getLevelPercent(self)))
+
+	msg:addU16(self:getClientExpDisplay())
+	msg:addU32(0) -- turnament exp (deprecated)
+	msg:addU16(0) -- low level bonus
+	msg:addU16(0) -- store exp bonus
+	msg:addU16(self:getClientStaminaBonusDisplay())
+	msg:addU16(0) -- exp boost remaining time
+	msg:addByte(0) -- enable exp boost store button
+
+	msg:addU16(self:getHealth())
+	msg:addU16(self:getMaxHealth())
+	msg:addU16(self:getMana())
+	msg:addU16(self:getMaxMana())
+	msg:addByte(self:getSoul())
+	msg:addU16(self:getStamina())
+
+	local foodTime = 0
+	local condition = self:getCondition(CONDITION_REGENERATION, CONDITIONID_DEFAULT)
+	if condition then
+		foodTime = condition:getTicks() /  1000
+	end
+
+	msg:addU16(foodTime)
+	msg:addU16(self:getOfflineTrainingTime() / 60 / 1000)
+	msg:addU16(self:getSpeed() / 2)
+	msg:addU16(self:getBaseSpeed() / 2)
+	msg:addU32(self:getCapacity()) -- cap + boost
+	msg:addU32(self:getCapacity())
+	msg:addU32(self:getFreeCapacity())
+
+	msg:addByte(8) -- ??
+	msg:addByte(1) -- ??
+
+	msg:addU16(self:getMagicLevel())
+	msg:addU16(self:getBaseMagicLevel())
+
+	-- loyalty bonus
+	msg:addU16(self:getBaseMagicLevel())
+	msg:addU16(self:getSkillPercent(SKILL_MAGLEVEL))
+
+	for i = SKILL_FIST, SKILL_FISHING, 1 do
+		msg:addByte(cyclopediaSkillMap[skillId])
+		msg:addU16(self:getEffectiveSkillLevel(i))
+		msg:addU16(self:getSkillLevel(i))
+
+		-- loyalty bonus
+		msg:addU16(self:getSkillLevel(i))
+		msg:addU16(self:getSkillPercent(i) * 100)
+	end
+
+	msg:addByte(0)
+
+	msg:sendToPlayer(self)
+	msg:delete()
+	return true
+end
+
 local BASIC_INFO = 0x00
+local GENERAL_STATS = 0x01
 
 --[[
-	local GENERAL_STATS = 0x01
 	local COMBAT_STATS = 0x02
 	local RECENT_DEATHS = 0x03
 	local RECENT_PVP_KILLS = 0x04
@@ -42,8 +110,9 @@ local BASIC_INFO = 0x00
 
 local handlers = {
 	[BASIC_INFO] = sendBasicInfo,
+	[GENERAL_STATS] = sendGeneralStats,
+
 	--[[
-		[GENERAL_STATS] = "sendGeneralStats",
 		[COMBAT_STATS] = "sendCombatStats",
 		[RECENT_DEATHS] = "sendRecentDeaths",
 		[RECENT_PVP_KILLS] = "sendRecentPvpKills",
