bb9635e07e679ee88bbf207b4cd2b073a29657b2
MillhioreBT <sarahelizabetwesker@gmail.com>
2023-05-28 11:25:45 -0400

    fix fmt exception

--- src/definitions.h
+++ src/definitions.h
@@ -47,6 +47,7 @@ static constexpr auto AUTHENTICATOR_PERIOD = 30U;
 #pragma warning(disable : 4319) // '~': zero extending 'unsigned int' to 'lua_Number' of greater size
 #pragma warning(disable : 4351) // new behavior: elements of array will be default initialized
 #pragma warning(disable : 4458) // declaration hides class member
+#pragma warning(disable : 4275) // can be ignored in Visual C++ if you are deriving from a type in the C++ STL
 #endif
 
 #ifndef _WIN32_WINNT
