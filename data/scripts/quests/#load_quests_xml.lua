5c9230faea22f04a3fcf939a68ce87c60031bc80
MillhioreBT <sarahelizabetwesker@gmail.com>
2023-05-30 22:52:20 -0400

    better load_quests_xml.lua

--- data/scripts/quests/#load_quests_xml.lua
+++ data/scripts/quests/#load_quests_xml.lua
@@ -1,53 +1,47 @@
---If you don't intend to use quests.xml, you can delete this file.
-local globalEvent = GlobalEvent("LoadQuestsXml")
+-- If you don't intend to use quests.xml, you can delete this file.
+local xmlQuest = GlobalEvent("Load XML Quests")
 
-function globalEvent.onStartup()
-	local questXml = XMLDocument("data/XML/quests.xml")
-	if not questXml then
-		io.write("[Warning - GlobalEvent::onStartup] Cannot load quests.xml!")
-		return true
-	end
+function xmlQuest.onStartup()
+    local questDoc = XMLDocument("data/XML/quests.xml")
+    if not questDoc then
+        io.write(
+            "[Warning - GlobalEvent::onStartup] Could not load quests.xml.\n")
+        return true
+    end
 
-	local child = questXml:child("quests")
-	local firstChild = child:firstChild()
+    local quests = questDoc:child("quests")
+    for questNode in quests:children() do
+        local missions = {}
+        for missionNode in questNode:children() do
+            local mission = {
+                name = missionNode:attribute("name") or "",
+                storageId = tonumber(missionNode:attribute("storageid")),
+                startValue = tonumber(missionNode:attribute("startvalue")),
+                endValue = tonumber(missionNode:attribute("endvalue")),
+                ignoreEndValue = tobool(missionNode:attribute("ignoreendvalue")),
+                description = missionNode:attribute("description")
+            }
 
-	while firstChild do
-		local missionNode = firstChild:firstChild()
-		local missions = {}
-		while missionNode do
-			local mission = {
-				name = tostring(missionNode:attribute("name")),
-				storageId = tonumber(missionNode:attribute("storageid")),
-				startValue = tonumber(missionNode:attribute("startvalue")),
-				endValue = tonumber(missionNode:attribute("endvalue")),
-				ignoreEndValue = table.contains({'1', 'y', 't'}, tostring(missionNode:attribute("ignoreendvalue"):sub(1, 1):lower())),
-				description = missionNode:attribute("description")
-			}
+            if not mission.description then
+                local description = {}
+                for missionState in missionNode:children() do
+                    description[tonumber(missionState:attribute("id"))] =
+                        missionState:attribute("description") or ""
+                end
 
-			if not mission.description then
-				local missionState = missionNode:firstChild()
-				local description = {}
-				while missionState do
-					description[tonumber(missionState:attribute("id"))] = missionState:attribute("description") or ""
-					missionState = missionState:nextSibling()
-				end
+                mission.description = description
+            end
 
-				mission.description = description
-			end
+            missions[#missions + 1] = mission
+        end
 
-			missions[#missions + 1] = mission
-			missionNode = missionNode:nextSibling()
-		end
-
-		Game.createQuest(firstChild:attribute("name"), {
-			storageId = tonumber(firstChild:attribute("startstorageid")),
-			storageValue = tonumber(firstChild:attribute("startstoragevalue")),
-			missions = missions
-		}):register()
-
-		firstChild = firstChild:nextSibling()
-	end
-	return true
+        Game.createQuest(questNode:attribute("name"), {
+            storageId = tonumber(questNode:attribute("startstorageid")),
+            storageValue = tonumber(questNode:attribute("startstoragevalue")),
+            missions = missions
+        }):register()
+    end
+    return true
 end
 
-globalEvent:register()
+xmlQuest:register()
