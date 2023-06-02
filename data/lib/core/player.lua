78195c72f362e8917c0df63d0ee1f5b865f7f7c1
Marco <marcomoa@hotmail.com>
2023-06-01 22:09:26 -0300

    feat: client take screenshot (#4459)

--- data/lib/core/player.lua
+++ data/lib/core/player.lua
@@ -617,6 +617,19 @@ function Player.sendHighscores(self, entries, params)
 	return true
 end
 
+function Player.takeScreenshot(self, screenshotType, ignoreConfig)
+	if not ignoreConfig and (screenshotType < SCREENSHOT_TYPE_FIRST or screenshotType > SCREENSHOT_TYPE_LAST) then
+		return false
+	end
+
+	local msg = NetworkMessage()
+	msg:addByte(0x75)
+	msg:addByte(screenshotType)
+	msg:sendToPlayer(self)
+	msg:delete()
+	return true
+end
+
 function Player.getBlessings(self)
 	local blessings = 0
 	for i = 1, 6 do
