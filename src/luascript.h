66d079615b5c684294e1ddffc54afd6c52fa0431
Ranieri Althoff <1993083+ranisalt@users.noreply.github.com>
2023-07-09 16:57:31 +0200

    Add storage values to creatures (#4495)

--- src/luascript.h
+++ src/luascript.h
@@ -840,6 +840,9 @@ private:
 
 	static int luaCreatureGetZone(lua_State* L);
 
+	static int luaCreatureGetStorageValue(lua_State* L);
+	static int luaCreatureSetStorageValue(lua_State* L);
+
 	// Player
 	static int luaPlayerCreate(lua_State* L);
 
@@ -936,9 +939,6 @@ private:
 
 	static int luaPlayerGetBankBalance(lua_State* L);
 	static int luaPlayerSetBankBalance(lua_State* L);
-
-	static int luaPlayerGetStorageValue(lua_State* L);
-	static int luaPlayerSetStorageValue(lua_State* L);
 
 	static int luaPlayerAddItem(lua_State* L);
 	static int luaPlayerAddItemEx(lua_State* L);
