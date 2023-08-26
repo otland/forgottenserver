88c2d13df2a73a3333bee271a1162eb37222f61d
tenlilnet <tenlilnet@mailfence.com>
2023-08-26 12:59:22 -0400

    Dc f*ed up space breaks

--- src/map.cpp
+++ src/map.cpp
@@ -516,7 +516,7 @@ bool checkSteepLine(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint8_t 
 	if (y1 < y0) {
 		slope *= -1;
 	}
-    float yi = y0 + slope + (y0 == y1 ? 0 : ((reversed && y0 < y1) || (!reversed && y1 < y0) ? 0.85 : 0.05));
+	float yi = y0 + slope + (y0 == y1 ? 0 : ((reversed && y0 < y1) || (!reversed && y1 < y0) ? 0.85 : 0.05));
 
 	for (uint16_t x = x0 + 1; x < x1; ++x) {
 		// g_game.addMagicEffect(Position(std::floor(yi + 0.1),x,z), CONST_ME_POFF); // debugging
@@ -527,28 +527,28 @@ bool checkSteepLine(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint8_t 
 		yi += slope;
 	}
 	
-    return true;
+	return true;
 }
 
 bool checkSlightLine(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint8_t z, bool reversed)
 {
-    float dx = x1 - x0;
-    float slope = (dx == 0) ? 1 : std::abs((y1 - y0) / dx);
-    if (y1 < y0) {
-        slope *= -1;
-    }
-    float yi = y0 + slope + (y0 == y1 ? 0 : ((reversed && y0 < y1) || (!reversed && y1 < y0) ? 0.05 : 0.85));
+	float dx = x1 - x0;
+	float slope = (dx == 0) ? 1 : std::abs((y1 - y0) / dx);
+	if (y1 < y0) {
+		slope *= -1;
+	}
+	float yi = y0 + slope + (y0 == y1 ? 0 : ((reversed && y0 < y1) || (!reversed && y1 < y0) ? 0.05 : 0.85));
 
-    for (uint16_t x = x0 + 1; x < x1; ++x) {
-        // g_game.addMagicEffect(Position(x,std::floor(yi + 0.1),z), CONST_ME_EXPLOSIONHIT); // debugging
-        //0.1 is necessary to avoid loss of precision during calculation
-        if (!g_game.map.isTileClear(x, std::floor(yi + 0.1), z)) {
-            return false;
-        }
-        yi += slope;
-    }
+	for (uint16_t x = x0 + 1; x < x1; ++x) {
+		// g_game.addMagicEffect(Position(x,std::floor(yi + 0.1),z), CONST_ME_EXPLOSIONHIT); // debugging
+		//0.1 is necessary to avoid loss of precision during calculation
+		if (!g_game.map.isTileClear(x, std::floor(yi + 0.1), z)) {
+			return false;
+		}
+		yi += slope;
+	}
 
-    return true;
+	return true;
 }
 
 } // namespace
