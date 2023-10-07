7463aed2f5b586bc94ad65c1096df4117b22796d
MillhioreBT <sarahelizabetwesker@gmail.com>
2023-10-07 01:43:20 -0400

    Only notify if the master is a player

--- data/lib/core/creature.lua
+++ data/lib/core/creature.lua
@@ -105,7 +105,10 @@ function Creature:addSummon(monster)
 	summon:setDropLoot(false)
 	summon:setSkillLoss(false)
 	summon:setMaster(self)
-	summon:getPosition():notifySummonAppear(summon)
+
+	if self:isPlayer() then
+		summon:getPosition():notifySummonAppear(summon)
+	end
 
 	return true
 end
