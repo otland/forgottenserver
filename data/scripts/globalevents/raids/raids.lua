67d7925cc00d32b06ed85e22f84e35092d177288
Marco <marcomoa@hotmail.com>
2023-12-13 01:07:32 -0300

    changing spaces to tabs

--- data/scripts/globalevents/raids/raids.lua
+++ data/scripts/globalevents/raids/raids.lua
@@ -9,28 +9,28 @@ local running = nil
 local lastRaidEnd = 0
 
 function event.onTime(interval)
-    io.write("Executing raids event...\n")
-    if running then
-        return
-    end
+	io.write("Executing raids event...\n")
+	if running then
+		return
+	end
 
-    local now = os.mtime()
+	local now = os.mtime()
 
-    local raids = Raid.getRaids()
-    for key, raid in pairs(raids) do
-        local chance = (MAX_RAND_RANGE * CHECK_RAIDS_INTERVAL) / raid.interval
-        if now >= lastRaidEnd + raid.margin and chance >= math.random(0, MAX_RAND_RANGE) then
-            running = key
+	local raids = Raid.getRaids()
+	for key, raid in pairs(raids) do
+		local chance = (MAX_RAND_RANGE * CHECK_RAIDS_INTERVAL) / raid.interval
+		if now >= lastRaidEnd + raid.margin and chance >= math.random(0, MAX_RAND_RANGE) then
+			running = key
 
-            io.write("Executing raid: " .. raid.name .. "\n")
-            raid:execute()
+			io.write("Executing raid: " .. raid.name .. "\n")
+			raid:execute()
 
-            if not raid.repeats then
-                raids[key] = nil
-            end
-            break
-        end
-    end
+			if not raid.repeats then
+				raids[key] = nil
+			end
+			break
+		end
+	end
 end
 
 event:register()
