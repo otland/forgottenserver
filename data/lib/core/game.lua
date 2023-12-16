8c6d84b3bd3a47ff95101885c4689d83fef3ba4a
ramon-bernardo <ramonbernardo.contato@gmail.com>
2023-12-16 14:21:20 -0300

    Move save to game core

--- data/lib/core/game.lua
+++ data/lib/core/game.lua
@@ -258,4 +258,33 @@ do
 		end
 		accountsStorage[accountId][key] = value
 	end
+
+	function Game.saveAccountsStorage()
+		local transaction = DBTransaction()
+		if not transaction:begin() then
+			return false
+		end
+	
+		local result = db.query("DELETE FROM `account_storage`")
+		if not result then
+			return false
+		end
+	
+		local accountsStorage = Game.getAccountsStorage()
+		for accountId, accountStorage in pairs(accountsStorage) do
+			local query = DBInsert("INSERT INTO `account_storage` (`account_id`, `key`, `value`) VALUES")
+			for key, value in pairs(accountStorage) do
+				local success = query:addRow(accountId .. ", " .. key .. ", " .. value)
+				if not success then
+					return false
+				end
+			end
+	
+			if not query:execute() then
+				return false
+			end
+		end
+	
+		return transaction:commit()
+	end
 end
--- data/scripts/globalevents/account_storage.lua
+++ data/scripts/globalevents/account_storage.lua
@@ -21,39 +21,10 @@ end
 load:register()
 
 -- save accounts storages
-local function trySaveAccountsStorage()
-	local transaction = DBTransaction()
-	if not transaction:begin() then
-		return false
-	end
-
-	local result = db.query("DELETE FROM `account_storage`")
-	if not result then
-		return false
-	end
-
-	local accountsStorage = Game.getAccountsStorage()
-	for accountId, accountStorage in pairs(accountsStorage) do
-		local query = DBInsert("INSERT INTO `account_storage` (`account_id`, `key`, `value`) VALUES")
-		for key, value in pairs(accountStorage) do
-			local success = query:addRow(accountId .. ", " .. key .. ", " .. value)
-			if not success then
-				return false
-			end
-		end
-
-		if not query:execute() then
-			return false
-		end
-	end
-
-	return transaction:commit()
-end
-
 local save = GlobalEvent("SaveAccountStorage")
 
 function save.onSave()
-	local success = trySaveAccountsStorage()
+	local success = Game.saveAccountsStorage()
 	if not success then
 		print("Failed to save account-level storage values.")
 	end
