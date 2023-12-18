1bb2951e8b07fc297d5aed217b57d48a21172039
ramon-bernardo <ramonbernardo.contato@gmail.com>
2023-12-18 00:47:35 -0300

    Remove unused stages map from game

--- src/game.h
+++ src/game.h
@@ -512,7 +512,6 @@ private:
 	std::unordered_map<uint32_t, Player*> mappedPlayerGuids;
 	std::unordered_map<uint32_t, Guild*> guilds;
 	std::unordered_map<uint16_t, Item*> uniqueItems;
-	std::map<uint32_t, uint32_t> stages;
 
 	std::list<Item*> decayItems[EVENT_DECAY_BUCKETS];
 	std::list<Creature*> checkCreatureLists[EVENT_CREATURECOUNT];
