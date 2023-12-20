c16929d8b44c87b53f69bdcbde5ed3573d8a3115
ramon-bernardo <ramonbernardo.contato@gmail.com>
2023-12-19 21:28:47 -0300

    Some fix

--- data/scripts/creaturescripts/player/idle_time.lua
+++ data/scripts/creaturescripts/player/idle_time.lua
@@ -5,9 +5,9 @@ local KICK_AFTER_WARN = 1 * 60000 -- 1 minute
 local event = CreatureEvent("RegisterIdleTime")
 
 function event.onLogin(player)
-    if not player:getGroup():getAccess() then
-        player:registerEvent("IdleTime")
-    end
+	if not player:getGroup():getAccess() then
+		player:registerEvent("IdleTime")
+	end
 	return true
 end
 
@@ -17,19 +17,19 @@ event:register()
 event = CreatureEvent("IdleTime")
 
 function event.onThink(player, interval)
-    local tile = Tile(player:getPosition())
-    if not tile or tile:hasFlag(TILESTATE_NOLOGOUT)then
-        return true
-    end
+	local tile = Tile(player:getPosition())
+	if not tile or tile:hasFlag(TILESTATE_NOLOGOUT) then
+		return true
+	end
 
-    local time = player:getIdleTime() + interval
-    player:setIdleTime(time)
-    
-    if time > MAX_IDLE_TIME + KICK_AFTER_WARN then
-        player:remove()
-    elseif player:hasClient() and time == MAX_IDLE_TIME then
-        player:sendTextMessage(MESSAGE_STATUS_WARNING, "There was no variation in your behaviour for " .. kickAfterMinutes .. " minutes. You will be disconnected in one minute if there is no change in your actions until then.")
-    end
+	local time = player:getIdleTime() + interval
+	player:setIdleTime(time)
+	
+	if time > MAX_IDLE_TIME + KICK_AFTER_WARN then
+		player:remove()
+	elseif player:hasClient() and time == MAX_IDLE_TIME then
+		player:sendTextMessage(MESSAGE_STATUS_WARNING, "There was no variation in your behaviour for " .. kickAfterMinutes .. " minutes. You will be disconnected in one minute if there is no change in your actions until then.")
+	end
 	return true
 end
 
