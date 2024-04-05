4a8b11a9f8b8cfb47e5249d8f235406208c0122e
NRH-AA <nheinz1995@live.com>
2024-04-05 08:22:01 -0700

    Fix summon walk behavior

--- src/monster.cpp
+++ src/monster.cpp
@@ -1178,21 +1178,26 @@ bool Monster::getNextStep(Direction& direction, uint32_t& flags)
 			result = getRandomStep(getPosition(), direction);
 		}
 	} else if ((isSummon() && isMasterInRange) || followCreature || walkingToSpawn) {
-		randomStepping = false;
-		result = Creature::getNextStep(direction, flags);
-		if (result) {
-			flags |= FLAG_PATHFINDING;
+		if (!hasFollowPath && !getMaster()->getPlayer()) {
+			randomStepping = true;
+			result = getRandomStep(getPosition(), direction);
 		} else {
-			if (ignoreFieldDamage) {
-				ignoreFieldDamage = false;
-				updateMapCache();
-			}
-			// target dancing
-			if (attackedCreature && attackedCreature == followCreature) {
-				if (isFleeing()) {
-					result = getDanceStep(getPosition(), direction, false, false);
-				} else if (mType->info.staticAttackChance < static_cast<uint32_t>(uniform_random(1, 100))) {
-					result = getDanceStep(getPosition(), direction);
+			randomStepping = false;
+			result = Creature::getNextStep(direction, flags);
+			if (result) {
+				flags |= FLAG_PATHFINDING;
+			} else {
+				if (ignoreFieldDamage) {
+					ignoreFieldDamage = false;
+					updateMapCache();
+				}
+				// target dancing
+				if (attackedCreature && attackedCreature == followCreature) {
+					if (isFleeing()) {
+						result = getDanceStep(getPosition(), direction, false, false);
+					} else if (mType->info.staticAttackChance < static_cast<uint32_t>(uniform_random(1, 100))) {
+						result = getDanceStep(getPosition(), direction);
+					}
 				}
 			}
 		}
