a945927f0f25e4640373e6ba8dfa15184c8e69bf
EvilHero90 <Shadow280690@hotmail.de>
2024-06-02 01:13:28 +0200

    tfs 1.6

--- src/definitions.h
+++ src/definitions.h
@@ -5,7 +5,7 @@
 #define FS_DEFINITIONS_H
 
 static constexpr auto STATUS_SERVER_NAME = "The Forgotten Server";
-static constexpr auto STATUS_SERVER_VERSION = "1.5";
+static constexpr auto STATUS_SERVER_VERSION = "1.6";
 static constexpr auto STATUS_SERVER_DEVELOPERS = "The Forgotten Server Team";
 
 static constexpr auto CLIENT_VERSION_MIN = 1310;
