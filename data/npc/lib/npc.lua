e9b6383f2fd75399011c889d069cdba4a7dd2053
Siz <102702255+Coldensjo@users.noreply.github.com>
2024-06-04 18:43:17 +0200

    [bugfix] fixes bug when player asks banker to "withdraw" "x" (#4732)
    thrown an error when it didn't find a single number included in the message

--- data/npc/lib/npc.lua
+++ data/npc/lib/npc.lua
@@ -101,31 +101,37 @@ function doPlayerBuyItemContainer(cid, containerid, itemid, count, cost, charges
 	end
 	return true
 end
-
 function getCount(string)
-	local b, e = string:find("%d+")
-	local count = tonumber(string:sub(b, e))
-	if count > 2 ^ 32 - 1 then
-		print("Warning: Casting value to 32bit to prevent crash\n" .. debug.traceback())
-	end
-	return b and e and math.min(2 ^ 32 - 1, count) or -1
+    local b, e = string:find("%d+")
+    if not b then
+        return -1
+    end
+    local count = tonumber(string:sub(b, e))
+    if count > 2 ^ 32 - 1 then
+        print("Warning: Casting value to 32bit to prevent crash\n" .. debug.traceback())
+    end
+    return b and e and math.min(2 ^ 32 - 1, count) or -1
 end
+
 
 function isValidMoney(money)
 	return isNumber(money) and money > 0
 end
 
 function getMoneyCount(string)
-	local b, e = string:find("%d+")
-	local count = tonumber(string:sub(b, e))
-	if count > 2 ^ 32 - 1 then
-		print("Warning: Casting value to 32bit to prevent crash\n" .. debug.traceback())
-	end
-	local money = b and e and math.min(2 ^ 32 - 1, count) or -1
-	if isValidMoney(money) then
-		return money
-	end
-	return -1
+    local b, e = string:find("%d+")
+    if not b then
+        return -1
+    end
+    local count = tonumber(string:sub(b, e))
+    if count > 2 ^ 32 - 1 then
+        print("Warning: Casting value to 32bit to prevent crash\n" .. debug.traceback())
+    end
+    local money = b and e and math.min(2 ^ 32 - 1, count) or -1
+    if isValidMoney(money) then
+        return money
+    end
+    return -1
 end
 
 function getMoneyWeight(money)
