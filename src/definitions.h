751435a6e8368d9eb67044cdb7877e91b2632bb1
EvilHero90 <Shadow280690@hotmail.de>
2024-06-06 00:46:34 +0200

    Update definitions.h

--- src/definitions.h
+++ src/definitions.h
@@ -8,7 +8,6 @@ static constexpr auto STATUS_SERVER_NAME = "The Forgotten Server";
 
 static constexpr auto STATUS_SERVER_VERSION = "1.7";
 static constexpr auto STATUS_SERVER_DEVELOPERS = "The Forgotten Server Team";
-
 static constexpr auto CLIENT_VERSION_MIN = 1310;
 static constexpr auto CLIENT_VERSION_MAX = 1311;
 static constexpr auto CLIENT_VERSION_STR = "13.10";
