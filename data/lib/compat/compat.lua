36bae9e6f7d78ab0641862e1939cbcae0f2f0e98
danyelvarejao <danyelvarejao@gmail.com>
2024-06-06 17:56:24 -0300

    Remove `dump` function from compat.lua

--- data/lib/compat/compat.lua
+++ data/lib/compat/compat.lua
@@ -1674,28 +1674,6 @@ function table.maxn(t)
 	return max
 end
 
-function dump(t, indent, done)
-    done = done or {}
-    indent = indent or 0
-
-    done[t] = true
-
-    for key, value in pairs(t) do
-        print(string.rep("\t", indent))
-
-        if type(value) == "table" and not done[value] then
-            done[value] = true
-            print(key, ":\n")
-
-            dump(value, indent + 2, done)
-            done[value] = nil
-        else
-            print(key, "\t=\t", value, "\n")
-        end
-    end
-end
-
-
 ItemType.getDuration = ItemType.getDurationMin
 
 function getFormattedWorldTime()
