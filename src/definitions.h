c677f0383d345b3cab6e93385d50761c7bed67b4
Mario <Shadow280690@hotmail.de>
2024-06-06 19:11:23 +0200

    The Forgotten Server 1.7 Development (#4719)
    The start of `The Forgotten Server 1.7` Development cycle

--- src/definitions.h
+++ src/definitions.h
@@ -5,9 +5,9 @@
 #define FS_DEFINITIONS_H
 
 static constexpr auto STATUS_SERVER_NAME = "The Forgotten Server";
-static constexpr auto STATUS_SERVER_VERSION = "1.6";
-static constexpr auto STATUS_SERVER_DEVELOPERS = "The Forgotten Server Team";
 
+static constexpr auto STATUS_SERVER_VERSION = "1.7";
+static constexpr auto STATUS_SERVER_DEVELOPERS = "The Forgotten Server Team";
 static constexpr auto CLIENT_VERSION_MIN = 1310;
 static constexpr auto CLIENT_VERSION_MAX = 1311;
 static constexpr auto CLIENT_VERSION_STR = "13.10";
