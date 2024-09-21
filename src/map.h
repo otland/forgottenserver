5603c6d1cdeceaffe1705f525b1a7772f0d66069
Ranieri Althoff <ranisalt@gmail.com>
2024-09-21 21:55:28 +0200

    Cleanup, consistency fixes

--- src/map.h
+++ src/map.h
@@ -166,9 +166,8 @@ public:
 
 	/**
 	 * Load a map.
-	 * \returns true if the map was loaded successfully
 	 */
-	bool loadMap(const std::string& identifier, bool loadHouses, bool isCalledByLua = true);
+	void loadMap(const std::string& identifier, bool loadHouses, bool isCalledByLua = true);
 
 	/**
 	 * Save a map.
