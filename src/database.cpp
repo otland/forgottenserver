d80abe06fc07cfd571f12cb50b93f755eaa4c01b
Marcin Jałocha <nekiro.dev@gmail.com>
2024-10-07 10:36:11 +0200

    Fix executeQuery handle desync

--- src/database.cpp
+++ src/database.cpp
@@ -106,7 +106,14 @@ bool Database::commit()
 bool Database::executeQuery(const std::string& query)
 {
 	std::lock_guard<std::recursive_mutex> lockGuard(databaseLock);
-	return ::executeQuery(handle, query, retryQueries);
+	auto success = ::executeQuery(handle, query, retryQueries);
+
+	// executeQuery can be called with command that produces result (e.g. SELECT)
+	// we have to store that result, even though we do not need it, otherwise handle will get blocked
+	mysql_store_result(handle.get())
+	mysql_free_result(handle.get());
+	
+	return success;
 }
 
 DBResult_ptr Database::storeQuery(std::string_view query)
