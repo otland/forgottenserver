e2191770c3c398541143d8209cf43369161bf053
ramon-bernardo <ramonbernardo.contato@gmail.com>
2024-10-07 20:07:56 -0300

    Remove duplicated header

--- src/wildcardtree.h
+++ src/wildcardtree.h
@@ -1,5 +1,4 @@
 // Copyright 2023 The Forgotten Server Authors. All rights reserved.
-// Copyright 2023 The Forgotten Server Authors. All rights reserved.
 // Use of this source code is governed by the GPL-2.0 License that can be found in the LICENSE file.
 
 #ifndef FS_WILDCARDTREE_H
