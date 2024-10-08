2e5f2aea20baccbe8c94355feb11e7eb9b837f92
Marcin Jałocha <nekiro.dev@gmail.com>
2024-10-08 21:03:18 +0200

    Update src/connection.cpp
    Co-authored-by: Ranieri Althoff <1993083+ranisalt@users.noreply.github.com>

--- src/connection.cpp
+++ src/connection.cpp
@@ -280,7 +280,7 @@ void Connection::send(const OutputMessage_ptr& msg)
 		try {
 			boost::asio::post(socket.get_executor(),
 			                  [thisPtr = shared_from_this(), msg] { thisPtr->internalSend(msg); });
-		} catch (boost::system::system_error& e) {
+		} catch (const boost::system::system_error& e) {
 			std::cout << "[Network error - Connection::send] " << e.what() << std::endl;
 			messageQueue.clear();
 			close(FORCE_CLOSE);
