4401c3ed2e2ca51b202fb285d749d3b589529214
Marco <marcomoa@hotmail.com>
2024-11-11 09:51:14 -0300

    refactor: ipban to use string formatting in messages

--- data/scripts/talkactions/ban.lua
+++ data/scripts/talkactions/ban.lua
@@ -1,7 +1,7 @@
 local ban = TalkAction("/ban")
 
 function ban.onSay(player, words, param)
-	if not player:getGroup():getAccess() or player:getAccountType() < ACCOUNT_TYPE_GAMEMASTER then
+	if not player:getGroup():getAccess() then
 		return true
 	end
 
@@ -11,7 +11,7 @@ function ban.onSay(player, words, param)
 		return true
 	end
 
-	local name = params[1]:trim()
+	local targetName = params[1]:trim()
 	local banDuration = tonumber(params[2]:trim())
 	local banReason = params[3]:trim()
 
@@ -20,7 +20,7 @@ function ban.onSay(player, words, param)
 		return true
 	end
 
-	local accountId = Game.getPlayerAccountId(name)
+	local accountId = Game.getPlayerAccountId(targetName)
 	if accountId == 0 then
 		return true
 	end
@@ -35,18 +35,71 @@ function ban.onSay(player, words, param)
 	local expirationTime = currentTime + (banDuration * 24 * 60 * 60)
 	db.query(string.format("INSERT INTO `account_bans` (`account_id`, `reason`, `banned_at`, `expires_at`, `banned_by`) VALUES (%d, %s, %d, %d, %d)", accountId, db.escapeString(banReason), currentTime, expirationTime, player:getGuid()))
 
-	local target = Player(name)
+	local target = Player(targetName)
 	if target then
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("%s has been banned for %d days.", target:getName(), banDuration))
 		target:remove()
 	else
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("%s has been banned for %d days.", name, banDuration))
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("%s has been banned for %d days.", targetName, banDuration))
 	end
 	return true
 end
 
 ban:separator(" ")
 ban:register()
+
+local ipban = TalkAction("/ipban")
+
+function ipban.onSay(player, words, param)
+	if not player:getGroup():getAccess() then
+		return true
+	end
+
+	local params = param:split(",")
+	if #params < 3 then
+		player:sendCancelMessage("Command requires 3 parameters: /ipban <player name>, <duration in days>, <reason>")
+		return true
+	end
+
+	local targetName = params[1]:trim()
+	local ipBanDuration = tonumber(params[2]:trim())
+	local ipBanReason = params[3]:trim()
+
+	if not ipBanDuration or ipBanDuration <= 0 then
+		player:sendCancelMessage("Ban duration must be a positive number.")
+		return true
+	end
+
+	local resultId = db.storeQuery("SELECT `name`, `lastip` FROM `players` WHERE `name` = " .. db.escapeString(targetName))
+	if not resultId then
+		return true
+	end
+
+	local targetIp = result.getString(resultId, "lastip")
+	result.free(resultId)
+
+	if targetIp == "0" then
+		player:sendTextMessage(MESSAGE_STATUS_WARNING, string.format("Invalid IP for player %s.", targetName))
+		return true
+	end
+
+	local checkBanQuery = db.storeQuery("SELECT 1 FROM `ip_bans` WHERE `ip` = " .. db.escapeString(targetIp))
+	if checkBanQuery then
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("%s is already IP banned.", targetName))
+		result.free(checkBanQuery)
+		return true
+	end
+
+	local currentTime = os.time()
+	local expirationTime = currentTime + (ipBanDuration * 24 * 60 * 60)
+	db.query(string.format("INSERT INTO `ip_bans` (`ip`, `reason`, `banned_at`, `expires_at`, `banned_by`) VALUES (%s, %s, %d, %d, %d)", db.escapeString(targetIp), db.escapeString(ipBanReason), currentTime, expirationTime, player:getGuid()))
+
+	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("%s has been IP banned for %d days.", targetName, ipBanDuration))
+	return true
+end
+
+ipban:separator(" ")
+ipban:register()
 
 local unban = TalkAction("/unban")
 
@@ -72,7 +125,7 @@ function unban.onSay(player, words, param)
 	db.asyncQuery("DELETE FROM `account_bans` WHERE `account_id` = " .. db.escapeString(tostring(accountId)))
 	db.asyncQuery("DELETE FROM `ip_bans` WHERE `ip` = " .. db.escapeString(lastIp))
 
-	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, param .. " has been unbanned.")
+	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, string.format("%s has been unbanned.", param))
 	return true
 end
 
