23911351a33102be60d0b088397d5d64c95b430c
Marcin Jałocha <nekiro.dev@gmail.com>
2025-02-06 08:53:57 +0100

    Don't recreate stackables every move (#4873)

--- src/game.cpp
+++ src/game.cpp
@@ -1168,17 +1168,15 @@ ReturnValue Game::internalMoveItem(Cylinder* fromCylinder, Cylinder* toCylinder,
 		return retMaxCount;
 	}
 
-	uint32_t m;
+	uint32_t moveCount;
 	if (item->isStackable()) {
-		m = std::min<uint32_t>(count, maxQueryCount);
+		moveCount = std::min<uint32_t>(count, maxQueryCount);
 	} else {
-		m = maxQueryCount;
+		moveCount = maxQueryCount;
 	}
 
-	Item* moveItem = item;
-
 	// check if we can remove this item
-	ret = fromCylinder->queryRemove(*item, m, flags, actor);
+	ret = fromCylinder->queryRemove(*item, moveCount, flags, actor);
 	if (ret != RETURNVALUE_NOERROR) {
 		return ret;
 	}
@@ -1198,38 +1196,45 @@ ReturnValue Game::internalMoveItem(Cylinder* fromCylinder, Cylinder* toCylinder,
 		}
 	}
 
-	// remove the item
+	Item* moveItem = item;
 	int32_t itemIndex = fromCylinder->getThingIndex(item);
 	Item* updateItem = nullptr;
-	fromCylinder->removeThing(item, m);
 
-	// update item(s)
 	if (item->isStackable()) {
-		uint32_t n;
+		// lets find out how much we need to move
+		uint32_t allowedCount = 0;
 
-		if (item->equals(toItem)) {
-			n = std::min<uint32_t>(ITEM_STACK_SIZE - toItem->getItemCount(), m);
-			toCylinder->updateThing(toItem, toItem->getID(), toItem->getItemCount() + n);
-			updateItem = toItem;
+		// when item is moved onto another equal item
+		if (item->equals(toItem) && moveCount != ITEM_STACK_SIZE) {
+			allowedCount = std::min<uint32_t>(ITEM_STACK_SIZE - toItem->getItemCount(), moveCount);
+			if (allowedCount > 0) {
+				fromCylinder->removeThing(item, allowedCount);
+				toCylinder->updateThing(toItem, toItem->getID(), toItem->getItemCount() + allowedCount);
+				updateItem = toItem;
+				moveCount -= allowedCount;
+
+				// we fully merged two stacks, so we have nothing to move
+				if (moveCount == 0) {
+					moveItem = nullptr;
+				}
+			}
 		} else {
-			n = 0;
-		}
+			int32_t newCount = moveCount - allowedCount;
+			if (newCount != item->getItemCount() && newCount > 0) {
+				// we get part of the source, clone the item and remove moved count from source
+				moveItem = item->clone();
+				moveItem->setItemCount(newCount);
 
-		int32_t newCount = m - n;
-		if (newCount > 0) {
-			moveItem = item->clone();
-			moveItem->setItemCount(newCount);
-		} else {
-			moveItem = nullptr;
-		}
-
-		if (item->isRemoved()) {
-			ReleaseItem(item);
+				if (item->isRemoved()) {
+					ReleaseItem(item);
+				}
+			}
 		}
 	}
 
 	// add item
-	if (moveItem /*m - n > 0*/) {
+	if (moveItem) {
+		fromCylinder->removeThing(item, moveCount);
 		toCylinder->addThing(index, moveItem);
 	}
 
