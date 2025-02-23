d4701a0759edc019ca28f59ec40d258e7934c011
NRH-AA <nheinz1995@live.com>
2025-02-22 16:07:50 -0800

    Modify config values for best performance while maintaining proper following behavior

--- src/configmanager.cpp
+++ src/configmanager.cpp
@@ -292,8 +292,8 @@ bool ConfigManager::load()
 	integer[QUEST_TRACKER_PREMIUM_LIMIT] = getGlobalNumber(L, "questTrackerPremiumLimit", 15);
 	integer[STAMINA_REGEN_MINUTE] = getGlobalNumber(L, "timeToRegenMinuteStamina", 3 * 60);
 	integer[STAMINA_REGEN_PREMIUM] = getGlobalNumber(L, "timeToRegenMinutePremiumStamina", 6 * 60);
-	integer[PATHFINDING_INTERVAL] = getGlobalNumber(L, "pathfindingInterval", 100);
-	integer[PATHFINDING_DELAY] = getGlobalNumber(L, "pathfindingDelay", 200);
+	integer[PATHFINDING_INTERVAL] = getGlobalNumber(L, "pathfindingInterval", 200);
+	integer[PATHFINDING_DELAY] = getGlobalNumber(L, "pathfindingDelay", 300);
 
 	expStages = loadXMLStages();
 	if (expStages.empty()) {
