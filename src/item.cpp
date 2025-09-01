74f2554b412b7220a306b74d466930429b4a85d5
Ranieri Althoff <ranisalt@gmail.com>
2025-09-01 10:21:18 +0200

    Use explicit pointers to try and make Windows happy

--- src/item.cpp
+++ src/item.cpp
@@ -335,7 +335,7 @@ void Item::setSubType(uint16_t n)
 	}
 }
 
-void Item::readAttr(AttrTypes_t attr, OTB::iterator& first, const OTB::iterator last)
+void Item::readAttr(AttrTypes_t attr, const char*& first, const char* const last)
 {
 	switch (attr) {
 		case ATTR_CHARGES:
@@ -529,7 +529,7 @@ void Item::readAttr(AttrTypes_t attr, OTB::iterator& first, const OTB::iterator 
 	}
 }
 
-void Item::unserializeAttr(OTB::iterator& first, const OTB::iterator last)
+void Item::unserializeAttr(const char*& first, const char* const last)
 {
 	while (first != last) {
 		auto attr = OTB::read<uint8_t>(first, last);
@@ -537,7 +537,7 @@ void Item::unserializeAttr(OTB::iterator& first, const OTB::iterator last)
 	}
 }
 
-void Item::unserializeItemNode(OTB::iterator& first, const OTB::iterator last, const OTB::Node&)
+void Item::unserializeItemNode(const char*& first, const char* const last, const OTB::Node&)
 {
 	return unserializeAttr(first, last);
 }
