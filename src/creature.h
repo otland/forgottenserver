9e956053985ae24bb69ad8fa01a28aafadb59e11
Leilani A. <168607226+kaleohanopahala@users.noreply.github.com>
2025-09-06 23:48:53 -0300

    fix clang²

--- src/creature.h
+++ src/creature.h
@@ -169,13 +169,15 @@ public:
 	void setCurrentOutfit(Outfit_t outfit) { currentOutfit = outfit; }
 	const Outfit_t getDefaultOutfit() const { return defaultOutfit; }
 	bool isInvisible() const;
-	ZoneType_t getZone() const {
+	ZoneType_t getZone() const 
+    {
 		const Tile* tile = getTile();
 		if (!tile) {
 			return ZONE_NORMAL;
 		}
 		return tile->getZone();
 	}
+
 	// creature icons
 	CreatureIconHashMap& getIcons() { return creatureIcons; }
 	const CreatureIconHashMap& getIcons() const { return creatureIcons; }
