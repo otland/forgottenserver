d4ba9627781791ef7a57a49432c42c7bf400f2ad
KrecikOnDexin <krecikondexin@gmail.com>
2025-10-05 21:41:09 +0200

    fix

--- src/iomarket.h
+++ src/iomarket.h
@@ -9,29 +9,29 @@
 
 namespace tfs::iomarket {
 
-	MarketOfferList getActiveOffers(MarketAction_t action, uint16_t itemId);
-	MarketOfferList getOwnOffers(MarketAction_t action, uint32_t playerId);
-	HistoryMarketOfferList getOwnHistory(MarketAction_t action, uint32_t playerId);
+MarketOfferList getActiveOffers(MarketAction_t action, uint16_t itemId);
+MarketOfferList getOwnOffers(MarketAction_t action, uint32_t playerId);
+HistoryMarketOfferList getOwnHistory(MarketAction_t action, uint32_t playerId);
 
-	void processExpiredOffers(DBResult_ptr result, bool);
-	void checkExpiredOffers();
+void processExpiredOffers(DBResult_ptr result, bool);
+void checkExpiredOffers();
 
-	uint32_t getPlayerOfferCount(uint32_t playerId);
-	MarketOfferEx getOfferByCounter(uint32_t timestamp, uint16_t counter);
+uint32_t getPlayerOfferCount(uint32_t playerId);
+MarketOfferEx getOfferByCounter(uint32_t timestamp, uint16_t counter);
 
-	void createOffer(uint32_t playerId, MarketAction_t action, uint32_t itemId, uint16_t amount, uint64_t price,
-	                        bool anonymous);
-	void acceptOffer(uint32_t offerId, uint16_t amount);
-	void deleteOffer(uint32_t offerId);
+void createOffer(uint32_t playerId, MarketAction_t action, uint32_t itemId, uint16_t amount, uint64_t price,
+                 bool anonymous);
+void acceptOffer(uint32_t offerId, uint16_t amount);
+void deleteOffer(uint32_t offerId);
 
-	void appendHistory(uint32_t playerId, MarketAction_t type, uint16_t itemId, uint16_t amount, uint64_t price,
-	                          time_t timestamp, MarketOfferState_t state);
-	bool moveOfferToHistory(uint32_t offerId, MarketOfferState_t state);
+void appendHistory(uint32_t playerId, MarketAction_t type, uint16_t itemId, uint16_t amount, uint64_t price,
+                   time_t timestamp, MarketOfferState_t state);
+bool moveOfferToHistory(uint32_t offerId, MarketOfferState_t state);
 
-	void updateStatistics();
+void updateStatistics();
 
-	MarketStatistics* getPurchaseStatistics(uint16_t itemId);
-	MarketStatistics* getSaleStatistics(uint16_t itemId);
+MarketStatistics* getPurchaseStatistics(uint16_t itemId);
+MarketStatistics* getSaleStatistics(uint16_t itemId);
 
 } // namespace tfs::iomarket
 
