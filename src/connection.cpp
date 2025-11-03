29051a8f509c9b122d9354a46b7787c1bdb9f777
Ranieri Althoff <ranisalt@gmail.com>
2025-11-03 01:45:19 +0100

    Fix crash on logout

--- src/connection.cpp
+++ src/connection.cpp
@@ -74,16 +74,17 @@ void Connection::close(bool force)
 
 void Connection::closeSocket()
 {
-	if (socket.is_open()) {
-		try {
-			readTimer.cancel();
-			writeTimer.cancel();
-			boost::system::error_code error;
-			socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both, error);
-			socket.close(error);
-		} catch (boost::system::system_error& e) {
-			std::cout << "[Network error - Connection::closeSocket] " << e.what() << std::endl;
-		}
+	if (!socket.is_open()) {
+		return;
+	}
+
+	try {
+		readTimer.cancel();
+		writeTimer.cancel();
+		socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+		socket.close();
+	} catch (boost::system::system_error& e) {
+		std::cout << "[Network error - Connection::closeSocket] " << e.what() << std::endl;
 	}
 }
 
