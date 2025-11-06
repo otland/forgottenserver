caaf688a72f4ca33ec131105f4c1f8a9d372ee3e
ramon-bernardo <ramonbernardo.contato@gmail.com>
2025-11-06 11:03:12 -0300

    feat(talkactions): extend create item command with dynamic destinations and target player support

--- data/talkactions/scripts/create_item.lua
+++ data/talkactions/scripts/create_item.lua
@@ -2,6 +2,33 @@ local invalidIds = {
 	1, 2, 3, 4, 5, 6, 7, 10, 11, 13, 14, 15, 19, 21, 26, 27, 28, 35, 43
 }
 
+local destinations = { "inbox", "storeinbox", "tile", "house", "depot" }
+
+local function getDestination(target, param, index)
+	if not param then
+		return target
+	end
+
+	param = param:lower()
+	if param == "inbox" then
+		return target:getInbox()
+	elseif param == "storeinbox" then
+		return target:getStoreInbox()
+	elseif param == "tile" then
+		return target:getTile()
+	elseif param == "house" then
+		local house = target:getHouse()
+		local doors = house and house:getDoors()
+		local randomDoor = doors and doors[math.random(#doors)]
+		return randomDoor and randomDoor:getTile() or target
+	elseif param == "depot" then
+		local depotId = tonumber(index) or 0
+		local depot = target:getDepotChest(depotId, true)
+		return depot or target
+	end
+	return target
+end
+
 function onSay(player, words, param)
 	if not player:getGroup():getAccess() then
 		return true
@@ -26,11 +53,39 @@ function onSay(player, words, param)
 		return false
 	end
 
-	local keyNumber = 0
-	local count = tonumber(split[2])
-	local subType = 1
-	if not itemType:isStackable() and split[3] then
-		subType = math.max(1, tonumber(split[3]) or 1)
+    local count, subType, keyNumber, destinationParam, targetName, indexParam = nil, 1, 0, nil, nil, 0
+
+	for i = 2, #split do
+        local arg = split[i]:trim()
+        local lowerArg = arg:lower()
+        local numArg = tonumber(arg)
+
+        if i == 2 then
+            count = numArg or 1
+        elseif i == 3 and not table.contains(destinations, lowerArg) then
+            if numArg then
+                subType = numArg
+            else
+                targetName = arg
+            end
+        elseif table.contains(destinations, lowerArg) then
+            destinationParam = lowerArg
+        elseif numArg then
+            indexParam = numArg
+        elseif not targetName then
+            targetName = arg
+        end
+    end
+
+	local targetPlayer = player
+	if targetName then
+		local found = Player(targetName)
+		if not found then
+			player:sendCancelMessage("Player \"" .. targetName .. "\" not found.")
+			return false
+		end
+
+		targetPlayer = found
 	end
 
 	if count then
@@ -50,25 +105,37 @@ function onSay(player, words, param)
 		end
 	end
 
-	local result = nil
+	local destination = getDestination(targetPlayer, destinationParam, indexParam)
+	
+	local result = {}
 	if itemType:isStackable() then
-		result = player:addItem(itemType:getId(), count, true, subType)
+		local item = destination:addItem(itemType:getId(), count, true, subType)
+		if item then
+			if type(result) == "table" then
+				table.concat(result, item)
+			else
+				table.insert(result, item)
+			end
+		end
 	else
-		result = player:addItem(itemType:getId(), subType, true, count)
+		for i = 1, count do
+			local item = destination:addItem(itemType:getId(), subType, true, 1)
+			if item then
+				if type(result) == "table" then
+					table.concat(result, item)
+				else
+					table.insert(result, item)
+				end
+			end
+		end
 	end
 
 	if result then
-		if not itemType:isStackable() then
-			if type(result) == "table" then
-				for _, item in ipairs(result) do
-					item:decay()
-				end
-			else
-				if itemType:isKey() then
-					result:setAttribute(ITEM_ATTRIBUTE_ACTIONID, keyNumber)
-				end
-				result:decay()
+		for _, item in ipairs(result) do
+			if itemType:isKey() then
+				item:setAttribute(ITEM_ATTRIBUTE_ACTIONID, keyNumber)
 			end
+			item:decay()
 		end
 		player:getPosition():sendMagicEffect(CONST_ME_MAGIC_GREEN)
 	end
