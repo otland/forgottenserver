dd7c4af5f80d37b0fe68985fb4d3b296d3f1f512
ramon-bernardo <ramonbernardo.contato@gmail.com>
2025-11-28 19:28:30 -0300

    chore(copyright): update year to 2025

--- src/protocol.h
+++ src/protocol.h
@@ -1,4 +1,4 @@
-// Copyright 2023 The Forgotten Server Authors. All rights reserved.
+// Copyright 2025 The Forgotten Server Authors. All rights reserved.
 // Use of this source code is governed by the GPL-2.0 License that can be found in the LICENSE file.
 
 #ifndef FS_PROTOCOL_H
